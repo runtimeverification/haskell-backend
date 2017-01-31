62e0b0c0bf71964c5ac839c051638ba0e8a0f25a
Manasvi Saxena <msaxena2@illinois.edu>
2017-01-31 12:28:33 -0600

    changes to implement 2 interfaces

--- src/main/scala/org/kframework/minikore/MiniKore.scala
+++ src/main/scala/org/kframework/minikore/MiniKore.scala
@@ -3,11 +3,30 @@ package org.kframework.minikore
 import org.kframework.minikore.MiniKore._
 
 
-object MiniKore{
+object MiniKore {
+
+  trait AST
+
+  trait Node[T] extends AST {
+    val label: T
+    val children: Seq[AST]
+  }
+
+  object Node {
+    def unapply(arg: Node[_]): Option[(_, Seq[AST])] = Some((arg.label, arg.children))
+  }
+
+  trait Leaf[T] extends AST {
+    val content: T
+  }
+
+  object Leaf {
+    def unapply(arg: Leaf[_]): Option[_] = Some(arg.content)
+  }
 
   type Attributes = Seq[Pattern]
 
-  abstract class Definition {
+  trait Definition {
     val modules: Seq[Module]
     val att: Attributes
   }
@@ -16,7 +35,7 @@ object MiniKore{
     def unapply(arg: Definition): Option[(Seq[Module], Attributes)] = Some((arg.modules, arg.att))
   }
 
-  abstract class Module {
+  trait Module {
     val name: String
     val sentences: Seq[Sentence]
     val att: Attributes
@@ -28,7 +47,7 @@ object MiniKore{
 
   trait Sentence
 
-  abstract class Import extends Sentence {
+  trait Import extends Sentence {
     val name: String
     val att: Attributes
   }
@@ -37,7 +56,7 @@ object MiniKore{
     def unapply(arg: Import): Option[(String, Attributes)] = Some((arg.name, arg.att))
   }
 
-  abstract class SortDeclaration extends Sentence {
+  trait SortDeclaration extends Sentence {
     val sort: String
     val att: Attributes
   }
@@ -46,7 +65,7 @@ object MiniKore{
     def unapply(arg: SortDeclaration): Option[(String, Attributes)] = Some((arg.sort, arg.att))
   }
 
-  abstract class SymbolDeclaration extends Sentence {
+  trait SymbolDeclaration extends Sentence {
     val sort: String
     val label: String
     val args: Seq[String]
@@ -57,7 +76,7 @@ object MiniKore{
     def unapply(arg: SymbolDeclaration): Option[(String, String, Seq[String], Attributes)] = Some((arg.sort, arg.label, arg.args, arg.att))
   }
 
-  abstract class Rule extends Sentence {
+  trait Rule extends Sentence {
     val p: Pattern
     val att: Attributes
   }
@@ -67,7 +86,7 @@ object MiniKore{
   }
 
 
-  abstract class Axiom extends Sentence {
+  trait Axiom extends Sentence {
     val p: Pattern
     val att: Attributes
   }
@@ -77,10 +96,10 @@ object MiniKore{
   }
 
 
-  // Pattern trait + abstract classes
-  trait Pattern
+  // Pattern trait + traits
+  trait Pattern extends AST
 
-  abstract class Variable extends Pattern {
+  trait Variable extends Pattern with Leaf[(String, String)]{
     val name: String
     val sort: String
   }
@@ -89,7 +108,7 @@ object MiniKore{
     def unapply(arg: Variable): Option[(String, String)] = Some((arg.name, arg.sort))
   }
 
-  abstract class Application extends Pattern {
+  trait Application extends Pattern with Node[String] {
     val label: String
     val args: Seq[Pattern]
   }
@@ -98,7 +117,7 @@ object MiniKore{
     def unapply(arg: Application): Option[(String, Seq[Pattern])] = Some((arg.label, arg.args))
   }
 
-  abstract class DomainValue extends Pattern {
+  trait DomainValue extends Pattern with Leaf[(String, String)] {
     val label: String
     val value: String
   }
@@ -108,13 +127,13 @@ object MiniKore{
   }
 
   //
-  abstract class True extends Pattern
+  trait True extends Pattern with Leaf[Boolean]
 
   object True {
     def unapply(arg: True): Boolean = true
   }
 
-  abstract class False extends Pattern
+  trait False extends Pattern with Leaf[Boolean]
 
   object False {
     def unapply(arg: False): Boolean = true
@@ -122,7 +141,7 @@ object MiniKore{
 
 
   //
-  abstract class And extends Pattern {
+  trait And extends Pattern with Node[String] {
     val p: Pattern
     val q: Pattern
   }
@@ -131,7 +150,7 @@ object MiniKore{
     def unapply(arg: And): Option[(Pattern, Pattern)] = Some((arg.p, arg.q))
   }
 
-  abstract class Or extends Pattern {
+  trait Or extends Pattern with Node[String] {
     val p: Pattern
     val q: Pattern
   }
@@ -140,7 +159,7 @@ object MiniKore{
     def unapply(arg: Or): Option[(Pattern, Pattern)] = Some((arg.p, arg.q))
   }
 
-  abstract class Not extends Pattern {
+  trait Not extends Pattern with Node[String] {
     val p: Pattern
   }
 
@@ -148,7 +167,7 @@ object MiniKore{
     def unapply(arg: Not): Option[Pattern] = Some(arg.p)
   }
 
-  abstract class Implies extends Pattern {
+  trait Implies extends Pattern with Node[String] {
     val p: Pattern
     val q: Pattern
   }
@@ -157,7 +176,7 @@ object MiniKore{
     def unapply(arg: Implies): Option[(Pattern, Pattern)] = Some((arg.p, arg.q))
   }
 
-  abstract class Exists extends Pattern {
+  trait Exists extends Pattern with Node[String]{
     val v: Variable
     val p: Pattern
   }
@@ -166,7 +185,7 @@ object MiniKore{
     def unapply(arg: Exists): Option[(Pattern, Pattern)] = Some((arg.v, arg.p))
   }
 
-  abstract class ForAll extends Pattern {
+  trait ForAll extends Pattern with Node[String] {
     val v: Variable
     val p: Pattern
   }
@@ -176,7 +195,7 @@ object MiniKore{
   }
 
   //
-  abstract class Next extends Pattern {
+  trait Next extends Pattern with Node[String] {
     val p: Pattern
   }
 
@@ -184,7 +203,7 @@ object MiniKore{
     def unapply(arg: Next): Option[Pattern] = Some(arg.p)
   }
 
-  abstract class Rewrite extends Pattern {
+  trait Rewrite extends Pattern with Node[String] {
     val p: Pattern
     val q: Pattern
   }
@@ -193,7 +212,7 @@ object MiniKore{
     def unapply(arg: Rewrite): Option[(Pattern, Pattern)] = Some((arg.p, arg.q))
   }
 
-  abstract class Equals extends Pattern {
+  trait Equals extends Pattern with Node[String] {
     val p: Pattern
     val q: Pattern
   }
@@ -220,32 +239,69 @@ object DefaultImplementation {
 
   case class ConcreteAxiom(p: Pattern, att: Attributes) extends Axiom
 
-  case class ConcreteVariable(name: String, sort: String) extends Variable
+  case class ConcreteVariable(name: String, sort: String) extends Variable {
+    override val content: (String, String) = (name, sort)
+  }
 
-  case class ConcreteApplication(label: String, args: Attributes) extends Application
+  case class ConcreteApplication(label: String, args: Attributes) extends Application {
+    override val children: Seq[AST] = args
+  }
 
-  case class ConcreteDomainValue(label: String, value: String) extends DomainValue
+  case class ConcreteDomainValue(label: String, value: String) extends DomainValue {
+    override val content = (label, value)
+  }
 
-  case class ConcreteTrue() extends True
+  case class ConcreteTrue() extends True {
+    override val content: Boolean = true
+  }
 
-  case class ConcreteFalse() extends False
+  case class ConcreteFalse() extends False {
+    override val content: Boolean = false
+  }
 
-  case class ConcreteAnd(p: Pattern, q: Pattern) extends And
+  case class ConcreteAnd(p: Pattern, q: Pattern) extends And {
+    override val label: String = "\\and"
+    override val children: Seq[AST] = Seq(p, q)
+  }
 
-  case class ConcreteOr(p: Pattern, q: Pattern) extends Or
+  case class ConcreteOr(p: Pattern, q: Pattern) extends Or {
+    override val label: String = "\\or"
+    override val children: Seq[AST] = Seq(p, q)
+  }
 
-  case class ConcreteNot(p: Pattern) extends Not
+  case class ConcreteNot(p: Pattern) extends Not {
+    override val label: String = "\\not"
+    override val children: Seq[AST] = Seq(p)
+  }
 
-  case class ConcreteExists(v: Variable, p: Pattern) extends Exists
+  case class ConcreteExists(v: Variable, p: Pattern) extends Exists {
+    override val label: String = "\\exists"
+    override val children: Seq[AST] = Seq(v, p)
+  }
 
-  case class ConcreteForAll(v: Variable, p: Pattern) extends ForAll
+  case class ConcreteForAll(v: Variable, p: Pattern) extends ForAll {
+    override val label: String = "\\forAll"
+    override val children: Seq[AST] = Seq(v, p)
+  }
 
-  case class ConcreteNext(p: Pattern) extends Next
+  case class ConcreteNext(p: Pattern) extends Next {
+    override val label: String = "\\next"
+    override val children: Seq[AST] = Seq(p)
+  }
 
-  case class ConcreteRewrite(p: Pattern, q: Pattern) extends Rewrite
+  case class ConcreteRewrite(p: Pattern, q: Pattern) extends Rewrite {
+    override val label: String = "\\rewrite"
+    override val children: Seq[AST] = Seq(p, q)
+  }
 
-  case class ConcreteEquals(p: Pattern, q: Pattern) extends Equals
+  case class ConcreteEquals(p: Pattern, q: Pattern) extends Equals {
+    override val label: String = "\\equals"
+    override val children: Seq[AST] = Seq(p, q)
+  }
 
-  case class ConcreteImplies(p: Pattern, q: Pattern) extends Implies
+  case class ConcreteImplies(p: Pattern, q: Pattern) extends Implies {
+    override val label: String = "\\implies"
+    override val children: Seq[AST] = Seq(p, q)
+  }
 
 }
