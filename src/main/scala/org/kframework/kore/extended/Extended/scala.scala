0fef40f240afbdc6e358ec8166ba8634aa9a2630
Manasvi Saxena <msaxena2@illinois.edu>
2017-04-30 14:44:51 -0500

    Experimenting with Extended

---
+++ src/main/scala/org/kframework/kore/extended/Extended/scala.scala
@@ -0,0 +1,8 @@
+package org.kframework.kore.extended.Extended
+
+/**
+  * Created by manasvi on 4/30/17.
+  */
+class scala {
+
+}
---
+++ src/test/scala/org/kframework/kore/extended/ExtendedTest.scala
@@ -0,0 +1,8 @@
+package org.kframework.kore.extended
+
+/**
+  * Created by manasvi on 4/30/17.
+  */
+class ExtendedTest {
+
+}
