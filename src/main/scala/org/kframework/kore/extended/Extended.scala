96bb14fea3aec807f8cffa2c5488ee8deef56783
Manasvi Saxena <msaxena2@illinois.edu>
2017-05-01 13:32:49 -0500

    wip on extended. RichModules Working with Implicit Defs

--- src/main/scala/org/kframework/kore/extended/Extended.scala
+++ src/main/scala/org/kframework/kore/extended/Extended.scala
@@ -2,15 +2,39 @@ package org.kframework.kore.extended
 
 import org.kframework.kore
 
-trait RichDefinition {
-  def apply(defintion: kore.Definition): RichDefinition
+
+class RichDefinition(val koreDefinition: kore.Definition) {
+
 }
 
-trait RichModule {
-  def apply(module: kore.Module): RichModule
+object RichDefinition {
+  def apply(koreDefinition: kore.Definition): RichDefinition = new RichDefinition(koreDefinition)
 }
 
-trait RichAttributes {
-  def apply(attributes: kore.Attributes) : RichAttributes
+class RichModule(val koreModule: kore.Module, val imports: Seq[kore.Module]) {
+
 }
+
+object RichModule {
+  def apply(koreModule: kore.Module, imports: Seq[kore.Module]): RichModule = new RichModule(koreModule, imports)
+}
+
+
+object Adaptors {
+
+  implicit def asRichDefinition(d: kore.Definition): RichDefinition = RichDefinition(d)
+
+  implicit def asRichModule(m: kore.Module)(implicit d: kore.Definition): RichModule = {
+
+    val importDecs = m.sentences.collect({
+      case kore.Import(n, _) => n
+    })
+
+    val relevantImports = d.modules.filter(x => importDecs.contains(x.name))
+
+    RichModule(m, relevantImports)
+  }
+}
+
+
 
--- src/test/scala/org/kframework/kore/extended/ExtendedTest.scala
+++
@@ -1,8 +0,0 @@
-package org.kframework.kore.extended
-
-/**
-  * Created by manasvi on 4/30/17.
-  */
-class ExtendedTest {
-
-}
