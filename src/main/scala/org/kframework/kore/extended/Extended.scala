5fe91f5fd11b2b6416b845c09813af6bbf797188
Manasvi Saxena <msaxena2@illinois.edu>
2017-05-02 15:45:35 -0500

    experimenting with implicit classes

--- src/main/scala/org/kframework/kore/extended/Extended.scala
+++ src/main/scala/org/kframework/kore/extended/Extended.scala
@@ -2,34 +2,45 @@ package org.kframework.kore.extended
 
 import org.kframework.kore
 
+import Adaptors._
 // Each Construct only has the minimal information required to derive everything.
 
-class RichDefinition(val koreDefinition: kore.Definition) {
+object implicits {
+
+  implicit class RichDefinition(val koreDefinition: kore.Definition) {
+    val mainModule = ???
+  }
+
+  object RichDefinition {
+    def apply(koreDefinition: kore.Definition): RichDefinition = new RichDefinition(koreDefinition)
+  }
+
+  implicit class RichModule(val koreModule: kore.Module)(implicit val d: kore.Definition) {
+
+    lazy val moduleImportsMap: Map[kore.ModuleName, kore.Module] = d.modules.groupBy(_.name).mapValues(_.head)
+
+    lazy val imports: Seq[kore.Module] = koreModule.sentences.collect({
+      case kore.Import(m, _) => moduleImportsMap(m)
+    })
+
+    lazy val localSentences = koreModule.sentences
+
+    lazy val allSentences: Seq[kore.Sentence] = localSentences ++ importedSentences
+
+    lazy val importedSentences: Seq[kore.Sentence] = imports.flatMap(_.allSentences)
+
+    lazy val sorts: Seq[kore.Sort] = allSentences.collect({
+      case kore.SymbolDeclaration(s, _, _, _) => s
+      case kore.SortDeclaration(s, _) => s
+    })
+
+  }
+
+  class RichAttributes(val attributes: kore.Attributes) {
+
+  }
+
 }
-
-object RichDefinition {
-  def apply(koreDefinition: kore.Definition): RichDefinition = new RichDefinition(koreDefinition)
-}
-
-class RichModule(val koreModule: kore.Module, val imports: Seq[RichModule]) {
-
-  val sentences: Seq[kore.Sentence] = koreModule.sentences
-
-  lazy val allSentences: Seq[kore.Sentence] = sentences ++ importedSentences
-
-  lazy val importedSentences: Seq[kore.Sentence] = imports.flatMap(_.allSentences)
-
-  lazy val sorts: Seq[kore.Sort]  = allSentences.collect({
-    case kore.SymbolDeclaration(s, _, _, _) => s
-    case kore.SortDeclaration(s, _) => s
-  })
-
-}
-
-object RichModule {
-  def apply(koreModule: kore.Module, imports: Seq[RichModule]): RichModule = new RichModule(koreModule, imports)
-}
-
 //Rewriter may need a module to begin with. Still a WIP.
 //Needs a definition and a Module to start with.
 trait Rewriter {
@@ -43,6 +54,5 @@ trait Backend extends kore.Definition with kore.Builders
 //Way to Create a backend give a Kore Definition. Since Backends need the entire definition to
 //Function, they can only provide Builders once they've processed the definition
 trait BackendCreator extends (kore.Definition => Backend)
-
 
 
