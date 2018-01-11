cfefc8c4537efb06720ba438a0717806373a2064
Xiaohong Chen <xiaohong993@foxmail.com>
2018-01-11 12:10:26 -0600

    Sync scala parser with the document.
    Change List:
    - Remove \subset
    - Remove \next
    - Remove \rewrites
    - Add \ceil
    - Add \floor
    - Rename Sentence to Declaration
    - Rename isSymbolChar to isIdChar

--- src/test/scala/org/kframework/TextToKoreTest.scala
+++ src/test/scala/org/kframework/TextToKoreTest.scala
@@ -117,15 +117,6 @@ class TextToKoreTest {
   @Test def test_pattern_11(): Unit = {
     parseFromFile("test-pattern-11.kore")
   }
-  @Test def test_pattern_12(): Unit = {
-    parseFromFile("test-pattern-12.kore")
-  }
-  @Test def test_pattern_13(): Unit = {
-    parseFromFile("test-pattern-13.kore")
-  }
-  @Test def test_pattern_14(): Unit = {
-    parseFromFile("test-pattern-14.kore")
-  }
   /**
     * Tests for parsing attributes.
     */
