2e0d359d00bb32dcc68569dc3e6b73682150b96a
Virgil Serbanuta <virgil@poarta.org>
2018-01-16 20:23:56 +0200

    Some raw documentation.

--- src/haskell/parser/src/KoreParser.hs
+++ src/haskell/parser/src/KoreParser.hs
@@ -1,3 +1,27 @@
+{-|
+Module      : KoreParser
+Description : Parser for the Kore language
+Copyright   : (c) Runtime Verification, 2018
+License     : UIUC/NCSA
+Maintainer  : virgil.serbanuta@runtimeverification.com
+Stability   : experimental
+Portability : POSIX
+
+This is a parser for the Kore language. Sample usage:
+@
+import KoreParser
+
+import Data.Attoparsec.ByteString.Char8 (parseOnly, endOfInput)
+import qualified Data.ByteString as ByteString
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+    (fileName:_) <- getArgs
+    contents <- ByteString.readFile fileName
+    print (parseOnly (skipWhitespace *> definitionParser <* endOfInput) contents)
+@
+-}
 module KoreParser ( definitionParser
                   , skipWhitespace
                   ) where
