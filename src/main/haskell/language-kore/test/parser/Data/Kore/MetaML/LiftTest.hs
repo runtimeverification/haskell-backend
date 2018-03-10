e90a52e1df1139ed8f07bd2bbbde816f4f58a223
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2018-03-10 09:04:31 +0200

    tmp

---
+++ src/main/haskell/language-kore/test/parser/Data/Kore/MetaML/LiftTest.hs
@@ -0,0 +1,74 @@
+module Data.Kore.MetaML.LiftTest where
+
+import           Test.Tasty            (TestTree, testGroup)
+import           Test.Tasty.HUnit      (assertEqual, testCase)
+
+import           Data.Fix
+import           Data.Kore.AST
+import           Data.Kore.MetaML.AST
+import           Data.Kore.MetaML.Lift
+
+liftTests :: TestTree
+liftTests =
+    testGroup
+        "Lifting Tests"
+        [ testCase "Testing lifting a pure object pattern."
+            (assertEqual ""
+                ( Fix
+                    ( ApplicationPattern Application
+                        { applicationSymbolOrAlias = SymbolOrAlias
+                            { symbolOrAliasConstructor = Id "#\\not"
+                            , symbolOrAliasParams = []
+                            }
+                        , applicationChildren =
+                            [ Fix
+                                ( VariablePattern Variable
+                                    { variableName = Id "#a"
+                                    , variableSort = SortActualSort SortActual
+                                        { sortActualName = Id "#Sort"
+                                        , sortActualSorts = []
+                                        }
+                                    }
+                                )
+                            , Fix
+                                ( ApplicationPattern Application
+                                    { applicationSymbolOrAlias = SymbolOrAlias
+                                        { symbolOrAliasConstructor = Id "#\\top"
+                                        , symbolOrAliasParams = []
+                                        }
+                                    , applicationChildren =
+                                        [ Fix
+                                            ( VariablePattern Variable
+                                                { variableName = Id "#a"
+                                                , variableSort =
+                                                    SortActualSort SortActual
+                                                        { sortActualName =
+                                                            Id "#Sort"
+                                                        , sortActualSorts = []
+                                                        }
+                                                }
+                                            )
+                                        ]
+                                    }
+                                )
+                            ]
+                        }
+                    )
+                )
+                ( liftToMeta
+                    ( ObjectPattern
+                        ( NotPattern Not
+                            { notSort = SortVariableSort
+                                (SortVariable (Id "a"))
+                            , notChild = ObjectPattern
+                                ( TopPattern Top
+                                    { topSort = SortVariableSort
+                                        (SortVariable (Id "a"))
+                                    }
+                                )
+                            }
+                        )
+                    ::UnifiedPattern)
+                )
+            )
+        ]
