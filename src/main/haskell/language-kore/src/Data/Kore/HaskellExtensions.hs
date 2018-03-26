1f4cc3c08282cc867bdb99a33d1639249d167e75
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2018-03-26 15:22:59 +0300

    Forgot a new file.

---
+++ src/main/haskell/language-kore/src/Data/Kore/HaskellExtensions.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE PolyKinds #-}
+module Data.Kore.HaskellExtensions where
+
+{-|'Rotate31' is a helper type useful to bring the first argument
+of a type paramaterized by three arguments to the last position,
+shifting the other argumetns to the left.
+This is used in conjunction with 'MetaOrObject' constructs such as
+'applyMetaObjectFunction' or 'UnifiedThing'.
+-}
+newtype
+    Rotate31 t pat variable level
+  = Rotate31 { unRotate31 :: t level pat variable}
+
+{-|'Rotate41' is a helper type useful to bring the first argument
+of a type paramaterized by three arguments to the last position,
+shifting the other argumetns to the left.
+This is used in conjunction with 'MetaOrObject' constructs such as
+'applyMetaObjectFunction' or 'UnifiedThing'.
+-}
+newtype
+    Rotate41 t sortParam pat variable level
+  = Rotate41 { unRotate41 :: t level sortParam pat variable}
+
+{-|The '<..>' operator offers function composition functionality when the
+second function has two arguments.  It satisfies that
+
+* @(g <..> f) a1 a2 == g (f a1 a2)@
+-}
+(<..>) :: (b -> c) -> (a1 -> a2 -> b) -> (a1 -> a2 -> c)
+(<..>) = (.) . (.)
+
+{-|The '<...>' operator offers function composition functionality when the
+second function has two arguments.  It satisfies that
+
+* @(g <...> f) a1 a2 a3 == g (f a1 a2 a3)@
+-}
+(<...>) :: (b -> c) -> (a1 -> a2 -> a3 -> b) -> (a1 -> a2 -> a3 -> c)
+(<...>) = (<..>) . (.)
+
+{-|The '<....>' operator offers function composition functionality when the
+second function has two arguments.  It satisfies that
+
+* @(g <...> f) a1 a2 a3 a4 == g (f a1 a2 a3 a4)@
+-}
+(<....>)
+    :: (b -> c)
+    -> (a1 -> a2 -> a3 -> a4 -> b)
+    -> (a1 -> a2 -> a3 -> a4 -> c)
+(<....>) = (<...>) . (.)
