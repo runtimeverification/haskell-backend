27452b7570decdeaedc15be9601d26f49f641e5b
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2018-03-28 17:02:15 +0300

    Forgot files

---
+++ src/main/haskell/language-kore/test/parser/Test/Tasty/HUnit/Extensions.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE FlexibleContexts #-}
+module Test.Tasty.HUnit.Extensions where
+import           Control.Monad
+import           Data.CallStack
+import           Test.Tasty.HUnit (assertFailure)
+
+assertEqualWithPrinter
+    :: (Eq a, HasCallStack)
+    => (a -> String)
+    -> String -- ^ The message prefix
+    -> a      -- ^ The expected value
+    -> a      -- ^ The actual value
+    -> IO ()
+assertEqualWithPrinter printer preface expected actual =
+    unless (actual == expected) (assertFailure msg)
+  where
+    msg =
+        (if null preface then "" else preface ++ "\n")
+        ++ "expected: " ++ printer expected ++ "\n but got: " ++ printer actual
