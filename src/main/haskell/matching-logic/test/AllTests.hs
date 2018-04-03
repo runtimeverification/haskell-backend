2dc106576ac25eb77a43e54fec41f711e5bfb749
Manasvi Saxena <msaxena2@illinois.edu>
2018-04-03 12:08:11 -0500

    **/test,RuleParser.hs: Add tests for parser/unparser

--- src/main/haskell/matching-logic/test/AllTests.hs
+++ src/main/haskell/matching-logic/test/AllTests.hs
@@ -1,11 +1,16 @@
 import           Test.Tasty                                        (TestTree,
                                                                     testGroup)
+import           Test.Tasty.HUnit
 import           Test.Tasty.Runners                                (consoleTestReporter,
                                                                     defaultMainWithIngredients,
                                                                     listingTests)
 import           Test.Tasty.Runners.AntXML                         (antXMLRunner)
 
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
 import           Kore.MatchingLogic.ProofSystem.ProofAssistantTest
+import           RuleParserTests
 
 main :: IO ()
 main =
@@ -25,4 +30,13 @@ unitTests =
     testGroup
         " Unit Tests"
         [ proofAssistantTests
+        , parserUnitTests
         ]
+
+
+parserUnitTests :: TestTree
+parserUnitTests = testGroup
+                    " Rule Parser/Unparser Unit Tests"
+                    [  ruleParsingTests
+                      ,ruleUnparsingTests
+                    ]
