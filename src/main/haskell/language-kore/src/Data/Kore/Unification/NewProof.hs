0d9006c0f75857845996eba098656761837de963
Phillip Harris <palindromeh@gmail.com>
2018-06-05 11:08:57 -0500

    .

--- src/main/haskell/language-kore/src/Data/Kore/Unification/NewProof.hs
+++ src/main/haskell/language-kore/src/Data/Kore/Unification/NewProof.hs
@@ -17,11 +17,17 @@ module Data.Kore.Unification.NewProof where
 
 import qualified Data.Set as S
 import qualified Data.Map as M
+import           Data.Fix
 import           Control.Lens
+import           Control.Lens.Operators
 import           Control.Monad.State
+import           Data.Kore.AST.Common
+import           Data.Kore.AST.Kore
 -- Proof datatype that allows for creating and discharging assumptions
 -- Allows for easier, more compact proofs
 -- Can be converted to the old type
+
+type Formula level var = Fix (Pattern level var)
 
 data Line ix rule formula
   = Line
@@ -32,27 +38,48 @@ data Line ix rule formula
 
 type Proof ix rule formula = M.Map ix (Line ix rule formula)
 
+discharge 
+  :: (Ord ix, MonadState s m)
+  => ix 
+  -> ix 
+  -> Lens' s (Proof ix rule (Formula level var)) 
+  -> m ix
 discharge ix aix proof = do
   Just line <- M.lookup ix <$> use proof
-  Just assumption <- M.lookup aix <$> use proof
+  Just assumption <- M.lookup ix <$> use proof
   let line' = line 
         { assumptions = S.delete ix (assumptions line) 
         , claim = implies assumption line
         }
   addLine line' proof
   
-implies = undefined
+implies a b = Fix $ ImpliesPattern $ Implies 
+  { impliesSort = placeholderSort
+  , impliesFirst  = claim a
+  , impliesSecond = claim b
+  }
 
+placeholderSort =
+  SortVariableSort $ SortVariable { getSortVariable = noLocationId "FIXME: The whole AST datatype" }
+
+addLine 
+  :: (Ord ix, MonadState s m) 
+  => Line ix rule (Formula level var) 
+  -> Lens' s (Proof ix rule (Formula level var)) 
+  -> m ix
 addLine line proof = do
   ix <- getNextIx <$> use proof
   proof %= M.insert ix line
   return ix
 
-assume :: (Ord ix, MonadState s m) => Line ix rule formula -> Lens' s (Proof ix rule formula) -> m ix
+assume 
+  :: (Ord ix, MonadState s m) 
+  => Line ix rule (Formula level var) 
+  -> Lens' s (Proof ix rule (Formula level var)) 
+  -> m ix
 assume line proof = do
   ix <- getNextIx <$> use proof
-  let line' = line { assumptions = S.singleton ix }
-  proof %= M.insert ix line'
+  proof %= M.insert ix line { assumptions = S.singleton ix }
   return ix 
 
 getNextIx = undefined
