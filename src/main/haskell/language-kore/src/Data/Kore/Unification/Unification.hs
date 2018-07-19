fc37d5774af762849f729f57aa6d9ad7acc70174
Phillip Harris <palindromeh@gmail.com>
2018-07-19 09:57:28 -0500

    --pedantic

--- src/main/haskell/language-kore/src/Data/Kore/Unification/Unification.hs
+++ src/main/haskell/language-kore/src/Data/Kore/Unification/Unification.hs
@@ -13,58 +13,85 @@ Portability : portable
 , LambdaCase
 , DeriveGeneric
 #-}
-module Data.Kore.Unification.Unification where
+{-# OPTIONS_GHC -Wno-unused-matches    #-}
+{-# OPTIONS_GHC -Wno-name-shadowing    #-}
+module Data.Kore.Unification.Unification 
+( unificationProof
+) where
 
 
 
-import           Data.Kore.AST.Common
-import           Data.Kore.AST.MLPatterns
-import           Data.Kore.AST.PureML
 import           Data.Kore.AST.MetaOrObject
-import           Data.Kore.FixTraversals
 import           Data.Kore.IndexedModule.MetadataTools
 import           Data.Kore.ASTUtils.SmartConstructors 
 import           Data.Kore.ASTUtils.Substitution
 
 import           Data.Kore.Proof.Proof
-import           Data.Kore.Proof.Dummy
+-- import           Data.Kore.Proof.Dummy
 import           Data.Kore.Proof.Util
 import           Data.Kore.Proof.ConstructorAxioms
 
-import           Control.Monad                         (foldM)
-import           Data.Fix
 import           Data.Reflection
-import           Data.Function                         (on)
-import           Data.List                             (groupBy, partition,
-                                                        sortBy)
 import qualified Data.Set as S
-import           Data.Kore.Unparser.Unparse
 import           Data.Hashable
 import           GHC.Generics (Generic)
 
-import Debug.Trace
-import Text.Groom 
+-- | Given terms `t1` and `t2`, outputs a proof that 
+-- `t1 /\ t2 <-> t1 /\ <MGU EQNS>`, where `<MGU EQNS>` is
+-- a conjunction of the most general unifier equations. 
+unificationProof 
+    :: Given (MetadataTools Object) 
+    => Term 
+    -> Term 
+    -> Either UnificationError Proof 
+unificationProof a b = do 
+    forwards <- unificationForwardsProof a b 
+    let mguEqns = 
+            case getConclusion forwards of 
+                     Implies_ _ _ b -> b
+                     _ -> impossible 
+    let backwards = unificationBackwardsProof mguEqns a b 
+    return $ useRule $ AndIntro forwards backwards 
 
-spy x = trace (groom x) x 
+unificationBackwardsProof
+    :: Given (MetadataTools Object)
+    => Term -- conjunction of eqs 
+    -> Term -- LHS 
+    -> Term -- RHS
+    -> Proof 
+unificationBackwardsProof eqns a b = useRule $ Discharge eqns aEqb 
+    where eqns' = andElimN $ assume eqns
+          transform k p = 
+            foldr 
+            (\eq p -> provablySubstitute eq [k] p) 
+            (useRule $ EqualityIntro p) eqns'
+          aEqSubstA = transform 1 a 
+          substBEqB = transform 0 b 
+          aEqb = provablySubstitute substBEqB [1] aEqSubstA
 
-go :: Given (MetadataTools Object) 
-   => [Proof] 
-   -> [Proof] 
-   -> Either UnificationError Proof
-go finished [] = Right $ andIntroN finished
-go finished (eq : eqs) = case getConclusion eq of 
-    And_ s a b -> go finished $ 
-      useRule (AndElimL eq) : useRule (AndElimR eq) : eqs
-    Equals_ s1 s2 (Var_ x) b 
-      | occursCheck eq -> 
-          go (eq : finished) (map (provablySubstitute eq []) eqs)
-      | otherwise -> 
-          Left $ OccursCheck eq 
-    Equals_ s1 s2 a (Var_ x) -> go finished $ 
-      flipEqn eq : eqs
-    Equals_ s1 s2 a b -> do 
-      eq' <- splitConstructor eq 
-      go finished (eq' : eqs)
+unificationForwardsProof 
+    :: Given (MetadataTools Object)
+    => Term 
+    -> Term  
+    -> Either UnificationError Proof
+unificationForwardsProof a b = do 
+    proof <- go [] [assume $ mkEquals a b]
+    return $ useRule $ Discharge (mkEquals a b) proof 
+        where 
+          go finished [] = return $ andIntroN finished
+          go finished (eq : eqs) = case getConclusion eq of 
+              And_ s a b -> go finished $ 
+                useRule (AndElimL eq) : useRule (AndElimR eq) : eqs
+              Equals_ s1 s2 (Var_ x) b 
+                | occursCheck eq -> 
+                    go (eq : finished) (map (provablySubstitute eq []) eqs)
+                | otherwise -> 
+                    Left $ OccursCheck eq 
+              Equals_ s1 s2 a (Var_ x) -> go finished (flipEqn eq : eqs)
+              Equals_ s1 s2 a b -> do 
+                eq' <- splitConstructor eq 
+                go finished (eq' : eqs)
+              _ -> impossible 
 
 data UnificationError
   = ConstructorClash Term Term 
@@ -98,9 +125,9 @@ splitConstructor eq =
              forallElimN (ca ++ cb) $ 
              (assume $ generateInjectivityAxiom ha (getSort a) (map getSort ca))
          | otherwise -> Left $ ConstructorClash a b 
+        _ -> impossible
     otherwise -> impossible
-    where instantiateForalls args pat = 
-              foldr (\arg pat -> useRule $ ForallElim arg pat) pat args
+    
 
 --testing:
 --  putStrLn $ groom $ dummyEnvironment $ generateInjectivityAxiom (sym "f") (testSort "R") [testSort "A", testSort "B", testSort "C"]
@@ -115,34 +142,45 @@ flipEqn eq = case getConclusion eq of
       -- i.e. substitute a=b in the first position of a=a to get b=a
     _ -> impossible
 
-ex1 :: Given (MetadataTools Object) => [Proof]
-ex1 =  [
-  assume $ 
-  mkEquals 
-    (mkApp (sym "f") 
-        [Var_ $ var "a", mkApp (sym "g") 
-        [Var_ $ var "b", Var_ $ var "c"]]
-    )
-    (mkApp (sym "f") [Var_ $ var "d", Var_ $ var "e"]) 
-  ]
+-- exampleTerm1 :: Given (MetadataTools Object) => Term 
+-- exampleTerm1 =     
+--     (mkApp (sym "f") 
+--         [Var_ $ var "a", mkApp (sym "g") 
+--         [Var_ $ var "b", Var_ $ var "c"]]
+--     )
 
-ex2 :: Given (MetadataTools Object) => [Proof]
-ex2 =  [
-  assume $ 
-  mkEquals 
-    (mkApp (sym "f") [
-        Var_ $ var "a"
-      , mkApp (sym "g") [
-            Var_ $ var "b"
-          , Var_ $ var "c"
-        ]
-    ])
-    (mkApp (sym "f") [
-        mkApp (sym "q") [
-            Var_ $ var "d"
-        ]
-        , Var_ $ var "e"
-    ])
-  ]
+-- exampleTerm2 :: Given (MetadataTools Object) => Term 
+-- exampleTerm2 = 
+--     (mkApp (sym "f") [Var_ $ var "d", Var_ $ var "e"]) 
+
+-- ex1 :: Given (MetadataTools Object) => [Proof]
+-- ex1 =  [
+--   assume $ 
+--   mkEquals 
+--     (mkApp (sym "f") 
+--         [Var_ $ var "a", mkApp (sym "g") 
+--         [Var_ $ var "b", Var_ $ var "c"]]
+--     )
+--     (mkApp (sym "f") [Var_ $ var "d", Var_ $ var "e"]) 
+--   ]
+
+-- ex2 :: Given (MetadataTools Object) => [Proof]
+-- ex2 =  [
+--   assume $ 
+--   mkEquals 
+--     (mkApp (sym "f") [
+--         Var_ $ var "a"
+--       , mkApp (sym "g") [
+--             Var_ $ var "b"
+--           , Var_ $ var "c"
+--         ]
+--     ])
+--     (mkApp (sym "f") [
+--         mkApp (sym "q") [
+--             Var_ $ var "d"
+--         ]
+--         , Var_ $ var "e"
+--     ])
+--   ]
 
 
