531439523c84ba9fe95a7816b9263e8c34a47bb1
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2018-08-06 11:32:51 -0500

    Control.DeepSeq.Orphans: Add module header
    No detailed module description seems necessary.

--- src/main/haskell/kore/src/Control/DeepSeq/Orphans.hs
+++ src/main/haskell/kore/src/Control/DeepSeq/Orphans.hs
@@ -1,5 +1,13 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
+{- |
+Module: Control.DeepSeq.Orphans
+Description: Orphan instances for "Control.DeepSeq"
+Copyright: (c) 2018 Runtime Verification, Inc.
+License: UIUC/NCSA
+Maintainer: thomas.tuegel@runtimeverification.com
+-}
+
 module Control.DeepSeq.Orphans where
 
 import Control.DeepSeq
