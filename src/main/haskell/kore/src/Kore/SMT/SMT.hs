f9e73f9c4fbfb56fdfb693ae7f29380e2f0affda
Phillip Harris <palindromeh@gmail.com>
2018-08-21 10:12:26 -0500

    stuck on how to handle free variables in the Symbolic monad

--- src/main/haskell/kore/src/Kore/SMT/SMT.hs
+++ src/main/haskell/kore/src/Kore/SMT/SMT.hs
@@ -8,18 +8,20 @@ Stability   : experimental
 Portability : portable
 -}
 module Kore.SMT.SMT
-( HookLookup(..)
-, translate
+( translate
 , provePattern
 ) where
 
-import Data.Default
+import           Data.Default
+import           Data.Set
+import           Control.Monad
 
 import           Kore.AST.Common
 import           Kore.AST.MetaOrObject
 import           Kore.AST.PureML
 import           Kore.ASTUtils.SmartPatterns
 import           Kore.ASTUtils.SmartConstructors
+import           Kore.ASTUtils.Substitution
 import           Kore.Attribute.Parser
                  ( ParseAttributes (..) )
 import           Kore.Builtin.Hook
@@ -29,11 +31,11 @@ import           Kore.Implicit.Attributes
 import           Data.Reflection 
 import           Data.SBV
 
-data HookLookup
-  = HookLookup
-  { getSymbolHook_ :: SymbolOrAlias Object -> Hook
-  , getSortHook_   :: Sort Object -> Hook
-  }
+-- data HookLookup
+--   = HookLookup
+--   { getSymbolHook_ :: SymbolOrAlias Object -> Hook
+--   , getSortHook_   :: Sort Object -> Hook
+--   }
 
 data SMTAttributes
   = SMTAttributes
@@ -51,22 +53,29 @@ instance ParseAttributes SMTAttributes where
 -- TODO: No hardcoded strings.
 
 provePattern
-    :: Given HookLookup
-    => CommonPurePattern Object
+    :: CommonPurePattern Object
     -> IO ThmResult
 provePattern = prove . translate
 
 translate 
-    :: Given HookLookup
-    => CommonPurePattern Object
+    :: CommonPurePattern Object
     -> Predicate
 translate p = goBoolean p
   where
-    goBoolean (And_ _ x1 x2)
-     = do 
-       tx1 <- goBoolean x1 
-       tx2 <- goBoolean x2
-       return (tx1 &&& tx2)
+    goUnaryOp op cont x1 = 
+      do
+      tx1 <- cont x1
+      return (op tx1)
+    goBinaryOp op cont x1 x2 = 
+      do
+      tx1 <- cont x1
+      tx2 <- cont x2
+      return (tx1 `op` tx2)
+    goBoolean (And_     _ x1 x2) = goBinaryOp (&&&) goBoolean x1 x2
+    goBoolean (Or_      _ x1 x2) = goBinaryOp (|||) goBoolean x1 x2
+    goBoolean (Implies_ _ x1 x2) = goBinaryOp (==>) goBoolean x1 x2
+    goBoolean (Iff_     _ x1 x2) = goBinaryOp (<=>) goBoolean x1 x2
+    goBoolean (Not_     _ x1)    = goUnaryOp (bnot) goBoolean x1 
     goBoolean (App_ h [x1, x2])
      | getSymbolHook h == Hook (Just "BOOL.le")
        = do
@@ -90,17 +99,24 @@ translate p = goBoolean p
      | getSortHook (variableSort v) == Hook (Just "INT.Int")
        = free $ getId $ variableName v
      | otherwise = error $ "Expected variable with hook " ++ "INT.Int"
+    handleFreeVars = forM (toList $ freeVars p) $ \var -> do
+      handleVar var
+    handleVar :: Variable Object -> Symbolic ()
+    handleVar var = 
+      case getSortHook (variableSort var) of
+        Hook (Just "BOOL.Bool") -> undefined
+        Hook (Just "INT.Int")   -> undefined
 
 -- getId . symbolOrAliasConstructor
 
 getSymbolHook 
-    :: Given HookLookup 
-    => SymbolOrAlias Object 
+    :: SymbolOrAlias Object 
     -> Hook
-getSymbolHook sym = getSymbolHook_ given $ sym
+getSymbolHook sym = Hook $ Just $ getId $ symbolOrAliasConstructor sym --getSymbolHook_ given $ sym
 
 getSortHook 
-    :: Given HookLookup
-    => Sort Object 
+    :: Sort Object 
     -> Hook
-getSortHook sort = getSortHook_ given $ sort
+getSortHook sort = Hook $ Just $ getId $ (\s -> case s of (SortActualSort (SortActual n _)) -> n ; _ -> error (show s)) sort --getSortHook_ given $ sort
+
+
