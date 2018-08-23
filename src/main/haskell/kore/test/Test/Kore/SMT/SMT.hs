2aad93de0c304da0ad3f0d5269b237cbd9e0801c
Phillip Harris <palindromeh@gmail.com>
2018-08-23 10:55:09 -0500

    .

---
+++ src/main/haskell/kore/test/Test/Kore/SMT/SMT.hs
@@ -0,0 +1,15 @@
+module Test.Kore.SMT.SMT (test_SMT) where
+
+import           Test.Tasty                           (TestTree, testGroup)
+import           Test.Tasty.HUnit                     (Assertion, assertEqual,
+                                                       testCase)
+
+import           Kore.AST.Common
+import           Kore.AST.MetaOrObject
+
+import           Kore.ASTUtils.SmartConstructors
+import           Kore.ASTUtils.SmartPatterns
+
+test_SMT = undefined
+
+
