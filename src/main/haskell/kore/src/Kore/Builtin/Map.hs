aff44d7c714e497f964c0b0e2f4e53571000d3fc
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2018-08-23 13:24:22 -0500

    Kore.Builtin.Map: Fix build with --pedantic

--- src/main/haskell/kore/src/Kore/Builtin/Map.hs
+++ src/main/haskell/kore/src/Kore/Builtin/Map.hs
@@ -11,7 +11,7 @@ This module is intended to be imported qualified, to avoid collision with other
 builtin modules.
 
 @
-    import qualified Kore.Builtin.Int as Int
+    import qualified Kore.Builtin.Map as Map
 @
  -}
 module Kore.Builtin.Map
@@ -21,35 +21,22 @@ module Kore.Builtin.Map
     , builtinFunctions
     ) where
 
-import           Control.Monad
-                 ( void )
-import           Data.Functor.Foldable
-import           Data.Map
 import qualified Data.HashMap.Strict as HashMap
-import qualified Text.Megaparsec.Char.Lexer as Parsec
+import           Data.Map
 
 import           Kore.AST.Common
 import           Kore.AST.MetaOrObject
 import           Kore.AST.PureML
-
 import           Kore.ASTUtils.SmartPatterns
-import           Kore.ASTUtils.SmartConstructors
-
-import           Kore.IndexedModule.MetadataTools
-
-import           Kore.Variables.Fresh.IntCounter 
- ( IntCounter )
-
-import           Kore.Step.Function.Data
-import           Kore.Step.StepperAttributes
-import           Kore.Step.ExpandedPattern
-import           Kore.Predicate.Predicate
-import           Kore.Step.OrOfExpandedPattern
-
-import           Kore.Builtin.Hook
 import qualified Kore.Builtin.Builtin as Builtin
-
-import Kore.Step.Simplification.Data
+import           Kore.Builtin.Hook
+import           Kore.IndexedModule.MetadataTools
+import           Kore.Predicate.Predicate
+import           Kore.Step.ExpandedPattern
+import           Kore.Step.Function.Data
+import           Kore.Step.OrOfExpandedPattern
+import           Kore.Step.Simplification.Data
+import           Kore.Step.StepperAttributes
 
 
 {- | Builtin name of the @Map@ sort.
@@ -82,15 +69,15 @@ symbolVerifiers :: Builtin.SymbolVerifiers
 symbolVerifiers =
     HashMap.fromList
     [
-      ("MAP.bind" 
-      , Builtin.verifySymbol trivialVerifier 
+      ("MAP.bind"
+      , Builtin.verifySymbol trivialVerifier
           [ trivialVerifier
           , trivialVerifier
           , trivialVerifier
           ]
       )
     , ("MAP.emp"
-      , Builtin.verifySymbol trivialVerifier 
+      , Builtin.verifySymbol trivialVerifier
           [ trivialVerifier
           , trivialVerifier
           , trivialVerifier
@@ -111,98 +98,75 @@ symbolVerifiers =
     , ("MAP.lookup"
       , Builtin.verifySymbol trivialVerifier
           [ trivialVerifier
-          , trivialVerifier
-          , trivialVerifier
+          , assertSort
           ]
       )
     ]
-  where 
+  where
     trivialVerifier :: Builtin.SortVerifier
     trivialVerifier = const $ const $ Right ()
 
-isHook 
+isHook
     :: MetadataTools Object StepperAttributes
     -> SymbolOrAlias Object
     -> String
     -> Bool
-isHook tools sym hookName = 
+isHook tools sym hookName =
     hook (attributes tools sym) == Hook (Just hookName)
 
 trivialExpandedPattern
-    :: MetaOrObject level 
-    => PureMLPattern level var 
+    :: MetaOrObject level
+    => PureMLPattern level var
     -> ExpandedPattern level var
-trivialExpandedPattern p = 
+trivialExpandedPattern p =
     ExpandedPattern p makeTruePredicate []
 
 trivialEvalResult
-    :: (Applicative f, MetaOrObject level1) 
+    :: (Applicative f, MetaOrObject level1)
     => PureMLPattern level1 variable
     -> f (AttemptedFunction level1 variable, SimplificationProof level2)
-trivialEvalResult p = 
+trivialEvalResult p =
     pure (Applied (MultiOr [trivialExpandedPattern p]), SimplificationProof)
 
 failedToEval
-    :: Simplifier 
+    :: Simplifier
          (AttemptedFunction level1 variable, SimplificationProof level2)
 failedToEval = pure (NotApplicable, SimplificationProof)
-
-evalElement :: Builtin.Function
-evalElement = 
-    ApplicationFunctionEvaluator evalElement0
-  where
-    evalElement0 tools contEval pat = 
-      trivialEvalResult (Fix $ ApplicationPattern pat) 
 
 evalBind :: Builtin.Function
 evalBind =
     ApplicationFunctionEvaluator evalBind0
   where
-    evalBind0 tools contEval pat = 
-      case pat of
-        Application h [k, v, m]
-         | hook (attributes tools h) == Hook (Just "MAP.bind") ->  failedToEval
-        _ -> failedToEval
-
-evalMerge :: Builtin.Function
-evalMerge = 
-    ApplicationFunctionEvaluator evalMerge0
-  where
-    evalMerge0 tools contEval pat = 
-      case pat of
-        Application h [m1, m2]
-         | isHook tools h "MAP.merge" -> trivialEvalResult $  goMerge m1 m2
-      where 
-        goMerge m1 m2 = undefined
+    evalBind0 _ _ _ = failedToEval
 
 -- FIXME: proper equality modulo alpha?
 evalLookup :: Builtin.Function
-evalLookup = 
+evalLookup =
     ApplicationFunctionEvaluator evalLookup0
   where
-    evalLookup0 tools contEval pat = 
-      case pat of
-        Application h [k, m]
-         | hook (attributes tools h) == Hook (Just "MAP.lookup") -> goFind k m
-        _ -> failedToEval
-      where 
+    evalLookup0 tools _ pat =
+          case pat of
+              Application _ [k, m] -> goFind k m
+              _ -> failedToEval
+      where
         goFind k m = case m of
-          App_ h [k', v]
-           | isHook tools h "MAP.element" -> 
-              if k == k'
-                then trivialEvalResult v 
-                else failedToEval
-          App_ h [k', v, m']
-           | isHook tools h "MAP.bind" -> 
-              if k == k' 
-                then trivialEvalResult v
-                else goFind k m'
+            App_ h [k', v]
+              | isHook tools h "MAP.element" ->
+                  if k == k'
+                      then trivialEvalResult v
+                      else failedToEval
+            App_ h [k', v, m']
+              | isHook tools h "MAP.bind" ->
+                  if k == k'
+                      then trivialEvalResult v
+                      else goFind k m'
+            _ -> failedToEval
 
 
 {- | Implement builtin function evaluation.
  -}
 builtinFunctions :: Map String Builtin.Function
-builtinFunctions = 
+builtinFunctions =
   fromList
     [
       ("MAP.bind", evalBind)
