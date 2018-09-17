78d100febd3ba36842259dac7f81c58a214e6cca
Virgil <25692529+virgil-serbanuta@users.noreply.github.com>
2018-09-17 12:09:05 +0300

    Ceil for total patterns (#216)

--- src/main/haskell/kore/src/Kore/Step/PatternAttributesError.hs
+++ src/main/haskell/kore/src/Kore/Step/PatternAttributesError.hs
@@ -11,17 +11,41 @@ Portability : portable
 module Kore.Step.PatternAttributesError
     ( FunctionError (..)
     , FunctionalError (..)
+    , TotalError (..)
     ) where
 
 import Kore.AST.Common
        ( SymbolOrAlias )
 
+{-| An error explaining why a pattern is not composed of function heads and
+things like StringLiteral, DomainValue and variables.
+-}
 data FunctionError level
     = NonFunctionHead (SymbolOrAlias level)
+    -- ^ The pattern was the application of a non-function head to something
     | NonFunctionPattern
+    -- ^ The pattern is neither an application pattern nor one of the basic
+    -- pattern types (e.g. domain values).
     deriving (Eq, Show)
 
+{-| An error explaining why a pattern is not composed of functional heads and
+things like StringLiteral, DomainValue and variables.
+-}
 data FunctionalError level
     = NonFunctionalHead (SymbolOrAlias level)
+    -- ^ The pattern was the application of a non-functional head to something
     | NonFunctionalPattern
+    -- ^ The pattern is neither an application pattern nor one of the basic
+    -- pattern types (e.g. domain values).
+    deriving (Eq, Show)
+
+{-| An error explaining why a pattern is not composed of total heads and
+things like StringLiteral, DomainValue and variables.
+-}
+data TotalError level
+    = NonTotalHead (SymbolOrAlias level)
+    -- ^ The pattern was the application of a non-total head to something
+    | NonTotalPattern
+    -- ^ The pattern is neither an application pattern nor one of the basic
+    -- pattern types (e.g. domain values).
     deriving (Eq, Show)
