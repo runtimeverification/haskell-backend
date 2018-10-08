08ae9ff65fefc019a45350f6ad0d3024213d751b
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2018-10-08 10:01:07 -0500

    Add Kore.Proof.Value

---
+++ src/main/haskell/kore/src/Kore/Proof/Value.hs
@@ -0,0 +1,137 @@
+{- |
+Module      : Kore.Proof.Value
+Description : Proof that a pattern is a fully-evaluated term
+Copyright   : (c) Runtime Verification, 2018
+License     : NCSA
+Maintainer  : thomas.tuegel@runtimeverification.com
+ -}
+
+{-# LANGUAGE TemplateHaskell #-}
+
+module Kore.Proof.Value
+    ( ValueF (..)
+    , Value
+    , fromPattern
+    , fromConcretePurePattern
+    , asPattern
+    , asConcretePurePattern
+    ) where
+
+import           Data.Deriving
+                 ( deriveEq1, deriveOrd1, deriveShow1 )
+import           Data.Functor.Foldable
+                 ( Fix (..) )
+import qualified Data.Functor.Foldable as Functor.Foldable
+import           Data.Reflection
+                 ( give )
+
+import           Kore.AST.Common
+                 ( Concrete, ConcretePurePattern, Pattern (..) )
+import qualified Kore.AST.Common as Pattern
+import           Kore.AST.MetaOrObject
+import           Kore.IndexedModule.MetadataTools
+import           Kore.Step.StepperAttributes
+
+{- | Proof (by construction) that a pattern is a normalized value.
+
+    A normal pattern head is either a constructor (or a constructor-like domain
+    value), a sort injection, or a domain value.
+ -}
+data ValueF level child where
+    Constructor :: !(Pattern.Application level child) -> ValueF level child
+    SortInjection :: !(Pattern.Application level child) -> ValueF level child
+    DomainValue
+        :: !(Pattern.DomainValue Object (Pattern.BuiltinDomain child))
+        -> ValueF Object child
+
+deriving instance Eq child => Eq (ValueF level child)
+deriving instance Ord child => Ord (ValueF level child)
+deriving instance Show child => Show (ValueF level child)
+
+deriving instance Functor (ValueF level)
+deriving instance Foldable (ValueF level)
+deriving instance Traversable (ValueF level)
+
+deriveEq1 ''ValueF
+deriveOrd1 ''ValueF
+deriveShow1 ''ValueF
+
+type Value level = Fix (ValueF level)
+
+{- | Project a sort injection head to @Nothing@.
+
+    Used in 'fromPattern' to ensure that the children of a sort injection are
+    not sort injections, i.e. that the triangle axiom for sort injections has
+    been fully applied.
+ -}
+eraseSortInjection :: Value level -> Maybe (Value level)
+eraseSortInjection val =
+    case Functor.Foldable.project val of
+        Constructor _ -> Just val
+        DomainValue _ -> Just val
+        SortInjection _ -> Nothing
+
+{- | Embed the normalized pattern head if its children are normal values.
+
+    See also: 'fromConcretePurePattern'.
+
+ -}
+fromPattern
+    :: MetadataTools level StepperAttributes
+    -> Pattern level Concrete (Maybe (Value level))
+    -> Maybe (Value level)
+fromPattern tools =
+    \case
+        ApplicationPattern
+            appP@Pattern.Application
+                { applicationSymbolOrAlias = symbolOrAlias }
+          | isConstructor symbolOrAlias ->
+            -- The constructor application is normal if all its children are
+            -- normal.
+            Fix . Constructor <$> sequence appP
+          | isSortInjection symbolOrAlias ->
+            -- The sort injection application is normal if all its children are
+            -- normal and none are sort injections.
+            Fix . SortInjection <$> traverse (>>= eraseSortInjection) appP
+        DomainValuePattern dvP ->
+            -- A domain value is not technically a constructor, but it is
+            -- constructor-like for builtin domains, at least from the
+            -- perspective of normalization.
+            -- TODO (thomas.tuegel): Builtin domain parsers may violate the
+            -- assumption that domain values are concrete. We should remove
+            -- BuiltinDomainPattern and always run the stepper with internal
+            -- representations only.
+            Fix . DomainValue <$> traverse sequence dvP
+        _ -> Nothing
+  where
+    isConstructor = give tools isConstructor_
+    isSortInjection = give tools isSortInjection_
+
+{- | View a 'ConcretePurePattern' as a normalized value.
+
+    The pattern is considered normalized if it is a domain value, a constructor,
+    or a sort injection applied only to normalized value.
+
+    See also: 'fromPattern'
+
+ -}
+fromConcretePurePattern
+    :: MetadataTools level StepperAttributes
+    -> ConcretePurePattern level
+    -> Maybe (Value level)
+fromConcretePurePattern tools =
+    Functor.Foldable.fold (fromPattern tools)
+
+{- | Project a 'Value' to a concrete 'Pattern' head.
+ -}
+asPattern :: Value level -> Pattern level Concrete (Value level)
+asPattern val =
+    case Functor.Foldable.project val of
+        Constructor appP -> ApplicationPattern appP
+        SortInjection appP -> ApplicationPattern appP
+        DomainValue dvP -> DomainValuePattern dvP
+
+{- | View a normalized value as a 'ConcretePurePattern'.
+ -}
+asConcretePurePattern :: Value level -> ConcretePurePattern level
+asConcretePurePattern = Functor.Foldable.unfold asPattern
---
+++ src/main/haskell/kore/test/Test/Kore/Proof/Value.hs
@@ -0,0 +1,199 @@
+module Test.Kore.Proof.Value where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import qualified Data.Functor.Foldable as Functor.Foldable
+import           Data.Reflection
+                 ( give )
+
+import           Kore.AST.Common
+import           Kore.AST.MetaOrObject
+import           Kore.AST.MLPatterns
+import           Kore.AST.PureML
+import           Kore.ASTHelpers
+                 ( ApplicationSorts (..) )
+import qualified Kore.ASTUtils.SmartConstructors as Kore
+import           Kore.IndexedModule.MetadataTools
+                 ( MetadataTools )
+import qualified Kore.Proof.Value as Value
+import           Kore.Step.StepperAttributes
+
+import           Test.Kore
+import           Test.Kore.Builtin.Int
+                 ( intSort )
+import qualified Test.Kore.IndexedModule.MockMetadataTools as Mock
+
+unit_constructorUnit :: Assertion
+unit_constructorUnit = assertValue unitPattern
+
+unit_domainValue :: Assertion
+unit_domainValue = assertValue onePattern
+
+unit_injConstructor :: Assertion
+unit_injConstructor = assertValue (mkInj unitPattern)
+
+unit_injInj :: Assertion
+unit_injInj = assertNotValue (mkInj (mkInj unitPattern))
+
+unit_pairConstructor :: Assertion
+unit_pairConstructor = assertValue (mkPair unitPattern unitPattern)
+
+test_pairDomainValue :: [TestTree]
+test_pairDomainValue =
+    [ testValue "(0, 0)" (mkPair zeroPattern zeroPattern)
+    , testValue "(0, 1)" (mkPair zeroPattern onePattern)
+    , testValue "(1, 1)" (mkPair onePattern onePattern)
+    , testValue "(1, 0)" (mkPair onePattern zeroPattern)
+    ]
+
+unit_fun :: Assertion
+unit_fun = assertNotValue (mkApp funSymbol [onePattern])
+
+mkApp
+    :: SymbolOrAlias Object
+    -> [PureMLPattern Object var]
+    -> PureMLPattern Object var
+mkApp = give symbolOrAliasSorts Kore.mkApp
+
+mkInj :: CommonPurePattern Object -> CommonPurePattern Object
+mkInj input = mkApp (injSymbol inputSort supSort) [input]
+  where
+    inputSort =
+        getPatternResultSort
+            symbolOrAliasSorts
+            (Functor.Foldable.project input)
+
+mkPair
+    :: CommonPurePattern Object
+    -> CommonPurePattern Object
+    -> CommonPurePattern Object
+mkPair a b = mkApp (pairSymbol inputSort) [a, b]
+  where
+    inputSort =
+        getPatternResultSort
+            symbolOrAliasSorts
+            (Functor.Foldable.project a)
+
+mkDomainValue
+    :: Sort Object
+    -> BuiltinDomain (PureMLPattern Object var)
+    -> PureMLPattern Object var
+mkDomainValue = give symbolOrAliasSorts Kore.mkDomainValue
+
+unitPattern :: CommonPurePattern Object
+unitPattern = mkApp unitSymbol []
+
+onePattern :: CommonPurePattern Object
+onePattern =
+    (mkDomainValue intSort . BuiltinDomainPattern)
+        (Kore.mkStringLiteral "1")
+
+zeroPattern :: CommonPurePattern Object
+zeroPattern =
+    (mkDomainValue intSort . BuiltinDomainPattern)
+        (Kore.mkStringLiteral "1")
+
+unitSort :: Sort Object
+unitSort =
+    SortActualSort SortActual
+        { sortActualName = testId "Unit"
+        , sortActualSorts = []
+        }
+
+unitSymbol :: SymbolOrAlias Object
+unitSymbol =
+    SymbolOrAlias
+        { symbolOrAliasConstructor = testId "unit"
+        , symbolOrAliasParams = []
+        }
+
+pairSort :: Sort Object -> Sort Object
+pairSort sort =
+    SortActualSort SortActual
+        { sortActualName = testId "Pair"
+        , sortActualSorts = [sort]
+        }
+
+pairSymbol :: Sort Object -> SymbolOrAlias Object
+pairSymbol sort =
+    SymbolOrAlias
+        { symbolOrAliasConstructor = testId "pair"
+        , symbolOrAliasParams = [sort]
+        }
+
+injSymbol :: Sort Object -> Sort Object -> SymbolOrAlias Object
+injSymbol sub sup =
+    SymbolOrAlias
+        { symbolOrAliasConstructor = testId "inj"
+        , symbolOrAliasParams = [sub, sup]
+        }
+
+funSymbol :: SymbolOrAlias Object
+funSymbol =
+    SymbolOrAlias
+        { symbolOrAliasConstructor = testId "fun"
+        , symbolOrAliasParams = []
+        }
+
+symbolOrAliasSorts :: SymbolOrAlias Object -> ApplicationSorts Object
+symbolOrAliasSorts =
+    Mock.makeSymbolOrAliasSorts
+        [ (unitSymbol, ApplicationSorts [] unitSort)
+        , (injSymbol subSort supSort, ApplicationSorts [subSort] supSort)
+        , (injSymbol unitSort supSort, ApplicationSorts [unitSort] supSort)
+        , (injSymbol supSort supSort, ApplicationSorts [supSort] supSort)
+        , ( pairSymbol unitSort
+          , ApplicationSorts [unitSort, unitSort] (pairSort unitSort)
+          )
+        , ( pairSymbol intSort
+          , ApplicationSorts [intSort, intSort] (pairSort intSort)
+          )
+        , (funSymbol, ApplicationSorts [intSort] intSort)
+        ]
+
+subSort :: Sort level
+subSort = (SortVariableSort . SortVariable) (testId "sub")
+
+supSort :: Sort level
+supSort = (SortVariableSort . SortVariable) (testId "sup")
+
+symbolOrAliasAttrs :: [(SymbolOrAlias Object, StepperAttributes)]
+symbolOrAliasAttrs =
+    [ (unitSymbol, Mock.constructorAttributes)
+    , (injSymbol subSort supSort, Mock.sortInjectionAttributes)
+    , (injSymbol unitSort supSort, Mock.sortInjectionAttributes)
+    , (injSymbol supSort supSort, Mock.sortInjectionAttributes)
+    , (pairSymbol unitSort, Mock.constructorAttributes)
+    , (pairSymbol intSort, Mock.constructorAttributes)
+    , (funSymbol, Mock.functionAttributes)
+    ]
+
+tools :: MetadataTools Object StepperAttributes
+tools =
+    Mock.makeMetadataTools
+        symbolOrAliasSorts
+        symbolOrAliasAttrs
+        []
+
+assertValue :: CommonPurePattern Object -> Assertion
+assertValue purePattern =
+    assertEqual "Expected normalized pattern"
+        concretePattern
+        (concretePattern >>= roundTrip)
+  where
+    concretePattern = asConcretePurePattern purePattern
+    roundTrip patt = do
+        value <- Value.fromConcretePurePattern tools patt
+        return (Value.asConcretePurePattern value)
+
+testValue :: TestName -> CommonPurePattern Object -> TestTree
+testValue name = testCase name . assertValue
+
+assertNotValue :: CommonPurePattern Object -> Assertion
+assertNotValue purePattern =
+    assertEqual "Unexpected normalized pattern"
+        Nothing
+        (concretePattern >>= Value.fromConcretePurePattern tools)
+  where
+    concretePattern = asConcretePurePattern purePattern
