149b6085488889e09a27e4a6950fae00acb8fbf3
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2018-10-12 11:42:06 -0500

    Makefile: Add a target for Jenkins

--- Makefile
+++ Makefile
@@ -1,16 +1,35 @@
 include include.mk
 
-.PHONY: all test test-kore test-k distclean clean clean-submodules
+jenkins: STACK_OPTS += --test --bench --coverage
+export STACK_OPTS
 
-all: kore-exec
+.PHONY: all docs haddock jenkins kore test test-kore test-k distclean clean clean-submodules
+
+kore:
+	stack build $(STACK_BUILD_OPTS)
+
+docs: haddock
+
+haddock:
+	stack haddock --no-haddock-deps $(STACK_HADDOCK_OPTS)
+	if [ -n "$$BUILD_NUMBER" ]; then \
+		cp -r $$(stack path --local-doc-root) haskell_documentation; \
+	fi
+
+all: kore
 
 test: test-kore test-k
 
 test-kore:
-	$(STACK) test $(STACK_TEST_OPTS)
+	stack test $(STACK_TEST_OPTS)
+	if [ -n "$$BUILD_NUMBER" ]; then \
+		cp -r $$(stack path --local-hpc-root) coverage_report; \
+	fi
 
 test-k:
 	$(MAKE) -C src/main/k/working test-k
+
+jenkins: all test docs
 
 distclean: clean
 	cd $(K_SUBMODULE) \
@@ -20,6 +39,3 @@ distclean: clean
 clean: clean-submodules
 	stack clean
 	$(MAKE) -C src/main/k/working clean
-
-clean-submodules:
-	rm -rf $(K_TIMESTAMP)
--- include.mk
+++ include.mk
@@ -8,6 +8,15 @@ K_BIN := $(K_SUBMODULE)/k-distribution/target/release/k/bin
 KOMPILE := $(K_BIN)/kompile
 KRUN := $(K_BIN)/krun
 
+STACK_OPTS ?= --pedantic
+STACK_BUILD_OPTS = $(STACK_OPTS) --no-run-tests --no-run-benchmarks
+STACK_HADDOCK_OPTS = $(STACK_OPTS) --no-run-tests --no-run-benchmarks
+STACK_TEST_OPTS = $(STACK_OPTS) --no-run-benchmarks
+
+ifdef BUILD_NUMBER
+STACK_TEST_OPTS += --ta --xml=test-results.xml
+endif
+
 STACK_LOCAL_INSTALL_ROOT != stack path --local-install-root
 KORE_EXEC ?= $(STACK_LOCAL_INSTALL_ROOT)/bin/kore-exec
 
@@ -16,11 +25,6 @@ KRUN_OPTS ?= --haskell-backend-command $(KORE_EXEC)
 
 KOMPILE_TARGETS := $(KOMPILE)
 KRUN_TARGETS := $(KRUN) $(KORE_EXEC)
-
-STACK ?= stack
-STACK_OPTS ?= --test --bench --pedantic
-STACK_BUILD_OPTS ?= $(STACK_OPTS) --no-run-tests --no-run-benchmarks
-STACK_TEST_OPTS ?= $(STACK_OPTS) --no-run-benchmarks
 
 # Targets
 
@@ -31,7 +35,7 @@ $(K_SUBMODULE): FORCE
 	cd $(K_SUBMODULE) && touch -d $$(git log --format=format:%cI -n 1) .
 
 $(KORE_EXEC): FORCE
-	$(STACK) build $(STACK_BUILD_OPTS) kore:exe:kore-exec
+	stack build $(STACK_BUILD_OPTS) kore:exe:kore-exec
 
 $(KOMPILE): $(K_SUBMODULE)
 	cd $(K_SUBMODULE) && mvn package -q -DskipTests -U
