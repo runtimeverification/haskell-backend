c67b2aa3b52250fdf9448d8fe9ec6d667540d9f4
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2018-10-23 14:58:22 -0500

    extractFunctionAxioms: Extract all axioms in scope
    `extractFunctionAxioms` now extracts axiom patterns from all modules in scope,
    not only the top-most module (as it incorrectly did before).

--- src/main/haskell/kore/src/Kore/Step/Function/Registry.hs
+++ src/main/haskell/kore/src/Kore/Step/Function/Registry.hs
@@ -12,13 +12,12 @@ module Kore.Step.Function.Registry
     , axiomPatternsToEvaluators
     ) where
 
-import           Data.Function
-                 ( on )
-import           Data.List
-                 ( groupBy, sortBy )
+import qualified Data.Foldable as Foldable
+import           Data.Map
+                 ( Map )
 import qualified Data.Map as Map
 import           Data.Maybe
-                 ( mapMaybe )
+                 ( fromMaybe, mapMaybe )
 
 import Kore.AST.Common
 import Kore.AST.Kore
@@ -28,9 +27,9 @@ import Kore.AST.MetaOrObject
 import Kore.AST.PureML
        ( fromPurePattern )
 import Kore.AST.Sentence
-       ( AsSentence (..), SentenceAxiom (..) )
+       ( AsSentence (..), KoreSentenceAxiom, SentenceAxiom (..) )
 import Kore.IndexedModule.IndexedModule
-       ( IndexedModule (..), KoreIndexedModule )
+       ( IndexedModule (..), KoreIndexedModule, indexedModulesInScope )
 import Kore.Step.AxiomPatterns
 import Kore.Step.Function.Data
        ( ApplicationFunctionEvaluator (..) )
@@ -39,29 +38,52 @@ import Kore.Step.Function.UserDefined
 import Kore.Step.StepperAttributes
        ( StepperAttributes )
 
-{-|Given a 'MetaOrObject' @level@ and a 'KoreIndexedModule',
-creates a registry mapping function symbol identifiers to their
-corresponding 'AxiomPattern's.
--}
+{- | Create a mapping from symbol identifiers to their defining axioms.
+
+ -}
 extractFunctionAxioms
-    :: MetaOrObject level
+    ::  forall level.
+        MetaOrObject level
     => level
     -> KoreIndexedModule StepperAttributes
-    -> Map.Map (Id level) [AxiomPattern level]
-extractFunctionAxioms level indexedModule =
-    Map.fromList (map extractPrefix groupedAxiomPatterns)
+    -> Map (Id level) [AxiomPattern level]
+extractFunctionAxioms level =
+    \imod ->
+        Foldable.foldl'
+            extractModuleAxioms
+            Map.empty
+            (indexedModulesInScope imod)
   where
-    extractPrefix []                  = error "unexpected case"
-    extractPrefix ((a, b) : reminder) = (a, b : map snd reminder)
-    groupedAxiomPatterns =
-        groupBy
-            ((==) `on` fst)
-            (sortBy
-                (compare `on` fst)
-                (mapMaybe
-                    (axiomToIdAxiomPatternPair level)
-                    (snd <$> indexedModuleAxioms indexedModule))
-            )
+    -- | Update the map of function axioms with all the axioms in one module.
+    extractModuleAxioms
+        :: Map (Id level) [AxiomPattern level]
+        -> KoreIndexedModule StepperAttributes
+        -> Map (Id level) [AxiomPattern level]
+    extractModuleAxioms axioms imod =
+        Foldable.foldl' extractSentenceAxiom axioms sentences
+      where
+        sentences = indexedModuleAxioms imod
+
+    -- | Extract an axiom from one sentence and update the map of function
+    -- axioms with it. The map is returned unmodified in case the sentence is
+    -- not a function axiom.
+    extractSentenceAxiom
+        :: Map (Id level) [AxiomPattern level]
+        -> (attrs, KoreSentenceAxiom)
+        -> Map (Id level) [AxiomPattern level]
+    extractSentenceAxiom axioms (_, sentence) =
+        let
+            namedAxiom = axiomToIdAxiomPatternPair level sentence
+        in
+            Foldable.foldl' insertAxiom axioms namedAxiom
+
+    -- | Update the map of function axioms by inserting the axiom at the key.
+    insertAxiom
+        :: Map (Id level) [AxiomPattern level]
+        -> (Id level, AxiomPattern level)
+        -> Map (Id level) [AxiomPattern level]
+    insertAxiom axioms (name, patt) =
+        Map.alter (Just . (patt :) . fromMaybe []) name axioms
 
 axiomToIdAxiomPatternPair
     ::  ( MetaOrObject level )
