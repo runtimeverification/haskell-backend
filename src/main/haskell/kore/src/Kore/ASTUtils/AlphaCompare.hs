b390e2e589f17d44ad4c67954cd2afb09ba8f48d
Phillip Harris <palindromeh@gmail.com>
2018-11-15 15:27:08 -0600

    added support for domain value comparison

--- src/main/haskell/kore/src/Kore/ASTUtils/AlphaCompare.hs
+++ src/main/haskell/kore/src/Kore/ASTUtils/AlphaCompare.hs
@@ -12,9 +12,13 @@ module Kore.ASTUtils.AlphaCompare
     ( alphaEq
     ) where
 
-import Data.List
+import           Data.List
+import qualified Data.Map as Map
+import qualified Data.Sequence as Seq
+import qualified Data.Set as Set
 
 import Kore.AST.Common
+import Kore.AST.MetaOrObject
 import Kore.ASTUtils.SmartPatterns
 
 -- alphaEq compares terms modulo renaming of bound variables
@@ -30,79 +34,134 @@ import Kore.ASTUtils.SmartPatterns
 -- lowest index if there are multiple occurences of `elem`.
 
 alphaEq
-    :: Eq (var level)
+    :: forall var level .
+            ( MetaOrObject level
+            , Eq (var level)
+            , Ord (var level)
+            , Eq (var Meta)
+            , Ord (var Meta)
+            , Eq (var Object)
+            , Ord (var Object)
+            )
     => PureMLPattern level var
     -> PureMLPattern level var
     -> Bool
 alphaEq e1' e2' = go [] [] e1' e2'
-  where
-    go ctx1 ctx2 e1 e2 = case (e1, e2) of
-        (And_ s1 a1 b1, And_ s2 a2 b2) ->
-            s1 == s2
-            && go ctx1 ctx2 a1 a2
-            && go ctx1 ctx2 b1 b2
-        (Bottom_ s1, Bottom_ s2) ->
-            s1 == s2
-        (Ceil_ s1 ss1 a1, Ceil_ s2 ss2 a2) ->
-            s1 == s2
-            && ss1 == ss2
-            && go ctx1 ctx2 a1 a2
-        (Equals_ s1 ss1 a1 b1, Equals_ s2 ss2 a2 b2) ->
-            s1 == s2
-            && ss1 == ss2
-            && go ctx1 ctx2 a1 a2
-            && go ctx1 ctx2 b1 b2
-        (Exists_ s1 v1 a1, Exists_ s2 v2 a2) ->
-            s1 == s2
-            && go (v1 : ctx1) (v2 : ctx2) a1 a2
-        (Forall_ s1 v1 a1, Forall_ s2 v2 a2) ->
-            s1 == s2
-            && go (v1 : ctx1) (v2 : ctx2) a1 a2
-        (Floor_ s1 ss1 a1, Floor_ s2 ss2 a2) ->
-            s1 == s2
-            && ss1 == ss2
-            && go ctx1 ctx2 a1 a2
-        (Iff_ s1 a1 b1, Iff_ s2 a2 b2) ->
-            s1 == s2
-            && go ctx1 ctx2 a1 a2
-            && go ctx1 ctx2 b1 b2
-        (Implies_ s1 a1 b1, Implies_ s2 a2 b2) ->
-            s1 == s2
-            && go ctx1 ctx2 a1 a2
-            && go ctx1 ctx2 b1 b2
-        (In_ s1 ss1 a1 b1, In_ s2 ss2 a2 b2) ->
-            s1 == s2
-            && ss1 == ss2
-            && go ctx1 ctx2 a1 a2
-            && go ctx1 ctx2 b1 b2
-        (Next_ s1 a1, Next_ s2 a2) ->
-            s1 == s2
-            && go ctx1 ctx2 a1 a2
-        (Not_ s1 a1, Not_ s2 a2) ->
-            s1 == s2
-            && go ctx1 ctx2 a1 a2
-        (Or_ s1 a1 b1, Or_ s2 a2 b2) ->
-            s1 == s2
-            && go ctx1 ctx2 a1 a2
-            && go ctx1 ctx2 b1 b2
-        (Rewrites_ s1 a1 b1, Rewrites_ s2 a2 b2) ->
-            s1 == s2
-            && go ctx1 ctx2 a1 a2
-            && go ctx1 ctx2 b1 b2
-        (Top_ s1, Top_ s2) ->
-            s1 == s2
-        (StringLiteral_ s1, StringLiteral_ s2) ->
-            s1 == s2
-        (CharLiteral_ c1, CharLiteral_ c2) ->
-            c1 == c2
-        (App_ h1 c1, App_ h2 c2) ->
-            h1 == h2
-            && and (zipWith (go ctx1 ctx2) c1 c2)
-        (Var_ v1, Var_ v2) ->
-            case elemIndex v1 ctx1 of
-                Just i -> (Just i) == elemIndex v2 ctx2
-                Nothing -> v1 == v2
-        (DV_ s1 dv1, DV_ s2 dv2) ->
-            s1 == s2
-            && dv1 == dv2
-        _ -> False
+
+go
+    :: forall var level .
+        ( MetaOrObject level
+        , Eq (var level)
+        , Ord (var level)
+        , Eq (var Meta)
+        , Ord (var Meta)
+        , Eq (var Object)
+        , Ord (var Object)
+        )
+    => [var level]
+    -> [var level]
+    -> PureMLPattern level var
+    -> PureMLPattern level var
+    -> Bool
+go ctx1 ctx2 e1 e2 = case (e1, e2) of
+    (And_ s1 a1 b1, And_ s2 a2 b2) ->
+        s1 == s2
+        && go ctx1 ctx2 a1 a2
+        && go ctx1 ctx2 b1 b2
+    (Bottom_ s1, Bottom_ s2) ->
+        s1 == s2
+    (Ceil_ s1 ss1 a1, Ceil_ s2 ss2 a2) ->
+        s1 == s2
+        && ss1 == ss2
+        && go ctx1 ctx2 a1 a2
+    (Equals_ s1 ss1 a1 b1, Equals_ s2 ss2 a2 b2) ->
+        s1 == s2
+        && ss1 == ss2
+        && go ctx1 ctx2 a1 a2
+        && go ctx1 ctx2 b1 b2
+    (Exists_ s1 v1 a1, Exists_ s2 v2 a2) ->
+        s1 == s2
+        && go (v1 : ctx1) (v2 : ctx2) a1 a2
+    (Forall_ s1 v1 a1, Forall_ s2 v2 a2) ->
+        s1 == s2
+        && go (v1 : ctx1) (v2 : ctx2) a1 a2
+    (Floor_ s1 ss1 a1, Floor_ s2 ss2 a2) ->
+        s1 == s2
+        && ss1 == ss2
+        && go ctx1 ctx2 a1 a2
+    (Iff_ s1 a1 b1, Iff_ s2 a2 b2) ->
+        s1 == s2
+        && go ctx1 ctx2 a1 a2
+        && go ctx1 ctx2 b1 b2
+    (Implies_ s1 a1 b1, Implies_ s2 a2 b2) ->
+        s1 == s2
+        && go ctx1 ctx2 a1 a2
+        && go ctx1 ctx2 b1 b2
+    (In_ s1 ss1 a1 b1, In_ s2 ss2 a2 b2) ->
+        s1 == s2
+        && ss1 == ss2
+        && go ctx1 ctx2 a1 a2
+        && go ctx1 ctx2 b1 b2
+    (Next_ s1 a1, Next_ s2 a2) ->
+        s1 == s2
+        && go ctx1 ctx2 a1 a2
+    (Not_ s1 a1, Not_ s2 a2) ->
+        s1 == s2
+        && go ctx1 ctx2 a1 a2
+    (Or_ s1 a1 b1, Or_ s2 a2 b2) ->
+        s1 == s2
+        && go ctx1 ctx2 a1 a2
+        && go ctx1 ctx2 b1 b2
+    (Rewrites_ s1 a1 b1, Rewrites_ s2 a2 b2) ->
+        s1 == s2
+        && go ctx1 ctx2 a1 a2
+        && go ctx1 ctx2 b1 b2
+    (Top_ s1, Top_ s2) ->
+        s1 == s2
+    (StringLiteral_ s1, StringLiteral_ s2) ->
+        s1 == s2
+    (CharLiteral_ c1, CharLiteral_ c2) ->
+        c1 == c2
+    (App_ h1 c1, App_ h2 c2) ->
+        h1 == h2
+        && and (zipWith (go ctx1 ctx2) c1 c2)
+    (Var_ v1, Var_ v2) ->
+        case elemIndex v1 ctx1 of
+            Just i -> (Just i) == elemIndex v2 ctx2
+            Nothing -> v1 == v2
+    (DV_ s1 dv1, DV_ s2 dv2) ->
+        s1 == s2
+        && compareDV ctx1 ctx2 dv1 dv2
+    _ -> False
+
+compareDV
+    :: forall var level .
+        ( MetaOrObject level
+        , Eq (var level)
+        , Ord (var level)
+        , Eq (var Meta)
+        , Ord (var Meta)
+        , Eq (var Object)
+        , Ord (var Object)
+        )
+    => [var level]
+    -> [var level]
+    -> BuiltinDomain (PureMLPattern level var)
+    -> BuiltinDomain (PureMLPattern level var)
+    -> Bool
+compareDV ctx1 ctx2 dv1 dv2 = case (dv1, dv2) of
+    (BuiltinDomainList l1, BuiltinDomainList l2) ->
+        and $ Seq.zipWith (go ctx1 ctx2) l1 l2
+    (BuiltinDomainSet s1, BuiltinDomainSet s2) ->
+        and $ zipWith
+            (go [] [])
+            (sort $ Set.toList s1)
+            (sort $ Set.toList s2)
+    (BuiltinDomainMap m1, BuiltinDomainMap m2) ->
+        and $ zipWith (go ctx1 ctx2)
+            (map snd $ sort $ Map.toList m1)
+            (map snd $ sort $ Map.toList m2)
+    (BuiltinDomainPattern p1, BuiltinDomainPattern p2) ->
+        go [] [] p1 p2
+    _ -> False
+
--- src/main/haskell/kore/test/Test/Kore/ASTUtils/AlphaCompare.hs
+++ src/main/haskell/kore/test/Test/Kore/ASTUtils/AlphaCompare.hs
@@ -12,11 +12,13 @@ module Test.Kore.ASTUtils.AlphaCompare
     ( test_alphaEq
     ) where
 
-import Kore.AST.Common
-import Kore.AST.MetaOrObject
-import Kore.ASTUtils.AlphaCompare
-import Kore.ASTUtils.SmartConstructors
-import Kore.ASTUtils.SmartPatterns
+import qualified Data.Map as Map
+import qualified Data.Sequence as Seq
+import           Kore.AST.Common
+import           Kore.AST.MetaOrObject
+import           Kore.ASTUtils.AlphaCompare
+import           Kore.ASTUtils.SmartConstructors
+import           Kore.ASTUtils.SmartPatterns
 
 import Test.Kore
 
@@ -28,6 +30,8 @@ test_alphaEq = testGroup ""
     [ alphaComparePositives
     , alphaCompareNegatives
     , alphaEq1
+    , alphaEqMap
+    , alphaEqList
     ]
 
 alphaComparePositives :: TestTree
@@ -52,9 +56,29 @@ alphaCompareNegatives =
 alphaEq1 :: TestTree
 alphaEq1 =
     QC.testProperty
-    "" $
+    "(forall a. a) = (forall b. b)" $
     alphaEq (Forall_ s v1 (Var_ v1)) (Forall_ s v2 (Var_ v2))
-      where
-        s = mkSort "S"
-        v1 = varS "a" s :: Variable Object
-        v2 = varS "b" s :: Variable Object
+
+alphaEqList :: TestTree
+alphaEqList =
+    QC.testProperty
+    "forall a. [a, x] = forall b. [b, x]" $
+    alphaEq
+        (Forall_ s v1 (DV_ s $ BuiltinDomainList $ Seq.fromList [Var_ v1, Var_ v3]))
+        (Forall_ s v2 (DV_ s $ BuiltinDomainList $ Seq.fromList [Var_ v2, Var_ v3]))
+
+
+alphaEqMap :: TestTree
+alphaEqMap =
+    QC.testProperty
+    "(forall a. x |-> a) = (forall b. x |-> b)" $
+    alphaEq
+        (Forall_ s v1 (DV_ s $ BuiltinDomainMap $ Map.fromList [(Top_ s, Var_ v1)]))
+        (Forall_ s v2 (DV_ s $ BuiltinDomainMap $ Map.fromList [(Top_ s, Var_ v2)]))
+
+s :: Sort Object
+s = mkSort "S"
+v1, v2, v3 :: Variable Object
+v1 = varS "a" s
+v2 = varS "b" s
+v3 = varS "c" s
