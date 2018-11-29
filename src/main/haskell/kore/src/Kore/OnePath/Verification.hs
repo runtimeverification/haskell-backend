1df98a8a4bf6b066bd43a236ccbf639f82a49f34
Virgil <25692529+virgil-serbanuta@users.noreply.github.com>
2018-11-29 15:42:45 +0200

    One-path verification (#346)

---
+++ src/main/haskell/kore/src/Kore/OnePath/Verification.hs
@@ -0,0 +1,201 @@
+{-|
+Module      : Kore.OnePath.Verification
+Description : One-path verification
+Copyright   : (c) Runtime Verification, 2018
+License     : NCSA
+Maintainer  : virgil.serbanuta@runtimeverification.com
+
+This should be imported qualified.
+-}
+
+module Kore.OnePath.Verification
+    ( Axiom (..)
+    , Claim (..)
+    , defaultStrategy
+    , verify
+    ) where
+
+import Control.Monad.Trans.Except
+       ( ExceptT, throwE )
+import Data.Default
+       ( def )
+import Numeric.Natural
+       ( Natural )
+
+import qualified Control.Monad.Trans as Monad.Trans
+import           Data.Limit
+                 ( Limit )
+import qualified Data.Limit as Limit
+import           Kore.AST.Common
+                 ( Variable )
+import           Kore.AST.MetaOrObject
+                 ( MetaOrObject )
+import           Kore.ASTUtils.SmartConstructors
+                 ( mkBottom )
+import           Kore.IndexedModule.MetadataTools
+                 ( MetadataTools )
+import           Kore.OnePath.Step
+                 ( Prim, StrategyPattern, onePathFirstStep,
+                 onePathFollowupStep, transitionRule )
+import qualified Kore.OnePath.Step as StrategyPattern
+                 ( StrategyPattern (..) )
+import           Kore.Predicate.Predicate
+                 ( makeTruePredicate )
+import           Kore.Step.AxiomPatterns
+                 ( RewriteRule (RewriteRule), RulePattern (RulePattern) )
+import           Kore.Step.AxiomPatterns as RulePattern
+                 ( RulePattern (..) )
+import           Kore.Step.ExpandedPattern
+                 ( CommonExpandedPattern, Predicated (Predicated) )
+import           Kore.Step.ExpandedPattern as Predicated
+                 ( Predicated (..) )
+import           Kore.Step.Pattern
+                 ( CommonStepPattern )
+import           Kore.Step.Simplification.Data
+                 ( PredicateSubstitutionSimplifier, Simplifier,
+                 StepPatternSimplifier )
+import           Kore.Step.StepperAttributes
+                 ( StepperAttributes )
+import           Kore.Step.Strategy
+                 ( Strategy, pickFinal, runStrategy )
+
+{- | Wrapper for a rewrite rule that should be used as a claim.
+-}
+newtype Claim level = Claim (RewriteRule level)
+
+{- | Wrapper for a rewrite rule that should be used as an axiom.
+-}
+newtype Axiom level = Axiom (RewriteRule level)
+
+{- | Verifies a set of claims. When it verifies a certain claim, after the
+first step, it also uses the claims as axioms (i.e. it does coinductive proofs).
+
+If the verification fails, returns an error containing a pattern that could
+not be rewritten (either because no axiom could be applied or because we
+didn't manage to verify a claim within the its maximum number of steps.
+
+If the verification succeeds, it returns ().
+-}
+verify
+    :: MetaOrObject level
+    => MetadataTools level StepperAttributes
+    -> StepPatternSimplifier level Variable
+    -- ^ Simplifies normal patterns through, e.g., function evaluation
+    -> PredicateSubstitutionSimplifier level Simplifier
+    -- ^ Simplifies predicates
+    -> (CommonStepPattern level -> [Strategy (Prim (RewriteRule level))])
+    -- ^ Creates a one-step strategy from a target pattern. See
+    -- 'defaultStrategy'.
+    -> [(Claim level, Limit Natural)]
+    -- ^ List of claims, together with a maximum number of verification steps
+    -- for each.
+    -> ExceptT
+        (CommonExpandedPattern level)
+        Simplifier
+        ()
+verify
+    metadataTools
+    simplifier
+    substitutionSimplifier
+    strategyBuilder
+  =
+    mapM_
+        (verifyClaim
+            metadataTools
+            simplifier
+            substitutionSimplifier
+            strategyBuilder
+        )
+
+{- | Default implementation for a one-path strategy. You can apply it to the
+first two arguments and pass the resulting function to 'verify'.
+
+Things to note when implementing your own:
+
+1. The first step does not use the reachability claims
+
+2. You can return an infinite list.
+-}
+defaultStrategy
+    :: forall level
+    .   (MetaOrObject level)
+    => [Claim level]
+    -- The claims that we wnt to prove
+    -> [Axiom level]
+    -> CommonStepPattern level
+    -> [Strategy (Prim (RewriteRule level))]
+defaultStrategy
+    claims
+    axioms
+    target
+  =
+    onePathFirstStep destinationRemovalRewrite rewrites
+    : repeat
+        (onePathFollowupStep
+            destinationRemovalRewrite
+            coinductiveRewrites
+            rewrites
+        )
+  where
+    rewrites :: [RewriteRule level]
+    rewrites = map unwrap axioms
+      where
+        unwrap (Axiom a) = a
+    coinductiveRewrites :: [RewriteRule level]
+    coinductiveRewrites = map unwrap claims
+      where
+        unwrap (Claim c) = c
+    destinationRemovalRewrite :: RewriteRule level
+    destinationRemovalRewrite =
+        -- TODO (virgil) : Make the sort for 'right' and 'requires'
+        -- match the sort for 'left'
+        RewriteRule RulePattern
+            { left = target
+            , right = mkBottom
+            , requires = makeTruePredicate
+            , attributes = def
+            }
+
+verifyClaim
+    :: forall level . (MetaOrObject level)
+    => MetadataTools level StepperAttributes
+    -> StepPatternSimplifier level Variable
+    -> PredicateSubstitutionSimplifier level Simplifier
+    -> (CommonStepPattern level -> [Strategy (Prim (RewriteRule level))])
+    -> (Claim level, Limit Natural)
+    -> ExceptT
+        (CommonExpandedPattern level)
+        Simplifier
+        ()
+verifyClaim
+    metadataTools
+    simplifier
+    substitutionSimplifier
+    strategyBuilder
+    (Claim (RewriteRule RulePattern {left, right, requires}), stepLimit)
+  = do
+    let
+        strategy =
+            Limit.takeWithin
+                stepLimit
+                (strategyBuilder right)
+        startPattern :: StrategyPattern (CommonExpandedPattern level)
+        startPattern =
+            StrategyPattern.RewritePattern
+                Predicated
+                    {term = left, predicate = requires, substitution = mempty}
+    executionGraph <- Monad.Trans.lift $ runStrategy
+        (transitionRule metadataTools substitutionSimplifier simplifier)
+        strategy
+        ( startPattern, mempty )
+    let
+        finalNodes = pickFinal executionGraph
+        nonBottomNodes = filter notBottom (map fst finalNodes)
+        notBottom StrategyPattern.Bottom = False
+        notBottom _ = True
+    case nonBottomNodes of
+        [] -> return ()
+        StrategyPattern.RewritePattern p : _ -> throwE p
+        StrategyPattern.Stuck p : _ -> throwE p
+        StrategyPattern.Bottom : _ -> error "Unexpected bottom pattern."
+
---
+++ src/main/haskell/kore/test/Test/Kore/OnePath/Verification.hs
@@ -0,0 +1,349 @@
+module Test.Kore.OnePath.Verification
+    ( test_onePathVerification
+    ) where
+
+import Test.Tasty
+       ( TestTree )
+import Test.Tasty.HUnit
+       ( testCase )
+
+import           Control.Monad.Trans.Except
+                 ( runExceptT )
+import           Data.Default
+                 ( def )
+import qualified Data.Map as Map
+import           Data.Reflection
+                 ( give )
+import           Numeric.Natural
+                 ( Natural )
+
+import           Data.Limit
+                 ( Limit (..) )
+import           Kore.AST.MetaOrObject
+import           Kore.ASTUtils.SmartConstructors
+                 ( mkVar )
+import           Kore.IndexedModule.MetadataTools
+                 ( MetadataTools (..), SymbolOrAliasSorts )
+import qualified Kore.OnePath.Verification as OnePath
+import           Kore.Predicate.Predicate
+                 ( makeEqualsPredicate, makeNotPredicate, makeTruePredicate )
+import           Kore.Step.AxiomPatterns
+                 ( RewriteRule (RewriteRule), RulePattern (RulePattern) )
+import           Kore.Step.AxiomPatterns as RulePattern
+                 ( RulePattern (..) )
+import           Kore.Step.ExpandedPattern
+                 ( Predicated (Predicated) )
+import           Kore.Step.ExpandedPattern as Predicated
+                 ( Predicated (..) )
+import           Kore.Step.ExpandedPattern as ExpandedPattern
+                 ( CommonExpandedPattern, fromPurePattern )
+import           Kore.Step.Pattern
+                 ( CommonStepPattern )
+import           Kore.Step.Simplification.Data
+                 ( evalSimplifier )
+import qualified Kore.Step.Simplification.Simplifier as Simplifier
+import           Kore.Step.StepperAttributes
+import qualified SMT
+
+import           Test.Kore.Comparators ()
+import qualified Test.Kore.IndexedModule.MockMetadataTools as Mock
+import qualified Test.Kore.Step.MockSimplifiers as Mock
+import qualified Test.Kore.Step.MockSymbols as Mock
+import           Test.Tasty.HUnit.Extensions
+
+test_onePathVerification :: [TestTree]
+test_onePathVerification = give symbolOrAliasSorts
+    [ testCase "Runs zero steps" $ do
+        -- Axiom: a => b
+        -- Claim: a => b
+        -- Expected: error a
+        actual <- runVerification
+            metadataTools
+            (Limit 0)
+            [simpleAxiom Mock.a Mock.b]
+            [simpleClaim Mock.a Mock.b]
+        assertEqualWithExplanation ""
+            (Left $ ExpandedPattern.fromPurePattern Mock.a)
+            actual
+    , testCase "Runs one step" $ do
+        -- Axiom: a => b
+        -- Claim: a => b
+        -- Expected: error b
+        actual <- runVerification
+            metadataTools
+            (Limit 1)
+            [simpleAxiom Mock.a Mock.b]
+            [simpleClaim Mock.a Mock.b]
+        assertEqualWithExplanation ""
+            (Left $ ExpandedPattern.fromPurePattern Mock.b)
+            actual
+    , testCase "Verifies one claim" $ do
+        -- Axiom: a => b
+        -- Claim: a => b
+        -- Expected: success
+        actual <- runVerification
+            metadataTools
+            (Limit 2)
+            [simpleAxiom Mock.a Mock.b]
+            [simpleClaim Mock.a Mock.b]
+        assertEqualWithExplanation ""
+            (Right ())
+            actual
+    , testCase "Verifies one claim multiple steps" $ do
+        -- Axiom: a => b
+        -- Axiom: b => c
+        -- Claim: a => c
+        -- Expected: success
+        actual <- runVerification
+            metadataTools
+            (Limit 3)
+            [ simpleAxiom Mock.a Mock.b
+            , simpleAxiom Mock.b Mock.c
+            ]
+            [simpleClaim Mock.a Mock.c]
+        assertEqualWithExplanation ""
+            (Right ())
+            actual
+    , testCase "Verifies one claim stops early" $ do
+        -- Axiom: a => b
+        -- Axiom: b => c
+        -- Claim: a => b
+        -- Expected: success
+        actual <- runVerification
+            metadataTools
+            (Limit 3)
+            [ simpleAxiom Mock.a Mock.b
+            , simpleAxiom Mock.b Mock.c
+            ]
+            [simpleClaim Mock.a Mock.c]
+        assertEqualWithExplanation ""
+            (Right ())
+            actual
+    , testCase "Verifies one claim with branching" $ do
+        -- Axiom: constr11(a) => b
+        -- Axiom: constr11(x) => b
+        -- Axiom: constr10(x) => constr11(x)
+        -- Claim: constr10(x) => b
+        -- Expected: success
+        actual <- runVerification
+            metadataTools
+            (Limit 3)
+            [ simpleAxiom (Mock.functionalConstr11 Mock.a) Mock.b
+            , simpleAxiom (Mock.functionalConstr11 (mkVar Mock.x)) Mock.b
+            , simpleAxiom
+                (Mock.functionalConstr10 (mkVar Mock.x))
+                (Mock.functionalConstr11 (mkVar Mock.x))
+            ]
+            [simpleClaim (Mock.functionalConstr10 (mkVar Mock.x)) Mock.b]
+        assertEqualWithExplanation ""
+            (Right ())
+            actual
+    , testCase "Partial verification failure" $ do
+        -- Axiom: constr11(a) => b
+        -- Axiom: constr10(x) => constr11(x)
+        -- Claim: constr10(x) => b
+        -- Expected: error constr11(x) and x != a
+        actual <- runVerification
+            metadataTools
+            (Limit 3)
+            [ simpleAxiom (Mock.functionalConstr11 Mock.a) Mock.b
+            , simpleAxiom
+                (Mock.functionalConstr10 (mkVar Mock.x))
+                (Mock.functionalConstr11 (mkVar Mock.x))
+            ]
+            [simpleClaim (Mock.functionalConstr10 (mkVar Mock.x)) Mock.b]
+        assertEqualWithExplanation ""
+            (Left Predicated
+                { term = Mock.functionalConstr11 (mkVar Mock.x)
+                , predicate =
+                    makeNotPredicate (makeEqualsPredicate (mkVar Mock.x) Mock.a)
+                , substitution = mempty
+                }
+            )
+            actual
+    , testCase "Verifies two claims" $ do
+        -- Axiom: a => b
+        -- Axiom: b => c
+        -- Axiom: d => e
+        -- Claim: a => c
+        -- Claim: d => e
+        -- Expected: success
+        actual <- runVerification
+            metadataTools
+            (Limit 3)
+            [ simpleAxiom Mock.a Mock.b
+            , simpleAxiom Mock.b Mock.c
+            , simpleAxiom Mock.d Mock.e
+            ]
+            [ simpleClaim Mock.a Mock.c
+            , simpleClaim Mock.d Mock.e
+            ]
+        assertEqualWithExplanation ""
+            (Right ())
+            actual
+    , testCase "fails first of two claims" $ do
+        -- Axiom: a => b
+        -- Axiom: b => c
+        -- Axiom: d => e
+        -- Claim: a => e
+        -- Claim: d => e
+        -- Expected: error c
+        actual <- runVerification
+            metadataTools
+            (Limit 3)
+            [ simpleAxiom Mock.a Mock.b
+            , simpleAxiom Mock.b Mock.c
+            , simpleAxiom Mock.d Mock.e
+            ]
+            [ simpleClaim Mock.a Mock.e
+            , simpleClaim Mock.d Mock.e
+            ]
+        assertEqualWithExplanation ""
+            (Left $ ExpandedPattern.fromPurePattern Mock.c)
+            actual
+    , testCase "fails second of two claims" $ do
+        -- Axiom: a => b
+        -- Axiom: b => c
+        -- Axiom: d => e
+        -- Claim: a => c
+        -- Claim: d => c
+        -- Expected: error e
+        actual <- runVerification
+            metadataTools
+            (Limit 3)
+            [ simpleAxiom Mock.a Mock.b
+            , simpleAxiom Mock.b Mock.c
+            , simpleAxiom Mock.d Mock.e
+            ]
+            [ simpleClaim Mock.a Mock.c
+            , simpleClaim Mock.d Mock.c
+            ]
+        assertEqualWithExplanation ""
+            (Left $ ExpandedPattern.fromPurePattern Mock.e)
+            actual
+    , testCase "second proves first but fails" $ do
+        -- Axiom: a => b
+        -- Axiom: c => d
+        -- Claim: a => d
+        -- Claim: b => c
+        -- Expected: error b
+        actual <- runVerification
+            metadataTools
+            (Limit 4)
+            [ simpleAxiom Mock.a Mock.b
+            , simpleAxiom Mock.c Mock.d
+            ]
+            [ simpleClaim Mock.a Mock.d
+            , simpleClaim Mock.b Mock.c
+            ]
+        assertEqualWithExplanation ""
+            (Left $ ExpandedPattern.fromPurePattern Mock.b)
+            actual
+    , testCase "first proves second but fails" $ do
+        -- Axiom: a => b
+        -- Axiom: c => d
+        -- Claim: b => c
+        -- Claim: a => d
+        -- Expected: error b
+        actual <- runVerification
+            metadataTools
+            (Limit 4)
+            [ simpleAxiom Mock.a Mock.b
+            , simpleAxiom Mock.c Mock.d
+            ]
+            [ simpleClaim Mock.b Mock.c
+            , simpleClaim Mock.a Mock.d
+            ]
+        assertEqualWithExplanation ""
+            (Left $ ExpandedPattern.fromPurePattern Mock.b)
+            actual
+    , testCase "Prefers using claims for rewriting" $ do
+        -- Axiom: a => b
+        -- Axiom: b => c
+        -- Axiom: c => d
+        -- Claim: a => d
+        -- Claim: b => e
+        -- Expected: error e
+        --    first verification: a=>b=>e,
+        --        without second claim would be: a=>b=>c=>d
+        --    second verification: b=>c=>d, not visible here
+        actual <- runVerification
+            metadataTools
+            (Limit 4)
+            [ simpleAxiom Mock.a Mock.b
+            , simpleAxiom Mock.b Mock.c
+            , simpleAxiom Mock.c Mock.d
+            ]
+            [ simpleClaim Mock.a Mock.d
+            , simpleClaim Mock.b Mock.e
+            ]
+        assertEqualWithExplanation ""
+            (Left $ ExpandedPattern.fromPurePattern Mock.e)
+            actual
+    ]
+  where
+    symbolOrAliasSorts :: SymbolOrAliasSorts Object
+    symbolOrAliasSorts =
+        Mock.makeSymbolOrAliasSorts Mock.symbolOrAliasSortsMapping
+    metadataTools :: MetadataTools Object StepperAttributes
+    metadataTools =
+        Mock.makeMetadataTools
+            symbolOrAliasSorts
+            Mock.attributesMapping
+            Mock.headTypeMapping
+            Mock.subsorts
+
+simpleAxiom
+    :: MetaOrObject level
+    => CommonStepPattern level
+    -> CommonStepPattern level
+    -> OnePath.Axiom level
+simpleAxiom left right =
+    OnePath.Axiom $ simpleRewrite left right
+
+simpleClaim
+    :: MetaOrObject level
+    => CommonStepPattern level
+    -> CommonStepPattern level
+    -> OnePath.Claim level
+simpleClaim left right =
+    OnePath.Claim $ simpleRewrite left right
+
+simpleRewrite
+    :: MetaOrObject level
+    => CommonStepPattern level
+    -> CommonStepPattern level
+    -> RewriteRule level
+simpleRewrite left right =
+    RewriteRule RulePattern
+        { left = left
+        , right = right
+        , requires = makeTruePredicate
+        , attributes = def
+        }
+
+runVerification
+    :: MetaOrObject level
+    => MetadataTools level StepperAttributes
+    -- ^functions yielding metadata for pattern heads
+    -> Limit Natural
+    -> [OnePath.Axiom level]
+    -> [OnePath.Claim level]
+    -> IO (Either (CommonExpandedPattern level) ())
+runVerification
+    metadataTools
+    stepLimit
+    axioms
+    claims
+  =
+    SMT.runSMT SMT.defaultConfig
+    $ evalSimplifier
+    $ runExceptT
+    $ OnePath.verify
+        metadataTools
+        simplifier
+        (Mock.substitutionSimplifier metadataTools)
+        (OnePath.defaultStrategy claims axioms)
+        (map (\c -> (c, stepLimit)) claims)
+  where
+    simplifier = Simplifier.create metadataTools Map.empty
