ttuegel/kore

Kore.AST.Common: Write instances without redundant constraints

src/main/haskell/kore/src/Kore/AST/Pure.hs
@@ -113,8 +113,7 @@ instance
 deriving instance
     ( Show ann
     , Show (var lvl)
-    , Show (dom child)
-    , child ~ Cofree (Pattern lvl dom var) ann
+    , Show1 dom
     ) =>
     Show (PurePattern lvl dom var ann)
 

runtimeverification/haskell-backend

src/main/haskell/kore/src/Kore/AST/Pure.hs
@@ -114,7 +114,7 @@ instance
 deriving instance
     ( Show annotation
     , Show (variable level)
-    , Show (domain child)
+    , Show1 domain
     , child ~ Cofree (Pattern level domain variable) annotation
     ) =>
     Show (PurePattern level domain variable annotation)