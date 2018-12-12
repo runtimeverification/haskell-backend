3a3d88e4926bfa116a7d2b11eeef32292c69d54c
Daniele Filaretti <dfilaretti@gmail.com>
2018-12-12 13:09:38 +0000

    implement STRING.chr builtin (#380)

--- src/main/haskell/kore/src/Kore/Builtin/String.hs
+++ src/main/haskell/kore/src/Kore/Builtin/String.hs
@@ -37,6 +37,8 @@ import           Control.Error
                  ( MaybeT )
 import           Control.Monad
                  ( void )
+import           Data.Char
+                 ( chr )
 import qualified Data.HashMap.Strict as HashMap
 import           Data.List
                  ( findIndex, isPrefixOf, tails )
@@ -250,6 +252,11 @@ string2BaseKey = "STRING.string2base"
 string2BaseKeyT :: Text
 string2BaseKeyT = "STRING.string2base"
 
+chrKey :: String
+chrKey = "STRING.chr"
+chrKeyT :: Text
+chrKeyT = "STRING.chr"
+
 evalSubstr :: Builtin.Function
 evalSubstr = Builtin.functionEvaluator evalSubstr0
   where
@@ -380,6 +387,27 @@ evalString2Int = Builtin.functionEvaluator evalString2Int0
                 . fmap fst . listToMaybe . readSigned readDec
                 $ _str
 
+evalChr :: Builtin.Function
+evalChr = Builtin.functionEvaluator evalChr0
+    where
+    evalChr0
+        :: Ord (variable Object)
+        => MetadataTools Object StepperAttributes
+        -> StepPatternSimplifier Object variable
+        -> Sort Object
+        -> [StepPattern Object variable]
+        -> Simplifier (AttemptedFunction Object variable)
+    evalChr0 _ _ resultSort arguments =
+        Builtin.getAttemptedFunction $ do
+            let _n =
+                    case arguments of
+                        [_n] -> _n
+                        _    -> Builtin.wrongArity lengthKey
+            _n <- Int.expectBuiltinInt chrKey _n
+            Builtin.appliedFunction
+                . asExpandedPattern resultSort
+                $ [ chr (fromIntegral _n) ]
+
 {- | Implement builtin function evaluation.
  -}
 builtinFunctions :: Map Text Builtin.Function
@@ -392,6 +420,7 @@ builtinFunctions =
     , (findKeyT, evalFind)
     , (string2BaseKeyT, evalString2Base)
     , (string2IntKeyT, evalString2Int)
+    , (chrKeyT, evalChr)
     ]
   where
     comparator name op =
