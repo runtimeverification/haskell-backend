81e08055136a3efff6fa74f42e3cddb966f907f6
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2018-12-19 17:31:13 +0200

    Added Trusted Attribute

---
+++ src/main/haskell/kore/src/Kore/Attribute/Trusted.hs
@@ -0,0 +1,60 @@
+{-|
+Module      : Kore.Attribute.Trusted
+Description : Trusted claim sentence attribute
+Copyright   : (c) Runtime Verification, 2018
+License     : NCSA
+Maintainer  : thomas.tuegel@runtimeverification.com
+
+-}
+module Kore.Attribute.Trusted
+    ( Trusted (..)
+    , trustedId, trustedSymbol, trustedAttribute
+    ) where
+
+import qualified Control.Monad as Monad
+import           Data.Default
+
+import           Kore.AST.Kore
+import           Kore.Attribute.Parser
+                 ( ParseAttributes (..) )
+import qualified Kore.Attribute.Parser as Parser
+
+{- | @Trusted@ represents the @trusted@ attribute for claim sentences.
+ -}
+newtype Trusted = Trusted { isTrusted :: Bool }
+    deriving (Eq, Ord, Show)
+
+instance Default Trusted where
+    def = Trusted False
+
+-- | Kore identifier representing the @trusted@ attribute symbol.
+trustedId :: Id Object
+trustedId = "trusted"
+
+-- | Kore symbol representing the @trusted@ attribute.
+trustedSymbol :: SymbolOrAlias Object
+trustedSymbol =
+    SymbolOrAlias
+        { symbolOrAliasConstructor = trustedId
+        , symbolOrAliasParams = []
+        }
+
+-- | Kore pattern representing the @trusted@ attribute.
+trustedAttribute :: CommonKorePattern
+trustedAttribute =
+    (asCommonKorePattern . ApplicationPattern)
+        Application
+            { applicationSymbolOrAlias = trustedSymbol
+            , applicationChildren = []
+            }
+
+instance ParseAttributes Trusted where
+    parseAttribute =
+        withApplication $ \params args Trusted { isTrusted } -> do
+            Parser.getZeroParams params
+            Parser.getZeroArguments args
+            Monad.when isTrusted failDuplicate
+            return Trusted { isTrusted = True }
+      where
+        withApplication = Parser.withApplication trustedId
+        failDuplicate = Parser.failDuplicate trustedId
