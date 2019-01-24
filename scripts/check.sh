560640711475bc88d8ba6cc6f5077f84a479a498
Thomas Tuegel <ttuegel@mailbox.org>
2019-01-24 15:59:31 -0600

    Parallel integration tests (#421)
    * include.mk: Make some programs overridable
    
    * Makefile: Run jenkins target in order
    
    * Makefile: Add kore-exec target
    
    * Makefile: haddock is not a phony target
    
    * include.mk: Sub-processes inherit Stack paths from parent
    
    Having every sub-process invoke Stack to compute these paths is slow and has
    race conditions.
    
    * Makefile: Generate haskell_documentation and coverage_report in parallel
    
    * Makefile: Generate test-results.xml for Jenkins
    
    * Makefile: Parallelize test targets
    
    * Makefile: Remove stylish target
    
    The Makefile target stylish is now ./scripts/stylish.sh
    
    * Makefile: Remove target check
    
    The Makefile target check is now scripts/check.sh
    
    * Makefile: Remove target jenkins
    
    The Makefile target jenkins is now scripts/ci.sh

---
+++ scripts/check.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export TOP=${TOP:-$(git rev-parse --show-toplevel)}
+UPSTREAM_BRANCH=${UPSTREAM_BRANCH:-origin/master}
+
+if ! $TOP/scripts/git-assert-clean.sh; \
+then \
+  echo >&2 "Please commit your changes!"; \
+  exit 1; \
+fi
+UPSTREAM_REV=$(git rev-parse $UPSTREAM_BRANCH)
+if ! $TOP/scripts/git-rebased-on.sh $UPSTREAM_REV --linear; \
+then \
+  echo >&2 "Please rebase your branch onto ‘$UPSTREAM_BRANCH’!"; \
+  exit 1; \
+fi
+$TOP/scripts/stylish.sh
+if ! $TOP/scripts/git-assert-clean.sh; \
+then \
+  echo >&2 "Please run ‘scripts/stylish.sh’ to fix style errors!"; \
+  exit 1; \
+fi
