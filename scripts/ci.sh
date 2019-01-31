e96ead65e3eab50349e529dbc4f6b56bd75d9cfe
Everett Hildenbrandt <hildenb2@illinois.edu>
2019-01-31 08:38:33 -0700

    scripts/: split out individual steps for CI server

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,15 +5,6 @@ set -u
 
 export TOP=${TOP:-$(git rev-parse --show-toplevel)}
 
-export PATH="$(stack path --local-bin)${PATH:+:$PATH}"
-
-if ! command -v stylish-haskell >/dev/null; then
-    stack install stylish-haskell
-fi
-
 $TOP/scripts/check.sh
-
-MAKE="make -O -j --directory $TOP"
-$MAKE clean
-$MAKE k-frontend
-$MAKE coverage_report haskell_documentation test-k
+$TOP/scripts/build.sh
+$TOP/scripts/ktest.sh
