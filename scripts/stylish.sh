93c5716f0ac8ac591f7112d6a6f1635396529efb
Everett Hildenbrandt <hildenb2@illinois.edu>
2019-01-31 11:46:21 -0700

    scripts/stylish: make sure local bin is on PATH

--- scripts/stylish.sh
+++ scripts/stylish.sh
@@ -9,6 +9,8 @@ export TOP=${TOP:-$(git rev-parse --show-toplevel)}
 HS_TOP="$TOP/src/main/haskell/kore"
 HS_SOURCE_DIRS="$HS_TOP/src $HS_TOP/app $HS_TOP/test $HS_TOP/bench"
 
+export PATH=$PATH:$HOME/.local/bin
+
 if ! command -v stylish-haskell >/dev/null; then
     stack install stylish-haskell
 fi
