f83569b1de9ef3e630f98eac6a3e48ae9d5f4fbf
Brian Marick <marick@exampler.com>
2019-02-01 14:56:01 -0600

    Test.Terse: Allow to specify the test's intention

--- src/main/haskell/kore/test/Test/Kore/Step/Simplification/Or.hs
+++ src/main/haskell/kore/test/Test/Kore/Step/Simplification/Or.hs
@@ -4,7 +4,6 @@ import Test.Kore
        ( testId )
 import Test.Tasty
 import Test.Tasty.HUnit
-import Test.Tasty.HUnit.Extensions
 import Test.Terse
 
 import qualified Data.List as List
@@ -14,15 +13,13 @@ import qualified Data.Text.Prettyprint.Doc as Pretty
 
 import           Kore.AST.Pure
 import           Kore.AST.Valid
-import qualified Kore.Domain.Builtin as Domain
 import           Kore.Predicate.Predicate
-                 ( CommonPredicate, makeEqualsPredicate, makeFalsePredicate,
-                 makeOrPredicate, makeTruePredicate )
 import           Kore.Step.ExpandedPattern
                  ( ExpandedPattern, Predicated (..), isBottom, isTop )
 import           Kore.Step.OrOfExpandedPattern
                  ( MultiOr (..), OrOfExpandedPattern )
 import qualified Kore.Step.OrOfExpandedPattern as OrOfExpandedPattern
+import           Kore.Step.Pattern
 import           Kore.Step.Simplification.Or
                  ( simplify, simplifyEvaluated )
 import           Kore.Unification.Substitution
@@ -35,21 +32,15 @@ import           Kore.Step.Simplification.Data
 import           Test.Kore.Comparators ()
 import qualified Test.Kore.Step.MockSymbols as Mock
 
-          {- Part 1: `SimplifyEvaluated` is the core function. It converts two
-             `OrOfExpandedPattern` values into a simplifier that is to
-             produce a single `OrOfExpandedPattern`. We run the simplifier to
-             check correctness.
-          -}
+-- * Part 1: 'simplifyEvaluated'
 
--- Key for variable names:
--- 1. `OrOfExpandedPattern` values are represented by a tuple containing
---    the term, predicate, and substitution, in that order. They're
---    also tagged with `t`, `p`, and `s`.
--- 2. The second character has this meaning:
---    T : top
---    _ : bottom
---    m or M : a character neither top nor bottom. Two values
---             named `pm` and `pM` are expected to be unequal.
+{-
+
+`SimplifyEvaluated` is the core function. It converts two `OrOfExpandedPattern`
+values into a simplifier that is to produce a single `OrOfExpandedPattern`. We
+run the simplifier to check correctness.
+
+-}
 
 test_topTermAnnihilates :: TestTree
 test_topTermAnnihilates =
@@ -63,7 +54,7 @@ test_topTermAnnihilates =
             -- simplification. Otherwise, the term should not simplify.
             expectation
               | p1 == p2 && s1 == s2 = simplifiesTo
-              | otherwise            = doesNotSimplify
+              | otherwise            = \initial _ -> doesNotSimplify initial
         -- These cases are handled by OrOfExpandedPattern.filterOr, so they are
         -- not tested here.
         , any not [isTop t1, isTop p1, isTop s1]
@@ -85,7 +76,7 @@ test_disjoinPredicates =
             -- simplification. Otherwise, the predicates should not be merged.
             expectation
               | t1 == t2 && s1 == s2 = simplifiesTo
-              | otherwise            = doesNotSimplify
+              | otherwise            = \initial _ -> doesNotSimplify initial
             p' = makeOrPredicate p1 p2
         ]
   where
@@ -95,41 +86,40 @@ test_disjoinPredicates =
 
 test_anyBottom :: TestTree
 test_anyBottom =
-  testGroup "Any bottom is removed from the result"
-  [ ((tM, pM, sM), (t_, pm, sm)) `simplifiesTo` (tM, pM, sM)
-  , ((tM, pM, sM), (tm, p_, sm)) `simplifiesTo` (tM, pM, sM)
+    testGroup "Any bottom is removed from the result"
+        [ ((tM, pM, sM), (t_, pm, sm)) `simplifiesTo` (tM, pM, sM)
+        , ((tM, pM, sM), (tm, p_, sm)) `simplifiesTo` (tM, pM, sM)
 
-  , ((t_, pm, sm), (tM, pM, sM)) `simplifiesTo` (tM, pM, sM)
-  , ((tm, p_, sm), (tM, pM, sM)) `simplifiesTo` (tM, pM, sM)
+        , ((t_, pm, sm), (tM, pM, sM)) `simplifiesTo` (tM, pM, sM)
+        , ((tm, p_, sm), (tM, pM, sM)) `simplifiesTo` (tM, pM, sM)
 
-  -- Both bottom turns into an empty multiOr
-  , ((t_, pm, sm), (tm, p_, sm)) `becomes` []
+        -- Both bottom turns into an empty multiOr
+        , ((t_, pm, sm), (tm, p_, sm)) `becomes` []
 
-  , testGroup "check this test's expectations"
-    [ orChild (t_, pm, sm) `satisfies_` isBottom
-    , orChild (tm, p_, sm) `satisfies_` isBottom
-      -- Note that it's impossible for the substitution to be bottom.
-    ]
-  ]
+        , testGroup "check this test's expectations"
+            [ orChild (t_, pm, sm) `satisfies_` isBottom
+            , orChild (tm, p_, sm) `satisfies_` isBottom
+            -- Note that it's impossible for the substitution to be bottom.
+            ]
+        ]
 
 test_deduplicateMiddle :: TestTree
 test_deduplicateMiddle =
     testGroup "Middle patterns are deduplicated"
-    [ ((tM, pM, sM), (tM, pM, sM)) `becomes` [orChild (tM, pM, sM)]
-    , ((tm, pm, sm), (tm, pm, sm)) `becomes` [orChild (tm, pm, sm)]
-    ]
+        [ ((tM, pM, sM), (tM, pM, sM)) `simplifiesTo` (tM, pM, sM)
+        , ((tm, pm, sm), (tm, pm, sm)) `simplifiesTo` (tm, pm, sm)
+        ]
 
 
-        {- Part 2: `simplify` is just a trivial use of `simplifyEvaluated` -}
+-- * Part 2: `simplify` is just a trivial use of `simplifyEvaluated`
 
 test_simplify :: TestTree
 test_simplify =
-  testGroup "`simplify` just calls `simplifyEvaluated`"
-  [
-    equals_
-      (simplify $        binaryOr orPattern1 orPattern2 )
-      (simplifyEvaluated          orPattern1 orPattern2 )
-  ]
+    testGroup "`simplify` just calls `simplifyEvaluated`"
+        [ equals_
+            (simplify $        binaryOr orPattern1 orPattern2 )
+            (simplifyEvaluated          orPattern1 orPattern2 )
+        ]
   where
     orPattern1 :: OrOfExpandedPattern Object Variable
     orPattern1 = wrapInOrPattern (tM, pM, sM)
@@ -141,19 +131,31 @@ test_simplify =
       :: OrOfExpandedPattern Object Variable
       -> OrOfExpandedPattern Object Variable
       -> Or Object (OrOfExpandedPattern Object Variable)
-    binaryOr first second =
-        Or { orFirst = first
-           , orSecond = second
-           , orSort = Mock.testSort
-           }
+    binaryOr orFirst orSecond =
+        Or { orSort = Mock.testSort, orFirst, orSecond }
 
 
-        {- Part 3: The values and functions relevant to this test -}
+-- * Part 3: The values and functions relevant to this test
 
+{-
+Key for variable names:
+1. `OrOfExpandedPattern` values are represented by a tuple containing
+   the term, predicate, and substitution, in that order. They're
+   also tagged with `t`, `p`, and `s`.
+2. The second character has this meaning:
+   T : top
+   _ : bottom
+   m or M : a character neither top nor bottom. Two values
+            named `pm` and `pM` are expected to be unequal.
+-}
 
+{- | Short-hand for: @ExpandedPattern Object Variable@
 
-type TestTerm =
-  PurePattern Object Domain.Builtin Variable (Valid (Variable Object) Object)
+See also: 'orChild'
+ -}
+type TestConfig = (TestTerm, TestPredicate, TestSubstitution)
+
+type TestTerm = StepPattern Object Variable
 
 tT :: TestTerm
 tT = mkTop Mock.testSort
@@ -170,26 +172,25 @@ t_ = mkBottom Mock.testSort
 testVar :: Text -> Variable Object
 testVar ident = Variable (testId ident) Mock.testSort
 
-type TestPredicate = CommonPredicate Object
+type TestPredicate = Predicate Object Variable
 
 pT :: TestPredicate
 pT = makeTruePredicate
 
 pm :: TestPredicate
 pm =
-  makeEqualsPredicate
-    (mkVar $ testVar "left")
-    (mkVar $ testVar "right")
+    makeEqualsPredicate
+        (mkVar $ testVar "left")
+        (mkVar $ testVar "right")
 
 pM :: TestPredicate
 pM =
-  makeEqualsPredicate
-    (mkVar $ testVar "LEFT")
-    (mkVar $ testVar "RIGHT")
+    makeEqualsPredicate
+        (mkVar $ testVar "LEFT")
+        (mkVar $ testVar "RIGHT")
 
 p_ :: TestPredicate
 p_ = makeFalsePredicate
-
 
 type TestSubstitution = Substitution Object Variable
 
@@ -202,135 +203,106 @@ sm = Substitution.wrap [(Mock.x, Mock.a)] -- I'd rather these were meaningful
 sM :: TestSubstitution
 sM = Substitution.wrap [(Mock.y, Mock.b)] -- I'd rather these were meaningful
 
-
 test_valueProperties :: TestTree
 test_valueProperties =
-  testGroup "The values have properties that fit their ids"
-  [ tT `has_` [ (isTop, True),   (isBottom, False) ]
-  , tm `has_` [ (isTop, False),  (isBottom, False) ]
-  , tM `has_` [ (isTop, False),  (isBottom, False) ]
-  , t_ `has_` [ (isTop, False),  (isBottom, True) ]
-  , tm `unequals_` tM
+    testGroup "The values have properties that fit their ids"
+        [ tT `has_` [ (isTop, True),   (isBottom, False) ]
+        , tm `has_` [ (isTop, False),  (isBottom, False) ]
+        , tM `has_` [ (isTop, False),  (isBottom, False) ]
+        , t_ `has_` [ (isTop, False),  (isBottom, True) ]
+        , tm `unequals_` tM
 
-  , pT `has_` [ (isTop, True),   (isBottom, False) ]
-  , pm `has_` [ (isTop, False),  (isBottom, False) ]
-  , pM `has_` [ (isTop, False),  (isBottom, False) ]
-  , p_ `has_` [ (isTop, False),  (isBottom, True) ]
-  , pm `unequals_` pM
+        , pT `has_` [ (isTop, True),   (isBottom, False) ]
+        , pm `has_` [ (isTop, False),  (isBottom, False) ]
+        , pM `has_` [ (isTop, False),  (isBottom, False) ]
+        , p_ `has_` [ (isTop, False),  (isBottom, True) ]
+        , pm `unequals_` pM
 
-  , sT `has_` [ (isTop, True),   (isBottom, False) ]
-  , sm `has_` [ (isTop, False),  (isBottom, False) ]
-  , sM `has_` [ (isTop, False),  (isBottom, False) ]
-  , sm `unequals_` sM
-  -- There is no bottom substitution
-  ]
+        , sT `has_` [ (isTop, True),   (isBottom, False) ]
+        , sm `has_` [ (isTop, False),  (isBottom, False) ]
+        , sM `has_` [ (isTop, False),  (isBottom, False) ]
+        , sm `unequals_` sM
+        -- There is no bottom substitution
+        ]
 
 
-                        -- Functions
+-- * Test functions
+
+becomes
+  :: HasCallStack
+  => (TestConfig, TestConfig)
+  -> [ExpandedPattern Object Variable]
+  -> TestTree
+becomes (orChild -> or1, orChild -> or2) (MultiOr . List.sort -> expected) =
+    actual_expected_name_intention
+        (simplifyEvaluated (MultiOr [or1]) (MultiOr [or2]))
+        (expected, SimplificationProof)
+        "or becomes"
+        (stateIntention
+            [ prettyOr or1 or2
+            , "to become:"
+            , Unparser.unparse $ OrOfExpandedPattern.toExpandedPattern expected
+            ]
+        )
+
+simplifiesTo
+    :: HasCallStack
+    => (TestConfig, TestConfig)
+    -> TestConfig
+    -> TestTree
+simplifiesTo (orChild -> or1, orChild -> or2) (orChild -> simplified) =
+    actual_expected_name_intention
+        (simplifyEvaluated (MultiOr [or1]) (MultiOr [or2]))
+        (MultiOr [simplified], SimplificationProof)
+        "or does simplify"
+        (stateIntention
+            [ prettyOr or1 or2
+            , "to simplify to:"
+            , Unparser.unparse simplified
+            ]
+        )
+
+doesNotSimplify
+    :: HasCallStack
+    => (TestConfig, TestConfig)
+    -> TestTree
+doesNotSimplify (orChild -> or1, orChild -> or2) =
+    actual_expected_name_intention
+        (simplifyEvaluated (MultiOr [or1]) (MultiOr [or2]))
+        (MultiOr $ List.sort [or1, or2], SimplificationProof)
+        "or does not simplify"
+        (stateIntention
+            [ prettyOr or1 or2
+            , "does not simplify."
+            ]
+        )
+
+-- * Support Functions
+
+prettyOr
+    :: ExpandedPattern Object Variable
+    -> ExpandedPattern Object Variable
+    -> Pretty.Doc a
+prettyOr orFirst orSecond =
+    Unparser.unparse Or { orSort, orFirst, orSecond }
+  where
+    Valid { patternSort = orSort } = extract term
+      where
+        Predicated { term } = orFirst
+
+stateIntention :: [Pretty.Doc ann] -> String
+stateIntention actualAndSoOn =
+    Unparser.renderDefault $ Pretty.vsep ("expected: " : actualAndSoOn)
 
 orChild
-  :: (TestTerm, TestPredicate, TestSubstitution)
-  -> ExpandedPattern Object Variable
+    :: (TestTerm, TestPredicate, TestSubstitution)
+    -> ExpandedPattern Object Variable
 orChild (term, predicate, substitution) =
-  Predicated
-  { term = term
-  , predicate = predicate
-  , substitution = substitution
-  }
-
+    Predicated { term, predicate, substitution }
 
 -- Note: we intentionally take care *not* to simplify out tops or bottoms
 -- during conversion of a Predicated into an OrOfExpandedPattern
 wrapInOrPattern
-  :: (TestTerm, TestPredicate, TestSubstitution)
-  -> OrOfExpandedPattern Object Variable
-wrapInOrPattern tuple =
-    MultiOr [orChild tuple]
-
-
-becomes
-  :: HasCallStack
-  => ( (TestTerm, TestPredicate, TestSubstitution)
-     , (TestTerm, TestPredicate, TestSubstitution)
-     )
-  -> [ExpandedPattern Object Variable]
-  -> TestTree
-becomes (orChild -> or1, orChild -> or2) (MultiOr . List.sort -> expects) =
-    testCase "\\or becomes" $
-        assertEqualWithExplanation failureComment expected actual
-  where
-    actual = simplifyEvaluated (MultiOr [or1]) (MultiOr [or2])
-    expected = (expects, SimplificationProof)
-    failureComment =
-        Unparser.renderDefault $ Pretty.vsep
-            [ "expected:"
-            , Unparser.unparse Or
-                { orSort
-                , orFirst = or1
-                , orSecond = or2
-                }
-            , "becomes:"
-            , Unparser.unparse $ OrOfExpandedPattern.toExpandedPattern expects
-            ]
-      where
-        Valid { patternSort = orSort } = extract term
-          where
-            Predicated { term } = or1
-
-simplifiesTo
-  :: HasCallStack
-  => ( (TestTerm, TestPredicate, TestSubstitution)
-     , (TestTerm, TestPredicate, TestSubstitution)
-     )
-  -> (TestTerm, TestPredicate, TestSubstitution)
-  -> TestTree
-simplifiesTo (orChild -> or1, orChild -> or2) (orChild -> simplified) =
-    testCase "\\or does simplify" $
-        assertEqualWithExplanation failureComment expected actual
-  where
-    actual = (simplifyEvaluated (MultiOr [or1]) (MultiOr [or2]))
-    expected =  (MultiOr [simplified], SimplificationProof)
-    failureComment =
-        Unparser.renderDefault $ Pretty.vsep
-            [ "expected:"
-            , Unparser.unparse Or
-                { orSort
-                , orFirst = or1
-                , orSecond = or2
-                }
-            , "simplifies to:"
-            , Unparser.unparse simplified
-            ]
-      where
-        Valid { patternSort = orSort } = extract term
-          where
-            Predicated { term } = or1
-
-doesNotSimplify
-    :: HasCallStack
-    =>  ( (TestTerm, TestPredicate, TestSubstitution)
-        , (TestTerm, TestPredicate, TestSubstitution)
-        )
-    -> (TestTerm, TestPredicate, TestSubstitution)
-    -> TestTree
-doesNotSimplify (orChild -> or1, orChild -> or2) _ =
-    testCase "\\or does not simplify" $
-      assertEqualWithExplanation failureComment expected actual
-    where
-      actual = (simplifyEvaluated (MultiOr [or1]) (MultiOr [or2]))
-      expected = (MultiOr $ List.sort [or1, or2], SimplificationProof)
-      failureComment =
-        (Unparser.renderDefault $ Pretty.vsep
-            [ "expected:"
-            , Unparser.unparse Or
-                { orSort
-                , orFirst = or1
-                , orSecond = or2
-                }
-            , "does not simplify."
-            ]
-        )
-        where
-          Valid { patternSort = orSort } = extract term
-            where
-              Predicated { term } = or1
+    :: (TestTerm, TestPredicate, TestSubstitution)
+    -> OrOfExpandedPattern Object Variable
+wrapInOrPattern tuple = MultiOr [orChild tuple]
