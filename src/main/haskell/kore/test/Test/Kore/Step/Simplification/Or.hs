e13670e52198816793654e4a1de310390b2e1fb4
Thomas Tuegel <ttuegel@mailbox.org>
2019-02-01 13:24:33 -0600

    Test disjoining predicates and \top annihilation

--- src/main/haskell/kore/test/Test/Kore/Step/Simplification/Or.hs
+++ src/main/haskell/kore/test/Test/Kore/Step/Simplification/Or.hs
@@ -4,11 +4,13 @@ import Test.Kore
        ( testId )
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.HUnit.Extensions
 import Test.Terse
 
 import qualified Data.List as List
 import           Data.Text
                  ( Text )
+import qualified Data.Text.Prettyprint.Doc as Pretty
 
 import           Kore.AST.Pure
 import           Kore.AST.Valid
@@ -20,11 +22,13 @@ import           Kore.Step.ExpandedPattern
                  ( ExpandedPattern, Predicated (..), isBottom, isTop )
 import           Kore.Step.OrOfExpandedPattern
                  ( MultiOr (..), OrOfExpandedPattern )
+import qualified Kore.Step.OrOfExpandedPattern as OrOfExpandedPattern
 import           Kore.Step.Simplification.Or
                  ( simplify, simplifyEvaluated )
 import           Kore.Unification.Substitution
                  ( Substitution )
 import qualified Kore.Unification.Substitution as Substitution
+import qualified Kore.Unparser as Unparser
 
 import           Kore.Step.Simplification.Data
                  ( SimplificationProof (..) )
@@ -47,38 +51,59 @@ import qualified Test.Kore.Step.MockSymbols as Mock
 --    m or M : a character neither top nor bottom. Two values
 --             named `pm` and `pM` are expected to be unequal.
 
-test_highestTop :: TestTree
-test_highestTop =
-  testGroup "Top dominates only when all its components are top"
-  [ ((tT, pT, sT), (tm, pm, sm)) `simplifiesTo_` (tT, pT, sT)
-  , ((tm, pm, sm), (tT, pT, sT)) `simplifiesTo_` (tT, pT, sT)
-  , ((tT, pT, sT), (tT, pT, sT)) `simplifiesTo_` (tT, pT, sT)
-  ]
-
-test_mergePredicates :: TestTree
-test_mergePredicates =
-    testGroup "Merge predicates when first term is Top and substitutions are equal"
-    [ ((tT, pM, sT), (tm, pm, sT)) `simplifiesTo_` (tT, makeOrPredicate pM pm, sT)
-    -- This fails because the halfSimplifyEvaluated is not commutative:
-    -- , ((tm, pm, sT), (tT, pM, sT)) `simplifiesTo_` (tT, makeOrPredicate pm pM, sT)
-    , ((tT, pM, sm), (tm, pm, sm)) `simplifiesTo_` (tT, makeOrPredicate pM pm, sm)
-    , testGroup "Inequal substitutions prevent merging"
-        [ ((tT, pM, sT), (tm, pm, sm)) `becomes_` MultiOr [orChild (tT, pM, sT), orChild (tm, pm, sm)]
-        , ((tm, pm, sm), (tT, pM, sT)) `becomes_` MultiOr [orChild (tT, pM, sT), orChild (tm, pm, sm)]
+test_topTermAnnihilates :: TestTree
+test_topTermAnnihilates =
+    testGroup "\\top term annihilates \\or when other components are equal"
+        [ expectation ((t1, p1, s1), (t2, p2, s2)) (tT, p1, s1)
+        | (t1, t2) <- [ (tT, tm), (tm, tT) ]  -- test commutativity over term
+        , p1 <- predicates, p2 <- predicates
+        , s1 <- substitutions, s2 <- substitutions
+        , let
+            -- If the predicates and substitutions are equal, expect the given
+            -- simplification. Otherwise, the term should not simplify.
+            expectation
+              | p1 == p2 && s1 == s2 = simplifiesTo
+              | otherwise            = doesNotSimplify
+        -- These cases are handled by OrOfExpandedPattern.filterOr, so they are
+        -- not tested here.
+        , any not [isTop t1, isTop p1, isTop s1]
+        , any not [isTop t2, isTop p2, isTop s2]
         ]
-    ]
+  where
+    predicates = [ pT, pM, pm ]
+    substitutions = [ sT, sM, sm ]
+
+test_disjoinPredicates :: TestTree
+test_disjoinPredicates =
+    testGroup "Disjoin predicates when other components are equal"
+        [ expectation ((t1, p1, s1), (t2, p2, s2)) (t1, p', s1)
+        | t1 <- terms, t2 <- terms
+        , p1 <- predicates, p2 <- predicates
+        , s1 <- substitutions, s2 <- substitutions
+        , let
+            -- If the terms and substitutions are equal, expect the given
+            -- simplification. Otherwise, the predicates should not be merged.
+            expectation
+              | t1 == t2 && s1 == s2 = simplifiesTo
+              | otherwise            = doesNotSimplify
+            p' = makeOrPredicate p1 p2
+        ]
+  where
+    terms = [ tM, tm ]
+    predicates = [ pT, pM, pm ]
+    substitutions = [ sT, sM, sm ]
 
 test_anyBottom :: TestTree
 test_anyBottom =
   testGroup "Any bottom is removed from the result"
-  [ ((tM, pM, sM), (t_, pm, sm)) `simplifiesTo_` (tM, pM, sM)
-  , ((tM, pM, sM), (tm, p_, sm)) `simplifiesTo_` (tM, pM, sM)
+  [ ((tM, pM, sM), (t_, pm, sm)) `simplifiesTo` (tM, pM, sM)
+  , ((tM, pM, sM), (tm, p_, sm)) `simplifiesTo` (tM, pM, sM)
 
-  , ((t_, pm, sm), (tM, pM, sM)) `simplifiesTo_` (tM, pM, sM)
-  , ((tm, p_, sm), (tM, pM, sM)) `simplifiesTo_` (tM, pM, sM)
+  , ((t_, pm, sm), (tM, pM, sM)) `simplifiesTo` (tM, pM, sM)
+  , ((tm, p_, sm), (tM, pM, sM)) `simplifiesTo` (tM, pM, sM)
 
   -- Both bottom turns into an empty multiOr
-  , ((t_, pm, sm), (tm, p_, sm)) `becomes_` (MultiOr [])
+  , ((t_, pm, sm), (tm, p_, sm)) `becomes` []
 
   , testGroup "check this test's expectations"
     [ orChild (t_, pm, sm) `satisfies_` isBottom
@@ -87,19 +112,11 @@ test_anyBottom =
     ]
   ]
 
-test_distinctMiddle :: TestTree
-test_distinctMiddle =
-    testGroup "Distinct middle patterns are not simplified"
-    [ ((tM, pm, sm), (tm, pm, sm)) `becomes_` MultiOr [orChild (tM, pm, sm), orChild (tm, pm, sm)]
-    , ((tm, pM, sm), (tm, pm, sm)) `becomes_` MultiOr [orChild (tm, pM, sm), orChild (tm, pm, sm)]
-    , ((tm, pm, sM), (tm, pm, sm)) `becomes_` MultiOr [orChild (tm, pm, sM), orChild (tm, pm, sm)]
-    ]
-
 test_deduplicateMiddle :: TestTree
 test_deduplicateMiddle =
     testGroup "Middle patterns are deduplicated"
-    [ ((tM, pM, sM), (tM, pM, sM)) `becomes_` MultiOr [orChild (tM, pM, sM)]
-    , ((tm, pm, sm), (tm, pm, sm)) `becomes_` MultiOr [orChild (tm, pm, sm)]
+    [ ((tM, pM, sM), (tM, pM, sM)) `becomes` [orChild (tM, pM, sM)]
+    , ((tm, pm, sm), (tm, pm, sm)) `becomes` [orChild (tm, pm, sm)]
     ]
 
 
@@ -231,28 +248,89 @@ wrapInOrPattern tuple =
     MultiOr [orChild tuple]
 
 
-simplifiesTo_
+becomes
+  :: HasCallStack
+  => ( (TestTerm, TestPredicate, TestSubstitution)
+     , (TestTerm, TestPredicate, TestSubstitution)
+     )
+  -> [ExpandedPattern Object Variable]
+  -> TestTree
+becomes (orChild -> or1, orChild -> or2) (MultiOr . List.sort -> expects) =
+    testCase "\\or becomes" $
+        assertEqualWithExplanation failureComment expected actual
+  where
+    actual = simplifyEvaluated (MultiOr [or1]) (MultiOr [or2])
+    expected = (expects, SimplificationProof)
+    failureComment =
+        Unparser.renderDefault $ Pretty.vsep
+            [ "expected:"
+            , Unparser.unparse Or
+                { orSort
+                , orFirst = or1
+                , orSecond = or2
+                }
+            , "becomes:"
+            , Unparser.unparse $ OrOfExpandedPattern.toExpandedPattern expects
+            ]
+      where
+        Valid { patternSort = orSort } = extract term
+          where
+            Predicated { term } = or1
+
+simplifiesTo
   :: HasCallStack
   => ( (TestTerm, TestPredicate, TestSubstitution)
      , (TestTerm, TestPredicate, TestSubstitution)
      )
   -> (TestTerm, TestPredicate, TestSubstitution)
   -> TestTree
-simplifiesTo_ tuples rawExpected =
-  becomes_ tuples $ wrapInOrPattern rawExpected
+simplifiesTo (orChild -> or1, orChild -> or2) (orChild -> simplified) =
+    testCase "\\or does simplify" $
+        assertEqualWithExplanation failureComment expected actual
+  where
+    actual = (simplifyEvaluated (MultiOr [or1]) (MultiOr [or2]))
+    expected =  (MultiOr [simplified], SimplificationProof)
+    failureComment =
+        Unparser.renderDefault $ Pretty.vsep
+            [ "expected:"
+            , Unparser.unparse Or
+                { orSort
+                , orFirst = or1
+                , orSecond = or2
+                }
+            , "simplifies to:"
+            , Unparser.unparse simplified
+            ]
+      where
+        Valid { patternSort = orSort } = extract term
+          where
+            Predicated { term } = or1
 
-becomes_
-  :: HasCallStack
-  => ( (TestTerm, TestPredicate, TestSubstitution)
-     , (TestTerm, TestPredicate, TestSubstitution)
-     )
-  -> OrOfExpandedPattern Object Variable
-  -> TestTree
-becomes_ (raw1, raw2) (MultiOr expected) =
-  let
-    or1 = wrapInOrPattern raw1
-    or2 = wrapInOrPattern raw2
-  in
-    equals_
-      (simplifyEvaluated or1 or2)
-      (MultiOr $ List.sort expected, SimplificationProof)
+doesNotSimplify
+    :: HasCallStack
+    =>  ( (TestTerm, TestPredicate, TestSubstitution)
+        , (TestTerm, TestPredicate, TestSubstitution)
+        )
+    -> (TestTerm, TestPredicate, TestSubstitution)
+    -> TestTree
+doesNotSimplify (orChild -> or1, orChild -> or2) _ =
+    testCase "\\or does not simplify" $
+      assertEqualWithExplanation failureComment expected actual
+    where
+      actual = (simplifyEvaluated (MultiOr [or1]) (MultiOr [or2]))
+      expected = (MultiOr $ List.sort [or1, or2], SimplificationProof)
+      failureComment =
+        (Unparser.renderDefault $ Pretty.vsep
+            [ "expected:"
+            , Unparser.unparse Or
+                { orSort
+                , orFirst = or1
+                , orSecond = or2
+                }
+            , "does not simplify."
+            ]
+        )
+        where
+          Valid { patternSort = orSort } = extract term
+            where
+              Predicated { term } = or1
