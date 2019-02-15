fdd9c9f01d7a31491f0e0709c0d81ebd3aa91a69
Thomas Tuegel <ttuegel@mailbox.org>
2019-02-15 16:50:11 -0600

    Test externalizeFreshVariables

--- src/main/haskell/kore/test/Test/Kore/Step/Pattern.hs
+++ src/main/haskell/kore/test/Test/Kore/Step/Pattern.hs
@@ -6,6 +6,8 @@ import Test.Tasty.HUnit
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 
+import Data.Sup
+import Kore.AST.Common
 import Kore.AST.Valid
 import Kore.Step.Pattern
 import Kore.Variables.Fresh
@@ -13,6 +15,7 @@ import Kore.Variables.Fresh
 import           Test.Kore.Comparators ()
 import qualified Test.Kore.Step.MockSymbols as Mock
 import           Test.Tasty.HUnit.Extensions
+import           Test.Terse
 
 test_substitute :: [TestTree]
 test_substitute =
@@ -87,3 +90,51 @@ test_substitute =
             , testCase "Forall" (renaming mkForall)
             ]
     ]
+
+test_externalizeFreshVariables :: [TestTree]
+test_externalizeFreshVariables =
+    [ becomes (mkVar x_0) (mkVar x0) "Append counter"
+    , testGroup "No aliasing"
+        [ becomes (mk (mkVar x0) (mkVar x_0)) (mk (mkVar x0) (mkVar x1)) comment
+        | (mk, comment) <- binaryPatterns
+        ]
+    , testGroup "No capturing - Original free"
+        [ becomes (mk x_0 $ mkVar x0) (mk x1 $ mkVar x0) comment
+        | (mk, comment) <- quantifiers
+        ]
+    , testGroup "No capturing - Generated free"
+        [ becomes (mk x0 $ mkVar x_0) (mk x00 $ mkVar x0) comment
+        | (mk, comment) <- quantifiers
+        ]
+    ]
+  where
+    binaryPatterns =
+        [ (mkAnd, "And")
+        , (mkEquals_, "Equals")
+        , (mkIff, "Iff")
+        , (mkImplies, "Implies")
+        , (mkIn_, "In")
+        , (mkOr, "Or")
+        , (mkRewrites, "Rewrites")
+        ]
+    quantifiers =
+        [ (mkExists, "Exists")
+        , (mkForall, "Forall")
+        ]
+    becomes original expected =
+        equals (externalizeFreshVariables original) expected
+
+x :: Variable Object
+x = Mock.x
+
+x_0 :: Variable Object
+x_0 = x { variableCounter = Just (Element 0) }
+
+x0 :: Variable Object
+x0 = x { variableName = "x0" }
+
+x00 :: Variable Object
+x00 = x { variableName = "x00" }
+
+x1 :: Variable Object
+x1 = x { variableName = "x1" }
