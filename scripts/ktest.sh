e54788a7ea686b7b83969b02546c676989dd8664
Everett Hildenbrandt <hildenb2@illinois.edu>
2019-02-27 13:16:38 -0700

    scripts/ktest, src/main/k/working/Makefile: short options are for shells

--- scripts/ktest.sh
+++ scripts/ktest.sh
@@ -5,5 +5,5 @@ set -exuo pipefail
 export TOP=${TOP:-$(git rev-parse --show-toplevel)}
 export PATH="$(stack path --local-bin)${PATH:+:$PATH}"
 
-MAKE="make -O -j --directory $TOP"
+MAKE="make --output-sync --jobs --directory $TOP"
 $MAKE test-k
