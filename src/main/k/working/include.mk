23e18812813f2ac974268e419ee33c513ca6e079
Everett Hildenbrandt <hildenb2@illinois.edu>
2019-02-27 22:15:23 -0700

    working/**/Makefile: parameterize tests in DEFINITION_NAME and factor out

--- src/main/k/working/include.mk
+++ src/main/k/working/include.mk
@@ -3,3 +3,32 @@ ifeq ($(origin TOP), undefined)
 endif
 
 include $(TOP)/include.mk
+
+KOMPILED := $(DEFINITION_NAME)-kompiled
+DEFINITION := $(KOMPILED)/definition.kore
+
+$(DEFINITION) : $(DEFINITION_NAME).k
+	$(KOMPILE) $(KOMPILE_OPTS) $<
+
+%.krun: %.$(DEFINITION_NAME) $(DEFINITION) $(KORE_EXEC)
+	$(KRUN) $(KRUN_OPTS) $<
+
+%.kprove: %.k $(DEFINITION) $(KORE_EXEC)
+	$(KPROVE) $(KPROVE_OPTS) -d . -m VERIFICATION $<
+
+%.search.final.output: %.$(DEFINITION) $(DEFINITION) $(KORE_EXEC)
+	$(KRUN) $(KRUN_OPTS) $< --output-file $@ --search-final
+
+%.krepl: %.k $(DEFINITION) $(KORE_EXEC)
+	$(KPROVE) $(KPROVE_REPL_OPTS) -d . -m VERIFICATION $<
+
+%.output: %.$(DEFINITION_NAME) $(DEFINITION) $(KORE_EXEC)
+	$(KRUN) $(KRUN_OPTS) $< --output-file $@
+
+%.test: %.output
+	diff -u $<.golden $<
+
+%.output.golden: %.output
+	mv $< $<.golden
+
+.PHONY: test-k test golden clean %.test %.krun
