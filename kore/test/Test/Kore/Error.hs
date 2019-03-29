318e0e84d41661e253ab4fa639bf007136632ced
Brian Marick <marick@exampler.com>
2019-03-29 14:14:18 -0500

    First working version of catching error

--- kore/test/Test/Kore/Error.hs
+++ kore/test/Test/Kore/Error.hs
@@ -5,11 +5,12 @@ License     : NCSA
 module Test.Kore.Error where
 
 import Test.Tasty
--- import Test.Tasty.HUnit
--- import Test.Terse
+import Test.Tasty.HUnit
+import Test.Tasty.HUnit.Extensions
+import Test.Terse
 
+import Control.Exception
 import Data.Void
-       ( Void )
 import Kore.Error
 
 type DontCare = Void
@@ -18,7 +19,20 @@ type Wrapper = Either (Error DontCare) String
 test_assertRight :: TestTree
 test_assertRight =
     testGroup "assertRight"
-        [ -- assertRight (Right "wrapped" :: Wrapper) `equals_` "wrapped"
+        [ assertRight (mkRight "expected") `equals_` "expected"
+    --    , assertRight (mkLeft someError) `throws_` (printerr someError)
+        , testCase "case" $ handler (assertRight (mkLeft $ koreError "foo"))
         ]
+    where
+        someError = koreError "the error message"
+        mkRight x = Right x :: Wrapper
+        mkLeft x = Left x :: Wrapper
+        mkError msg =
+            Error { errorContext = ["test context"], errorError = msg }
 
--- The above test depends on Tom's PR test for tomorrow.
+        handler :: a -> Assertion
+        handler x =
+            do
+              catch (evaluate x >> assertFailure "didn't throw") $ \ (ErrorCall msg) ->
+                assertEqualWithExplanation "ksdj" msg (printError (mkError "foo"))
+              return ()
