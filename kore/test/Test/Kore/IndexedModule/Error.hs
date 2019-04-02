1d1705145ebcfd47f85eb95a422b333a94038c4c
Brian Marick <marick@exampler.com>
2019-04-02 14:39:43 -0500

    More work on tests and test support functions. (#541)
    * Move error functions from `Resolvers` to better places
    
    * First test for `Test.Kore.Error` (deferred)
    
    * Placeholder for `Kore.IndexedModule.Error` tests
    
    * Demonstrate use of creational function in test
    
    * Stylish
    
    * First working version of catching error
    
    * cleanup
    
    * Pulled out a `throws`
    
    * Move `throws_` into `Test.Terse`
    
    * Use `run` instead of `try`
    
    * Add `throws` variants to Test.Terse
    
    * Could only write two `IndexedModule.Error` tests
    
    * Typos discovered in review

--- kore/test/Test/Kore/IndexedModule/Error.hs
+++ kore/test/Test/Kore/IndexedModule/Error.hs
@@ -4,18 +4,23 @@ License     : NCSA
 -}
 module Test.Kore.IndexedModule.Error where
 
--- import Test.Kore
---        ( testId )
+-- import Test.Kore.Builtin.Definition
+--        ( builtinSymbol )
+import Test.Kore
+       ( testId )
 import Test.Tasty
--- import Test.Tasty.HUnit
--- import Test.Terse
+import Test.Terse
 
--- import Kore.IndexedModule.Error
+import Kore.IndexedModule.Error
 
 test_undefineds :: TestTree
 test_undefineds =
     testGroup "error strings for undefined objects"
-        [ -- noSort (testId "a") `equals_` "Sort '#a' not defined"
-        ]
+        [ noAlias (testId "#a")  `equals_` "Alias '#a' not defined."
+        , noSymbol (testId "#b") `equals_` "Symbol '#b' not defined."
 
--- Awaiting Tom's fix to string explanations
+        -- The following two lines print full structure dumps.
+        -- They should perhaps print something better?
+        -- , noHead (builtinSymbol "#c") `equals_` "Head '#c' not defined."
+        -- s, noSort (testId "#d") `equals_` "Sort '#d' not defined."
+        ]
