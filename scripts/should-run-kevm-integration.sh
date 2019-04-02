5e4165f6b5dd6607eb9164e73cdd7cfe66418406
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-04-02 14:23:38 -0600

    scripts/should-run-kevm-integration: make sure to fetch base branch

--- scripts/should-run-kevm-integration.sh
+++ scripts/should-run-kevm-integration.sh
@@ -6,6 +6,8 @@ TOP=${TOP:-$(git rev-parse --show-toplevel)}
 UPSTREAM_REMOTE=${UPSTREAM_REMOTE:-origin}
 UPSTREAM_BRANCH=${UPSTREAM_BRANCH:-master}
 
+git fetch $(git remote get-url $UPSTREAM_REMOTE) $UPSTREAM_BRANCH
+
 TRIGGER_STRING="$1" ; shift
 
 if git --no-pager log --oneline "$UPSTREAM_REMOTE/$UPSTREAM_BRANCH..HEAD" | grep "$TRIGGER_STRING" &>/dev/null; then
