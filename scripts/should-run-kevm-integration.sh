ae5122d21597bff0796078ca8012f9ef416da8fc
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-04-02 14:50:43 -0600

    scripts/should-run-kevm-integration: use FETCH_HEAD directly instead of variables

--- scripts/should-run-kevm-integration.sh
+++ scripts/should-run-kevm-integration.sh
@@ -6,10 +6,10 @@ TOP=${TOP:-$(git rev-parse --show-toplevel)}
 UPSTREAM_REMOTE=${UPSTREAM_REMOTE:-origin}
 UPSTREAM_BRANCH=${UPSTREAM_BRANCH:-master}
 
-git fetch $(git remote get-url $UPSTREAM_REMOTE) $UPSTREAM_BRANCH
-
 TRIGGER_STRING="$1" ; shift
 
-if git --no-pager log --oneline "$UPSTREAM_REMOTE/$UPSTREAM_BRANCH..HEAD" | grep "$TRIGGER_STRING" &>/dev/null; then
+git fetch $(git remote get-url $UPSTREAM_REMOTE) $UPSTREAM_BRANCH
+
+if git --no-pager log --oneline 'FETCH_HEAD..HEAD' | grep "$TRIGGER_STRING" &>/dev/null; then
     echo 'true'
 fi
