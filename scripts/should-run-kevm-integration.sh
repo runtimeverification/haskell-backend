68867c76700b0561dba24577fd6b4e0a62dabfb0
ehildenb <everett.hildenbrandt@gmail.com>
2019-04-03 09:00:10 -0500

    Update K Nightly version used for integration testing (#549)
    * include.mk: update K nightly version
    
    * README: remove CodeShip badge
    
    * scripts/kevm-integration: setup opam by default, skip if user sets OPAM_SETUP_SKIP
    
    * scripts/should-run-kevm-integration: script to tell whether we should run the integration tests
    
    * Jenkinsfile: script which explicitely decides in KEVM integration tests should run
    
    * scripts/should-run-kevm-integration: make sure to fetch base branch
    
    * scripts/should-run-kevm-integration: use FETCH_HEAD directly instead of variables
    
    * Jenkinsfile: make sure its kevm-integration wrapped in brackets

---
+++ scripts/should-run-kevm-integration.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+TOP=${TOP:-$(git rev-parse --show-toplevel)}
+UPSTREAM_REMOTE=${UPSTREAM_REMOTE:-origin}
+UPSTREAM_BRANCH=${UPSTREAM_BRANCH:-master}
+
+TRIGGER_STRING="$1" ; shift
+
+git fetch $(git remote get-url $UPSTREAM_REMOTE) $UPSTREAM_BRANCH
+
+if git --no-pager log --oneline 'FETCH_HEAD..HEAD' | grep "$TRIGGER_STRING" &>/dev/null; then
+    echo 'true'
+fi
