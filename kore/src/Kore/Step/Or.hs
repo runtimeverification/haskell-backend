9389cd064d20004f9c9dddad0f3e13327c3f1517
Thomas Tuegel <ttuegel@mailbox.org>
2019-04-26 15:15:17 -0500

    (WIP) Kore.Step.Or

--- kore/src/Kore/Step/Or.hs
+++ kore/src/Kore/Step/Or.hs
@@ -1,19 +1,11 @@
-{-|
-Module      : Kore.Step.Representation.OrOfExpandedPattern
-Description : Data structures and functions for manipulating
-              OrOfExpandedPatterns, which occur naturally during
-              pattern simplification.
+{- |
 Copyright   : (c) Runtime Verification, 2018
 License     : NCSA
-Maintainer  : virgil.serbanuta@runtimeverification.com
-Stability   : experimental
-Portability : portable
+
 -}
-module Kore.Step.Representation.OrOfExpandedPattern
-    ( CommonOrOfExpandedPattern
-    , CommonOrOfPredicateSubstitution
-    , OrOfExpandedPattern
-    , OrOfPredicateSubstitution
+module Kore.Step.Or
+    ( Pattern
+    , PredicateSubstitution
     , isFalse
     , isTrue
     , makeFromSinglePurePattern
@@ -28,10 +20,8 @@ import Data.List
 import           Kore.AST.Valid
 import           Kore.Predicate.Predicate
                  ( Predicate, makeMultipleOrPredicate, makeTruePredicate )
-import           Kore.Step.Pattern
-                 ( Conditional (..), Object, Pattern, SortedVariable,
-                 Variable )
-import qualified Kore.Step.Pattern as ExpandedPattern
+import qualified Kore.Step.Conditional as Conditional
+import qualified Kore.Step.Pattern as Pattern
 import           Kore.Step.Representation.MultiOr
                  ( MultiOr )
 import qualified Kore.Step.Representation.MultiOr as MultiOr
@@ -41,46 +31,32 @@ import           Kore.TopBottom
                  ( TopBottom (..) )
 import           Kore.Unparser
 
-type OrOfConditional level variable term =
-    MultiOr (Conditional level variable term)
+type Conditional level variable term =
+    MultiOr (Conditional.Conditional level variable term)
 
-{-| 'OrOfExpandedPattern' is a 'MultiOr' of 'ExpandedPatterns', which is the
+{-| 'Pattern' is a 'MultiOr' of 'Patterns', which is the
 most common case.
 -}
-type OrOfExpandedPattern level variable
-    = OrOfConditional level variable (TermLike variable)
+type Pattern level variable = MultiOr (Pattern.Pattern level variable)
 
-{-| 'OrOfPredicateSubstitution' is a 'MultiOr' of 'PredicateSubstitution'.
+{-| 'PredicateSubstitution' is a 'MultiOr' of 'PredicateSubstitution.PredicateSubstitution'.
 -}
-type OrOfPredicateSubstitution level variable
-    = OrOfConditional level variable ()
+type PredicateSubstitution level variable =
+    MultiOr (PredicateSubstitution.PredicateSubstitution level variable)
 
 {-| 'OrOfPredicate' is a 'MultiOr' of 'Predicate'.
 -}
-type OrOfPredicate variable =
-    MultiOr (Predicate variable)
+type Predicate variable = MultiOr (Predicate variable)
 
-{-| 'CommonOrOfExpandedPattern' particularizes 'OrOfExpandedPattern' to
-'Variable', following the same convention as the other Common* types.
--}
-type CommonOrOfExpandedPattern level = OrOfExpandedPattern level Variable
-
-{-| 'CommonOrOfOrOfPredicateSubstitution' particularizes
-'OrOfPredicateSubstitution' to 'Variable', following the same convention
-as the other Common* types.
--}
-type CommonOrOfPredicateSubstitution level =
-    OrOfPredicateSubstitution level Variable
-
-{-| Constructs a normalized 'OrOfExpandedPattern' from
-'TermLikes'.
+{-| Constructs a normalized 'Pattern' from
+'TermLike's.
 -}
 makeFromSinglePurePattern
     :: Ord (variable Object)
     => TermLike variable
-    -> OrOfExpandedPattern Object variable
+    -> Pattern Object variable
 makeFromSinglePurePattern patt =
-    MultiOr.make [ ExpandedPattern.fromPurePattern patt ]
+    MultiOr.make [ Pattern.fromPurePattern patt ]
 
 {-| 'isFalse' checks if the 'Or' is composed only of bottom items.
 -}
