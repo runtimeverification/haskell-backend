4bf21cecb4a045749bcdee154213f2669224d772
Xiaohong Chen <xc.math@gmail.com>
2019-04-29 11:27:02 -0500

    Remove unparseLowerID

--- kore/src/Kore/AST/Common.hs
+++ kore/src/Kore/AST/Common.hs
@@ -157,10 +157,10 @@ instance Unparse (Variable level) where
         <> Pretty.colon
         <> unparse variableSort
     unparse2 Variable { variableName, variableCounter } =
-        unparseIdLower variableName
+        unparse2 variableName
         <> Pretty.pretty variableCounter
     unparse2BindingVariables Variable { variableName, variableCounter, variableSort } =
-        unparseIdLower variableName
+        unparse2 variableName
         <> Pretty.pretty variableCounter
         <> Pretty.colon
         <> unparse2 variableSort
