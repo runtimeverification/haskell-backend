e7d0926161901085525f6dada0f447e7e8b434bc
Thomas Tuegel <ttuegel@mailbox.org>
2019-05-01 10:55:30 -0500

    Kore.Syntax.Sentence

---
+++ kore/src/Kore/Syntax/Sentence.hs
@@ -0,0 +1,85 @@
+{-|
+Copyright   : (c) Runtime Verification, 2019
+License     : NCSA
+
+-}
+
+module Kore.Syntax.Sentence
+    ( Symbol (..)
+    , groundSymbol
+    , Alias (..)
+    ) where
+
+import Control.DeepSeq
+       ( NFData (..) )
+import Data.Hashable
+       ( Hashable (..) )
+import GHC.Generics
+       ( Generic )
+
+import Kore.AST.Pure as Pure
+import Kore.Unparser
+
+{-|'Symbol' corresponds to the
+@object-head-constructor{object-sort-variable-list}@ part of the
+@object-symbol-declaration@ and @meta-symbol-declaration@ syntactic categories
+from the Semantics of K, Section 9.1.6 (Declaration and Definitions).
+
+The 'level' type parameter is used to distiguish between the meta- and object-
+versions of symbol declarations. It should verify 'MetaOrObject level'.
+
+Note that this is very similar to 'SymbolOrAlias'.
+-}
+data Symbol level = Symbol
+    { symbolConstructor :: !Id
+    , symbolParams      :: ![SortVariable]
+    }
+    deriving (Show, Eq, Ord, Generic)
+
+instance Hashable (Symbol level)
+
+instance NFData (Symbol level)
+
+instance Unparse (Symbol level) where
+    unparse Symbol { symbolConstructor, symbolParams } =
+        unparse symbolConstructor
+        <> parameters symbolParams
+
+    unparse2 Symbol { symbolConstructor } =
+        unparse2 symbolConstructor
+
+
+-- |Given an 'Id', 'groundSymbol' produces the unparameterized 'Symbol'
+-- corresponding to that argument.
+groundSymbol :: Id -> Symbol Object
+groundSymbol ctor = Symbol
+    { symbolConstructor = ctor
+    , symbolParams = []
+    }
+
+{-|'Alias' corresponds to the
+@object-head-constructor{object-sort-variable-list}@ part of the
+@object-alias-declaration@ and @meta-alias-declaration@ syntactic categories
+from the Semantics of K, Section 9.1.6 (Declaration and Definitions).
+
+The 'level' type parameter is used to distiguish between the meta- and object-
+versions of symbol declarations. It should verify 'MetaOrObject level'.
+
+Note that this is very similar to 'SymbolOrAlias'.
+-}
+data Alias level = Alias
+    { aliasConstructor :: !Id
+    , aliasParams      :: ![SortVariable]
+    }
+    deriving (Show, Eq, Ord, Generic)
+
+instance Hashable (Alias level)
+
+instance NFData (Alias level)
+
+instance Unparse (Alias level) where
+    unparse Alias { aliasConstructor, aliasParams } =
+        unparse aliasConstructor
+        <> parameters aliasParams
+    unparse2 Alias { aliasConstructor } =
+        unparse2 aliasConstructor
