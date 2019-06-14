c0f25ac18eeb45e51260792a02176d2f4634d70a
Thomas Tuegel <ttuegel@mailbox.org>
2019-06-14 09:54:22 -0500

    scripts/stylish.sh: Set PATH from Stack
    This uses the PATH which would be set by `stack exec`, which is more robust than
    trying to guess the location where `stylish-haskell` in installed.

--- scripts/stylish.sh
+++ scripts/stylish.sh
@@ -6,8 +6,7 @@ export TOP=${TOP:-$(git rev-parse --show-toplevel)}
 
 source $TOP/scripts/run-on-haskell.include.sh
 
-# Install stylish-haskell in the local .stack-work
 stack build stylish-haskell
-stylish="$(stack path --local-install-root)/bin/stylish-haskell"
+export PATH=$(stack path --bin-path)
 
-runOnHaskellFiles "$TOP" "$stylish" -i
+runOnHaskellFiles "$TOP" stylish-haskell -i
