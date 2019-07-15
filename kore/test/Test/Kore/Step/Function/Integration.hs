81130a3973f3e483c5970f2e92322b4743b01963
Thomas Tuegel <ttuegel@mailbox.org>
2019-07-15 15:52:59 -0500

    (RED) Test.Kore.Step.Function.Integration

--- kore/test/Test/Kore/Step/Function/Integration.hs
+++ kore/test/Test/Kore/Step/Function/Integration.hs
@@ -1,15 +1,22 @@
-module Test.Kore.Step.Function.Integration (test_functionIntegration) where
+module Test.Kore.Step.Function.Integration
+    ( test_functionIntegration
+    , test_Nat
+    ) where
 
 import Test.Tasty
-       ( TestTree )
 import Test.Tasty.HUnit
-       ( testCase )
 
+import           Data.Function
 import qualified Data.Map as Map
+import           Data.Maybe
+import qualified Data.Text.Prettyprint.Doc as Pretty
+import           Prelude hiding
+                 ( succ )
 
 import           Data.Sup
 import qualified Kore.Internal.OrPattern as OrPattern
 import           Kore.Internal.Pattern as Pattern
+import           Kore.Internal.Symbol
 import           Kore.Internal.TermLike
 import           Kore.Predicate.Predicate
                  ( makeAndPredicate, makeCeilPredicate, makeEqualsPredicate,
@@ -19,6 +26,8 @@ import qualified Kore.Predicate.Predicate as Syntax
 import           Kore.Step.Axiom.EvaluationStrategy
                  ( builtinEvaluation, definitionEvaluation,
                  firstFullEvaluation, simplifierWithFallback )
+import           Kore.Step.Axiom.Identifier
+                 ( AxiomIdentifier )
 import qualified Kore.Step.Axiom.Identifier as AxiomIdentifier
                  ( AxiomIdentifier (..) )
 import           Kore.Step.Axiom.UserDefined
@@ -36,11 +45,16 @@ import           Kore.Step.Simplification.Data
 import qualified Kore.Step.Simplification.TermLike as TermLike
                  ( simplify )
 import qualified Kore.Unification.Substitution as Substitution
+import           Kore.Unparser
 import           Kore.Variables.Fresh
 import qualified SMT
 
 import           Test.Kore
 import           Test.Kore.Comparators ()
+import qualified Test.Kore.Step.Axiom.EvaluationStrategy as Axiom
+                 ( evaluate )
+import           Test.Kore.Step.Axiom.Matcher
+                 ( match )
 import qualified Test.Kore.Step.MockSymbols as Mock
 import           Test.Tasty.HUnit.Extensions
 
@@ -482,13 +496,9 @@ test_functionIntegration =
 
     , testCase "Multiple definition branches." $ do
         let expect =
-                Conditional
-                    { term = mkOr
-                        (mkAnd Mock.a (mkCeil Mock.testSort Mock.cf))
-                        (mkAnd Mock.b (mkNot (mkCeil Mock.testSort Mock.cf)))
-                    , predicate = makeTruePredicate
-                    , substitution = mempty
-                    }
+                Pattern.fromTermLike $ mkOr
+                    (mkAnd Mock.a (mkCeil Mock.testSort Mock.cf))
+                    (mkAnd Mock.b (mkNot (mkCeil Mock.testSort Mock.cf)))
         actual <-
             evaluate
                 (Map.fromList
@@ -503,10 +513,7 @@ test_functionIntegration =
                                     (Mock.f (mkVar Mock.y))
                                     Mock.a
                                     (makeCeilPredicate Mock.cf)
-                                , axiom
-                                    (Mock.f (mkVar Mock.y))
-                                    Mock.b
-                                    makeTruePredicate
+                                , axiom_ (Mock.f (mkVar Mock.y)) Mock.b
                                 ]
                             )
                         )
@@ -515,6 +522,155 @@ test_functionIntegration =
                 (Mock.f (mkVar Mock.x))
         assertEqualWithExplanation "" expect actual
     ]
+
+test_Nat :: [TestTree]
+test_Nat =
+    [ plus zero varN `matches` plus zero one  $ "plus(0, N) ~ plus(0, 1)"
+    , plus (succ varM) varN `notMatches` plus zero one  $ "plus(Succ(M), N) !~ plus(0, 1) "
+    , plus (succ varM) varN `matches` plus one one  $ "plus(Succ(M), N) ~ plus(1, 1) "
+    , [plusZeroRule] `applies` plus zero one  $ "plus(0, N) => ... ~ plus (0, 1)"
+    , [plusZeroRule] `notApplies` plus one one  $ "plus(0, N) => ... ~ plus (1, 1)"
+    , [plusSuccRule] `notApplies` plus zero one  $ "plus(Succ(M), N) => ... ~ plus (0, 1)"
+    , [plusSuccRule] `applies` plus one one  $ "plus(Succ(M), N) => ... ~ plus (1, 1)"
+    , [plusZeroRule, plusSuccRule] `applies` plus zero one  $ "plus(0, 1) => ..."
+    , [plusZeroRule, plusSuccRule] `applies` plus one one  $ "plus(1, 1) => ..."
+    , plus zero one `equals` one  $ "0 + 1 = 1 : Nat"
+    , plus one one `equals` two  $ "0 + 1 = 1 : Nat"
+    , times zero one `equals` zero  $ "0 * 1 = 0 : Nat"
+    , times one one `equals` one  $ "1 * 1 = 1 : Nat"
+    , times one two `equals` two  $ "1 * 2 = 2 : Nat"
+    , times two one `equals` two  $ "2 * 1 = 2 : Nat"
+    , factorial zero `equals` one $ "0! = 1 : Nat"
+    , factorial one `equals` one $ "1! = 1 : Nat"
+    , factorial two `equals` two $ "2! = 2 : Nat"
+    , fibonacci zero `equals` one $ "fibonacci(0) = 1 : Nat"
+    , fibonacci one `equals` one $ "fibonacci(1) = 1 : Nat"
+    , fibonacci two `equals` two $ "fibonacci(2) = 2 : Nat"
+    ]
+  where
+    -- Matching tests: check that the terms match or not
+    withMatch check term1 term2 comment =
+        testCase comment $ do
+            actual <- match term1 term2
+            let message =
+                    Pretty.vsep
+                        [ "matching:"
+                        , Pretty.indent 4 (unparse term1)
+                        , Pretty.indent 2 "with:"
+                        , Pretty.indent 4 (unparse term2)
+                        ]
+            assertBool (show message) (check actual)
+    matches = withMatch (maybe False (not . isBottom))
+    notMatches = withMatch (maybe False isBottom)
+
+    -- Applied tests: check that one or more rules applies or not
+    withApplied check rules term comment =
+        testCase comment $ do
+            actual <- Axiom.evaluate (definitionEvaluation rules) term
+            assertBool "" (check actual)
+    applies = withApplied isApplicable
+    notApplies = withApplied isNotApplicable
+
+    -- Evaluation tests: check the result of evaluating the term
+    equals term expect comment =
+        testCase comment $ do
+            actual <- evaluate natSimplifiers term
+            assertEqualWithExplanation "" (Pattern.fromTermLike expect) actual
+
+natSort :: Sort
+natSort =
+    SortActualSort SortActual
+        { sortActualName = testId "Nat"
+        , sortActualSorts = []
+        }
+
+natM, natN :: Variable
+natM = varS "M" natSort
+natN = varS "N" natSort
+
+varM, varN :: TermLike Variable
+varM = mkVar natM
+varN = mkVar natN
+
+zeroSymbol, succSymbol :: Symbol
+zeroSymbol = Mock.symbol "Zero" [] natSort & constructor & functional
+succSymbol = Mock.symbol "Succ" [natSort] natSort & constructor & functional
+
+plusSymbol, timesSymbol :: Symbol
+plusSymbol = Mock.symbol "plus" [natSort, natSort] natSort & function
+timesSymbol = Mock.symbol "times" [natSort, natSort] natSort & function
+
+fibonacciSymbol, factorialSymbol :: Symbol
+fibonacciSymbol = Mock.symbol "fibonacci" [natSort] natSort & function
+factorialSymbol = Mock.symbol "factorial" [natSort] natSort & function
+
+zero :: TermLike Variable
+zero = mkApplySymbol zeroSymbol []
+
+one, two :: TermLike Variable
+one = succ zero
+two = succ one
+
+succ, fibonacci, factorial :: TermLike Variable -> TermLike Variable
+succ n = mkApplySymbol succSymbol [n]
+fibonacci n = mkApplySymbol fibonacciSymbol [n]
+factorial n = mkApplySymbol factorialSymbol [n]
+
+plus, times
+    :: TermLike Variable
+    -> TermLike Variable
+    -> TermLike Variable
+plus n1 n2 = mkApplySymbol plusSymbol [n1, n2]
+times n1 n2 = mkApplySymbol timesSymbol [n1, n2]
+
+functionEvaluator
+    :: Symbol
+    -> [EqualityRule Variable]  -- ^ Function definition rules
+    -> (AxiomIdentifier, BuiltinAndAxiomSimplifier)
+functionEvaluator symb rules =
+    (AxiomIdentifier.Application ident, definitionEvaluation rules)
+  where
+    ident = symbolConstructor symb
+
+plusZeroRule, plusSuccRule :: EqualityRule Variable
+plusZeroRule = axiom_ (plus zero varN) varN
+plusSuccRule = axiom_ (plus (succ varM) varN) (succ (plus varM varN))
+
+plusEvaluator :: (AxiomIdentifier, BuiltinAndAxiomSimplifier)
+plusEvaluator = functionEvaluator plusSymbol [plusZeroRule, plusSuccRule]
+
+timesEvaluator :: (AxiomIdentifier, BuiltinAndAxiomSimplifier)
+timesEvaluator =
+    functionEvaluator timesSymbol
+        [ axiom_ (times zero varN) zero
+        , axiom_ (times (succ varM) varN) (plus varN (times varM varN))
+        ]
+
+fibonacciEvaluator :: (AxiomIdentifier, BuiltinAndAxiomSimplifier)
+fibonacciEvaluator =
+    functionEvaluator fibonacciSymbol
+        [ axiom_ (fibonacci zero) one
+        , axiom_ (fibonacci one)  one
+        , axiom_
+            (fibonacci (succ (succ varN)))
+            (plus (fibonacci (succ varN)) (fibonacci varN))
+        ]
+
+factorialEvaluator :: (AxiomIdentifier, BuiltinAndAxiomSimplifier)
+factorialEvaluator =
+    functionEvaluator factorialSymbol
+        [ axiom_ (factorial zero)        (succ zero)
+        , axiom_ (factorial (succ varN)) (times (succ varN) (factorial varN))
+        ]
+
+natSimplifiers :: BuiltinAndAxiomSimplifierMap
+natSimplifiers =
+    Map.fromList
+        [ plusEvaluator
+        , timesEvaluator
+        , fibonacciEvaluator
+        , factorialEvaluator
+        ]
 
 axiomEvaluator
     :: TermLike Variable
@@ -531,6 +687,12 @@ axiom
     -> EqualityRule Variable
 axiom left right predicate =
     EqualityRule (RulePattern.rulePattern left right) { requires = predicate }
+
+axiom_
+    :: TermLike Variable
+    -> TermLike Variable
+    -> EqualityRule Variable
+axiom_ left right = axiom left right makeTruePredicate
 
 appliedMockEvaluator
     :: Pattern Variable -> BuiltinAndAxiomSimplifier
