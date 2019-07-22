dcfe9fc4f35a2a6088e818f1683cb9b755859abf
Thomas Tuegel <ttuegel@mailbox.org>
2019-07-22 12:48:07 -0500

    Test.Kore.Builtin.test_internalize

--- kore/test/Test/Kore/Builtin.hs
+++ kore/test/Test/Kore/Builtin.hs
@@ -4,6 +4,10 @@ module Test.Kore.Builtin
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import qualified Data.Map
+import           Prelude hiding
+                 ( concatMap )
+
 import qualified Kore.Attribute.Symbol as Attribute
 import qualified Kore.Builtin as Kore
 import           Kore.IndexedModule.MetadataTools
@@ -14,6 +18,7 @@ import qualified Test.Kore.Builtin.Builtin as Builtin
 import qualified Test.Kore.Builtin.Definition as Builtin
 import qualified Test.Kore.Builtin.Int as Int
 import qualified Test.Kore.Builtin.List as List
+import qualified Test.Kore.Builtin.Map as Map
 
 test_internalize :: [TestTree]
 test_internalize =
@@ -21,27 +26,56 @@ test_internalize =
         unitList
         (mkList [])
     , internalizes  "elementList"
-        (elementList (mkInt 0))
-        (mkList [mkInt 0])
+        (elementList zero)
+        (mkList [zero])
     , internalizes  "concatList(internal, internal)"
-        (concatList (mkList [mkInt 0]) (mkList [mkInt 1]))
-        (mkList [mkInt 0, mkInt 1])
+        (concatList (mkList [zero]) (mkList [one]))
+        (mkList [zero, one])
     , internalizes  "concatList(element, element)"
-        (concatList (elementList (mkInt 0)) (elementList (mkInt 1)))
-        (mkList [mkInt 0, mkInt 1])
+        (concatList (elementList zero) (elementList one))
+        (mkList [zero, one])
     , internalizes  "concatList(element, unit)"
-        (concatList (elementList (mkInt 0)) unitList)
-        (mkList [mkInt 0])
+        (concatList (elementList zero) unitList)
+        (mkList [zero])
     , internalizes  "concatList(unit, element)"
-        (concatList unitList (elementList (mkInt 1)))
-        (mkList [mkInt 1])
+        (concatList unitList (elementList one))
+        (mkList [one])
+
+    , internalizes  "unitMap"
+        unitMap
+        (mkMap [])
+    , internalizes  "elementMap"
+        (elementMap zero one)
+        (mkMap [(zero, one)])
+    , internalizes  "concatMap(internal, internal)"
+        (concatMap (mkMap [(zero, one)]) (mkMap [(one, two)]))
+        (mkMap [(zero, one), (one, two)])
+    , internalizes  "concatMap(element, element)"
+        (concatMap (elementMap zero one) (elementMap one two))
+        (mkMap [(zero, one), (one, two)])
+    , internalizes  "concatMap(element, unit)"
+        (concatMap (elementMap zero one) unitMap)
+        (mkMap [(zero, one)])
+    , internalizes  "concatMap(unit, element)"
+        (concatMap unitMap (elementMap one two))
+        (mkMap [(one, two)])
     ]
   where
     unitList = Builtin.unitList
     elementList = Builtin.elementList
     concatList = Builtin.concatList
     mkList = List.asInternal
+    unitMap = Builtin.unitMap
+    elementMap = Builtin.elementMap
+    concatMap = Builtin.concatMap
+    mkMap = Map.asInternal . Data.Map.fromList
+
+    mkInt :: Ord variable => Integer -> TermLike variable
     mkInt = Int.asInternal
+    zero, one, two :: Ord variable => TermLike variable
+    zero = mkInt 0
+    one = mkInt 1
+    two = mkInt 2
 
 withInternalized
     :: (TermLike Variable -> Assertion)
