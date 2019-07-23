2120530a51289a8b27beb78bd381faa392c2c706
Thomas Tuegel <ttuegel@mailbox.org>
2019-07-23 13:38:49 -0500

    Kore.Attribute.Axiom.Constructor

--- kore/src/Kore/Attribute/Axiom/Constructor.hs
+++ kore/src/Kore/Attribute/Axiom/Constructor.hs
@@ -53,4 +53,4 @@ instance ParseAttributes Constructor where
         failDuplicate' = Parser.failDuplicate constructorId
 
     toAttributes Constructor { isConstructor } =
-        Attributes $ if isConstructor then [constructorAttribute] else []
+        Attributes [constructorAttribute | isConstructor]
