e0d62803d0ccf8e3f864a3f7e05f4d791f3431c4
Thomas Tuegel <ttuegel@mailbox.org>
2019-07-30 13:02:41 -0500

    GOLD Test.Kore.Step.Simplification.Builtin

--- kore/test/Test/Kore/Step/Simplification/Builtin.hs
+++ kore/test/Test/Kore/Step/Simplification/Builtin.hs
@@ -3,18 +3,17 @@ module Test.Kore.Step.Simplification.Builtin
     ) where
 
 import Test.Tasty
-       ( TestTree )
 import Test.Tasty.HUnit
-       ( testCase )
 
-import qualified Data.Bifunctor as Bifunctor
 import qualified Data.Map.Strict as Map
 import qualified Data.Sequence as Seq
+import qualified GHC.Stack as GHC
 
 import qualified Kore.Domain.Builtin as Domain
 import           Kore.Internal.OrPattern
                  ( OrPattern )
 import qualified Kore.Internal.OrPattern as OrPattern
+import Kore.Internal.Pattern ( Pattern)
 import qualified Kore.Internal.Pattern as Pattern
 import           Kore.Internal.TermLike
 import           Kore.Step.Simplification.Builtin
@@ -26,45 +25,59 @@ import           Test.Tasty.HUnit.Extensions
 
 test_simplify :: [TestTree]
 test_simplify =
-    [ testCase "\\bottom propagates through builtin Map"
-        (assertEqualWithExplanation
-            "Expected \\bottom to propagate to the top level"
-            (OrPattern.fromPatterns [])
-            (evaluate
-                (mkMapDomainValue [(Mock.aConcrete, bottom)])
-            )
-        )
-    , testCase "\\bottom propagates through builtin List"
-        (assertEqualWithExplanation
-            "Expected \\bottom to propagate to the top level"
-            (OrPattern.fromPatterns [])
-            (evaluate
-                (mkListDomainValue [bottom])
-            )
-        )
+    [ becomes "\\bottom value becomes \\bottom Map" (mkMap [(a, bottom)] []) []
+    , becomes "\\bottom key becomes \\bottom Map" (mkMap [(bottom, a)] []) []
+    , becomes "\\bottom term becomes \\bottom Map" (mkMap [(a, b)] [bottom]) []
+    , becomes "\\bottom element becomes \\bottom List" (mkList [bottom]) []
+    , becomes "\\bottom element becomes \\bottom Set" (mkSet [bottom] []) []
+    , becomes "\\bottom term becomes \\bottom Set" (mkSet [] [bottom]) []
     ]
   where
+    a = OrPattern.fromTermLike Mock.a
+    b = OrPattern.fromTermLike Mock.b
     bottom = OrPattern.fromPatterns [Pattern.bottom]
+    becomes
+        :: GHC.HasCallStack
+        => TestName
+        -> Builtin (OrPattern Variable)
+        -> [Pattern Variable]
+        -> TestTree
+    becomes name origin expect =
+        testCase name
+        $ assertEqualWithExplanation ""
+            (OrPattern.fromPatterns expect)
+            (evaluate origin)
 
-mkMapDomainValue
-    :: [(TermLike Concrete, child)]
-    -> Builtin child
-mkMapDomainValue children =
+mkMap :: [(child, child)] -> [child] -> Builtin (child)
+mkMap elements opaque =
     Domain.BuiltinMap Domain.InternalAc
         { builtinAcSort = Mock.mapSort
         , builtinAcUnit = Mock.unitMapSymbol
         , builtinAcElement = Mock.elementMapSymbol
         , builtinAcConcat = Mock.concatMapSymbol
         , builtinAcChild = Domain.NormalizedMap Domain.NormalizedAc
-            { elementsWithVariables = []
-            , concreteElements =
-                Map.fromList (Bifunctor.second Domain.MapValue <$> children)
-            , opaque = []
+            { elementsWithVariables = Domain.MapElement <$> elements
+            , concreteElements = Map.empty
+            , opaque
             }
         }
 
-mkListDomainValue :: [child] -> Builtin child
-mkListDomainValue children =
+mkSet :: [child] -> [child] -> Builtin (child)
+mkSet elements opaque =
+    Domain.BuiltinSet Domain.InternalAc
+        { builtinAcSort = Mock.setSort
+        , builtinAcUnit = Mock.unitSetSymbol
+        , builtinAcElement = Mock.elementSetSymbol
+        , builtinAcConcat = Mock.concatSetSymbol
+        , builtinAcChild = Domain.NormalizedSet Domain.NormalizedAc
+            { elementsWithVariables = Domain.SetElement <$> elements
+            , concreteElements = Map.empty
+            , opaque
+            }
+        }
+
+mkList :: [child] -> Builtin child
+mkList children =
     Domain.BuiltinList Domain.InternalList
         { builtinListSort = Mock.listSort
         , builtinListUnit = Mock.unitListSymbol
