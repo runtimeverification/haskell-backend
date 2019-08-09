ada1653aa7230fca6ce6686e3932e929c12b5aa2
Virgil <25692529+virgil-serbanuta@users.noreply.github.com>
2019-08-09 16:41:00 +0300

    Update kore/src/Kore/Profiler/Data.hs
    Co-Authored-By: Thomas Tuegel <ttuegel@mailbox.org>

--- kore/src/Kore/Profiler/Data.hs
+++ kore/src/Kore/Profiler/Data.hs
@@ -10,26 +10,40 @@ module Kore.Profiler.Data
     , ProfileEvent (..)
     ) where
 
+import           Control.Monad.IO.Class
+                 ( MonadIO (liftIO) )
+import           Control.Monad.Morph
+                 ( MFunctor (..) )
 import           Control.Monad.Reader
+                 ( ReaderT )
 import qualified Control.Monad.State.Strict as Strict
 import           Control.Monad.Trans.Accum
+                 ( AccumT (AccumT), runAccumT )
 import           Control.Monad.Trans.Except
+                 ( ExceptT )
 import           Control.Monad.Trans.Identity
+                 ( IdentityT )
 import           Control.Monad.Trans.Maybe
-
+                 ( MaybeT )
 import           Debug.Trace
                  ( traceEventIO )
 import           ListT
                  ( ListT (..) )
 import qualified ListT
-                 ( gather, scatter )
+                 ( mapListT )
 import           System.CPUTime
                  ( getCPUTime )
 
 {- Monad that can also handle profiling events.
 -}
 class Monad profiler => MonadProfiler profiler where
+    --
     profileDuration :: [String] -> profiler a -> profiler a
+    default profileDuration
+        :: (MonadProfiler m, MFunctor t, profiler ~ t m)
+        => [String] -> profiler a -> profiler a
+    profileDuration a = hoist (profileDuration a)
+    {-# INLINE profileDuration #-}
 
 {- A profiler event.
 
@@ -39,11 +53,11 @@ value for @endPico@
 -}
 data ProfileEvent
     = ProfileEvent
-        { startPico :: Integer
+        { startPico :: !Integer
         -- The start CPU time, in picoseconds.
-        , endPico :: Maybe Integer
+        , endPico :: !(Maybe Integer)
         -- The end CPU time, in picoseconds. Nothing if this is a start event.
-        , tags :: [String]
+        , tags :: ![String]
         -- Tags for the event. If @tags=[t1, t2, t3]@ then this event will be
         -- counted as part of @t1@, @t1-t2@ and @t1-t2-t3@.
         }
@@ -80,28 +94,15 @@ profileDurationStartEnd event action = do
 nullProfileDuration :: Monad profiler => [String] -> profiler a -> profiler a
 nullProfileDuration _ = id
 
-instance (MonadProfiler m) => MonadProfiler (ReaderT thing m ) where
-    profileDuration a action =
-        ReaderT (profileDuration a . runReaderT action)
-    {-# INLINE profileDuration #-}
+instance (MonadProfiler m) => MonadProfiler (ReaderT thing m )
 
-instance MonadProfiler m => MonadProfiler (Strict.StateT s m) where
-    profileDuration a action =
-        Strict.StateT (profileDuration a . Strict.runStateT action)
-    {-# INLINE profileDuration #-}
+instance MonadProfiler m => MonadProfiler (Strict.StateT s m)
 
-instance MonadProfiler m => MonadProfiler (MaybeT m) where
-    profileDuration a action = MaybeT (profileDuration a (runMaybeT action))
-    {-# INLINE profileDuration #-}
+instance MonadProfiler m => MonadProfiler (MaybeT m)
 
-instance MonadProfiler m => MonadProfiler (IdentityT m) where
-    profileDuration a action =
-        IdentityT (profileDuration a (runIdentityT action))
-    {-# INLINE profileDuration #-}
+instance MonadProfiler m => MonadProfiler (IdentityT m)
 
-instance MonadProfiler m => MonadProfiler (ExceptT e m) where
-    profileDuration a action = ExceptT (profileDuration a (runExceptT action))
-    {-# INLINE profileDuration #-}
+instance MonadProfiler m => MonadProfiler (ExceptT e m)
 
 instance MonadProfiler m => MonadProfiler (ListT m) where
     profileDuration a action =
