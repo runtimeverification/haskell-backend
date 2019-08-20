30dbab4c8ec9188fee27ab3101ff8189796e1573
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2019-08-20 15:16:29 -0500

    Jenkinsfile: Run build steps in parallel

---
+++ scripts/clean.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+export TOP=${TOP:-$(git rev-parse --show-toplevel)}
+export PATH="$(stack path --local-bin)${PATH:+:$PATH}"
+
+MAKE="make -O -j --directory $TOP"
+$MAKE clean
