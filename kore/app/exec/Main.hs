bdbc1bb4e6037c2e359c2c67418de074adfa512d
ana-pantilie <ana.pantilie95@gmail.com>
2019-08-26 16:25:46 +0300

    removed type hint

--- kore/app/exec/Main.hs
+++ kore/app/exec/Main.hs
@@ -388,7 +388,7 @@ loadDefinition options = do
     let KoreExecOptions { definitionFileName } = options
     parsedDefinition <- parseDefinition definitionFileName
     definition@(indexedModules, _) <-
-        verifyDefinitionWithBase Nothing True (parsedDefinition :: ())
+        verifyDefinitionWithBase Nothing True parsedDefinition
     let KoreExecOptions { mainModuleName } = options
     mainModule <- lookupMainModule mainModuleName indexedModules
     return (mainModule, definition)
