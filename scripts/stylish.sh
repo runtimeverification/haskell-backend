d9a990dddc1a5bb974ea127cff59af44a4f75b38
Vladimir Ciobanu <admin@cvlad.info>
2019-09-03 17:37:53 +0300

    Setting path is no longer necessary

--- scripts/stylish.sh
+++ scripts/stylish.sh
@@ -7,6 +7,5 @@ export TOP=${TOP:-$(git rev-parse --show-toplevel)}
 source $TOP/scripts/run-on-haskell.include.sh
 
 stack install stylish-haskell
-export PATH=$(stack path --bin-path)
 
 runOnHaskellFiles "$TOP" .build/kore/bin/stylish-haskell -i
