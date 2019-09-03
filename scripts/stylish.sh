081ed9bcf25a992e7e94bb5ca2945cd9eec26e92
Vladimir Ciobanu <admin@cvlad.info>
2019-09-03 17:11:16 +0300

    Also install it..?

--- scripts/stylish.sh
+++ scripts/stylish.sh
@@ -6,7 +6,7 @@ export TOP=${TOP:-$(git rev-parse --show-toplevel)}
 
 source $TOP/scripts/run-on-haskell.include.sh
 
-stack build stylish-haskell
+stack install stylish-haskell
 export PATH=$(stack path --bin-path)
 
 runOnHaskellFiles "$TOP" stack exec stylish-haskell -- -i
