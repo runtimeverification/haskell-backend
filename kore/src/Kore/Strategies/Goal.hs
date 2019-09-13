9e4b6f6d13d0e274f72c826dcfc8d94bc33b7f07
ana-pantilie <ana.pantilie95@gmail.com>
2019-09-13 14:20:27 +0300

    Revert "Refactoring: add default implementations from OnePath.Actions for class Goal"
    This reverts commit b7c726f534d8d511c9bb5bc3835bf155c9df3631.

--- kore/src/Kore/Strategies/Goal.hs
+++ kore/src/Kore/Strategies/Goal.hs
@@ -2,79 +2,29 @@
 Copyright   : (c) Runtime Verification, 2019
 License     : NCSA
 -}
-module Kore.Strategies.Goal
-    ( Goal (..)
-    , Rule (..)
-    , Prim (..)
-    , onePathFirstStep
-    , onePathFollowupStep
-    , unprovenNodes
-    , transitionRule
-    , allPathStrategy
-    , proven
-    ) where
+module Kore.Strategies.Goal where
 
 import           Control.Applicative
                  ( Alternative (..) )
-import qualified Control.Monad.Trans as Monad.Trans
-import           Data.Coerce
-                 ( Coercible, coerce )
-import qualified Data.Default as Default
 import qualified Data.Foldable as Foldable
 import           Data.Maybe
                  ( mapMaybe )
-import qualified Data.Set as Set
-import qualified Data.Text.Prettyprint.Doc as Pretty
 import qualified Generics.SOP as SOP
 import           GHC.Generics as GHC
 
-import qualified Kore.Attribute.Axiom as Attribute.Axiom
-import qualified Kore.Attribute.Pattern.FreeVariables as Attribute.FreeVariables
-import qualified Kore.Attribute.Trusted as Attribute.Trusted
 import           Kore.Debug
-import qualified Kore.Internal.Conditional as Conditional
 import qualified Kore.Internal.MultiOr as MultiOr
-import           Kore.Internal.Pattern
-                 ( Pattern )
-import qualified Kore.Internal.Pattern as Pattern
-import           Kore.Internal.TermLike
-                 ( mkAnd )
-import qualified Kore.Predicate.Predicate as Predicate
-import qualified Kore.Predicate.Predicate as Syntax
-import qualified Kore.Step.Result as Result
 import           Kore.Step.Rule
-                 ( AllPathRule (..), OnePathRule (..), RewriteRule (..) )
-import           Kore.Step.Rule
-                 ( RulePattern (..) )
-import qualified Kore.Step.Rule as RulePattern
+                 ( OnePathRule (..), RewriteRule (..) )
 import           Kore.Step.Simplification.Data
                  ( MonadSimplify, SimplifierVariable )
-import           Kore.Step.Simplification.Pattern
-                 ( simplifyAndRemoveTopExists )
-import qualified Kore.Step.SMT.Evaluator as SMT.Evaluator
-import qualified Kore.Step.Step as Step
 import           Kore.Step.Strategy
                  ( Strategy )
 import qualified Kore.Step.Strategy as Strategy
 import qualified Kore.Strategies.OnePath.Actions as OnePath
 import           Kore.Strategies.ProofState
-import qualified Kore.Strategies.ProofState as Goal
-import           Kore.Syntax.Variable
-                 ( SortedVariable )
-import           Kore.Syntax.Variable
-                 ( Variable )
-import           Kore.TopBottom
-                 ( isBottom )
-import qualified Kore.Unification.Procedure as Unification
-import qualified Kore.Unification.Unify as Monad.Unify
-import           Kore.Unparser
-                 ( Unparse, unparse )
 import           Kore.Unparser
                  ( Unparse )
-import           Kore.Variables.Fresh
-                 ( FreshVariable )
-import           Kore.Variables.UnifiedVariable
-                 ( UnifiedVariable (ElemVar), isElemVar )
 
 {- | The final nodes of an execution graph which were not proven.
 
@@ -116,60 +66,15 @@ class Goal goal where
         :: MonadSimplify m
         => goal
         -> Strategy.TransitionT (Rule goal) m goal
-    default removeDestination
-        :: MonadSimplify m
-        => SimplifierVariable variable
-        => Coercible goal (RulePattern variable)
-        => goal
-        -> Strategy.TransitionT (Rule goal) m goal
-    removeDestination goal = do
-        let destination = getDestination goal
-            configuration = getConfiguration goal
-            removal = removalPredicate destination configuration
-            result = Conditional.andPredicate configuration removal
-        pure $ makeRuleFromPatterns result destination
 
     simplify
         :: MonadSimplify m
         => goal
         -> Strategy.TransitionT (Rule goal) m goal
-    default simplify
-        :: MonadSimplify m
-        => SimplifierVariable variable
-        => Coercible goal (RulePattern variable)
-        => goal
-        -> Strategy.TransitionT (Rule goal) m goal
-    simplify goal = do
-        let destination = getDestination goal
-            configuration = getConfiguration goal
-        configs <-
-            Monad.Trans.lift
-            $ simplifyAndRemoveTopExists configuration
-        filteredConfigs <- SMT.Evaluator.filterMultiOr configs
-        if null filteredConfigs
-            then pure $ makeRuleFromPatterns Pattern.bottom destination
-            else do
-                let simplifiedRules =
-                        fmap (`makeRuleFromPatterns` destination) filteredConfigs
-                Foldable.asum (pure <$> simplifiedRules)
 
     isTriviallyValid :: goal -> Bool
-    default isTriviallyValid
-        :: SimplifierVariable variable
-        => Coercible goal (RulePattern variable)
-        => goal -> Bool
-    isTriviallyValid = isBottom . RulePattern.left . coerce
 
     isTrusted :: goal -> Bool
-    default isTrusted
-        :: SimplifierVariable variable
-        => Coercible goal (RulePattern variable)
-        => goal -> Bool
-    isTrusted =
-        Attribute.Trusted.isTrusted
-        . Attribute.Axiom.trusted
-        . RulePattern.attributes
-        . coerce
 
     -- | Apply 'Rule's to the goal in parallel.
     derivePar
@@ -177,58 +82,6 @@ class Goal goal where
         => [Rule goal]
         -> goal
         -> Strategy.TransitionT (Rule goal) m (ProofState goal)
-    default derivePar
-        :: forall m variable
-        .  MonadSimplify m
-        => SimplifierVariable variable
-        => Coercible goal (RulePattern variable)
-        => Coercible (RulePattern variable) goal
-        => Coercible (Rule goal) (RulePattern variable)
-        => Coercible (RulePattern variable) (Rule goal)
-        => [Rule goal]
-        -> goal
-        -> Strategy.TransitionT (Rule goal) m (ProofState goal)
-    derivePar rules goal = do
-        let destination = getDestination goal
-            configuration :: Pattern variable
-            configuration = getConfiguration goal
-            rewrites = coerce <$> rules
-        eitherResults <-
-            Monad.Trans.lift
-            . Monad.Unify.runUnifierT
-            $ Step.applyRewriteRulesParallel
-                (Step.UnificationProcedure Unification.unificationProcedure)
-                rewrites
-                configuration
-        case eitherResults of
-            Left err ->
-                (error . show . Pretty.vsep)
-                [ "Not implemented error:"
-                , Pretty.indent 4 (Pretty.pretty err)
-                , "while applying a \\rewrite axiom to the pattern:"
-                , Pretty.indent 4 (unparse configuration)
-                ,   "We decided to end the execution because we don't \
-                    \understand this case well enough at the moment."
-                ]
-            Right results -> do
-                let mapRules =
-                        Result.mapRules
-                        $ coerce
-                        . RewriteRule
-                        . Step.unwrapRule
-                        . Step.withoutUnification
-                    traverseConfigs =
-                        Result.traverseConfigs
-                            (pure . Goal)
-                            removeDestSimplifyRemainder
-                let onePathResults =
-                        Result.mapConfigs
-                            (`makeRuleFromPatterns` destination)
-                            (`makeRuleFromPatterns` destination)
-                            (Result.mergeResults results)
-                results' <-
-                    traverseConfigs (mapRules onePathResults)
-                Result.transitionResults results'
 
     -- | Apply 'Rule's to the goal in sequence.
     deriveSeq
@@ -236,58 +89,6 @@ class Goal goal where
         => [Rule goal]
         -> goal
         -> Strategy.TransitionT (Rule goal) m (ProofState goal)
-    default deriveSeq
-        :: forall m variable
-        .  MonadSimplify m
-        => SimplifierVariable variable
-        => Coercible goal (RulePattern variable)
-        => Coercible (RulePattern variable) goal
-        => Coercible (Rule goal) (RulePattern variable)
-        => Coercible (RulePattern variable) (Rule goal)
-        => [Rule goal]
-        -> goal
-        -> Strategy.TransitionT (Rule goal) m (ProofState goal)
-    deriveSeq rules goal = do
-        let destination = getDestination goal
-            configuration = getConfiguration goal
-            rewrites = coerce <$> rules
-        eitherResults <-
-            Monad.Trans.lift
-            . Monad.Unify.runUnifierT
-            $ Step.applyRewriteRulesSequence
-                (Step.UnificationProcedure Unification.unificationProcedure)
-                configuration
-                rewrites
-        case eitherResults of
-            Left err ->
-                (error . show . Pretty.vsep)
-                [ "Not implemented error:"
-                , Pretty.indent 4 (Pretty.pretty err)
-                , "while applying a \\rewrite axiom to the pattern:"
-                , Pretty.indent 4 (unparse configuration)
-                ,   "We decided to end the execution because we don't \
-                    \understand this case well enough at the moment."
-                ]
-            Right results -> do
-                let mapRules =
-                        Result.mapRules
-                        $ coerce
-                        . RewriteRule
-                        . Step.unwrapRule
-                        . Step.withoutUnification
-                    traverseConfigs =
-                        Result.traverseConfigs
-                            (pure . Goal.Goal)
-                            removeDestSimplifyRemainder
-                let onePathResults =
-                        Result.mapConfigs
-                            (`makeRuleFromPatterns` destination)
-                            (`makeRuleFromPatterns` destination)
-                            (Result.mergeResults results)
-                results' <-
-                    traverseConfigs (mapRules onePathResults)
-                Result.transitionResults results'
-
 
 transitionRule
     :: (MonadSimplify m, Goal goal)
@@ -403,89 +204,3 @@ instance SOP.Generic (Rule (OnePathRule variable))
 instance SOP.HasDatatypeInfo (Rule (OnePathRule variable))
 
 instance Debug variable => Debug (Rule (OnePathRule variable))
-
-getConfiguration
-    :: forall rule variable
-    .  Ord variable
-    => Coercible rule (RulePattern variable)
-    => rule
-    -> Pattern variable
-getConfiguration (coerce -> RulePattern { left, requires }) =
-    Pattern.withCondition left (Conditional.fromPredicate requires)
-
-getDestination
-    :: forall rule variable
-    .  Ord variable
-    => Coercible rule (RulePattern variable)
-    => rule
-    -> Pattern variable
-getDestination (coerce -> RulePattern { right, ensures }) =
-    Pattern.withCondition right (Conditional.fromPredicate ensures)
-
-makeRuleFromPatterns
-    :: forall rule variable
-    .  SimplifierVariable variable
-    => Coercible (RulePattern variable) rule
-    => Pattern variable
-    -> Pattern variable
-    -> rule
-makeRuleFromPatterns configuration destination =
-    let (left, Conditional.toPredicate -> requires) =
-            Pattern.splitTerm configuration
-        (right, Conditional.toPredicate -> ensures) =
-            Pattern.splitTerm destination
-    in coerce RulePattern
-        { left, right, requires, ensures, attributes = Default.def }
-
-{- | The predicate to remove the destination from the present configuration.
- -}
-removalPredicate
-    :: SimplifierVariable variable
-    => Pattern variable
-    -- ^ Destination
-    -> Pattern variable
-    -- ^ Current configuration
-    -> Syntax.Predicate variable
-removalPredicate destination config =
-    let
-        -- The variables of the destination that are missing from the
-        -- configuration. These are the variables which should be existentially
-        -- quantified in the removal predicate.
-        configVariables =
-            Attribute.FreeVariables.getFreeVariables
-            $ Pattern.freeVariables config
-        destinationVariables =
-            Attribute.FreeVariables.getFreeVariables
-            $ Pattern.freeVariables destination
-        extraVariables = Set.toList
-            $ Set.difference destinationVariables configVariables
-        extraElementVariables = [v | ElemVar v <- extraVariables]
-        extraNonElemVariables = filter (not . isElemVar) extraVariables
-        quantifyPredicate = Predicate.makeMultipleExists extraElementVariables
-    in
-        if not (null extraNonElemVariables)
-        then error
-            ("Cannot quantify non-element variables: "
-                ++ show (unparse <$> extraNonElemVariables))
-        else Predicate.makeNotPredicate
-            $ quantifyPredicate
-            $ Predicate.makeCeilPredicate
-            $ mkAnd
-                (Pattern.toTermLike destination)
-                (Pattern.toTermLike config)
-
-removeDestSimplifyRemainder
-    :: forall m goal rule
-    .  MonadSimplify m
-    => Goal goal
-    => rule ~ Rule goal
-    => goal
-    -> Strategy.TransitionT
-        rule
-        m
-        (Goal.ProofState goal)
-removeDestSimplifyRemainder remainder = do
-    result <- removeDestination remainder >>= simplify
-    if isTriviallyValid result
-       then pure Goal.Proven
-       else pure . Goal.GoalRem $ result
