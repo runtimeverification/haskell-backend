9653c40087f7ccd863999cb89c33a2af1bb0ab9a
Andrei Burdusa <andrei.burdusa@runtimeverification.com>
2019-10-04 08:59:12 +0300

    changed back dv

--- kore/test/Test/Kore/Step/PatternAttributes.hs
+++ kore/test/Test/Kore/Step/PatternAttributes.hs
@@ -175,7 +175,7 @@ test_patternAttributes =
                 dv :: DomainValue Sort Symbol
                 dv = DomainValue
                         { domainValueSort = Mock.testSort
-                        , domainValueChild = undefined
+                        , domainValueChild = Mock.aSymbol  
                         }
             assertEqual "DomainValueF is constructor-like-top"
                 True
