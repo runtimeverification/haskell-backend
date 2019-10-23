c2e01dcac931d609b67e0b7cbb43b36b5518cd39
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2019-10-23 14:55:06 -0500

    kore-charts: User time charts

--- kore-charts/app/Main.hs
+++ kore-charts/app/Main.hs
@@ -15,9 +15,13 @@ import Data.Aeson (FromJSON)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as Lazy (ByteString)
 import Data.Map (Map)
-import Data.Monoid (First (getFirst))
 import Data.Scientific (Scientific)
 import GHC.Generics (Generic)
+
+import Control.Lens ((.~))
+import Control.Lens ((.=))
+import Data.Default (def)
+import Data.Monoid (First (getFirst))
 
 import qualified Data.Aeson as Aeson
 import qualified Data.Aeson.Types as Aeson
@@ -25,8 +29,14 @@ import qualified Data.Attoparsec.ByteString.Lazy as Attoparsec
 import qualified Data.ByteString as ByteString
 import qualified Data.List as List
 import qualified Data.Map.Strict as Map
+import qualified Data.Scientific as Scientific
+import qualified Data.Set as Set
 import qualified Data.Text as Text
 import qualified Control.Lens as Lens
+import qualified Graphics.Rendering.Chart as Chart
+import qualified Graphics.Rendering.Chart.Easy as Chart
+import qualified Graphics.Rendering.Chart.Grid as Chart
+import qualified Graphics.Rendering.Chart.Backend.Cairo as Chart.Backend
 import qualified Network.Wreq as Wreq
 
 jenkins :: String
@@ -38,7 +48,57 @@ main = do
     builds <- getBuilds job
     let profiled = mapMaybe matchProfiled builds
     buildProfiles <- Map.traverseWithKey (getBuildProfile builds) profiled
-    print buildProfiles
+    let tests = foldMap Map.keysSet buildProfiles
+        testProfiles = Map.fromSet (testProfile buildProfiles) tests
+        buildNumbers = Map.keysSet builds
+        firstBuild = Set.findMin buildNumbers
+        lastBuild = Set.findMax buildNumbers
+        buildRange = (firstBuild, lastBuild)
+        title =
+            "Kore"
+            & Chart.label style Chart.HTA_Centre Chart.VTA_Centre
+            & Chart.setPickFn Chart.nullPickFn
+          where
+            style =
+                def
+                & Chart.font_size .~ 15
+                & Chart.font_weight .~ Chart.FontWeightBold
+        grid =
+            Chart.aboveN
+                [ Chart.besideN
+                    [ Chart.layoutToGrid (layoutTest buildRange test profiles) ]
+                | (test, profiles) <- Map.toList testProfiles
+                ]
+            & Chart.wideAbove title
+    let fileOptions = def & Chart.Backend.fo_size .~ (800, 400 * Set.size tests)
+    _ <-
+        Chart.gridToRenderable grid
+        & Chart.fillBackground def
+        & Chart.Backend.renderableToFile fileOptions "kore-charts.png"
+    return ()
+
+layoutTest
+    :: (BuildNumber, BuildNumber)
+    -> String
+    -> Map BuildNumber Profile
+    -> Chart.Layout Double Double
+layoutTest (beg, end) title profiles = Chart.execEC $ do
+    let xRange = (double beg, double end)
+        userTime =
+            profiles
+            & Map.map Scientific.toRealFloat . Map.map user_sec
+            & Map.mapKeys double
+            & Map.toAscList
+        yRange = (0, 1.1 * maximum (snd <$> userTime))
+    Chart.layout_title .= title
+    Chart.layout_x_axis . Chart.laxis_generate .= Chart.scaledAxis def xRange
+    Chart.layout_y_axis . Chart.laxis_generate .= Chart.scaledAxis def yRange
+    Chart.plot $ Chart.line "user time (s)" [ userTime ]
+  where
+    double = fromInteger . unBuildNumber
+
+testProfile :: Map BuildNumber BuildProfile -> String -> Map BuildNumber Profile
+testProfile buildProfiles test = mapMaybe (Map.lookup test) buildProfiles
 
 getJob :: String -> String -> IO Job
 getJob project name = do
@@ -77,11 +137,12 @@ getBuilds Job { builds } = do
 
 type Builds = Map BuildNumber Build
 
-newtype BuildNumber = BuildNumber Integer
+newtype BuildNumber = BuildNumber { unBuildNumber :: Integer }
     deriving (Eq, Ord, Show)
     deriving (Generic)
 
-instance FromJSON BuildNumber
+instance FromJSON BuildNumber where
+    parseJSON value = BuildNumber <$> Aeson.parseJSON value
 
 data Build =
     Build
