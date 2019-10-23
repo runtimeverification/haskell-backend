4c6e8330b93f840f18e76b8baf7b63eda65c0cf0
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2019-10-23 13:50:16 -0500

    kore-charts: getBuildProfile

--- kore-charts/app/Main.hs
+++ kore-charts/app/Main.hs
@@ -3,10 +3,17 @@ module Main (main) where
 import Prelude hiding (filter)
 
 import Control.Applicative
+import Control.Monad
+import Control.Monad.Catch
+import Data.Foldable
+import Data.Function
 import Data.Traversable
 import Data.Witherable
 
+import Control.Lens (Traversal')
 import Data.Aeson (FromJSON)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as Lazy (ByteString)
 import Data.Map (Map)
 import Data.Monoid (First (getFirst))
 import Data.Scientific (Scientific)
@@ -14,7 +21,9 @@ import GHC.Generics (Generic)
 
 import qualified Data.Aeson as Aeson
 import qualified Data.Aeson.Types as Aeson
-import qualified Data.Attoparsec.ByteString as Attoparsec
+import qualified Data.Attoparsec.ByteString.Lazy as Attoparsec
+import qualified Data.ByteString as ByteString
+import qualified Data.List as List
 import qualified Data.Map.Strict as Map
 import qualified Data.Text as Text
 import qualified Control.Lens as Lens
@@ -121,17 +130,80 @@ getBuildProfile builds buildNumber Artifact { relativePath } = do
     let Just Build { url } = Map.lookup buildNumber builds
         url' = url <> "artifact/" <> relativePath
     response <- Wreq.get url'
-    Lens.view Wreq.responseBody <$> Wreq.asJSON response
+    profiles <- Lens.view Wreq.responseBody <$> asJSONs response
+    return (fold profiles)
 
 type BuildProfile = Map String Profile
 
+-- | The only valid whitespace in a JSON document is space, newline,
+-- carriage return, and tab.
+skipSpace :: Attoparsec.Parser ()
+skipSpace = Attoparsec.skipWhile $ \w -> any (== w) [ 0x20, 0x0a, 0x0d, 0x09 ]
+
+{- | Parse a sequence of JSON values.
+
+This is not a valid JSON document, but it's easy to work with and I don't care.
+
+ -}
 jsons :: Attoparsec.Parser [Aeson.Value]
-jsons = some Aeson.json <* Attoparsec.endOfInput
+jsons = some Aeson.json <* skipSpace <* Attoparsec.endOfInput
+
+responseContentType :: Traversal' (Wreq.Response body) ByteString
+responseContentType = Wreq.responseHeader "Content-Type"
 
 asJSONs
     :: FromJSON json
-    => Wreq.Response ByteString
+    => Wreq.Response Lazy.ByteString
     -> IO (Wreq.Response [json])
+asJSONs response = withContext $ do
+    assertContentTypeJSON response
+    let responseBody = Lens.view Wreq.responseBody response
+    values <- fromJSONs responseBody
+    responseBody' <- traverse fromValue values
+    return (responseBody' <$ response)
+  where
+    isContentTypeJSON contentType
+      | ByteString.isPrefixOf "application/json" contentType = True
+      | ByteString.isPrefixOf "application/" contentType
+      , ByteString.isSuffixOf "+json" contentType = True
+      | otherwise = False
+
+    unknownContentType =
+        throwM $ Wreq.JSONError "unknown content type of response"
+
+    wrongContentType contentType =
+        throwM . Wreq.JSONError
+        $ "content type of response is " ++ show contentType
+
+    assertContentTypeJSON resp = do
+        contentType <- getContentType resp
+        unless (isContentTypeJSON contentType) (wrongContentType contentType)
+
+    getContentType resp =
+        Lens.preview responseContentType resp
+        & maybe unknownContentType return
+
+    fromJSONs :: Lazy.ByteString -> IO [Aeson.Value]
+    fromJSONs body =
+        parseOnly jsons body
+        & either (throwM . Wreq.JSONError) return
+
+    fromValue value =
+        Aeson.parseEither Aeson.parseJSON value
+        & either (throwM . Wreq.JSONError) return
+
+    withContext =
+        flip onException $ do
+            putStrLn "While parsing:"
+            print (Lens.view Wreq.responseBody response)
+
+parseOnly :: Attoparsec.Parser a -> Lazy.ByteString -> Either String a
+parseOnly parser byteString =
+    case Attoparsec.parse parser byteString of
+        Attoparsec.Fail _ [] err   -> Left err
+        Attoparsec.Fail _ ctxs err ->
+            Left (List.intercalate " > " ctxs ++ ": " ++ err)
+        Attoparsec.Done _ a        -> Right a
 
 data Profile =
     Profile
