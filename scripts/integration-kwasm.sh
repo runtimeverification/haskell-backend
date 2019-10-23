dd0f5c8526cba90102f3e89226856b6383367377
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2019-10-23 11:27:09 -0500

    scripts/integration-kwasm.sh: Prove memory-spec.k

--- scripts/integration-kwasm.sh
+++ scripts/integration-kwasm.sh
@@ -30,7 +30,8 @@ make build-haskell -B
 
 for each in \
     tests/proofs/simple-arithmetic-spec.k.prove \
-    tests/proofs/loops-spec.k.prove
+    tests/proofs/loops-spec.k.prove \
+    tests/proofs/memory-spec.k.prove
 do
     command time -o "$TOP/profile.json" -a \
         -f "{ \"wasm-semantics/$each\": { \"user_sec\": %U, \"resident_kbytes\": %M } }" \
