73b26d4b0487d3fab717b2794b9c55c98cbaed5e
ana-pantilie <45069775+ana-pantilie@users.noreply.github.com>
2019-11-01 21:37:53 +0200

    Function rules need to have function symbols at the top (#1173)

--- kore/test/Test/Kore/Step/Axiom/Registry.hs
+++ kore/test/Test/Kore/Step/Axiom/Registry.hs
@@ -24,7 +24,7 @@ import Data.Text
 import Kore.ASTVerifier.DefinitionVerifier
 import qualified Kore.Attribute.Axiom as Attribute
 import Kore.Attribute.Simplification
-    ( simplificationSymbol
+    ( simplificationAttribute
     )
 import qualified Kore.Attribute.Symbol as Attribute
 import qualified Kore.Builtin as Builtin
@@ -115,12 +115,21 @@ testDef =
         (ModuleName "test")
         [ simpleSortSentence (SortName "S")
         , simpleSymbolSentence (SymbolName "s") (SortName "S")
+            & (updateAttributes . Attributes)
+                [ Attribute.functionalAttribute
+                , Attribute.constructorAttribute
+                ]
         , simpleSymbolSentence (SymbolName "t") (SortName "S")
+            & (updateAttributes . Attributes)
+                [ Attribute.functionalAttribute
+                , Attribute.constructorAttribute
+                ]
         , symbolSentenceWithParametersAndArguments
             (SymbolName "inj")
             [sortVar, sortVar1]
             sortVar1S
             [sortVarS]
+            & updateAttributes (Attributes [Attribute.functionAttribute])
         , updateAttributes
             (Attributes
                 [ Attribute.functionAttribute
@@ -210,8 +219,7 @@ testDef =
             }
         , SentenceAxiomSentence SentenceAxiom
             { sentenceAxiomParameters = [sortVar]
-            , sentenceAxiomAttributes =
-                Attributes [ attributePattern_ simplificationSymbol ]
+            , sentenceAxiomAttributes = Attributes [simplificationAttribute]
             , sentenceAxiomPattern =
                 Builtin.externalize $ mkImplies
                     (mkTop sortVarS)
@@ -239,7 +247,7 @@ testDef =
             }
         , SentenceAxiomSentence SentenceAxiom
             { sentenceAxiomParameters = [sortVar, sortVar1]
-            , sentenceAxiomAttributes = Attributes []
+            , sentenceAxiomAttributes = Attributes [simplificationAttribute]
             , sentenceAxiomPattern =
                 Builtin.externalize $ mkImplies
                     (mkTop sortVarS)
