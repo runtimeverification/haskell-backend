84e60563216b77810401919043bb94e2d26ba99b
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2019-11-05 11:24:17 -0600

    Test.Kore.Builtin.Encoding: Remove unused imports

--- kore/test/Test/Kore/Builtin/Encoding.hs
+++ kore/test/Test/Kore/Builtin/Encoding.hs
@@ -1,6 +1,5 @@
 module Test.Kore.Builtin.Encoding where
 
-import qualified Data.ByteString.Char8 as BS
 import Hedgehog hiding
     ( Concrete
     )
@@ -9,32 +8,11 @@ import qualified Hedgehog.Range as Range
 import Test.Tasty
 import Test.Tasty.Hedgehog
 
-import Data.ByteString
-    ( ByteString
-    )
-import Data.Char
-    ( ord
-    )
 import Data.Text
     ( Text
     )
-import qualified Data.Text as T
-import GHC.Stack
-    ( HasCallStack
-    )
 
 import Kore.Builtin.Encoding
-import Kore.Internal.Pattern
-import Kore.Internal.TermLike hiding
-    ( bytesSort
-    )
-
-import Test.Kore.Builtin.Builtin
-import Test.Kore.Builtin.Definition
-import qualified Test.Kore.Builtin.Int as Test.Int
-import qualified Test.Kore.Builtin.String as Test.String
-import Test.SMT
-
 
 genString :: Gen Text
 genString = Gen.text (Range.linear 0 256) Gen.latin1
