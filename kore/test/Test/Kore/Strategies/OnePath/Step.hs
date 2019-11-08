7de3e1182986ce8e4c99488de1a206cbd82a9d26
ana-pantilie <ana.pantilie95@gmail.com>
2019-11-08 16:32:56 +0200

    Tests for ReachabilityRule Goal instance

--- kore/test/Test/Kore/Strategies/OnePath/Step.hs
+++ kore/test/Test/Kore/Strategies/OnePath/Step.hs
@@ -91,10 +91,7 @@ makeOnePathRule term dest =
 
 makeReachabilityOnePathRule :: TermLike Variable -> TermLike Variable -> ReachabilityRule Variable
 makeReachabilityOnePathRule term dest =
-    OnePath . OnePathRule
-    $ makeRuleFromPatterns
-        (fromTermLike term)
-        (fromTermLike dest)
+    OnePath (makeOnePathRule term dest)
 
 test_onePathStrategy :: [TestTree]
 test_onePathStrategy =
@@ -125,7 +122,7 @@ test_onePathStrategy =
         assertEqual "onepath == reachability onepath"
             (fmap OnePath actual)
             actualReach
-    , testCase "FAILINGTEST Axiom priority, first step" $ do
+    , testCase "Axiom priority, first step" $ do
         -- Goal: a => a
         -- Coinductive axiom: a => b
         -- Normal axiom: a => c
@@ -155,9 +152,17 @@ test_onePathStrategy =
             (makeOnePathRule Mock.a Mock.d)
             [makeOnePathRule Mock.a Mock.b]
             [simpleRewrite Mock.a Mock.c]
+        [ _actualReach ] <- runOnePathSteps
+            (Limit 1)
+            (makeReachabilityOnePathRule Mock.a Mock.d)
+            [makeReachabilityOnePathRule Mock.a Mock.b]
+            [simpleReachabilityRewrite Mock.a Mock.c]
         assertEqual ""
             (ProofState.Goal $ makeOnePathRule Mock.c Mock.d)
             _actual
+        assertEqual "onepath == reachability onepath"
+            (fmap OnePath _actual)
+            _actualReach
     , testCase "Axiom priority, second step" $ do
         -- Goal: a => b
         -- Coinductive axiom: b => c
@@ -174,9 +179,22 @@ test_onePathStrategy =
             [ simpleRewrite Mock.b Mock.d
             , simpleRewrite Mock.a Mock.b
             ]
+        [ _actualReach ] <- runOnePathSteps
+            (Limit 2)
+            (makeReachabilityOnePathRule
+                Mock.a
+                Mock.b
+            )
+            [makeReachabilityOnePathRule Mock.b Mock.c]
+            [ simpleReachabilityRewrite Mock.b Mock.d
+            , simpleReachabilityRewrite Mock.a Mock.b
+            ]
         assertEqual ""
             ProofState.Proven
             _actual
+        assertEqual "onepath == reachability onepath"
+            (fmap OnePath _actual)
+            _actualReach
 
         -- Goal: a => e
         -- Coinductive axiom: b => c
@@ -190,6 +208,13 @@ test_onePathStrategy =
             [ simpleRewrite Mock.b Mock.d
             , simpleRewrite Mock.a Mock.b
             ]
+        [ _actual1Reach ] <- runOnePathSteps
+            (Limit 2)
+            (makeReachabilityOnePathRule Mock.a Mock.e)
+            [makeReachabilityOnePathRule Mock.b Mock.c]
+            [ simpleReachabilityRewrite Mock.b Mock.d
+            , simpleReachabilityRewrite Mock.a Mock.b
+            ]
         assertEqual ""
             (sort
                 [ ProofState.Goal $ makeOnePathRule Mock.c Mock.e
@@ -199,6 +224,9 @@ test_onePathStrategy =
                 [ _actual1
                 ]
             )
+        assertEqual "onepath == reachability onepath"
+            (fmap OnePath _actual1)
+            _actual1Reach
 
         -- Goal: a => e
         -- Coinductive axiom: e => c
@@ -212,6 +240,13 @@ test_onePathStrategy =
             [ simpleRewrite Mock.b Mock.d
             , simpleRewrite Mock.a Mock.b
             ]
+        [ _actualReach ] <- runOnePathSteps
+            (Limit 2)
+            (makeReachabilityOnePathRule Mock.a Mock.e)
+            [makeReachabilityOnePathRule Mock.e Mock.c]
+            [ simpleReachabilityRewrite Mock.b Mock.d
+            , simpleReachabilityRewrite Mock.a Mock.b
+            ]
         assertEqual ""
             (sort
                 [ ProofState.Goal $ makeOnePathRule Mock.d Mock.e
@@ -221,6 +256,9 @@ test_onePathStrategy =
                 [ _actual
                 ]
             )
+        assertEqual "onepath == reachability onepath"
+            (fmap OnePath _actual)
+            _actualReach
     , testCase "Differentiated axioms" $ do
         -- Goal: constr10(x) => constr11(a)
         -- Coinductive axiom: constr11(a) => g(a)
@@ -251,6 +289,26 @@ test_onePathStrategy =
                     (Mock.functionalConstr11 (TermLike.mkElemVar Mock.y))
                     (Mock.h (TermLike.mkElemVar Mock.y))
                 , simpleRewrite
+                    (Mock.functionalConstr10 (TermLike.mkElemVar Mock.y))
+                    (Mock.functionalConstr11 (TermLike.mkElemVar Mock.y))
+                ]
+        actualReach <-
+            runOnePathSteps
+                (Limit 2)
+                (makeReachabilityOnePathRule
+                    (Mock.functionalConstr10 (TermLike.mkElemVar Mock.x))
+                    (Mock.functionalConstr11 Mock.a)
+                )
+                [ makeReachabilityOnePathRule (Mock.functionalConstr11 Mock.a) (Mock.g Mock.a)
+                , makeReachabilityOnePathRule (Mock.functionalConstr11 Mock.b) (Mock.f Mock.b)
+                ]
+                [ simpleReachabilityRewrite (Mock.functionalConstr11 Mock.a) (Mock.g Mock.a)
+                , simpleReachabilityRewrite (Mock.functionalConstr11 Mock.b) (Mock.g Mock.b)
+                , simpleReachabilityRewrite (Mock.functionalConstr11 Mock.c) (Mock.f Mock.c)
+                , simpleReachabilityRewrite
+                    (Mock.functionalConstr11 (TermLike.mkElemVar Mock.y))
+                    (Mock.h (TermLike.mkElemVar Mock.y))
+                , simpleReachabilityRewrite
                     (Mock.functionalConstr10 (TermLike.mkElemVar Mock.y))
                     (Mock.functionalConstr11 (TermLike.mkElemVar Mock.y))
                 ]
@@ -302,6 +360,9 @@ test_onePathStrategy =
         assertEqual ""
             expected
             actual
+        assertEqual "onepath == reachability onepath"
+            (fmap (fmap OnePath) actual)
+            actualReach
     , testCase "Stuck pattern" $ do
         -- Goal: constr10(x) => constr11(a)
         -- Coinductive axiom: constr11(b) => f(b)
@@ -312,7 +373,7 @@ test_onePathStrategy =
         --   or (f(b) and x=b)
         --   or (f(c) and x=c)
         --   Stuck (functionalConstr11(x) and x!=a and x!=b and x!=c )
-        [ _actual1, _actual2, _actual3 ] <-
+        actual@[ _actual1, _actual2, _actual3 ] <-
             runOnePathSteps
                 (Limit 2)
                 (makeOnePathRule
@@ -323,6 +384,20 @@ test_onePathStrategy =
                 ]
                 [ simpleRewrite (Mock.functionalConstr11 Mock.c) (Mock.f Mock.c)
                 , simpleRewrite
+                    (Mock.functionalConstr10 (TermLike.mkElemVar Mock.y))
+                    (Mock.functionalConstr11 (TermLike.mkElemVar Mock.y))
+                ]
+        actualReach <-
+            runOnePathSteps
+                (Limit 2)
+                (makeReachabilityOnePathRule
+                    (Mock.functionalConstr10 (TermLike.mkElemVar Mock.x))
+                    (Mock.functionalConstr11 Mock.a)
+                )
+                [ makeReachabilityOnePathRule (Mock.functionalConstr11 Mock.b) (Mock.f Mock.b)
+                ]
+                [ simpleReachabilityRewrite (Mock.functionalConstr11 Mock.c) (Mock.f Mock.c)
+                , simpleReachabilityRewrite
                     (Mock.functionalConstr10 (TermLike.mkElemVar Mock.y))
                     (Mock.functionalConstr11 (TermLike.mkElemVar Mock.y))
                 ]
@@ -363,12 +438,15 @@ test_onePathStrategy =
             , _actual2
             , _actual3
             ]
+        assertEqual "onepath == reachability onepath"
+            (fmap (fmap OnePath) actual)
+            actualReach
     , testCase "Axiom with requires" $ do
         -- Goal:  constr10(b) => a
         -- Coinductive axiom: n/a
         -- Normal axiom: constr10(b) => a | f(b) == c
         -- Expected: a | f(b) == c
-        [ _actual1, _actual2 ] <- runOnePathSteps
+        actual@[ _actual1, _actual2 ] <- runOnePathSteps
             (Limit 2)
             (makeOnePathRule
                 (Mock.functionalConstr10 Mock.b)
@@ -376,6 +454,20 @@ test_onePathStrategy =
             )
             []
             [ rewriteWithPredicate
+                (Mock.functionalConstr10 Mock.b)
+                Mock.a
+                $ makeEqualsPredicate
+                    Mock.c
+                    $ Mock.f Mock.b
+            ]
+        actualReach <- runOnePathSteps
+            (Limit 2)
+            (makeReachabilityOnePathRule
+                (Mock.functionalConstr10 Mock.b)
+                Mock.a
+            )
+            []
+            [ rewriteReachabilityWithPredicate
                 (Mock.functionalConstr10 Mock.b)
                 Mock.a
                 $ makeEqualsPredicate
@@ -400,6 +492,9 @@ test_onePathStrategy =
             [ _actual1
             , _actual2
             ]
+        assertEqual "onepath == reachability onepath"
+            (fmap (fmap OnePath) actual)
+            actualReach
     , testCase "Stuck pattern simplification" $ do
         -- Goal: 0 => 1
         -- Coinductive axioms: none
@@ -422,9 +517,30 @@ test_onePathStrategy =
                         (Mock.builtinBool True)
                     )
                 ]
+        [ _actualReach ] <-
+            runOnePathSteps
+                (Limit 2)
+                ( makeReachabilityOnePathRule
+                    (Mock.builtinInt 0)
+                    (Mock.builtinInt 1)
+                )
+                []
+                [ rewriteReachabilityWithPredicate
+                    (TermLike.mkElemVar Mock.xInt)
+                    (Mock.builtinInt 1)
+                    (makeEqualsPredicate
+                        (Mock.lessInt
+                            (TermLike.mkElemVar Mock.xInt) (Mock.builtinInt 2)
+                        )
+                        (Mock.builtinBool True)
+                    )
+                ]
         assertEqual ""
             ProofState.Proven
             _actual
+        assertEqual "onepath == reachability onepath"
+            (fmap OnePath _actual)
+            _actualReach
     , testCase "Configuration with SMT pruning" $ do
         -- Goal: constr10(b) | f(b) < 0  =>  a
         -- Coinductive axiom: n/a
@@ -469,6 +585,44 @@ test_onePathStrategy =
                     (Mock.builtinBool False)
                 )
             ]
+        [ _actualReach ] <- runOnePathSteps
+            (Limit 1)
+            (OnePath $ makeRuleFromPatterns
+                (Conditional
+                    { term = Mock.functionalConstr10 Mock.b
+                    , predicate = makeEqualsPredicate
+                        (Mock.lessInt
+                            (Mock.fTestInt Mock.b)
+                            (Mock.builtinInt 0)
+                        )
+                        (Mock.builtinBool True)
+                    , substitution = mempty
+                    }
+                )
+                (fromTermLike Mock.a)
+            )
+            []
+            [ rewriteReachabilityWithPredicate
+                (Mock.functionalConstr10 (TermLike.mkElemVar Mock.x))
+                Mock.a
+                (makeEqualsPredicate
+                    (Mock.lessInt
+                        (Mock.fTestInt (TermLike.mkElemVar Mock.x))
+                        (Mock.builtinInt 0)
+                    )
+                    (Mock.builtinBool True)
+                )
+            , rewriteReachabilityWithPredicate
+                (Mock.functionalConstr10 (TermLike.mkElemVar Mock.x))
+                Mock.c
+                (makeEqualsPredicate
+                    (Mock.lessInt
+                        (Mock.fTestInt (TermLike.mkElemVar Mock.x))
+                        (Mock.builtinInt 0)
+                    )
+                    (Mock.builtinBool False)
+                )
+            ]
         assertEqual ""
             [ ProofState.Goal $ makeRuleFromPatterns
                 ( Conditional
@@ -487,6 +641,9 @@ test_onePathStrategy =
             ]
             [ _actual
             ]
+        assertEqual "onepath == reachability onepath"
+            (fmap OnePath _actual)
+            _actualReach
     , testCase "Stuck with SMT pruning" $ do
         -- Goal: constr10(b) | f(b) < 0  =>  a
         -- Coinductive axiom: n/a
@@ -520,6 +677,34 @@ test_onePathStrategy =
                     (Mock.builtinBool True)
                 )
             ]
+        [ _actualReach ] <- runOnePathSteps
+            (Limit 1)
+            (OnePath $ makeRuleFromPatterns
+                (Conditional
+                    { term = Mock.functionalConstr10 Mock.b
+                    , predicate = makeEqualsPredicate
+                        (Mock.lessInt
+                            (Mock.fTestInt Mock.b)
+                            (Mock.builtinInt 0)
+                        )
+                        (Mock.builtinBool True)
+                    , substitution = mempty
+                    }
+                )
+                (Pattern.fromTermLike Mock.a)
+            )
+            []
+            [ rewriteReachabilityWithPredicate
+                (Mock.functionalConstr10 (TermLike.mkElemVar Mock.x))
+                Mock.a
+                (makeEqualsPredicate
+                    (Mock.lessInt
+                        (Mock.fTestInt (TermLike.mkElemVar Mock.x))
+                        (Mock.builtinInt 0)
+                    )
+                    (Mock.builtinBool True)
+                )
+            ]
         assertEqual ""
             [ ProofState.Goal $ makeRuleFromPatterns
                 ( Conditional
@@ -537,6 +722,9 @@ test_onePathStrategy =
                 (Pattern.fromTermLike Mock.a)
             ]
             [ _actual ]
+        assertEqual "onepath == reachability onepath"
+            (fmap OnePath _actual)
+            _actualReach
     ]
 
 simpleRewrite
@@ -559,15 +747,7 @@ simpleReachabilityRewrite
     -> TermLike Variable
     -> Rule (ReachabilityRule Variable)
 simpleReachabilityRewrite left right =
-    OPRule . OnePathRewriteRule
-    $ RewriteRule RulePattern
-        { left = left
-        , antiLeft = Nothing
-        , right = right
-        , requires = makeTruePredicate
-        , ensures = makeTruePredicate
-        , attributes = def
-        }
+    OPRule (simpleRewrite left right)
 
 rewriteWithPredicate
     :: TermLike Variable
@@ -584,6 +764,14 @@ rewriteWithPredicate left right predicate =
         , ensures = makeTruePredicate
         , attributes = def
         }
+
+rewriteReachabilityWithPredicate
+    :: TermLike Variable
+    -> TermLike Variable
+    -> Predicate Variable
+    -> Rule (ReachabilityRule Variable)
+rewriteReachabilityWithPredicate left right predicate =
+    OPRule (rewriteWithPredicate left right predicate)
 
 runSteps
     :: Goal goal
