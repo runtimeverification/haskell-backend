e83ff5d59a53273da642ea15f4fd0ee22a7878bb
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2019-11-15 12:07:52 -0600

    RED  test_match

--- kore/test/Test/Kore/Builtin/Signedness.hs
+++ kore/test/Test/Kore/Builtin/Signedness.hs
@@ -1,28 +1,48 @@
 module Test.Kore.Builtin.Signedness
-    ( test_Signedness
+    ( test_verify
+    , test_match
     ) where
 
 import Test.Tasty
 
-import Kore.Error
-    ( assertRight
-    )
+import qualified GHC.Stack as GHC
+
 import Kore.Internal.TermLike
 
 import Test.Kore.Builtin.Builtin
 import Test.Kore.Builtin.Definition
+import Test.Kore.Step.Axiom.Matcher
+    ( doesn'tMatch
+    , matches
+    )
 import Test.Tasty.HUnit.Ext
 
-test_Signedness :: [TestTree]
-test_Signedness =
-    [ testCase "verify littleSignedBytes" $ do
-        let original = mkApplySymbol signedBytesSymbol []
-            actual = assertRight $ verifyPattern (Just signednessSort) original
-            expect = signedBytes
-        assertEqual "expected internal representation" expect actual
-    , testCase "verify bigSignedBytes" $ do
-        let original = mkApplySymbol unsignedBytesSymbol []
-            actual = assertRight $ verifyPattern (Just signednessSort) original
-            expect = unsignedBytes
-        assertEqual "expected internal representation" expect actual
+test_verify :: [TestTree]
+test_verify =
+    [ test "littleSignedBytes" signedBytesSymbol signedBytes
+    , test "verify bigSignedBytes" unsignedBytesSymbol unsignedBytes
+    ]
+  where
+    test
+        :: GHC.HasCallStack
+        => TestName
+        -> Symbol
+        -> TermLike Variable
+        -> TestTree
+    test name symbol expect =
+        testCase name $ do
+            let original = mkApplySymbol symbol []
+                actual = verifyPattern (Just signednessSort) original
+            assertEqual "expected verified pattern" (Right expect) actual
+
+test_match :: [TestTree]
+test_match =
+    [ matches "signedBytes" signedBytes signedBytes []
+    , doesn'tMatch "not unsignedBytes -> signedBytes"
+        signedBytes
+        unsignedBytes
+    , matches "unsignedBytes" unsignedBytes unsignedBytes []
+    , doesn'tMatch "not signedBytes -> unsignedBytes"
+        unsignedBytes
+        signedBytes
     ]
