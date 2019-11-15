3f61d12a52e2281193fbd1a079a9d5187fcf8d38
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2019-11-15 11:36:00 -0600

    Collect RTS statistics (#1237)
    * kore-exec: --rts-statistics
    
    * Jenkinsfile: Collect --rts-statistics artifacts

---
+++ kore/test/Test/Stats.hs
@@ -0,0 +1,24 @@
+module Test.Stats ( test_Stats ) where
+
+import Test.Tasty
+
+import System.IO
+    ( hClose
+    )
+import System.IO.Temp
+
+import Stats
+
+import Test.Tasty.HUnit.Ext
+
+test_Stats :: [TestTree]
+test_Stats =
+    [ testCase "read back what we wrote" $ do
+        original <- getStats
+        let roundtrip filePath hndl = do
+                hClose hndl
+                writeStats filePath original
+                readStats filePath
+        reread <- withSystemTempFile "kore-test-Test-Stats-.json" roundtrip
+        assertEqual "expected same stats" original reread
+    ]
