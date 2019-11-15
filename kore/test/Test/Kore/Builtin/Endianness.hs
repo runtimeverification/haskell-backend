83a563c0017042df203cb211c104a844fa0877df
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2019-11-15 11:56:45 -0600

    RED  test_match

--- kore/test/Test/Kore/Builtin/Endianness.hs
+++ kore/test/Test/Kore/Builtin/Endianness.hs
@@ -1,28 +1,48 @@
 module Test.Kore.Builtin.Endianness
-    ( test_Endianness
+    ( test_verify
+    , test_match
     ) where
 
 import Test.Tasty
 
-import Kore.Error
-    ( assertRight
-    )
+import qualified GHC.Stack as GHC
+
 import Kore.Internal.TermLike
 
 import Test.Kore.Builtin.Builtin
 import Test.Kore.Builtin.Definition
+import Test.Kore.Step.Axiom.Matcher
+    ( doesn'tMatch
+    , matches
+    )
 import Test.Tasty.HUnit.Ext
 
-test_Endianness :: [TestTree]
-test_Endianness =
-    [ testCase "verify littleEndianBytes" $ do
-        let original = mkApplySymbol littleEndianBytesSymbol []
-            actual = assertRight $ verifyPattern (Just endiannessSort) original
-            expect = littleEndianBytes
-        assertEqual "expected internal representation" expect actual
-    , testCase "verify bigEndianBytes" $ do
-        let original = mkApplySymbol bigEndianBytesSymbol []
-            actual = assertRight $ verifyPattern (Just endiannessSort) original
-            expect = bigEndianBytes
-        assertEqual "expected internal representation" expect actual
+test_verify :: [TestTree]
+test_verify =
+    [ test "littleEndianBytes" littleEndianBytesSymbol littleEndianBytes
+    , test "bigEndianBytes" bigEndianBytesSymbol bigEndianBytes
+    ]
+  where
+    test
+        :: GHC.HasCallStack
+        => TestName
+        -> Symbol
+        -> TermLike Variable
+        -> TestTree
+    test name symbol expect =
+        testCase name $ do
+            let original = mkApplySymbol symbol []
+                actual = verifyPattern (Just endiannessSort) original
+            assertEqual "expected verified pattern" (Right expect) actual
+
+test_match :: [TestTree]
+test_match =
+    [ matches "littleEndianBytes" littleEndianBytes littleEndianBytes []
+    , doesn'tMatch "not bigEndianBytes -> littleEndianBytes"
+        littleEndianBytes
+        bigEndianBytes
+    , matches "bigEndianBytes" bigEndianBytes bigEndianBytes []
+    , doesn'tMatch "not littleEndianBytes -> bigEndianBytes"
+        bigEndianBytes
+        littleEndianBytes
     ]
