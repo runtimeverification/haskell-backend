fbc6e802c1304aeca0f297def40bcf09c1820f81
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2019-11-15 14:46:29 -0600

    RED  test_unify

--- kore/test/Test/Kore/Builtin/Endianness.hs
+++ kore/test/Test/Kore/Builtin/Endianness.hs
@@ -1,13 +1,35 @@
 module Test.Kore.Builtin.Endianness
     ( test_verify
     , test_match
+    , test_unify
     ) where
 
 import Test.Tasty
 
 import qualified GHC.Stack as GHC
 
+import qualified Kore.Internal.Condition as Condition
+import Kore.Internal.Pattern
+    ( Pattern
+    )
+import qualified Kore.Internal.Pattern as Pattern
 import Kore.Internal.TermLike
+import Kore.Step.Simplification.AndTerms
+    ( termUnification
+    )
+import Kore.Step.Simplification.Data
+    ( runSimplifier
+    )
+import Kore.Unification.Error
+    ( UnificationOrSubstitutionError
+    )
+import Kore.Unification.UnifierT
+    ( runUnifierT
+    )
+import Kore.Variables.UnifiedVariable
+import SMT
+    ( runNoSMT
+    )
 
 import Test.Kore.Builtin.Builtin
 import Test.Kore.Builtin.Definition
@@ -46,3 +68,51 @@ test_match =
         bigEndianBytes
         littleEndianBytes
     ]
+
+test_unify :: [TestTree]
+test_unify =
+    [ unifies "littleEndianBytes" littleEndianBytes littleEndianBytes []
+    , doesn'tUnify "littleEndianBytes and bigEndianBytes"
+        littleEndianBytes
+        bigEndianBytes
+    , unifies "bigEndianBytes" bigEndianBytes bigEndianBytes []
+    , doesn'tUnify "bigEndianBytes and littleEndianBytes"
+        bigEndianBytes
+        littleEndianBytes
+    ]
+  where
+    unifies
+        :: GHC.HasCallStack
+        => TestName
+        -> TermLike Variable
+        -> TermLike Variable
+        -> [(UnifiedVariable Variable, TermLike Variable)]
+        -> TestTree
+    unifies name term1 term2 solution =
+        testCase name $ do
+            let expect =
+                    Pattern.withCondition term1
+                    $ mconcat (Condition.fromSingleSubstitution <$> solution)
+            actual <- unify term1 term2
+            assertEqual "expected unification solution" (Right [expect]) actual
+    doesn'tUnify
+        :: GHC.HasCallStack
+        => TestName
+        -> TermLike Variable
+        -> TermLike Variable
+        -> TestTree
+    doesn'tUnify name term1 term2 =
+        testCase name $ do
+            actual <- unify term1 term2
+            assertEqual "expected bottom" (Right []) actual
+
+unify
+    :: GHC.HasCallStack
+    => TermLike Variable
+    -> TermLike Variable
+    -> IO (Either UnificationOrSubstitutionError [Pattern Variable])
+unify term1 term2 =
+    runNoSMT mempty
+    $ runSimplifier testEnv
+    $ runUnifierT
+    $ termUnification term1 term2
