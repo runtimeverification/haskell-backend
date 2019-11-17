f57eed57f5bb0a01a8ac3b34aad8611ebcf7b6d7
Thomas Tuegel <ttuegel@mailbox.org>
2019-11-17 14:27:42 -0600

    Missing module header

--- kore/src/Kore/Logger/WarnSimplificationWithRemainder.hs
+++ kore/src/Kore/Logger/WarnSimplificationWithRemainder.hs
@@ -1,3 +1,9 @@
+{- |
+Copyright   : (c) Runtime Verification, 2019
+License     : NCSA
+
+-}
+
 module Kore.Logger.WarnSimplificationWithRemainder
     ( WarnSimplificationWithRemainder (..)
     , warnSimplificationWithRemainder
