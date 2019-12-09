3f8a3dd63b55bab35fcfbd0dd678ac27804e2ce0
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2019-12-09 13:37:26 -0600

    Pair: Add instances of Debug and Diff

--- kore/src/Pair.hs
+++ kore/src/Pair.hs
@@ -7,7 +7,10 @@ module Pair
     ( Pair (..)
     ) where
 
+import qualified Generics.SOP as SOP
 import qualified GHC.Generics as GHC
+
+import Debug
 
 {- | A tuple @(a, a)@ where both elements are the same type.
  -}
@@ -15,3 +18,11 @@ data Pair a = Pair !a !a
     deriving (Eq, Ord, Read, Show)
     deriving (GHC.Generic)
     deriving (Foldable, Functor, Traversable)
+
+instance SOP.Generic (Pair a)
+
+instance SOP.HasDatatypeInfo (Pair a)
+
+instance Debug a => Debug (Pair a)
+
+instance (Debug a, Diff a) => Diff (Pair a)
