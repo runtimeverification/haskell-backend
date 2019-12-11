281e0eb7027c6ecb0e168bb297a30a05790feb05
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2019-12-11 16:10:25 -0600

    WIP  Re-work integration test suite

--- test/working/include.mk
+++ test/working/include.mk
@@ -4,11 +4,29 @@ endif
 
 include $(TOP)/include.mk
 
-KOMPILED := $(DEFINITION_NAME)-kompiled
-DEFINITION := $(KOMPILED)/definition.kore
+DEF ?= test
+EXT ?= $(DEF)
 
-$(DEFINITION) : $(DEFINITION_NAME).k
-	$(KOMPILE) $(KOMPILE_OPTS) $<
+DEF_DIR ?= .
+TEST_DIR ?= .
+
+DIFF ?= diff -u
+
+KOMPILED := $(TEST_DIR)/$(DEF)-kompiled
+DEF_KORE := $(KOMPILED)/definition.kore
+
+KPROVE_MODULE ?= VERIFICATION
+
+TESTS ?= $(wildcard $(TEST_DIR)/*.$(EXT))
+SEARCH_TESTS ?= \
+	$(wildcard $(TEST_DIR)/*.$(EXT).search-*) \
+	$(wildcard $(TEST_DIR)/*.$(EXT).pattern-search-*)
+PROOF_TESTS ?= $(wildcard $(TEST_DIR)/*-spec.k)
+BMC_TESTS ?= $(wildcard $(TEST_DIR)/*-spec.bmc.k)
+
+$(DEF_KORE): $(DEF).k $(K) $(KOMPILE)
+	rm -fr $(KOMPILED)
+	$(KOMPILE) $(KOMPILE_OPTS) $< -d $(DEF_DIR)
 
 # From make 3.82 news: http://cvs.savannah.gnu.org/viewvc/*checkout*/make/make/NEWS?revision=2.120
 # * WARNING: Backward-incompatibility!
@@ -19,63 +37,75 @@ $(DEFINITION) : $(DEFINITION_NAME).k
 #   patterns are preferred. To detect this feature search for 'shortest-stem'
 #   in the .FEATURES special variable.
 
-%.krun: %.$(DEFINITION_NAME) $(DEFINITION) $(KORE_EXEC)
-	$(KRUN) $(KRUN_OPTS) $<
+%.$(EXT).out: %.$(EXT) $(DEF_KORE) $(KORE_EXEC) $(K) $(KRUN)
+	$(KRUN) $(KRUN_OPTS) -d $(DEF_DIR) $< --output-file $@
+	$(DIFF) $@.golden $@
 
-%.kprove: %.k $(DEFINITION) $(KORE_EXEC)
-	$(KPROVE) $(KPROVE_OPTS) -d . -m VERIFICATION $<
+%.golden: DIFF = true
+%.golden: %
+	cp $< $@
 
-%.kmerge: %.merge $(DEFINITION) $(KORE_EXEC)
-	$(KORE_EXEC) --definition $(DEFINITION) --merge-rules $<
+%-spec.k.out: %-spec.k $(DEF_KORE) $(KORE_EXEC) $(K) $(KPROVE)
+	$(KPROVE) $(KPROVE_OPTS) -d $(DEF_DIR) -m $(KPROVE_MODULE) $< --output-file $@ || true
+	$(DIFF) $@.golden $@
 
-%.search.final.output: %.$(DEFINITION_NAME) $(DEFINITION) $(KORE_EXEC)
-	$(KRUN) $(KRUN_OPTS) $< --output-file $@ --search-final
+%.merge.out: %.merge $(DEF_KORE) $(KORE_EXEC)
+	$(KORE_EXEC) $(DEF_KORE) --module $(MODULE) --merge-rules $< --output $@
+	$(DIFF) $@.golden $@
 
-%.psearch.final.output: %.$(DEFINITION_NAME) $(DEFINITION) $(KORE_EXEC)
-	$(KRUN) $(KRUN_OPTS) $< --output-file $@ --search-final \
-	    $(foreach pat, $(wildcard $*.search.pattern), --pattern "$$(cat $(pat))")
+%.$(EXT).search-final.out: %.$(EXT).search-final $(DEF_KORE) $(KORE_EXEC) $(K) $(KRUN)
+	$(KRUN) $(KRUN_OPTS) -d $(DEF_DIR) $< --output-file $@ --search-final
+	$(DIFF) $@.golden $@
 
-%.krepl: %.k $(DEFINITION) $(KORE_REPL)
-	$(KPROVE) $(KPROVE_REPL_OPTS) -d . -m VERIFICATION $<
+%.$(EXT).search-all.out: %.$(EXT).search-all $(DEF_KORE) $(KORE_EXEC) $(K) $(KRUN)
+	$(KRUN) $(KRUN_OPTS) -d $(DEF_DIR) $< --output-file $@ --search-all
+	$(DIFF) $@.golden $@
 
-%.kscript: % $(DEFINITION) $(KORE_REPL)
-	$(KPROVE) --haskell-backend-command "$(KORE_REPL) -r --repl-script $<" -d ../.. -m VERIFICATION $(SPEC_FILE)
+%.$(EXT).search-one-or-more-steps.out: %.$(EXT).search-one-or-more-steps $(DEF_KORE) $(KORE_EXEC) $(K) $(KRUN)
+	$(KRUN) $(KRUN_OPTS) -d $(DEF_DIR) $< --output-file $@ --search-one-or-more-steps
+	$(DIFF) $@.golden $@
 
-%.search.star.output: %.$(DEFINITION_NAME) $(DEFINITION) $(KORE_EXEC)
-	$(KRUN) $(KRUN_OPTS) $< --output-file $@ --search-all
+%.$(EXT).search-one-step.out: %.$(EXT).search-one-step $(DEF_KORE) $(KORE_EXEC) $(K) $(KRUN)
+	$(KRUN) $(KRUN_OPTS) -d $(DEF_DIR) $< --output-file $@ --search-one-step
+	$(DIFF) $@.golden $@
 
-%.search.plus.output: %.$(DEFINITION_NAME) $(DEFINITION) $(KORE_EXEC)
-	$(KRUN) $(KRUN_OPTS) $< --output-file $@ --search-one-or-more-steps
+%.$(EXT).pattern-search-final.out: %.$(EXT).pattern-search-final $*.k $(DEF_KORE) $(KORE_EXEC) $(K) $(KRUN)
+	$(KRUN) $(KRUN_OPTS) -d $(DEF_DIR) $< --output-file $@ --search-final --pattern "$$(cat $*.)"
+	$(DIFF) $@.golden $@
 
-%.search.one.output: %.$(DEFINITION_NAME) $(DEFINITION) $(KORE_EXEC)
-	$(KRUN) $(KRUN_OPTS) $< --output-file $@ --search-one-step
+%.$(EXT).pattern-search-all.out: %.$(EXT).pattern-search-all $*.k $(DEF_KORE) $(KORE_EXEC) $(K) $(KRUN)
+	$(KRUN) $(KRUN_OPTS) -d $(DEF_DIR) $< --output-file $@ --search-all --pattern "$$(cat $*.k)"
+	$(DIFF) $@.golden $@
 
-%.kbmc.output: $(DEFINITION) $(KORE_EXEC)
-	$(KBMC) $(KPROVE_OPTS) --debug --raw-spec $(basename $*).k -d . -m VERIFICATION --depth $(subst ., ,$(suffix $*)) --output-file $@ || exit 0
+%.$(EXT).pattern-search-one-step.out: %.$(EXT).pattern-search-one-step $*.k $(DEF_KORE) $(KORE_EXEC) $(K) $(KRUN)
+	$(KRUN) $(KRUN_OPTS) -d $(DEF_DIR) $< --output-file $@ --search-one-step --pattern "$$(cat $*.k)"
+	$(DIFF) $@.golden $@
 
-%.output: %.$(DEFINITION_NAME) $(DEFINITION) $(KORE_EXEC)
-	$(KRUN) $(KRUN_OPTS) $< --output-file $@
+%.$(EXT).pattern-search-one-or-more-steps.out: %.$(EXT).pattern-search-one-or-more-steps $*.k $(DEF_KORE) $(KORE_EXEC) $(K) $(KRUN)
+	$(KRUN) $(KRUN_OPTS) -d $(DEF_DIR) $< --output-file $@ --search-one-or-more-steps --pattern "$$(cat $*.k)"
+	$(DIFF) $@.golden $@
 
-%.merge-output: %.merge $(DEFINITION) $(KORE_EXEC)
-	$(KORE_EXEC) $(DEFINITION) --module $(MODULE_NAME) --merge-rules $< \
-		--output $@
+%-spec.k.repl.out: %-spec.k $(DEF_KORE) $(KORE_EXEC) $(K) $(KPROVE)
+	$(KPROVE) $(KPROVE_REPL_OPTS) -d $(DEF_DIR) -m $(KPROVE_MODULE) $< --output-file $@ || true
+	$(DIFF) $@.golden $@
 
-%.repl.output: % $(DEFINITION) $(KORE_REPL)
-	$(KPROVE) --haskell-backend-command "$(KORE_REPL) -r --repl-script $<" -d ../.. -m VERIFICATION $(SPEC_FILE) --output-file $@
+%-spec.k.repl-script.out: KPROVE_REPL_OPTS = --haskell-backend-command "$(KORE_REPL) -r --repl-script $<.repl"
+%-spec.k.repl-script.out: %-spec.k $(DEF_KORE) $(KORE_EXEC) $(K) $(KPROVE)
+	$(KPROVE) $(KPROVE_REPL_OPTS) -d $(DEF_DIR) -m $(KPROVE_MODULE) $< --output-file $@ || true
+	$(DIFF) $@.golden $@
 
-%.knotprove.output: %.k $(DEFINITION) $(KORE_EXEC)
-	$(KPROVE) $(KPROVE_OPTS) -d . -m VERIFICATION $< --output-file $@ || exit 0
+%-spec.bmc.out: %-spec.k $(DEF_KORE) $(KORE_EXEC) $(K) $(KBMC)
+	$(KBMC) $(KPROVE_OPTS) --debug --raw-spec $< -d $(DEF_DIR) -m $(MODULE) --depth $(KBMC_DEPTH) --output-file $@ || exit 0
 
-%.test: %.output
-	diff -u $<.golden $<
+test: test-k
 
-%.merge-test: %.merge-output
-	diff -u $(basename $<).merge-golden $<
+OUTS = $(foreach TEST, $(TESTS) $(SEARCH_TESTS) $(PROOF_TESTS) $(BMC_TESTS), $(TEST).out)
 
-%.output.golden: %.output
-	mv $< $<.golden
+test-k: $(OUTS)
 
-%.merge-golden: %.merge-output
-	mv $< $(basename $<).merge-golden
+golden: $(foreach OUT, $(OUTS), $(OUT).golden)
 
-.PHONY: test-k test golden clean %.test %.krun
+clean:
+	rm -fr $(KOMPILED) $(TEST_DIR)/*.out
+
+.PHONY: test-k test golden clean
