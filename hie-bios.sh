abad1a8d0e5cce98803d59e1b0f311037dbdfb4d
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2020-01-13 13:45:05 -0600

    hie.yaml: Use hie-bios.sh to setup mega-component cradle
    All components of package `kore` are loaded into a single environment because
    ghcide does not yet support multi-cradle projects. (It's not even clear to me
    that multi-cradle projects are a good idea to begin with.)

---
+++ hie-bios.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+out() {
+    while [[ "$#" -gt 0 ]]
+    do
+        if [[ -n "${HIE_BIOS_OUTPUT}" ]]
+        then
+            echo "$1" >> "${HIE_BIOS_OUTPUT}"
+        else
+            echo "$1"
+        fi
+        shift
+    done
+}
+
+out -i
+out -ikore/src/
+out -ikore/test/
+out -ikore/app/share/
+out -ikore/bench/
+out -ikore/$(stack path --dist-dir)/build/autogen/
+out -clear-package-db
+out -package-db $(stack path --local-pkg-db)
+out -package-db $(stack path --snapshot-pkg-db)
+out -package-db $(stack path --global-pkg-db)
+
+yq -r '. "default-extensions" []' < ./kore/package.yaml | while read ext
+do
+    out "-X${ext}"
+done
