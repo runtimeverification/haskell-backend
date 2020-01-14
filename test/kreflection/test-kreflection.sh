24651e5c2aa44cd24b80d1284b2922bc0707176d
andreiburdusa <andrei.burdusa@runtimeverification.com>
2020-01-14 14:57:20 +0200

    wip

--- test/kreflection/test-kreflection.sh
+++ test/kreflection/test-kreflection.sh
@@ -1,8 +1,9 @@
+#!/bin/sh
 TOP=$(git rev-parse --show-toplevel)
 KRUN=$TOP/.build/k/bin/krun
-#echo $KRUN
 KORE_EXEC=$TOP/.build/kore/bin/kore-exec
 KRUN_OPTS="--haskell-backend-command \"$KORE_EXEC --disable-log-timestamps\""
-#echo $KRUN_OPTS
 COMMAND="$KRUN $KRUN_OPTS -d . 1.kreflection 2>test-kreflection.sh.out || true"
+KOMPILE="$TOP/.build/k/bin/kompile kreflection.k --backend haskell"
+eval $KOMPILE
 eval $COMMAND
