efb83dcc6526ecd708adf4e46c4d3e6b301efdfd
Thomas Tuegel <ttuegel@mailbox.org>
2020-01-28 12:05:36 -0600

    Prelude.Kore: TODO: explicit export list

--- kore/src/Prelude/Kore.hs
+++ kore/src/Prelude/Kore.hs
@@ -8,6 +8,9 @@ module Prelude.Kore
     ( module Prelude
     ) where
 
+-- TODO (thomas.tuegel): Give an explicit export list so that the generated
+-- documentation is complete.
+
 import Prelude hiding
     ( log
     )
