dca7baa2d166d7c6fa26b840f7aa3abdcc94fa42
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2020-01-31 11:29:52 -0600

    Prelude.Kore: Export utilities from Data.Maybe

--- kore/src/Prelude/Kore.hs
+++ kore/src/Prelude/Kore.hs
@@ -6,11 +6,19 @@ License   : NCSA
 
 module Prelude.Kore
     ( module Prelude
+    , isJust
+    , isNothing
+    , fromMaybe
     ) where
 
 -- TODO (thomas.tuegel): Give an explicit export list so that the generated
 -- documentation is complete.
 
+import Data.Maybe
+    ( fromMaybe
+    , isJust
+    , isNothing
+    )
 import Prelude hiding
     ( log
     )
