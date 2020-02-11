612d10247da6df126925a5f4ebb0813d703fdbce
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2020-02-11 15:25:09 -0600

    Kore.Step.EquationalStep: Avoid side condition's free variables

--- kore/src/Kore/Step/EquationalStep.hs
+++ kore/src/Kore/Step/EquationalStep.hs
@@ -385,8 +385,7 @@ applyRulesSequence sideCondition initial rules =
     matchRules sideCondition initial rules'
     >>= finalizeRulesSequence sideCondition initial
   where
-    -- TODO (thomas.tuegel): Include freeVariables sideCondition
-    avoidConfigVars = freeVariables initial
+    avoidConfigVars = freeVariables sideCondition <> freeVariables initial
     rules' = EqualityPattern.targetRuleVariables avoidConfigVars <$> rules
 
 -- | Matches a list a rules against a configuration. See 'matchRule'.
