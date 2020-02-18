f007f0dddf0f010b527b60949505f3bc9bc0e2a9
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2020-02-18 14:15:33 -0600

    test_parseBase16

--- kore/test/Test/Kore/Builtin/Encoding.hs
+++ kore/test/Test/Kore/Builtin/Encoding.hs
@@ -1,5 +1,6 @@
 module Test.Kore.Builtin.Encoding
     ( test_decodeEncode
+    , test_parseBase16
     ) where
 
 import Prelude.Kore
@@ -12,11 +13,19 @@ import qualified Hedgehog.Range as Range
 import Test.Tasty
 import Test.Tasty.Hedgehog
 
+import qualified Data.ByteString as ByteString
 import Data.Text
     ( Text
     )
+import qualified Data.Text as Text
+import Data.Word
+    ( Word8
+    )
+import qualified Text.Megaparsec as Parsec
 
 import Kore.Builtin.Encoding
+
+import Test.Tasty.HUnit.Ext
 
 genString :: Gen Text
 genString = Gen.text (Range.linear 0 256) Gen.latin1
@@ -26,3 +35,28 @@ test_decodeEncode =
     testProperty "∀ t. decode (encode t) = t)" . property $ do
         str <- forAll genString
         (===) str (decode8Bit (encode8Bit str))
+
+test_parseBase16 :: [TestTree]
+test_parseBase16 =
+    [ valid "" []
+    , valid "00" [0x00]
+    , valid "ff" [0xff]
+
+    , invalid "0"
+    , invalid "fg"
+    ]
+  where
+    valid :: HasCallStack => String -> [Word8] -> TestTree
+    valid (Text.pack -> input) (ByteString.pack -> expect) =
+        testCase ("parseBase16 " <> show input)
+        $ either unexpected expected
+        $ Parsec.parse parseBase16 "<test>" input
+      where
+        unexpected = error . Parsec.errorBundlePretty
+        expected = assertEqual "" expect
+
+    invalid :: HasCallStack => String -> TestTree
+    invalid (Text.pack -> input) =
+        testCase ("parseBase16 " <> show input) $ do
+            let result = Parsec.parse parseBase16 "<test>" input
+            assertBool "expected parse error" (isLeft result)
