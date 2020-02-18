93ecdc89c770b8d4a0bf55d1cab33025f5916d11
ana-pantilie <ana.pantilie95@gmail.com>
2020-02-18 14:40:30 +0200

    Add tests

--- kore/test/Test/Kore/Internal/Substitution.hs
+++ kore/test/Test/Kore/Internal/Substitution.hs
@@ -25,6 +25,10 @@ import Kore.TopBottom
     ( isBottom
     , isTop
     )
+import Kore.Variables.Target
+    ( mkElementNonTarget
+    , mkElementTarget
+    )
 import Kore.Variables.UnifiedVariable
     ( UnifiedVariable (..)
     )
@@ -46,7 +50,7 @@ test_substitution =
     , nullTests
     , variablesTests
     , propertyTests
-    -- , reverseRhsTests
+    , orderRenameAndRenormalizeTODOTests
     ]
 
 propertyTests:: TestTree
@@ -206,86 +210,106 @@ variablesTests =
            . variables $ wrap singletonSubst
     ]
 
--- reverseRhsTests :: TestTree
--- reverseRhsTests =
---     testGroup "Reverse RHS if equal to variable"
---     [ testCase "empty subst unchanged"
---         $ assertEqual ""
---             emptySubst
---             (reverseIfRhsIsVar (ElemVar Mock.x) emptySubst)
---     , testCase "unnormalized without RHS unchanged" $ do
---         let
---             subst = wrap [(ElemVar Mock.x, Mock.a)]
---         assertEqual ""
---             subst
---             (reverseIfRhsIsVar (ElemVar Mock.x) subst)
---     , testCase "normalized without RHS unchanged" $ do
---         let
---             subst = unsafeWrap [(ElemVar Mock.x, Mock.a)]
---         assertEqual ""
---             subst
---             (reverseIfRhsIsVar (ElemVar Mock.x) subst)
---     , testCase "unnormalized reverses RHS" $ do
---         let
---             expectedSubst = wrap [(ElemVar Mock.x, mkElemVar Mock.y)]
---             originalSubst = wrap [(ElemVar Mock.y, mkElemVar Mock.x)]
---         assertEqual ""
---             expectedSubst
---             (reverseIfRhsIsVar (ElemVar Mock.x) originalSubst)
---     , testCase "normalized reverses RHS" $ do
---         let
---             expectedSubst = unsafeWrap [(ElemVar Mock.x, mkElemVar Mock.y)]
---             originalSubst = unsafeWrap [(ElemVar Mock.y, mkElemVar Mock.x)]
---         assertEqual ""
---             expectedSubst
---             (reverseIfRhsIsVar (ElemVar Mock.x) originalSubst)
---     , testCase "unnormalized reverses multiple RHS" $ do
---         let
---             expectedSubst = wrap
---                 [(ElemVar Mock.x, mkElemVar Mock.y), (ElemVar Mock.x, mkElemVar Mock.z)]
---             originalSubst = wrap
---                 [(ElemVar Mock.y, mkElemVar Mock.x), (ElemVar Mock.z, mkElemVar Mock.x)]
---         assertEqual ""
---             expectedSubst
---             (reverseIfRhsIsVar (ElemVar Mock.x) originalSubst)
---     , testCase "normalized reverses multiple RHS" $ do
---         let
---             expectedSubst = unsafeWrap
---                 [(ElemVar Mock.x, mkElemVar Mock.z), (ElemVar Mock.y, mkElemVar Mock.z)]
---             originalSubst = unsafeWrap
---                 [(ElemVar Mock.y, mkElemVar Mock.x), (ElemVar Mock.z, mkElemVar Mock.x)]
---         assertEqual ""
---             expectedSubst
---             (reverseIfRhsIsVar (ElemVar Mock.x) originalSubst)
---     , testCase "unnormalized does not substitute reverse RHS" $ do
---         let
---             expectedSubst = wrap
---                 [ (ElemVar Mock.x, mkElemVar Mock.y)
---                 , (ElemVar Mock.z, Mock.f (mkElemVar Mock.x))
---                 ]
---             originalSubst = wrap
---                 [ (ElemVar Mock.y, mkElemVar Mock.x)
---                 , (ElemVar Mock.z, Mock.f (mkElemVar Mock.x))
---                 ]
---         assertEqual ""
---             expectedSubst
---             (reverseIfRhsIsVar (ElemVar Mock.x) originalSubst)
---     , testCase "normalized substitutes reverse RHS" $ do
---         let
---             expectedSubst = unsafeWrap
---                 [ (ElemVar Mock.x, mkElemVar Mock.z)
---                 , (ElemVar Mock.y, mkElemVar Mock.z)
---                 , (ElemVar Mock.var_x_1, Mock.f (mkElemVar Mock.z))
---                 ]
---             originalSubst = unsafeWrap
---                 [ (ElemVar Mock.y, mkElemVar Mock.x)
---                 , (ElemVar Mock.z, mkElemVar Mock.x)
---                 , (ElemVar Mock.var_x_1, Mock.f (mkElemVar Mock.x))
---                 ]
---         assertEqual ""
---             expectedSubst
---             (reverseIfRhsIsVar (ElemVar Mock.x) originalSubst)
---     ]
+orderRenameAndRenormalizeTODOTests :: TestTree
+orderRenameAndRenormalizeTODOTests =
+    testGroup "Reverse RHS if equal to variable"
+    [ testCase "empty subst unchanged"
+        $ assertEqual ""
+            emptySubst
+            (orderRenameAndRenormalizeTODO (ElemVar Mock.x) emptySubst)
+    , testCase "unnormalized without RHS unchanged" $ do
+        let
+            subst = wrap [(ElemVar Mock.x, Mock.a)]
+        assertEqual ""
+            subst
+            (orderRenameAndRenormalizeTODO (ElemVar Mock.x) subst)
+    , testCase "normalized without RHS unchanged" $ do
+        let
+            subst = unsafeWrap [(ElemVar Mock.x, Mock.a)]
+        assertEqual ""
+            subst
+            (orderRenameAndRenormalizeTODO (ElemVar Mock.x) subst)
+    , testCase "unnormalized reverses RHS" $ do
+        let
+            expectedSubst = wrap [(targetVarX, nonTargetPattY)]
+            originalSubst = wrap [(nonTargetVarY, targetPattX)]
+        assertEqual ""
+            expectedSubst
+            ( orderRenameAndRenormalizeTODO
+                targetVarX
+                originalSubst
+            )
+    , testCase "unnormalized does not reverse RHS" $ do
+        let
+            expectedSubst = wrap [(targetVarX, nonTargetPattY)]
+            originalSubst = wrap [(targetVarX, nonTargetPattY)]
+        assertEqual ""
+            expectedSubst
+            ( orderRenameAndRenormalizeTODO
+                targetVarX
+                originalSubst
+            )
+    , testCase "normalized reverses RHS" $ do
+        let
+            expectedSubst = unsafeWrap [(ElemVar Mock.x, mkElemVar Mock.y)]
+            originalSubst = unsafeWrap [(ElemVar Mock.y, mkElemVar Mock.x)]
+        assertEqual ""
+            expectedSubst
+            (orderRenameAndRenormalizeTODO (ElemVar Mock.x) originalSubst)
+    , testCase "unnormalized reverses multiple RHS" $ do
+        let
+            expectedSubst = wrap
+                [ (targetVarX, nonTargetPattY), (targetVarX, nonTargetPattZ) ]
+            originalSubst = wrap
+                [ (nonTargetVarY, targetPattX), (nonTargetVarZ, targetPattX) ]
+        assertEqual ""
+            expectedSubst
+            (orderRenameAndRenormalizeTODO targetVarX originalSubst)
+    , testCase "normalized reverses multiple RHS" $ do
+        let
+            expectedSubst = unsafeWrap
+                [(ElemVar Mock.x, mkElemVar Mock.z), (ElemVar Mock.y, mkElemVar Mock.z)]
+            originalSubst = unsafeWrap
+                [(ElemVar Mock.y, mkElemVar Mock.x), (ElemVar Mock.z, mkElemVar Mock.x)]
+        assertEqual ""
+            expectedSubst
+            (orderRenameAndRenormalizeTODO (ElemVar Mock.x) originalSubst)
+    , testCase "unnormalized does not substitute reverse RHS" $ do
+        let
+            expectedSubst = wrap
+                [ (ElemVar Mock.x, mkElemVar Mock.y)
+                , (ElemVar Mock.z, Mock.f (mkElemVar Mock.x))
+                ]
+            originalSubst = wrap
+                [ (ElemVar Mock.y, mkElemVar Mock.x)
+                , (ElemVar Mock.z, Mock.f (mkElemVar Mock.x))
+                ]
+        assertEqual ""
+            expectedSubst
+            (orderRenameAndRenormalizeTODO (ElemVar Mock.x) originalSubst)
+    , testCase "normalized substitutes reverse RHS" $ do
+        let
+            expectedSubst = unsafeWrap
+                [ (ElemVar Mock.x, mkElemVar Mock.z)
+                , (ElemVar Mock.y, mkElemVar Mock.z)
+                , (ElemVar Mock.var_x_1, Mock.f (mkElemVar Mock.z))
+                ]
+            originalSubst = unsafeWrap
+                [ (ElemVar Mock.y, mkElemVar Mock.x)
+                , (ElemVar Mock.z, mkElemVar Mock.x)
+                , (ElemVar Mock.var_x_1, Mock.f (mkElemVar Mock.x))
+                ]
+        assertEqual ""
+            expectedSubst
+            (orderRenameAndRenormalizeTODO (ElemVar Mock.x) originalSubst)
+    ]
+  where
+    targetVarX = ElemVar . mkElementTarget $ Mock.x
+    targetPattX = mkElemVar . mkElementTarget $ Mock.x
+    nonTargetVarY = ElemVar . mkElementNonTarget $ Mock.y
+    nonTargetPattY = mkElemVar . mkElementNonTarget $ Mock.y
+    nonTargetVarZ = ElemVar . mkElementNonTarget $ Mock.z
+    nonTargetPattZ = mkElemVar . mkElementNonTarget $ Mock.z
 
 emptyRawSubst :: [(UnifiedVariable Variable, TermLike Variable)]
 emptyRawSubst = mempty
