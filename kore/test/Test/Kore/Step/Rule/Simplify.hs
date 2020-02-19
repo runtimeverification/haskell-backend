e9a5a3e64b0c88876467b69e4bc70acab2c45c4f
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2020-02-19 16:23:38 -0600

    test_simplifyClaimRule: Check for side condition during evaluation

--- kore/test/Test/Kore/Step/Rule/Simplify.hs
+++ kore/test/Test/Kore/Step/Rule/Simplify.hs
@@ -1,27 +1,37 @@
 module Test.Kore.Step.Rule.Simplify
     ( test_simplifyRule
+    , test_simplifyClaimRule
     ) where
 
 import Prelude.Kore
 
 import Test.Tasty
 
+import qualified Control.Lens as Lens
 import Data.Default
     ( def
     )
+import Data.Generics.Product
+    ( field
+    )
 
+import qualified Kore.Internal.Condition as Condition
 import qualified Kore.Internal.MultiAnd as MultiAnd
     ( extractPatterns
     )
+import qualified Kore.Internal.OrPattern as OrPattern
 import Kore.Internal.Predicate
     ( Predicate
     , makeAndPredicate
     , makeCeilPredicate
+    , makeEqualsPredicate
     , makeEqualsPredicate_
     , makeNotPredicate
     , makeTruePredicate
     , makeTruePredicate_
     )
+import qualified Kore.Internal.Predicate as Predicate
+import qualified Kore.Internal.SideCondition as SideCondition
 import Kore.Internal.TermLike
     ( TermLike
     , mkAnd
@@ -33,19 +43,29 @@ import Kore.Internal.TermLike
 import Kore.Log
     ( emptyLogger
     )
+import Kore.Sort
+    ( predicateSort
+    )
 import Kore.Step.Rule.Simplify
 import Kore.Step.RulePattern
-    ( OnePathRule (OnePathRule)
+    ( OnePathRule (..)
     , RHS (..)
     , RulePattern (RulePattern)
+    , rulePattern
     )
 import qualified Kore.Step.RulePattern as Rule.DoNotUse
 import Kore.Step.Simplification.Data
-    ( runSimplifier
+    ( Env (..)
+    , runSimplifier
+    )
+import Kore.Step.Simplification.Simplify
+    ( emptyConditionSimplifier
+    , termLikeSimplifier
     )
 import qualified Kore.Step.SMT.Declaration.All as SMT.All
 import Kore.Syntax.Variable
     ( Variable
+    , fromVariable
     )
 import qualified SMT
 
@@ -223,3 +243,76 @@ runSimplifyRule rule =
     $ runSimplifier Mock.env $ do
         SMT.All.declare Mock.smtDeclarations
         simplifyRuleLhs rule
+
+test_simplifyClaimRule :: [TestTree]
+test_simplifyClaimRule =
+    [ test "infers definedness"
+        rule1
+        [rule1']
+    , test "includes side condition"
+        rule2
+        [rule2']
+    ]
+  where
+    rule1, rule2, rule2' :: RulePattern Variable
+    rule1 = rulePattern (Mock.f Mock.a) Mock.b
+    rule1' = rule1 & requireDefined
+    rule2 =
+        rulePattern @Variable (Mock.g Mock.a) Mock.b
+        & Lens.set (field @"requires") requires
+    rule2' =
+        rule2
+        & requireDefined
+        & Lens.set (field @"left") (Mock.f Mock.a)
+
+    requires :: Predicate Variable
+    requires = makeEqualsPredicate Mock.testSort Mock.a Mock.b
+
+    requireDefined rule =
+        Lens.over
+            (field @"requires")
+            (flip makeAndPredicate
+                (makeCeilPredicate sort left)
+            )
+            rule
+      where
+        left = Lens.view (field @"left") rule
+        sort = termLikeSort left
+
+    test
+        :: HasCallStack
+        => TestName
+        -> RulePattern Variable
+        -> [RulePattern Variable]
+        -> TestTree
+    test name (OnePathRule -> input) (map OnePathRule -> expect) =
+        -- Test simplifyClaimRule through the OnePathRule instance.
+        testCase name $ do
+            actual <- run $ simplifyRuleLhs input
+            assertEqual "" expect (MultiAnd.extractPatterns actual)
+      where
+        run = SMT.runNoSMT emptyLogger . runSimplifier env
+        env =
+            Mock.env
+                { simplifierTermLike
+                , simplifierCondition = emptyConditionSimplifier
+                , simplifierAxioms = mempty
+                }
+        simplifierTermLike = termLikeSimplifier $ \sideCondition termLike -> do
+            let rule = getOnePathRule input
+                left = Lens.view (field @"left") rule
+                sort = termLikeSort left
+                expectSideCondition =
+                    makeAndPredicate requires (makeCeilPredicate sort left)
+                    & Predicate.mapVariables
+                        (fmap fromVariable)
+                        (fmap fromVariable)
+                    & Predicate.coerceSort predicateSort
+                    & Condition.fromPredicate
+                    & SideCondition.fromCondition
+            return . OrPattern.fromTermLike
+                $ if
+                    (termLike == Mock.g Mock.a)
+                    && (sideCondition == expectSideCondition)
+                    then Mock.f Mock.a
+                    else termLike
