12ca2dcadd88d252be954ff8e2fb819414d80bc5
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2020-02-27 14:48:09 -0600

    Changed

---
+++ kore/src/Changed.hs
@@ -0,0 +1,39 @@
+{- |
+Copyright   : (c) Runtime Verification, 2020
+License     : NCSA
+
+-}
+module Changed
+    ( Changed (..)
+    ) where
+
+import Prelude.Kore
+
+{- | @'Changed' thing@ carries a @thing@ and a marker.
+ -}
+data Changed thing
+    = Unchanged !thing
+    -- ^ The @thing@ was not changed.
+    | Changed   !thing
+    -- ^ The @thing@ was changed.
+    deriving (Eq, Functor, Show)
+
+instance Applicative Changed where
+    pure = Unchanged
+
+    Unchanged f <*> a = fmap f a
+    Changed   f <*> a = Changed (f $ extract a)
+
+instance Comonad Changed where
+    extract (Unchanged a) = a
+    extract (Changed   a) = a
+
+    extend f changed@(Unchanged _) = Unchanged (f changed)
+    extend f changed@(Changed   _) = Changed   (f changed)
+
+    duplicate changed@(Unchanged _) = Unchanged changed
+    duplicate changed@(Changed   _) = Changed   changed
+
+instance Monad Changed where
+    Unchanged a >>= f = f a
+    Changed a   >>= f = Changed $ extract $ f a
