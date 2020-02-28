d3f0e2aea722b17e490c38f7ef60d60b78a2f3cc
andreiburdusa <andrei.burdusa@zoho.com>
2020-02-28 16:58:16 +0200

    wip

--- kore/test/Test/Kore/Attribute/Axiom/Concrete.hs
+++ kore/test/Test/Kore/Attribute/Axiom/Concrete.hs
@@ -32,7 +32,7 @@ parseConcrete (Attributes attrs) =
 test_concrete :: TestTree
 test_concrete =
     testCase "[concrete{}()] :: Concrete"
-        $ expectSuccess Concrete { freeVariables = FreeVariables.freeVariables concreteAttribute }
+        $ expectSuccess Concrete { freeVariables = def }
         $ parseConcrete $ Attributes [ concreteAttribute ]
 
 test_Attributes :: TestTree
