23a4c794e794eb4275a007fbf81b4c740972952a
Thomas Tuegel <ttuegel@mailbox.org>
2020-03-26 20:01:07 -0500

    Kore.Equation.Equation (mapVariables, refreshVariables)

--- kore/src/Kore/Equation/Equation.hs
+++ kore/src/Kore/Equation/Equation.hs
@@ -7,6 +7,8 @@ License     : NCSA
 module Kore.Equation.Equation
     ( Equation (..)
     , mkEquation
+    , mapVariables
+    , refreshVariables
     ) where
 
 import Prelude.Kore
@@ -15,13 +17,17 @@ import Control.DeepSeq
     ( NFData
     )
 import qualified Data.Default as Default
+import qualified Data.Map.Strict as Map
 import qualified Generics.SOP as SOP
 import qualified GHC.Generics as GHC
 
 import Debug
 import qualified Kore.Attribute.Axiom as Attribute
-    ( Axiom
+import Kore.Attribute.Pattern.FreeVariables
+    ( FreeVariables
+    , HasFreeVariables (..)
     )
+import qualified Kore.Attribute.Pattern.FreeVariables as FreeVariables
 import Kore.Internal.Predicate
     ( Predicate
     )
@@ -30,14 +36,23 @@ import Kore.Internal.Symbol
     ( Symbol
     )
 import Kore.Internal.TermLike
-    ( InternalVariable
+    ( ElementVariable
+    , InternalVariable
+    , SetVariable
     , TermLike
     , Variable
     )
 import qualified Kore.Internal.TermLike as TermLike
+import Kore.Step.Step
+    ( Renaming
+    )
 import Kore.TopBottom
 import Kore.Unparser
     ( Unparse (..)
+    )
+import qualified Kore.Variables.Fresh as Fresh
+import Kore.Variables.UnifiedVariable
+    ( UnifiedVariable (..)
     )
 import Pretty
     ( Pretty (..)
@@ -141,3 +156,72 @@ instance
             )
       where
         Equation { left, requires, right, ensures } = equation
+
+instance
+    InternalVariable variable
+    => HasFreeVariables (Equation variable) variable
+  where
+    freeVariables rule@(Equation _ _ _ _ _) = case rule of
+        Equation { left, requires, right, ensures } ->
+            freeVariables left
+            <> freeVariables requires
+            <> freeVariables right
+            <> freeVariables ensures
+
+mapVariables
+    :: (Ord variable1, InternalVariable variable2)
+    => (ElementVariable variable1 -> ElementVariable variable2)
+    -> (SetVariable variable1 -> SetVariable variable2)
+    -> Equation variable1 -> Equation variable2
+mapVariables mapElemVar mapSetVar equation@(Equation _ _ _ _ _) =
+    equation
+        { requires = mapPredicateVariables requires
+        , left = mapTermLikeVariables left
+        , right = mapTermLikeVariables right
+        , ensures = mapPredicateVariables ensures
+        , attributes =
+            Attribute.mapAxiomVariables mapElemVar mapSetVar attributes
+        }
+  where
+    Equation { requires, left, right, ensures, attributes } = equation
+    mapTermLikeVariables = TermLike.mapVariables mapElemVar mapSetVar
+    mapPredicateVariables = Predicate.mapVariables mapElemVar mapSetVar
+
+refreshVariables
+    :: InternalVariable variable
+    => FreeVariables variable
+    -> Equation variable
+    -> (Renaming variable, Equation variable)
+refreshVariables
+    (FreeVariables.getFreeVariables -> avoid)
+    equation@(Equation _ _ _ _ _)
+  =
+    let rename = Fresh.refreshVariables avoid originalFreeVariables
+        mapElemVars elemVar =
+            case Map.lookup (ElemVar elemVar) rename of
+                Just (ElemVar elemVar') -> elemVar'
+                _ -> elemVar
+        mapSetVars setVar =
+            case Map.lookup (SetVar setVar) rename of
+                Just (SetVar setVar') -> setVar'
+                _ -> setVar
+        subst = TermLike.mkVar <$> rename
+        left' = TermLike.substitute subst left
+        requires' = Predicate.substitute subst requires
+        right' = TermLike.substitute subst right
+        ensures' = Predicate.substitute subst ensures
+        attributes' =
+            Attribute.mapAxiomVariables mapElemVars mapSetVars attributes
+        equation' =
+            equation
+                { left = left'
+                , requires = requires'
+                , right = right'
+                , ensures = ensures'
+                , attributes = attributes'
+                }
+    in (rename, equation')
+  where
+    Equation { left, requires, right, ensures, attributes } = equation
+    originalFreeVariables =
+        FreeVariables.getFreeVariables $ freeVariables equation
