20581f5adfbc9bd1ebe2f8a878e26fae7df9c2f0
Thomas Tuegel <ttuegel@mailbox.org>
2020-03-28 16:05:57 -0500

    test_fromSentenceAxiom: Test \implies axiom matching

--- kore/test/Test/Kore/Equation/Sentence.hs
+++ kore/test/Test/Kore/Equation/Sentence.hs
@@ -11,6 +11,9 @@ import Data.Default
     )
 
 import Kore.Equation
+import Kore.Internal.Predicate
+    ( wrapPredicate
+    )
 import Kore.Internal.TermLike
 
 import Test.Expect
@@ -20,17 +23,32 @@ import Test.Tasty.HUnit.Ext
 
 test_fromSentenceAxiom :: [TestTree]
 test_fromSentenceAxiom =
-    [ testCase "\\ceil(I1 / I2)" $ do
+    [ testCase "⌈I1 / I2⌉" $ do
         let term = Mock.tdivInt varI1 varI2
             original = mkAxiom [sortVariableR] $ mkCeil sortR term
             expect = mkEquation (mkCeil sortR term) (mkTop sortR)
         actual <- expectRight $ fromSentenceAxiom (def, original)
         assertEqual "" expect actual
-    , testCase "\\equals(I1 < I2, I2 >= I1)" $ do
+    , testCase "I1 < I2 = I2 >= I1" $ do
         let left = Mock.lessInt varI1 varI2
             right = Mock.greaterEqInt varI2 varI1
             original = mkAxiom [sortVariableR] $ mkEquals sortR left right
             expect = mkEquation left right
+        actual <- expectRight $ fromSentenceAxiom (def, original)
+        assertEqual "" expect actual
+    , testCase "⌈f(x))⌉ → f(x) = g(x) ∧ ⌈h(x)⌉" $ do
+        let requires = mkCeil sortR (Mock.f Mock.a)
+            ensures = mkCeil sortR (Mock.h Mock.b)
+            left = Mock.f (mkElemVar Mock.x)
+            right = Mock.g (mkElemVar Mock.x)
+            original =
+                mkAxiom [sortVariableR]
+                $ mkImplies requires
+                $ mkAnd (mkEquals sortR left right) ensures
+            expect = (mkEquation left right)
+                { requires = wrapPredicate requires
+                , ensures = wrapPredicate ensures
+                }
         actual <- expectRight $ fromSentenceAxiom (def, original)
         assertEqual "" expect actual
     ]
