198391be3d2ec42bc6b89eafcbd5034f7d82c876
Thomas Tuegel <ttuegel@mailbox.org>
2020-03-28 14:18:45 -0500

    Kore.Equation

---
+++ kore/src/Kore/Equation.hs
@@ -0,0 +1,11 @@
+{- |
+Copyright   : (c) Runtime Verification, 2020
+License     : NCSA
+
+-}
+
+module Kore.Equation
+    ( module Kore.Equation.Equation
+    ) where
+
+import Kore.Equation.Equation
