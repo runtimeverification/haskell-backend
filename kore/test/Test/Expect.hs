32d73c611ed3ff5f05bf50863038c64fc610cb6f
Thomas Tuegel <ttuegel@mailbox.org>
2020-03-29 19:44:51 -0500

    Test.Expect (expectLeft)

--- kore/test/Test/Expect.hs
+++ kore/test/Test/Expect.hs
@@ -1,5 +1,6 @@
 module Test.Expect
     ( expectRight
+    , expectLeft
     ) where
 
 import Prelude.Kore
@@ -10,3 +11,6 @@ import Test.Tasty.HUnit.Ext
 
 expectRight :: Debug left => Either left right -> IO right
 expectRight = either (assertFailure . show . debug) return
+
+expectLeft :: Debug right => Either left right -> IO left
+expectLeft = either return (assertFailure . show . debug)
