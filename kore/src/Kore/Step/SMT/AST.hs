b9a168a40dc244d01b431fff783bb6bfc002fcea
andreiburdusa <andrei.burdusa@zoho.com>
2020-04-03 18:16:29 +0300

    Remove appendToEncoding

--- kore/src/Kore/Step/SMT/AST.hs
+++ kore/src/Kore/Step/SMT/AST.hs
@@ -8,7 +8,7 @@ Maintainer  : virgil.serbanuta@runtimeverification.com
 
 module Kore.Step.SMT.AST
     ( Declarations (..)
-    , Encodable (AlreadyEncoded)
+    , Encodable (..)
     , IndirectSymbolDeclaration (..)
     , KoreSortDeclaration (..)
     , KoreSymbolDeclaration (..)
@@ -31,7 +31,6 @@ module Kore.Step.SMT.AST
     , UnresolvedSort
     , UnresolvedSortDeclaration
     , UnresolvedSymbol
-    , appendToEncoding
     , encodable
     , encode
     , mergePreferFirst
@@ -327,10 +326,6 @@ encodable Kore.Id {getId} = Encodable getId
 encode :: Encodable -> Text
 encode (AlreadyEncoded e) = e
 encode (Encodable e) = encodeName e
-
-appendToEncoding :: Encodable -> Text -> Encodable
-appendToEncoding (AlreadyEncoded e) t = AlreadyEncoded (e <> t)
-appendToEncoding (Encodable e) t = Encodable (e <> t)
 
 mergePreferFirst
     :: Declarations sort symbol name
