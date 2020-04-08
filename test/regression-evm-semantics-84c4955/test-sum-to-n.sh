e95961d01c65254d6621b7df25f1a8974211931e
ana-pantilie <ana.pantilie95@gmail.com>
2020-04-08 14:21:03 +0300

    Re-generate evm-semantics regression tests

---
+++ test/regression-evm-semantics-84c4955/test-sum-to-n.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+RV-K version 1.0-SNAPSHOT Git revision: d79861b Git branch: UNKNOWN Build date: Tue Apr 07 00:14:34 EEST 2020 $KORE_EXEC test-sum-to-n-vdefinition.kore --module VERIFICATION --prove test-sum-to-n-spec.kore --spec-module SUM-TO-N-SPEC "$@"
