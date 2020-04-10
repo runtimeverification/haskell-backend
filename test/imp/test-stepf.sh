5167977a8b1fbb349046cede1481d836930a3529
klamathix <mihai@journey.localdomain>
2020-04-10 15:59:31 +0300

    Use new repl-script make rule.

--- test/imp/test-stepf.sh
+++ test/imp/test-stepf.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-IGNORE=$($KPROVE --haskell-backend-command "$KORE_REPL -r --repl-script run-stepf-repl-script-spec.k.repl" -d . -m VERIFICATION sum-spec.k)
+#IGNORE=$($KPROVE --haskell-backend-command "$KORE_REPL -r --repl-script run-stepf-repl-script-spec.k.repl" -d . -m VERIFICATION sum-spec.k)
+IGNORE=$(make sum-spec.k.run-repl-script)
 
 if grep -q "Config at node 10" shouldBeAt10.out; then
     if grep -q "Config at node 18" shouldBeAt18.out; then
-        diff finished.out finished.out.golden
+        cat finished.out
     else
         echo "Current node is not equal to expected value 18 after second call to stepf."
     fi
@@ -12,6 +13,4 @@ else
     echo "Current node is not equal to expected value 10 after first call to stepf."
 fi
 
-rm shouldBeAt10.out
-rm shouldBeAt18.out
-rm finished.out
+rm shouldBeAt10.out shouldBeAt18.out
