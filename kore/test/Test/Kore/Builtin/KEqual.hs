d35c660926c2e6b7b43d43600bb1baf667ea8399
ana-pantilie <ana.pantilie95@gmail.com>
2020-04-21 16:24:07 +0300

    Fix KEqual unit tests

--- kore/test/Test/Kore/Builtin/KEqual.hs
+++ kore/test/Test/Kore/Builtin/KEqual.hs
@@ -55,6 +55,30 @@ test_KEqual =
         actual <- evaluate original
         assertEqual' "" expect actual
 
+    , testCaseWithSMT "kseq(x, dotk) equals kseq(x, dotk)" $ do
+        let expect = Pattern.top
+            original =
+                mkEquals_
+                    (Test.Bool.asInternal True)
+                    (keqBool
+                        (kseq (mkElemVar (elemVarS "x" kItemSort)) dotk)
+                        (kseq (mkElemVar (elemVarS "x" kItemSort)) dotk)
+                    )
+        actual <- evaluate original
+        assertEqual' "" expect actual
+
+    , testCaseWithSMT "kseq(inj(x), dotk) equals kseq(inj(x), dotk)" $ do
+        let expect = Pattern.fromTermLike $ Test.Bool.asInternal True
+            original =
+                mkAnd
+                    (Test.Bool.asInternal True)
+                    (keqBool
+                        (kseq (inj kItemSort (mkElemVar (elemVarS "x" idSort))) dotk)
+                        (kseq (inj kItemSort (mkElemVar (elemVarS "x" idSort))) dotk)
+                    )
+        actual <- evaluate original
+        assertEqual' "" expect actual
+
     , testCaseWithSMT "distinct domain values" $ do
         let expect = Pattern.fromTermLike $ Test.Bool.asInternal False
             original = keqBool (inj kSort dvT) (inj kSort dvX)
