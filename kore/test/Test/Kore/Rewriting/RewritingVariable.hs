cba40c36ad9c60e38d4582e5519dde5c123513c8
Thomas Tuegel <ttuegel@mailbox.org>
2020-04-27 13:53:43 -0500

    test_SortedVariable_RewritingVariable

--- kore/test/Test/Kore/Rewriting/RewritingVariable.hs
+++ kore/test/Test/Kore/Rewriting/RewritingVariable.hs
@@ -1,6 +1,7 @@
 module Test.Kore.Rewriting.RewritingVariable
     ( test_FreshPartialOrd_RewritingVariable
     , test_FreshPartialOrd_UnifiedVariable_RewritingVariable
+    , test_SortedVariable_RewritingVariable
     ) where
 
 import Prelude.Kore
@@ -9,15 +10,35 @@ import Hedgehog
 import qualified Hedgehog.Gen as Gen
 import Test.Tasty
 
+import qualified Control.Lens as Lens
+
 import Kore.Rewriting.RewritingVariable
+import Kore.Syntax.Variable
 import Kore.Variables.UnifiedVariable
 import Pair
 
+import Test.Kore
+    ( testId
+    )
+import qualified Test.Kore.Step.MockSymbols as Mock
 import Test.Kore.Variables.Fresh
     ( relatedUnifiedVariableGen
     , relatedVariableGen
     , testFreshPartialOrd
     )
+import Test.Tasty.HUnit.Ext
+
+test_SortedVariable_RewritingVariable :: [TestTree]
+test_SortedVariable_RewritingVariable =
+    [ testCase "over lensVariableSort id === id -- RuleVariable" $ do
+        let input = mkRuleVariable variable
+        assertEqual "" input (Lens.over lensVariableSort id input)
+    , testCase "over lensVariableSort id === id -- ConfigVariable" $ do
+        let input = mkConfigVariable variable
+        assertEqual "" input (Lens.over lensVariableSort id input)
+    ]
+  where
+    variable = Variable (testId "x") mempty Mock.testSort
 
 test_FreshPartialOrd_RewritingVariable :: TestTree
 test_FreshPartialOrd_RewritingVariable =
