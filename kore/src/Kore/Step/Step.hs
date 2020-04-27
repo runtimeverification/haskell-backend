97e188745382423b457ec49b0751a5cf86dbf92a
Thomas Tuegel <ttuegel@mailbox.org>
2020-04-26 20:49:50 -0500

    Extract Kore.Rewriting.UnifyingRule

--- kore/src/Kore/Step/Step.hs
+++ kore/src/Kore/Step/Step.hs
@@ -36,9 +36,6 @@ module Kore.Step.Step
 import Prelude.Kore
 
 import qualified Data.Foldable as Foldable
-import Data.Map.Strict
-    ( Map
-    )
 import qualified Data.Map.Strict as Map
 import Data.Set
     ( Set
@@ -51,8 +48,7 @@ import Branch
     )
 import qualified Branch
 import Kore.Attribute.Pattern.FreeVariables
-    ( FreeVariables (..)
-    , HasFreeVariables (freeVariables)
+    ( HasFreeVariables (freeVariables)
     )
 import qualified Kore.Attribute.Pattern.FreeVariables as FreeVariables
 import Kore.Internal.Condition
@@ -71,9 +67,6 @@ import Kore.Internal.Pattern
     ( Pattern
     )
 import qualified Kore.Internal.Pattern as Pattern
-import Kore.Internal.Predicate
-    ( Predicate
-    )
 import Kore.Internal.SideCondition
     ( SideCondition
     )
@@ -83,15 +76,13 @@ import qualified Kore.Internal.SideCondition as SideCondition
     )
 import qualified Kore.Internal.Substitution as Substitution
 import Kore.Internal.TermLike
-    ( ElementVariable
-    , InternalVariable
-    , SetVariable
-    , SortedVariable
+    ( InternalVariable
     , TermLike
     , Variable
     )
 import qualified Kore.Internal.TermLike as TermLike
 import Kore.Rewriting.RewritingVariable
+import Kore.Rewriting.UnifyingRule
 import qualified Kore.Step.Result as Result
 import qualified Kore.Step.Result as Results
 import qualified Kore.Step.Result as Step
@@ -103,9 +94,6 @@ import qualified Kore.Step.SMT.Evaluator as SMT.Evaluator
 import qualified Kore.TopBottom as TopBottom
 import Kore.Unification.UnificationProcedure
 import Kore.Unparser
-import Kore.Variables.Fresh
-    ( FreshPartialOrd
-    )
 import Kore.Variables.Target
     ( Target
     )
@@ -125,83 +113,6 @@ type Results rule variable =
     Step.Results
         (UnifiedRule rule RewritingVariable)
         (Pattern variable)
-
-type Renaming variable =
-    Map (UnifiedVariable variable) (UnifiedVariable variable)
-
-data InstantiationFailure variable
-    = ConcreteFailure (UnifiedVariable variable) (TermLike variable)
-    | SymbolicFailure (UnifiedVariable variable) (TermLike variable)
-    | UninstantiatedConcrete (UnifiedVariable variable)
-    | UninstantiatedSymbolic (UnifiedVariable variable)
-
-instance InternalVariable variable
-    => Pretty.Pretty (InstantiationFailure variable)
-  where
-    pretty (ConcreteFailure var term) =
-        Pretty.vsep
-            [ "Rule instantiation failure:"
-            , Pretty.indent 4 (unparse var <> " is marked as concrete.")
-            , Pretty.indent 4
-                ("However, " <> unparse term <> " is not concrete.")
-            ]
-    pretty (SymbolicFailure var term) =
-        Pretty.vsep
-            [ "Rule instantiation failure:"
-            , Pretty.indent 4 (unparse var <> " is marked as symbolic.")
-            , Pretty.indent 4
-                ("However, " <> unparse term <> " is not symbolic.")
-            ]
-    pretty (UninstantiatedConcrete var) =
-        Pretty.vsep
-            [ "Rule instantiation failure:"
-            , Pretty.indent 4 (unparse var <> " is marked as concrete.")
-            , Pretty.indent 4 "However, it was not instantiated."
-            ]
-    pretty (UninstantiatedSymbolic var) =
-        Pretty.vsep
-            [ "Rule instantiation failure:"
-            , Pretty.indent 4 (unparse var <> " is marked as symbolic.")
-            , Pretty.indent 4 "However, it was not instantiated."
-            ]
-
--- | A rule which can be unified against a configuration
-class UnifyingRule rule where
-    -- | The pattern used for matching/unifying the rule with the configuration.
-    matchingPattern :: rule variable -> TermLike variable
-
-    -- | The condition to be checked upon matching the rule
-    precondition :: rule variable -> Predicate variable
-
-    {-| Refresh the variables of a rule.
-    The free variables of a rule are implicitly quantified, so they are
-    renamed to avoid collision with any variables in the given set.
-     -}
-    refreshRule
-        :: InternalVariable variable
-        => FreeVariables variable  -- ^ Variables to avoid
-        -> rule variable
-        -> (Renaming variable, rule variable)
-
-    {-| Apply a given function to all variables in a rule. This is used for
-    distinguishing rule variables from configuration variables.
-    -}
-    mapRuleVariables
-        :: (Ord variable1, FreshPartialOrd variable2, SortedVariable variable2)
-        => (ElementVariable variable1 -> ElementVariable variable2)
-        -> (SetVariable variable1 -> SetVariable variable2)
-        -> rule variable1
-        -> rule variable2
-
-    -- | Checks whether a given substitution is acceptable for a rule
-    checkInstantiation
-        :: InternalVariable variable
-        => rule variable
-        -> Map.Map (UnifiedVariable variable) (TermLike variable)
-        -> [InstantiationFailure variable]
-    checkInstantiation _ _ = []
-    {-# INLINE checkInstantiation #-}
-
 
 -- |Unifies/matches a list a rules against a configuration. See 'unifyRule'.
 unifyRules
