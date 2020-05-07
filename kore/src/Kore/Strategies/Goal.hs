6c1bade14e37582e7584eb727fd3313bb4f7db0b
Thomas Tuegel <ttuegel@mailbox.org>
2020-05-07 09:22:27 -0500

    Kore.Strategies.Goal: Remove duplicate withConfiguration'

--- kore/src/Kore/Strategies/Goal.hs
+++ kore/src/Kore/Strategies/Goal.hs
@@ -843,7 +843,7 @@ removeDestination lensRulePattern mkState goal =
                 & GoalStuck
                 & pure
         & run
-        & withConfiguration' configuration
+        & withConfiguration configuration
       where
         configuration = Lens.view RulePattern.leftPattern rulePattern
         configFreeVars = freeVariables configuration
@@ -864,7 +864,7 @@ simplify
     -> Strategy.TransitionT (Rule goal) m goal
 simplify lensRulePattern =
     Lens.traverseOf (lensRulePattern . RulePattern.leftPattern) $ \config ->
-    withConfiguration' config $ do
+    withConfiguration config $ do
         configs <-
             simplifyTopConfiguration config >>= SMT.Evaluator.filterMultiOr
             & lift
@@ -918,7 +918,7 @@ deriveWith
 deriveWith lensRulePattern mkRule takeStep rewrites goal =
     (\x -> getCompose $ x goal)
     $ Lens.traverseOf (lensRulePattern . RulePattern.leftPattern)
-    $ \config -> Compose $ withConfiguration' config $ do
+    $ \config -> Compose $ withConfiguration config $ do
         results <- takeStep rewrites config & assertInstantiated config
         deriveResults mkRule results
   where
@@ -970,12 +970,8 @@ deriveResults mkRule Results { results, remainders } =
     fromAppliedRule =
         mkRule . RewriteRule . Step.unRewritingRule . Step.withoutUnification
 
-withConfiguration' :: MonadCatch m => Pattern Variable -> m a -> m a
-withConfiguration' configuration =
-    handle (throw . WithConfiguration configuration)
-
-withConfiguration' :: MonadCatch m => Pattern Variable -> m a -> m a
-withConfiguration' configuration =
+withConfiguration :: MonadCatch m => Pattern Variable -> m a -> m a
+withConfiguration configuration =
     handle (throw . WithConfiguration configuration)
 
 {- | The predicate to remove the destination from the present configuration.
