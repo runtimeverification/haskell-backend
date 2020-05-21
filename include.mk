1bd587834003f0ca89ab60a0e6884d9718484d90
Thomas Tuegel <ttuegel@mailbox.org>
2020-05-21 09:29:13 -0500

    include.mk: Use absolute path to prelude.kore

--- include.mk
+++ include.mk
@@ -72,5 +72,5 @@ $(K_RELEASE_DEFAULT)/lib/java/kernel-1.0-SNAPSHOT.jar:
 	curl --location --output $(K_RELEASE_TAR) $(K_RELEASE_TAR_URL)
 	mkdir -p $(K_RELEASE_DEFAULT)
 	tar --extract --file $(K_RELEASE_TAR) --strip-components 1 --directory $(K_RELEASE_DEFAULT)
-	cp src/main/kore/prelude.kore $(K_RELEASE_DEFAULT)/include/kore
+	cp $(TOP)/src/main/kore/prelude.kore $(K_RELEASE_DEFAULT)/include/kore
 	$(KRUN) --version
