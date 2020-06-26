9e0ae289a2505e3e431cacfb52820d81a8f53b4f
ana-pantilie <ana.pantilie95@gmail.com>
2020-06-26 14:07:10 +0300

    Small fix

--- kore/src/Kore/Builtin/Int.hs
+++ kore/src/Kore/Builtin/Int.hs
@@ -332,16 +332,16 @@ ediv n d
     | d == 0 = Nothing
     | n == d = Just 1
     | n < 0, d < 0 =
-        Just $ 1 + div n d
+        Just (1 + div n d)
     | d < 0 = Just (quot n d)
     | otherwise = Just (div n d)
-emod a b
-    | b == 0 = Nothing
-    | a == b = Just 0
-    | a < 0, b < 0 =
-        Just $ a - b * (1 + div a b)
-    | b < 0  = Just (rem a b)
-    | otherwise = Just (mod a b)
+emod n d
+    | d == 0 = Nothing
+    | n == d = Just 0
+    | n < 0, d < 0 =
+        Just (n - d * (1 + div n d))
+    | d < 0  = Just (rem n d)
+    | otherwise = Just (mod n d)
 pow b e
     | e < 0 = Nothing
     | otherwise = Just (b ^ e)
