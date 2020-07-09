fb082f1d80e38dda5ae3c0904cececf96ea0eebf
ana-pantilie <ana.pantilie95@gmail.com>
2020-07-09 12:40:38 +0300

    Review: mark only terms as defined during Defined simplification

--- kore/src/Kore/Step/Simplification/Defined.hs
+++ kore/src/Kore/Step/Simplification/Defined.hs
@@ -12,17 +12,17 @@ import Prelude.Kore
 import Kore.Internal.OrPattern
     ( OrPattern
     )
-import qualified Kore.Internal.OrPattern as OrPattern
+import qualified Kore.Internal.Pattern as Pattern
 import Kore.Internal.TermLike
 
--- | Marks an 'OrPattern' resulted from simplification
--- as 'Defined'. Does not do any actual simplification.
+-- | Marks the terms of an 'OrPattern' resulted
+-- from simplification as 'Defined'. Does not
+-- do any actual simplification.
 simplify
     :: InternalVariable variable
     => Defined (OrPattern variable)
     -> OrPattern variable
 simplify Defined { getDefined = defined } =
-    OrPattern.fromTermLike
-    $ markSimplified
-    $ mkDefined
-    $ OrPattern.toTermLike defined
+    Pattern.markSimplified
+    . fmap mkDefined
+    <$> defined
