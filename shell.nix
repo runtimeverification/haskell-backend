7597d766d3ed6751a93d3e402e835b062dd6994b
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2020-07-23 11:20:46 -0500

    shell.nix: Install Stack

--- shell.nix
+++ shell.nix
@@ -1,21 +1,30 @@
 { default ? import ./default.nix {} }:
 
 let
-  inherit (default) project pkgs;
+  inherit (default) project;
+
   local =
     if builtins.pathExists ./shell.local.nix
     then import ./shell.local.nix { inherit default; }
     else x: x;
   shellFor = args: project.shellFor (local args);
+
+  inherit (default.pkgs) ghcide hie-bios;
+
+  sources = import ./nix/sources.nix;
+  pkgs = import sources."nixpkgs" {};
+  inherit (pkgs) stack;
 in
 
 shellFor {
   buildInputs =
     with pkgs;
     [
-      ghcid gnumake yq z3
+      gnumake yq z3
+      ghcid
       ghcide hie-bios
       haskellPackages.ghc-events
+      stack
     ];
   tools = {
     cabal = "3.2.0.0";
