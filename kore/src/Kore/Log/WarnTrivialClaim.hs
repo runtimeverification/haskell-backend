1e57f4150ce6f46ff18477421e6d43c24c9f297c
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2020-08-10 10:37:09 -0500

    Kore.Log.WarnTrivialClaim: Documentation

--- kore/src/Kore/Log/WarnTrivialClaim.hs
+++ kore/src/Kore/Log/WarnTrivialClaim.hs
@@ -23,26 +23,30 @@ import qualified Pretty
 
 data WarnTrivialClaim
     = WarnProvenClaimZeroDepth ReachabilityRule
+    -- ^ Warning when a claim is proved without rewriting.
     | WarnTrivialClaimRemoved ReachabilityRule
+    -- ^ Warning when a claim is proved during initialization.
     deriving Show
 
 instance Pretty WarnTrivialClaim where
     pretty (WarnProvenClaimZeroDepth rule) =
         Pretty.hsep
-            [ "claim proven without taking any steps:"
+            [ "Claim proven without rewriting at:"
             , Pretty.pretty (from rule :: SourceLocation)
             ]
     pretty (WarnTrivialClaimRemoved rule) =
-        Pretty.hsep
-            [ "trivial claim removed:"
-            , Pretty.pretty (from rule :: SourceLocation)
+        Pretty.vsep
+            [ Pretty.hsep
+                [ "Claim proven during initialization:"
+                , Pretty.pretty (from rule :: SourceLocation)
+                ]
+            , "The left-hand side of the claim may be undefined."
             ]
 
 
 instance Entry WarnTrivialClaim where
     entrySeverity _ = Warning
-    helpDoc _ =
-        "warn when a claim is removed or proven without taking any steps"
+    helpDoc _ = "warn when a claim is proven without taking any steps"
 
 warnProvenClaimZeroDepth
     :: MonadLog log
