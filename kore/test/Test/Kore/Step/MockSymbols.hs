7164ea0d7bc2001be7719bbe9f0f24efa1fc2da0
andreiburdusa <andrei.burdusa@zoho.com>
2020-08-21 19:49:29 +0300

    MockSymbols.concatSetSymbol: function

--- kore/test/Test/Kore/Step/MockSymbols.hs
+++ kore/test/Test/Kore/Step/MockSymbols.hs
@@ -613,7 +613,7 @@ unitListSymbol = symbol unitListId [] listSort & functional & hook "LIST.unit"
 concatSetSymbol :: Symbol
 concatSetSymbol =
     symbol concatSetId [setSort, setSort] setSort
-    & functional & hook "SET.concat"
+    & function & hook "SET.concat"
 
 elementSetSymbol :: Symbol
 elementSetSymbol =
