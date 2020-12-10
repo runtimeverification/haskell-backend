dcaffa126777094d29a90086140bbeeb06620bcc
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2020-12-10 12:04:41 -0600

    Format with stylish-haskell automatically (#2309)
    * Format with stylish-haskell automatically
    
    * Run hlint on GitHub

--- scripts/check.sh
+++ scripts/check.sh
@@ -21,30 +21,3 @@ then
     echo >&2 "Did you forget to run 'stack build' after updating 'stack.yaml'?"
     exit 1
 fi
-
-changed=()
-for file in $(find_haskell_files $TOP)
-do
-    case $file in
-        *.hs|*.hs-boot) ;;
-        *) continue ;;
-    esac
-    stylish-haskell $file >$file.tmp
-    # (diff ...) exits with 0 when the files are the same
-    # (! diff ...) means the files are _different_
-    # Maybe it should have been named "same" :-\
-    if ! diff -Nau $file $file.tmp
-    then
-        changed+=($file)
-    fi
-    rm $file.tmp
-done
-
-if [[ ${#changed[@]} -ne 0 ]]
-then
-    echo >&2 "Please run 'stylish-haskell':"
-    echo >&2 "stack exec -- stylish-haskell -i" "${changed[@]}"
-    exit 1
-fi
-
-hlint kore
