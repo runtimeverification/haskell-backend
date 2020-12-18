76cdb7cd9e4c35e059c6d51246a0c795fe634f4b
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2020-12-18 16:17:45 -0600

    Update kore/src/Kore/Reachability/Claim.hs

--- kore/src/Kore/Reachability/Claim.hs
+++ kore/src/Kore/Reachability/Claim.hs
@@ -584,12 +584,12 @@ checkImplicationWorker (ClaimPattern.refreshExistentials -> claimPattern) =
         -> Pattern RewritingVariableName
         -> CheckImplicationResult ClaimPattern
     examine AnyUnified { didAnyUnify } stuck
-      | not didAnyUnify
-      , not (isBottom right) =
-          NotImplied claimPattern
       | didAnyUnify
       , isBottom condition =
           Implied
+      | not didAnyUnify
+      , not (isBottom right) =
+          NotImplied claimPattern
       | otherwise =
         Lens.set (field @"left") stuck claimPattern
         & NotImpliedStuck
