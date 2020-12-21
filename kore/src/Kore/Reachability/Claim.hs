ab18d55f5d95c3bc66f6b2f0af2b46ff0321ed49
ana-pantilie <45069775+ana-pantilie@users.noreply.github.com>
2020-12-21 10:19:04 +0200

    checkImplication.examine: check if RHS is \bottom (#2329)
    * checkImplication.examine: check if RHS is \bottom
    
    * Update kore/src/Kore/Reachability/Claim.hs
    
    Co-authored-by: Thomas Tuegel <thomas.tuegel@runtimeverification.com>

--- kore/src/Kore/Reachability/Claim.hs
+++ kore/src/Kore/Reachability/Claim.hs
@@ -584,8 +584,12 @@ checkImplicationWorker (ClaimPattern.refreshExistentials -> claimPattern) =
         -> Pattern RewritingVariableName
         -> CheckImplicationResult ClaimPattern
     examine AnyUnified { didAnyUnify } stuck
-      | not didAnyUnify = NotImplied claimPattern
-      | isBottom condition = Implied
+      | didAnyUnify
+      , isBottom condition =
+          Implied
+      | not didAnyUnify
+      , not (isBottom right) =
+          NotImplied claimPattern
       | otherwise =
         Lens.set (field @"left") stuck claimPattern
         & NotImpliedStuck
