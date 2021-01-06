64d1020d5210ab5e41ea3658174052e4b7740e1a
Evan Marzion <emarzion@gmail.com>
2021-01-05 22:15:11 -0600

    Update kore/app/exec/Main.hs
    Co-authored-by: Thomas Tuegel <thomas.tuegel@runtimeverification.com>

--- kore/app/exec/Main.hs
+++ kore/app/exec/Main.hs
@@ -528,9 +528,6 @@ writeOptionsAndKoreFiles
         (reportDirectory </> defaultDefinitionFilePath opts)
     for_ patternFileName
         $ flip copyFile (reportDirectory </> "pgm.kore")
-    -- for_ outputFileName
-    --    $ flip copyFile (reportDirectory </> "result.kore")
-    -- _ <- error "Test."
     writeKoreSolverFiles koreSolverOptions reportDirectory
     for_ koreSearchOptions
         (writeKoreSearchFiles reportDirectory)
