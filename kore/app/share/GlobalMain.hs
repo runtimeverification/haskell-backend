a03b90f88b18cd5bb9376d191ba05f07ad25bf20
emarzion <emarzion@gmail.com>
2021-01-25 22:28:24 -0600

    removing build date and adding gitDirty

--- kore/app/share/GlobalMain.hs
+++ kore/app/share/GlobalMain.hs
@@ -49,20 +49,13 @@ import Data.Text
     , pack
     )
 import qualified Data.Text.IO as Text
-import Data.Time.Format
-    ( defaultTimeLocale
-    , formatTime
-    )
-import Data.Time.LocalTime
-    ( ZonedTime
-    , getZonedTime
-    )
 import Data.Version
     ( showVersion
     )
 import Development.GitRev
     ( gitBranch
     , gitCommitDate
+    , gitDirty
     , gitHash
     )
 import GHC.Stack
@@ -289,27 +282,24 @@ mainGlobal
     -> IO      (MainOptions options)
 mainGlobal exeName maybeEnv localOptionsParser modifiers = do
     options <- commandLineParse exeName maybeEnv localOptionsParser modifiers
-    when (willVersion $ globalOptions options) (getZonedTime >>= mainVersion)
+    when (willVersion $ globalOptions options) mainVersion
     return options
 
 -- | main function to print version information
-mainVersion :: ZonedTime -> IO ()
-mainVersion time =
+mainVersion :: IO ()
+mainVersion =
       mapM_ putStrLn
       [ "Kore version "    ++ packageVersion
       , "Git:"
-      , "  revision:\t"    ++ $gitHash
+      , "  revision:\t"    ++ $gitHash ++ " (Repository is " ++ if $gitDirty then "dirty)" else "clean)"
       , "  branch:\t"      ++ $gitBranch
       , "  last commit:\t" ++  gitTime
-      , "Build date:\t"    ++  exeTime
       ]
     where
       packageVersion = showVersion MetaData.version
       formatGit (_:mm:dd:tt:yy:tz:_) = [yy,mm,dd,tt,tz]
       formatGit t                    = t
       gitTime = (unwords . formatGit . words) $gitCommitDate
-      exeTime = formatTime defaultTimeLocale  "%Y %b %d %X %z" time
-
 
 --------------------
 -- Option Parsers --
