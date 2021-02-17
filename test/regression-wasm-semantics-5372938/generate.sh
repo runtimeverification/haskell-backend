c9af1db5c86d86a7fd2394cb607f1835aa3f0a2c
emarzion <emarzion@gmail.com>
2021-02-17 17:34:48 -0600

    modifying command for test-wrc20

--- test/regression-wasm-semantics-5372938/generate.sh
+++ test/regression-wasm-semantics-5372938/generate.sh
@@ -34,6 +34,5 @@ do
 done
 
 kollect "test-wrc20" \
-    ./kwasm prove --backend haskell \
-        tests/proofs/wrc20-spec.k \
-        WRC20-LEMMAS
+    ./kwasm prove --backend haskell tests/proofs/wrc20-spec.k WRC20-LEMMAS --format-failures \
+    --concrete-rules WASM-DATA.wrap-Positive,WASM-DATA.setRange-Positive,WASM-DATA.getRange-Positive,WASM-DATA.get-Existing,WASM-DATA.set-Extend
