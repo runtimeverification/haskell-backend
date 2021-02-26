f5bebd9823eb14cc2d7f0d96c73c519f5805725f
Andrei Burdușa <andrei.burdusa@zoho.com>
2021-02-26 18:35:28 +0200

    Pedantic

--- kore/test/Test/Kore/Exec.hs
+++ kore/test/Test/Kore/Exec.hs
@@ -79,9 +79,6 @@ import Kore.Syntax.Definition hiding
 import qualified Kore.Syntax.Definition as Syntax
 import qualified Kore.Verified as Verified
 
-import Kore.Rewriting.RewritingVariable
-    ( mkRewritingTerm
-    )
 import Test.Kore
 import qualified Test.Kore.IndexedModule.MockMetadataTools as Mock
 import Test.SMT
