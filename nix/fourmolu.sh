c859b332a0e6fd95bcc5e8e262422b022608819d
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2021-03-12 13:22:42 -0600

    nix/fourmolu.sh: Run scripts/remove-import-groups.sh

--- nix/fourmolu.sh
+++ nix/fourmolu.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env nix-shell
 #!nix-shell ../shell.nix -i bash
 
+./scripts/remove-import-groups.sh
 fd '.*\.hs$' | xargs fourmolu -o -XTypeApplications -o -XPatternSynonyms -o -XBangPatterns -i
