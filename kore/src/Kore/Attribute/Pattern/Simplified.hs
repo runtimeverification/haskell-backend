d136446e3bb550af601f027281e751212075e624
ana-pantilie <45069775+ana-pantilie@users.noreply.github.com>
2021-03-16 17:49:51 +0200

    Fix Strict performance (#2447)
    * WIP: make evaluation of side condition representation lazy
    
    * WIP fix: best results so far
    
    * Replace undefined
    
    * WIP fix: best performance yet
    
    * Remove useless lazy bindings
    
    * Remove useless lazy bindings
    
    * Update kore/src/Kore/Attribute/Pattern/Simplified.hs
    
    Co-authored-by: Thomas Tuegel <thomas.tuegel@runtimeverification.com>
    
    * TermLike.setSimplified: make attributes lazy
    
    * TermLike.TermLike: make call to Attribute.traverseVariables lazy
    
    * Review: make Representation lazy
    
    * Make Attribute.Simplified lazy
    
    * Review: remove redundant
    
    Co-authored-by: Thomas Tuegel <thomas.tuegel@runtimeverification.com>
    
    * Review: make TypeRep strict
    
    Co-authored-by: Thomas Tuegel <thomas.tuegel@runtimeverification.com>
    
    * Make Simplified and SideCondition.SideCondition NoStrict
    
    * Remove redundant lazy bindings
    
    Co-authored-by: Thomas Tuegel <thomas.tuegel@runtimeverification.com>

--- kore/src/Kore/Attribute/Pattern/Simplified.hs
+++ kore/src/Kore/Attribute/Pattern/Simplified.hs
@@ -4,7 +4,7 @@ License     : NCSA
 
  -}
 
-{-# LANGUAGE Strict #-}
+{-# LANGUAGE NoStrict #-}
 
 module Kore.Attribute.Pattern.Simplified
     ( Simplified (..)
@@ -100,7 +100,7 @@ data Condition
     = Any
     -- ^ The term and all its subterms are simplified the same regardless
     -- of the side condition.
-    | Condition !SideCondition.Representation
+    | Condition SideCondition.Representation
     -- ^ The term is in its current simplified state only when using the
     -- given side condition. When the side condition changes, e.g. by
     -- adding extra conditions, then we may be able to further simplify the
@@ -134,8 +134,8 @@ instance Monoid Condition where
 
 data SimplifiedData =
     SimplifiedData
-        { sType :: !Type
-        , condition :: !Condition
+        { sType :: Type
+        , condition :: Condition
         }
     deriving (Eq, Ord, Show)
     deriving (GHC.Generic)
@@ -156,7 +156,7 @@ Most patterns are assumed un-simplified until marked otherwise, so the
 simplified status is reset by any substitution under the pattern.
 -}
 data Simplified
-    = Simplified !SimplifiedData
+    = Simplified SimplifiedData
     | NotSimplified
     deriving (Eq, Ord, Show)
     deriving (GHC.Generic)
