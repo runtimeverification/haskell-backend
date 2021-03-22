fd57bf2e2050579b8561a0a5b77299e5b2b1fc76
ana-pantilie <ana.pantilie95@gmail.com>
2021-03-22 13:18:08 +0200

    Write part of script which regenerates evm-semantics

--- scripts/generate-regression-tests.sh
+++ scripts/generate-regression-tests.sh
@@ -12,50 +12,64 @@ kollect() {
     chmod +x "$name.sh"
 }
 
-generate-evm() {
-    # git clone git@github.com:kframework/evm-semantics.git
-    # cd evm-semantics
-    # local evm_commit=$(git rev-parse --short HEAD)
-    # git submodule update --init --recursive
-    # make plugin-deps
-    # make build-haskell
-    # export PATH=$(pwd)/.build/usr/bin:$PATH
-    # 
-    # kollect test-pop1 env MODE=VMTESTS SCHEDULE=DEFAULT \
-    #     kevm run --backend haskell \
-    #         tests/ethereum-tests/VMTests/vmIOandFlowOperations/pop1.json
-    # 
-    # kollect test-add0 env MODE=VMTESTS SCHEDULE=DEFAULT \
-    #     kevm run --backend haskell \
-    #         tests/ethereum-tests/VMTests/vmArithmeticTest/add0.json \
-    # 
-    # kollect test-sumTo10 env MODE=VMTESTS SCHEDULE=DEFAULT \
-    #     kevm run --backend haskell \
-    #         tests/interactive/sumTo10.evm \
-    # 
-    # for search in \
-    #     branching-no-invalid straight-line-no-invalid \
-    #     branching-invalid straight-line
-    # do
-    #     kollect "test-$search" \
-    #         kevm search --backend haskell \
-    #             "tests/interactive/search/$search.evm" \
-    #             "<statusCode> EVMC_INVALID_INSTRUCTION </statusCode>"
-    # done
-    #         
-    # kollect test-sum-to-n \
-    #     kevm prove --backend haskell \
-    #         tests/specs/examples/sum-to-n-spec.k \
-    #         VERIFICATION --format-failures
-
+build-evm() {
+    cd $KORE
+    git clone git@github.com:kframework/evm-semantics.git
     cd evm-semantics
-    testdir=$KORE/tests/regression-evm-*
-    tests=test-*
-    rm -r $testdir
-    mkdir $KORE/tests/regression-evm-$evm_commit
-    mv $tests $KORE/tests/regression-evm-$evm_commit
-
+    git submodule update --init --recursive
+    make plugin-deps
+    make build-haskell
+    export PATH=$(pwd)/.build/usr/bin:$PATH
 }
 
-cd $KORE
+generate-evm() {
+    cd $KORE/evm-semantics
+
+    kollect test-pop1 env MODE=VMTESTS SCHEDULE=DEFAULT \
+        kevm run --backend haskell \
+            tests/ethereum-tests/VMTests/vmIOandFlowOperations/pop1.json
+    
+    kollect test-add0 env MODE=VMTESTS SCHEDULE=DEFAULT \
+        kevm run --backend haskell \
+            tests/ethereum-tests/VMTests/vmArithmeticTest/add0.json \
+    
+    kollect test-sumTo10 env MODE=VMTESTS SCHEDULE=DEFAULT \
+        kevm run --backend haskell \
+            tests/interactive/sumTo10.evm \
+    
+    for search in \
+        branching-no-invalid straight-line-no-invalid \
+        branching-invalid straight-line
+    do
+        kollect "test-$search" \
+            kevm search --backend haskell \
+                "tests/interactive/search/$search.evm" \
+                "<statusCode> EVMC_INVALID_INSTRUCTION </statusCode>"
+    done
+            
+    kollect test-sum-to-n \
+        kevm prove --backend haskell \
+            tests/specs/examples/sum-to-n-spec.k \
+            VERIFICATION --format-failures
+}
+
+replace-tests() {
+    local testdir=$KORE/$1
+    local tests=$KORE/$2/test-*
+
+    if [ -d $testdir ]
+    then
+        rm $testdir/test-*
+    else
+        mkdir $testdir
+        echo "include \$(CURDIR)/../include.mk" > $testdir/Makefile
+        echo "" >> $testdir/Makefile
+        echo "test-%.sh.out: \$(TEST_DIR)/test-%-*" >> $testdir/Makefile
+    fi
+    mv $tests $testdir
+}
+
+build-evm
 generate-evm
+replace-tests "test/regression-evm" "evm-semantics"
+rm -rf $KORE/evm-semantics
