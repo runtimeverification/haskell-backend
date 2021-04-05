c639b24232ecc062f65f35affd31a454c6f61940
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2021-04-05 10:50:03 -0500

    kore-exec-step.sh: Add --depth option

--- kore-exec-step.sh
+++ kore-exec-step.sh
@@ -4,13 +4,20 @@ set_results() {
     last_result="result-$(printf "%04d" $last).kore"
     next_result="result-$(printf "%04d" $next).kore"
 }
+(( last = 0 ))
+(( next = 1 ))
+set_results
 
 kore_exec_args=()
 while [[ $# -gt 0 ]]
 do
     case "$1" in
         --pattern)
-            last_result="$2"
+            pattern="$2"
+            shift
+            ;;
+        --depth)
+            depth="$2"
             shift
             ;;
         *)
@@ -19,13 +26,18 @@ do
     shift
 done
 
-(( next = 0 ))
-next_result="result-0000.kore"
-while ! [[ -f $next_result ]] || ! diff "$last_result" "$next_result" >/dev/null
+if [[ -z "$pattern" ]]
+then
+    echo >&2 "Error: Missing --pattern FILENAME argument"
+    exit 1
+fi
+last_result="$pattern"
+while [[ -z "$depth" ]] || [[ "$last" -lt "$depth" ]]
 do
     command time -f '%S,%U,%M' -o "kore-exec-time.csv" -a -q \
         kore-exec "${kore_exec_args[@]}" \
             --pattern "${last_result:?}" --output "${next_result:?}" --depth 1
+    if diff "$last_result" "$next_result" >/dev/null; then break; fi
     (( last = next ))
     (( next += 1 ))
     set_results
