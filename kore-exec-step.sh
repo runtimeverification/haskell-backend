32d6090b724e2545a466ad50cdd355250aca1915
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2021-04-07 10:49:30 -0500

    Add a script to time execution steps (#2527)
    * kore-exec-step.sh: Run programs step-wise
    
    * kore-exec-step.sh: Parse command line arguments
    
    * kore-exec-step.sh: Remove print debugging
    
    * kore-exec-step.sh: Add --depth option
    
    * kore-exec-step.sh: Exit loop on SIGINT
    
    * kore-exec-step.sh: Ignore --output flag
    
    * kore-exec-step.sh: Name CSV file after original pattern
    
    * kore-exec-step.sh: Add usage documentation
    
    Co-authored-by: rv-jenkins <admin@runtimeverification.com>

---
+++ kore-exec-step.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# USAGE: Pass the output of `krun --dry-run` to this script on the command line.
+
+set_results() {
+    last_result="result-$(printf "%04d" $last).kore"
+    next_result="result-$(printf "%04d" $next).kore"
+}
+(( last = 0 ))
+(( next = 1 ))
+set_results
+
+kore_exec_args=()
+while [[ $# -gt 0 ]]
+do
+    case "$1" in
+        --pattern)
+            pattern="$2"
+            shift
+            ;;
+        --output)
+            shift
+            ;;
+        --depth)
+            depth="$2"
+            shift
+            ;;
+        *)
+            kore_exec_args+=("$1")
+    esac
+    shift
+done
+
+if [[ -z "$pattern" ]]
+then
+    echo >&2 "Error: Missing --pattern FILENAME argument"
+    exit 1
+fi
+last_result="$pattern"
+csv_file="$pattern.csv"
+
+trap 'exit 1' SIGINT
+
+while [[ -z "$depth" ]] || [[ "$last" -lt "$depth" ]]
+do
+    command time -f '%S,%U,%M' -o "${csv_file:?}" -a -q \
+        kore-exec "${kore_exec_args[@]}" \
+            --pattern "${last_result:?}" --output "${next_result:?}" --depth 1
+    if diff "$last_result" "$next_result" >/dev/null; then break; fi
+    (( last = next ))
+    (( next += 1 ))
+    set_results
+done
