1f977780cf22cfdddef456a91c5f1a5b0335954a
Andrei Burdușa <andrei.burdusa@zoho.com>
2021-04-07 14:22:12 +0300

    EVM test scripts: fail when $KORE_EXEC is not set

--- test/regression-evm/test-straight-line-no-invalid.sh
+++ test/regression-evm/test-straight-line-no-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-$KORE_EXEC test-straight-line-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-no-invalid-search-initial.kore --searchType FINAL --search test-straight-line-no-invalid-search-pattern.kore "$@"
+${KORE_EXEC:?} test-straight-line-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-no-invalid-search-initial.kore --searchType FINAL --search test-straight-line-no-invalid-search-pattern.kore "$@"
