d61885e210341a1cfb3487a0d28f5bdc5b842494
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2021-04-15 16:33:45 -0500

    Remove kore/package.yaml (#2553)

--- kore/src/Kore/VersionInfo.hs
+++ kore/src/Kore/VersionInfo.hs
@@ -83,7 +83,7 @@ getPackageRoot = do
     let parents = getParents bot
     TH.runIO $ findPackageRoot bot parents
   where
-    isProjectRoot here = Directory.doesFileExist (here </> "package.yaml")
+    isProjectRoot here = Directory.doesFileExist (here </> "kore.cabal")
 
     getParents bottom =
         bottom
@@ -98,7 +98,7 @@ getPackageRoot = do
     -- Find the root directory of the current package. This module file can
     -- be moved safely because the package root is found at build time.
     findPackageRoot bot [] =
-        fail ("Could not find package.yaml above " ++ bot)
+        fail ("Could not find kore.cabal above " ++ bot)
     findPackageRoot bot (here : heres) = do
         foundRoot <- isProjectRoot here
         if foundRoot then Directory.makeAbsolute here else goUp
