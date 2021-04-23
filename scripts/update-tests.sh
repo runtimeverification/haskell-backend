965133b69a19cfa9c4ff89aa82996db35dcbc89d
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2021-04-23 06:49:42 -0500

    Automatically update regression tests (#2548)
    * editorconfig: Add section for Jenkinsfile
    
    * Add update Pipeline
    
    * Jenkinsfile.update: Fix syntax error
    
    * Dockerfile: Fix location of SSH config file
    
    * fixup! Add update Pipeline
    
    * fixup! Add update Pipeline
    
    * fixup! Add update Pipeline
    
    * Dockerfile: Install hub
    
    * Make for silent running
    
    * Dockerfile: hub not available in Ubuntu, despite upstream claims
    
    * Dockerfile: Install hub from GitHub
    
    * Dockerfile: Do not need sudo
    
    * Dockerfile: Rearrange phases
    
    * fixup! Add update Pipeline
    
    * fixup! Add update Pipeline
    
    * Jenkinsfile: Parameterize two distinct stages
    
    * fixup! Jenkinsfile: Parameterize two distinct stages
    
    * Make for silent running
    
    * Reuse existing remote branch if pull request exists
    
    * Only create pull request if none exists
    
    * Extract pull request logic to a script
    
    * chmod +x scripts/update-tests.sh
    
    * update-tests.sh: Print more debugging information
    
    * update-tests.sh: grep loudly
    
    * update-tests.sh: Check for pull request exactly
    
    * Jenkinsfile: Add timed UPDATE-TESTS job
    
    * Dockerfile: Add kore.cabal
    
    Co-authored-by: Octavian Mircea Sebe <mirceasebe@yahoo.com>

---
+++ scripts/update-tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+# Create a pull request to update the regression test suite.
+
+git fetch --all
+git checkout -B _update origin/_update || git checkout -B _update origin/master
+
+export KORE=$(pwd)
+./scripts/generate-regression-tests.sh
+
+git add test/regression-evm test/regression-wasm
+git commit -m 'Update regression tests'
+git push -u origin _update
+if ! hub pr list --format '%H%n' | grep -q '^_update$'; then
+  hub pull-request                      \
+    --head _update --base master        \
+    --reviewer ttuegel --assign ttuegel \
+    --labels automerge                  \
+    --message 'Update regression tests'
+fi
