7330ab775c148b0ac34fa3a33ceca5366225a18e
rv-jenkins <admin@runtimeverification.com>
2021-04-27 11:49:27 -0600

    Update regression tests (#2575)
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Trim variable parts of source path attributes
    
    Co-authored-by: Thomas Tuegel <thomas.tuegel@runtimeverification.com>

--- test/regression-evm/test-branching-no-invalid.sh
+++ test/regression-evm/test-branching-no-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.rscfMYRvLY --searchType FINAL --search test-branching-no-invalid-tmp.pattern.azNpCuI5ge "$@"
+${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.0Lwj7yUEcE --searchType FINAL --search test-branching-no-invalid-tmp.pattern.7h1bFPaVKX "$@"
