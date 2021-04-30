148782ea3a81b8ec0070ef0af274e99b4a63969e
Thomas Tuegel <ttuegel@mailbox.org>
2021-04-30 15:22:02 -0500

    Replay "Validate function definitions (#2168)"
    This reverts commit 8782a7086a9177d1a2d0e6fde6ff479935c7de95.

--- nix/kore.nix.d/kore.nix
+++ nix/kore.nix.d/kore.nix
@@ -210,6 +210,7 @@
           "Kore/Equation/Registry"
           "Kore/Equation/Sentence"
           "Kore/Equation/Simplification"
+          "Kore/Equation/Validate"
           "Kore/Error"
           "Kore/Exec"
           "Kore/IndexedModule/Error"
