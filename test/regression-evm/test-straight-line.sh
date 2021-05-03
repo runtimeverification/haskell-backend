d29d6aa685f35baaae03a9a91563cfd27dcb9d43
RV Jenkins <admin@runtimeverification.com>
2021-05-03 20:42:25 +0000

    Update regression tests

--- test/regression-evm/test-straight-line.sh
+++ test/regression-evm/test-straight-line.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-straight-line-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-tmp.in.qseSpMCXAo --searchType FINAL --search test-straight-line-tmp.pattern.36aSqKKp5l "$@"
+${KORE_EXEC:?} test-straight-line-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-tmp.in.bD9uQ0SgfK --searchType FINAL --search test-straight-line-tmp.pattern.VIE9A4PRCs "$@"
