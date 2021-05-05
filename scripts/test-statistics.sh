4cde50e51b2ac531b33d5b0c32412bfae8fbb159
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2021-05-05 11:32:35 -0500

    test-statistics.sh: accept multiple test directories

--- scripts/test-statistics.sh
+++ scripts/test-statistics.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env nix-shell
 #!nix-shell ../test.nix -i bash
 
-# Usage: test-statistics.sh KORE_DIR TEST_DIR
+# Usage: test-statistics.sh KORE_DIR TEST_DIR TEST_DIR ...
 # Output: tabular JSON on standard output
 
 set -eou pipefail
 
 kore_dir="${1:?}"; shift
-test_dir="${1:?}"; shift
-
 export KORE_EXEC="$(nix-build "$kore_dir" -A kore --arg release true --no-out-link)/bin/kore-exec"
 
-cd "$test_dir"
-
-for each in $(find ./ -name 'test-*.sh')
+while [[ $# -gt 0 ]]
 do
-    json="${each%sh}.json"
-    "${each}" --rts-statistics "$json" >/dev/null
-    jq ". | { name: \"$each\", allocated_bytes: .allocated_bytes, max_live_bytes: .max_live_bytes }" < "$json"
-    rm "$json"
+    test_dir="${1:?}"; shift
+    (
+        cd "$test_dir"
+        for each in $(find ./ -name 'test-*.sh')
+        do
+            json="${each%sh}.json"
+            "${each}" --rts-statistics "$json" >/dev/null
+            jq ". | { name: \"$test_dir${each#.}\", allocated_bytes: .allocated_bytes, max_live_bytes: .max_live_bytes }" < "$json"
+            rm "$json"
+        done
+    )
 done
+
+
+
