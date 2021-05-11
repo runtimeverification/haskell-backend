ee22efaee5ca1bafb55be7735a4c55c46ee58583
RV Jenkins <admin@runtimeverification.com>
2021-05-11 14:30:54 +0000

    Update regression tests

--- test/regression-evm/test-branching-invalid.sh
+++ test/regression-evm/test-branching-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-branching-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-invalid-tmp.in.7cf3srjbcr --searchType FINAL --search test-branching-invalid-tmp.pattern.tZ7kesqBaw "$@"
+${KORE_EXEC:?} test-branching-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-invalid-tmp.in.dppB9ifEC5 --searchType FINAL --search test-branching-invalid-tmp.pattern.2gsei3GIrI "$@"
--- test/regression-evm/test-pop1.sh
+++ test/regression-evm/test-pop1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-pop1-definition.kore --module ETHEREUM-SIMULATION --pattern test-pop1-tmp.in.64t9hI9HTU "$@"
+${KORE_EXEC:?} test-pop1-definition.kore --module ETHEREUM-SIMULATION --pattern test-pop1-tmp.in.y5QT50Ahvc "$@"
