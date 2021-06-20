54b6778a7e74a34d8f3c26f29df20e22e3ae0f43
RV Jenkins <admin@runtimeverification.com>
2021-06-20 21:34:15 +0000

    Update regression tests

--- test/regression-evm/test-branching-no-invalid.sh
+++ test/regression-evm/test-branching-no-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.M5hOTd75yk --searchType FINAL --search test-branching-no-invalid-tmp.pattern.oK52qZao7J "$@"
+${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.20EV92Tgrc --searchType FINAL --search test-branching-no-invalid-tmp.pattern.Q3KfJbNuLV "$@"
--- test/regression-evm/test-straight-line-no-invalid.sh
+++ test/regression-evm/test-straight-line-no-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-straight-line-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-no-invalid-tmp.in.1Ik98pqW3h --searchType FINAL --search test-straight-line-no-invalid-tmp.pattern.NpdeMdAvtU "$@"
+${KORE_EXEC:?} test-straight-line-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-no-invalid-tmp.in.oB3hDEbeid --searchType FINAL --search test-straight-line-no-invalid-tmp.pattern.nyCmEKg6p4 "$@"
