abd61eb439c4df729f993343660101f4a8d372cb
Max Kopinsky <zerglingk9012@gmail.com>
2021-06-23 12:03:02 -0500

    Actually commit new entry file

---
+++ kore/src/Kore/Log/DebugBeginClaim.hs
@@ -0,0 +1,31 @@
+module Kore.Log.DebugBeginClaim (
+  DebugBeginClaim (..),
+  debugBeginClaim
+) where
+
+import Kore.Reachability.SomeClaim (
+  SomeClaim (..)
+ )
+import Log
+import Prelude.Kore
+import Pretty (
+  Pretty (..)
+ )
+import qualified Pretty
+
+newtype DebugBeginClaim = DebugBeginClaim {claim :: SomeClaim}
+    deriving stock (Show)
+
+instance Pretty DebugBeginClaim where
+  pretty DebugBeginClaim{claim} =
+    Pretty.vsep ["Begin proof of claim:", Pretty.indent 4 (pretty claim)]
+
+instance Entry DebugBeginClaim where
+  entrySeverity _ = Debug
+  helpDoc _ = "log starting claims"
+
+debugBeginClaim ::
+  MonadLog log =>
+  SomeClaim ->
+  log ()
+debugBeginClaim claim = logEntry (DebugBeginClaim claim)
