8ee4b65e67d732b792afc9150f37c5d252d0a906
Thomas Tuegel <ttuegel@mailbox.org>
2021-06-27 14:12:05 -0500

    Test.Kore.Internal.OrPattern: Use explicit sorts

--- kore/test/Test/Kore/Internal/OrPattern.hs
+++ kore/test/Test/Kore/Internal/OrPattern.hs
@@ -122,7 +122,7 @@ test_distributeAnd =
     , testCase "\\top and (a or b) => a or b " $ do
         let conjunction =
                 MultiAnd.make
-                    [ MultiOr.singleton TermLike.mkTop_
+                    [ MultiOr.singleton (TermLike.mkTop Mock.testSort)
                     , MultiOr.make [a, b]
                     ]
             expect =
@@ -135,7 +135,7 @@ test_distributeAnd =
         let conjunction =
                 MultiAnd.make
                     [ MultiOr.singleton a
-                    , MultiOr.make [b, TermLike.mkBottom_]
+                    , MultiOr.make [b, TermLike.mkBottom Mock.testSort]
                     ]
             expect =
                 MultiOr.make
@@ -197,7 +197,7 @@ test_distributeApplication =
         let app =
                 sigma2
                     [ MultiOr.singleton a
-                    , MultiOr.make [b, TermLike.mkBottom_]
+                    , MultiOr.make [b, TermLike.mkBottom Mock.testSort]
                     ]
             expect =
                 MultiOr.make
