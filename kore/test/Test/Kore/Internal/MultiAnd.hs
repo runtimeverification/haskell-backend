cc79233de519e1108efb02b1337d29ea5e486e98
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2021-06-30 11:52:07 -0500

    Test MultiAnd.make

--- kore/test/Test/Kore/Internal/MultiAnd.hs
+++ kore/test/Test/Kore/Internal/MultiAnd.hs
@@ -1,19 +1,24 @@
 module Test.Kore.Internal.MultiAnd (
     test_multiAndTopBottom,
-    test_multiAndMake,
+    test_make,
 ) where
 
 import qualified GHC.Generics as GHC
 import qualified Generics.SOP as SOP
 import Kore.Debug
+import Kore.Internal.From
 import Kore.Internal.MultiAnd (
     MultiAnd,
  )
 import qualified Kore.Internal.MultiAnd as MultiAnd
+import Kore.Internal.Predicate (Predicate)
+import Kore.Internal.TermLike (ElementVariable, mkElemVar)
+import Kore.Rewriting.RewritingVariable (RewritingVariableName)
 import Kore.TopBottom (
     TopBottom (..),
  )
 import Prelude.Kore
+import qualified Test.Kore.Step.MockSymbols as Mock
 import Test.Tasty
 import Test.Tasty.HUnit.Ext
 
@@ -45,8 +50,8 @@ test_multiAndTopBottom =
     , assertIsBottom True (MultiAnd.make [TestBottom])
     ]
 
-test_multiAndMake :: [TestTree]
-test_multiAndMake =
+test_make :: [TestTree]
+test_make =
     [ MultiAnd.make [] `hasPatterns` []
     , MultiAnd.make [TestTop] `hasPatterns` []
     , MultiAnd.make [TestTop, TestOther 1] `hasPatterns` [TestOther 1]
@@ -55,7 +60,17 @@ test_multiAndMake =
     , MultiAnd.make [TestBottom] `hasPatterns` [TestBottom]
     , MultiAnd.make [TestOther 1, TestOther 2]
         `hasPatterns` [TestOther 1, TestOther 2]
+    , testCase "single \\equals" $ do
+        let input = [fromEquals_ (mkElemVar x) (mkElemVar y)]
+            expect = input
+            actual :: MultiAnd (Predicate RewritingVariableName)
+            actual = MultiAnd.make input
+        assertEqual "" expect (toList actual)
     ]
+  where
+    x, y :: ElementVariable RewritingVariableName
+    x = Mock.xConfig
+    y = Mock.yConfig
 
 hasPatterns :: MultiAnd TestTopBottom -> [TestTopBottom] -> TestTree
 hasPatterns actual expected =
