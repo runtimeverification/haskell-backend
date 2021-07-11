8bf317652ac7987908627f2d107b72e14b980103
RV Jenkins <admin@runtimeverification.com>
2021-07-11 11:08:57 +0000

    Update regression tests

--- test/regression-evm/test-branching-invalid.sh
+++ test/regression-evm/test-branching-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-branching-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-invalid-tmp.in.1VBoRx4bJg --searchType FINAL --search test-branching-invalid-tmp.pattern.B3AsVKUCxL "$@"
+${KORE_EXEC:?} test-branching-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-invalid-tmp.in.kwMCzvqDEp --searchType FINAL --search test-branching-invalid-tmp.pattern.mpKoj8xrOY "$@"
--- test/regression-evm/test-straight-line.sh
+++ test/regression-evm/test-straight-line.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-straight-line-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-tmp.in.0W0j8HEmxN --searchType FINAL --search test-straight-line-tmp.pattern.91EPBLqhJb "$@"
+${KORE_EXEC:?} test-straight-line-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-tmp.in.iJliXoU1Cg --searchType FINAL --search test-straight-line-tmp.pattern.uHhZc4GJMQ "$@"
