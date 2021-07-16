d2599776dea81eb936164507c35dac6ce268481b
dopamane <dwc1295@gmail.com>
2021-07-16 13:06:01 -0700

    add verifiedMyModule

--- kore/test/Test/Kore/CheckFunctions.hs
+++ kore/test/Test/Kore/CheckFunctions.hs
@@ -6,8 +6,46 @@ module Test.Kore.CheckFunctions (
     test_checkFunctions,
 ) where
 
+import qualified Data.Map.Strict as Map (
+    lookup,
+ )
+import Kore.Attribute.Attributes (
+    Attributes (..),
+ )
+import qualified Kore.Attribute.Symbol as Attribute (
+    Symbol,
+ )
+import Kore.BugReport (
+    ExitCode (ExitFailure, ExitSuccess),
+ )
+import qualified Kore.Builtin as Builtin (
+    koreVerifiers,
+ )
+import qualified Kore.Error
+import Kore.IndexedModule.IndexedModule (
+    VerifiedModule,
+ )
+import Kore.Syntax.Definition (
+    Definition (..),
+ )
+import Kore.Syntax.Module (
+    Module (..),
+    ModuleName (..),
+ )
+import qualified Kore.Verified as Verified (
+    Sentence,
+ )
+import Prelude.Kore
 import Test.Kore
+import Test.Kore.Builtin.External (
+    externalize,
+ )
 import Test.Tasty (
+    TestTree,
+    testGroup,
+ )
+import Test.Tasty.HUnit.Ext (
+    assertEqual,
     testCase,
  )
 
@@ -29,7 +67,7 @@ test_checkFunctions =
                                 ]
                             , moduleAttributes = Attributes []
                             }
-                actual = _
+                actual = checkFunctions _
                 expected = ExitSuccess
             assertEqual "" expected actual
         , testCase "Not every equation RHS is a function pattern." $ do
@@ -44,3 +82,23 @@ test_checkFunctions =
                 expected = ExitFailure 3
             assertEqual "" expected actual
         ]
+
+-- TODO: consider sharing code with verifiedMyModule from Test.Kore.Exec
+verifiedMyModule ::
+    Module Verified.Sentence ->
+    VerifiedModule Attribute.Symbol
+verifiedMyModule module_ = indexedModule
+  where
+    indexedModule =
+        fromMaybe
+            (error "Missing module: MY-MODULE")
+            (Map.lookup (ModuleName "MY-MODULE") indexedModules)
+    indexedModules =
+        Kore.Error.assertRight $
+            verifyAndIndexDefinition Builtin.koreVerifiers definition
+    definition =
+        Definition
+            { definitionAttributes = Attributes []
+            , definitionModules =
+                [(fmap . fmap) externalize module_]
+            }
