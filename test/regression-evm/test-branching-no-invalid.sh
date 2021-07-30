ee80293efbe8bee999c509ace511429a31578e3c
ana-pantilie <45069775+ana-pantilie@users.noreply.github.com>
2021-07-30 11:39:27 +0300

    Update regression tests (#2773)
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    Co-authored-by: RV Jenkins <admin@runtimeverification.com>

--- test/regression-evm/test-branching-no-invalid.sh
+++ test/regression-evm/test-branching-no-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.rscfMYRvLY --searchType FINAL --search test-branching-no-invalid-tmp.pattern.azNpCuI5ge "$@"
+${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.pSNulVjGug --searchType FINAL --search test-branching-no-invalid-tmp.pattern.tcSjgepTk5 "$@"
