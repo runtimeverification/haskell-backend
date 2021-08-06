c83fc59e87d28ee45dd194c3ed8289b4c32fbdcf
emarzion <emarzion@gmail.com>
2021-08-06 02:38:40 -0500

    updating script + new tests

--- test/regression-evm/test-straight-line.sh
+++ test/regression-evm/test-straight-line.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-straight-line-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-tmp.in.Hunq3jVYUO --searchType FINAL --search test-straight-line-tmp.pattern.wLtraaqzUC "$@"
+${KORE_EXEC:?} test-straight-line-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-tmp.in.BSSliTGXx7 --searchType FINAL --search test-straight-line-tmp.pattern.50vBlSXEji "$@"
