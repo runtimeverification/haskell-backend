d8aa42d28cb20593f146f8b83e54df32114aee6e
rv-jenkins <admin@runtimeverification.com>
2021-08-09 02:10:22 -0600

    Update regression tests (#2790)
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests

--- test/regression-evm/test-straight-line-no-invalid.sh
+++ test/regression-evm/test-straight-line-no-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-straight-line-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-no-invalid-tmp.in.x8qy89ycxK --searchType FINAL --search test-straight-line-no-invalid-tmp.pattern.wzE15SyD8j "$@"
+${KORE_EXEC:?} test-straight-line-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-no-invalid-tmp.in.13vN4gZuS5 --searchType FINAL --search test-straight-line-no-invalid-tmp.pattern.wmOiysXS9x "$@"
