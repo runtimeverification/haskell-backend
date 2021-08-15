a13d7263acaeabf2daecafd0b523449f48b88d94
RV Jenkins <admin@runtimeverification.com>
2021-08-15 04:15:41 +0000

    Update regression tests

--- test/regression-evm/test-pop1.sh
+++ test/regression-evm/test-pop1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-pop1-definition.kore --module ETHEREUM-SIMULATION --pattern test-pop1-tmp.in.Qd8pDZBmkf "$@"
+${KORE_EXEC:?} test-pop1-definition.kore --module ETHEREUM-SIMULATION --pattern test-pop1-tmp.in.SE8GZvbXeW "$@"
--- test/regression-evm/test-straight-line-no-invalid.sh
+++ test/regression-evm/test-straight-line-no-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-straight-line-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-no-invalid-tmp.in.13vN4gZuS5 --searchType FINAL --search test-straight-line-no-invalid-tmp.pattern.wmOiysXS9x "$@"
+${KORE_EXEC:?} test-straight-line-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-no-invalid-tmp.in.LTp7MfGkfm --searchType FINAL --search test-straight-line-no-invalid-tmp.pattern.EHwXMSJGJr "$@"
