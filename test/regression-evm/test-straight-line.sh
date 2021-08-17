edd0ca2a15383f8f733029c11817ee21b27c8c61
Evan Marzion <emarzion@gmail.com>
2021-08-17 10:10:28 -0500

    Fixing trim-source-paths.sh for macos (#2791)
    * Fixing trim-source-paths.sh for macos
    
    * updating script
    
    * updating tests
    
    * removing extra forward slash character
    
    * updating tests

--- test/regression-evm/test-straight-line.sh
+++ test/regression-evm/test-straight-line.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-straight-line-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-tmp.in.sh2JrZpptl --searchType FINAL --search test-straight-line-tmp.pattern.xxK4qqZnFZ "$@"
+${KORE_EXEC:?} test-straight-line-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-tmp.in.ARiRg2Mp8q --searchType FINAL --search test-straight-line-tmp.pattern.l4YxvvZ68K "$@"
