1f2597920f68994d365218fe8e54e760a7cbeac8
emarzion <emarzion@gmail.com>
2021-08-17 23:46:51 -0500

    update tests

--- test/regression-evm/test-mul0.sh
+++ test/regression-evm/test-mul0.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-mul0-definition.kore --module ETHEREUM-SIMULATION --pattern test-mul0-tmp.in.YLHBW6rMDP "$@"
+${KORE_EXEC:?} test-mul0-definition.kore --module ETHEREUM-SIMULATION --pattern test-mul0-tmp.in.8fU0NNqHmZ "$@"
--- test/regression-evm/test-straight-line.sh
+++ test/regression-evm/test-straight-line.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-straight-line-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-tmp.in.ARiRg2Mp8q --searchType FINAL --search test-straight-line-tmp.pattern.l4YxvvZ68K "$@"
+${KORE_EXEC:?} test-straight-line-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-tmp.in.vCDlm9NuZe --searchType FINAL --search test-straight-line-tmp.pattern.S2J65YjVp0 "$@"
