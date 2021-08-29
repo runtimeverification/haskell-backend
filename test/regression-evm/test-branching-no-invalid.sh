4537b848ec2d53b715eefd9113837b3a65ee6984
RV Jenkins <admin@runtimeverification.com>
2021-08-29 04:29:41 +0000

    Update regression tests

--- test/regression-evm/test-branching-no-invalid.sh
+++ test/regression-evm/test-branching-no-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.GVTZHJlVkP --searchType FINAL --search test-branching-no-invalid-tmp.pattern.3ayMlNrlDB "$@"
+${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.PYxPIWhAB5 --searchType FINAL --search test-branching-no-invalid-tmp.pattern.C5Hoj7U1TY "$@"
