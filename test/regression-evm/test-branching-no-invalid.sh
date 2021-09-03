816e0b16ae80a55cdabb20ea0b53498013a3e524
Raoul Schaffranek <raoul.schaffranek@runtimeverification.com>
2021-09-03 12:05:50 +0200

    Update regression-tests

--- test/regression-evm/test-branching-no-invalid.sh
+++ test/regression-evm/test-branching-no-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.S2xpjNhv7y --searchType FINAL --search test-branching-no-invalid-tmp.pattern.PdKumt2Jc3 "$@"
+${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.RnfxjnRgR2 --searchType FINAL --search test-branching-no-invalid-tmp.pattern.WwuBAXmcb1 "$@"
--- test/regression-evm/test-straight-line-no-invalid.sh
+++ test/regression-evm/test-straight-line-no-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-straight-line-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-no-invalid-tmp.in.iNiVVi1YGK --searchType FINAL --search test-straight-line-no-invalid-tmp.pattern.8USYfmDzea "$@"
+${KORE_EXEC:?} test-straight-line-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-no-invalid-tmp.in.0tPtwTnYtB --searchType FINAL --search test-straight-line-no-invalid-tmp.pattern.SnHr6Oa8go "$@"
