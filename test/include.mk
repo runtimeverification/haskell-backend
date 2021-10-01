55a15eb4616082a5ae16f3f0227a51a0356a732f
emarzion <emarzion@gmail.com>
2021-10-01 01:38:52 -0500

    removing -m flag

--- test/include.mk
+++ test/include.mk
@@ -35,7 +35,7 @@ GOLDEN += $(foreach OUT, $(OUTS), $(OUT).golden)
 
 KOMPILE_OPTS += -d $(DEF_DIR)
 KRUN_OPTS += -d $(DEF_DIR)
-KPROVE_OPTS += -d $(DEF_DIR) -m $(KPROVE_MODULE)
+KPROVE_OPTS += -d $(DEF_DIR)
 KORE_EXEC_OPTS += \
 	$(if $(STORE_PROOFS),\
 		--save-proofs $(STORE_PROOFS),\
