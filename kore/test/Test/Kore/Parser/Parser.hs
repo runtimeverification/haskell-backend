83f1489b2768d9a1757b148d1515f0b8596d8c15
Ana Pantilie <ana.pantilie95@gmail.com>
2021-10-05 15:57:33 +0300

    Add equivalent tests for \right-assoc

--- kore/test/Test/Kore/Parser/Parser.hs
+++ kore/test/Test/Kore/Parser/Parser.hs
@@ -559,6 +559,35 @@ applicationPatternParserTests =
                                 ]
                         }
             )
+        , success
+            "\\right-assoc{}(\\or{s}(\"a\", \"b\"))"
+            ( embedParsedPattern $
+                OrF
+                    Or
+                        { orFirst = StringLiteral "a" & Const & StringLiteralF & embedParsedPattern
+                        , orSecond = StringLiteral "b" & Const & StringLiteralF & embedParsedPattern
+                        , orSort = sortVariableSort "s" :: Sort
+                        }
+            )
+        , success
+            "\\right-assoc{}(\\or{s}(\"a\", \"b\", \"c\"))"
+            ( embedParsedPattern $
+                OrF
+                    Or
+                        { orFirst = StringLiteral "a" & Const & StringLiteralF & embedParsedPattern
+                        , orSecond =
+                            embedParsedPattern $
+                                OrF
+                                    Or
+                                        { orFirst =
+                                            StringLiteral "b" & Const & StringLiteralF & embedParsedPattern
+                                        , orSecond =
+                                            StringLiteral "c" & Const & StringLiteralF & embedParsedPattern
+                                        , orSort = sortVariableSort "s" :: Sort
+                                        }
+                        , orSort = sortVariableSort "s" :: Sort
+                        }
+            )
         , FailureWithoutMessage
             [ ""
             , "var"
