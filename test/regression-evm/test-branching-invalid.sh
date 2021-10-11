b7001f5b0d0c46aea5d255ef7c83499b634cac9a
rv-jenkins <admin@runtimeverification.com>
2021-10-11 02:10:28 -0600

    Update regression tests (#2871)

--- test/regression-evm/test-branching-invalid.sh
+++ test/regression-evm/test-branching-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-branching-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-invalid-tmp.in.DvbLsJsJbb --searchType FINAL --search test-branching-invalid-tmp.pattern.yt9BMYnWxF "$@"
+${KORE_EXEC:?} test-branching-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-invalid-tmp.in.qYUUQOY4i6 --searchType FINAL --search test-branching-invalid-tmp.pattern.mJgXmabpAR "$@"
--- test/regression-evm/test-branching-no-invalid.sh
+++ test/regression-evm/test-branching-no-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.ljvxfGfSxZ --searchType FINAL --search test-branching-no-invalid-tmp.pattern.hboKdG9kl4 "$@"
+${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.QtnoXkZKiq --searchType FINAL --search test-branching-no-invalid-tmp.pattern.d19duwx71Y "$@"
