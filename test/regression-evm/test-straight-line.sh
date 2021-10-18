2ac9cfd04c8ddae9c5850086d159e41cfa57e5fc
Ana Pantilie <ana.pantilie95@gmail.com>
2021-10-18 13:45:38 +0300

    Update KEVM regression tests

--- test/regression-evm/test-straight-line.sh
+++ test/regression-evm/test-straight-line.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-straight-line-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-tmp.in.J96MuzvKlo --searchType FINAL --search test-straight-line-tmp.pattern.bwKKfVQceF "$@"
+${KORE_EXEC:?} test-straight-line-definition.kore --module ETHEREUM-SIMULATION --pattern test-straight-line-tmp.in.rcL76pD3Mb --searchType FINAL --search test-straight-line-tmp.pattern.2KjnZcisX5 "$@"
