2197c6700efab7de3460e195c39b3271a5a7ec21
Ana Pantilie <ana.pantilie95@gmail.com>
2021-10-22 15:39:14 +0300

    Regenerate regression tests with modified prelude.kore

--- test/regression-evm/test-branching-no-invalid.sh
+++ test/regression-evm/test-branching-no-invalid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.UnujJ8by3U --searchType FINAL --search test-branching-no-invalid-tmp.pattern.52TXOeS6DM "$@"
+${KORE_EXEC:?} test-branching-no-invalid-definition.kore --module ETHEREUM-SIMULATION --pattern test-branching-no-invalid-tmp.in.FIQs9pbdGn --searchType FINAL --search test-branching-no-invalid-tmp.pattern.YLqyF9AEcz "$@"
