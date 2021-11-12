412966545f30d23a0c3d052498ee58833d190645
Andrei Burdușa <andrei.burdusa@zoho.com>
2021-11-12 13:56:07 +0200

    Regenerate other tests

--- test/regression-evm/test-pop1.sh
+++ test/regression-evm/test-pop1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-pop1-definition.kore --module ETHEREUM-SIMULATION --pattern test-pop1-tmp.in.pd3s8y7I8v "$@"
+${KORE_EXEC:?} test-pop1-definition.kore --module ETHEREUM-SIMULATION --pattern test-pop1-tmp.in.e033sSjACi "$@"
