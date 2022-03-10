a85b1b4f6a5962e258e3f4852affdd07d1570a46
JKTKops <zerglingk9012@gmail.com>
2022-03-10 20:42:42 +0000

    Add script for running tests from elrond-multisig

---
+++ scripts/elrond-regression-tests.sh
@@ -0,0 +1,24 @@
+set -euo pipefail
+
+prep-elrond() {
+    cd $KORE
+    git clone git@github.com:runtimeverification/elrond-multisig.git
+    cd elrond-multisig
+    cd kompile-tool
+    ./prepare-k.sh
+    bazel clean
+}
+
+run-elrond() {
+    cd $KORE/elrond-multisig
+    bazel run //protocol-correctness/proof/lemmas/0/signers:lemma-at-most-this-signer-0-count-can-sign-function
+    bazel run //protocol-correctness/proof/lemmas/1/list/contains:lemma-list-contains-last-to-start
+    bazel run //protocol-correctness/proof/functions:proof-change-user-role-New
+    bazel run //protocol-correctness/proof/functions:proof-perform-action-add-proposer-None
+    bazel run //protocol-correctness/proof/malicious-user/can-be-deleted/run-external-call-from-user:proof-recfu-sign-error
+}
+
+export KORE=$(pwd)
+prep-elrond
+run-elrond
+rm -rf elrond-multisig
