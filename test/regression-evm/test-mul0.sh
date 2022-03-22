83578db5428222bfe50524e43cc2a0f52c529b6b
rv-jenkins <admin@runtimeverification.com>
2022-03-22 05:32:38 -0600

    Update regression tests (#2998)
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    * Update regression tests
    
    Co-authored-by: ana-pantilie <45069775+ana-pantilie@users.noreply.github.com>

--- test/regression-evm/test-mul0.sh
+++ test/regression-evm/test-mul0.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${KORE_EXEC:?} test-mul0-definition.kore --module ETHEREUM-SIMULATION --pattern test-mul0-tmp.in.r6gGKt2ZXW "$@"
+${KORE_EXEC:?} test-mul0-definition.kore --module ETHEREUM-SIMULATION --pattern test-mul0-tmp.in.6d9Nl3sfF4 "$@"
