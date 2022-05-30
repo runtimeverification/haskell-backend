dc2e668418a8e592e563203faac31e5f20b86a71
Sam Balco <goodlyrottenapple@gmail.com>
2022-05-30 08:55:52 +0100

    format with nixfmt

--- default.nix
+++ default.nix
@@ -1,72 +1,59 @@
-{ profiling ? false
-, release ? false
-, threaded ? !profiling
+{ profiling ? false, release ? false, threaded ? !profiling
 , checkMaterialization ? false
 
-# Override `src` when this project is imported as a Git submodule:
-#
-# > ttuegel.cleanGitSubtree {
-# >   name = "kore";
-# >   src = ./parent/repo;
-# >   subDir = "path/to/submodule";
-# > };
-#
-# Use `cleanGitSubtree` whenever possible to preserve the same source code
-# layout as the runtimeverification/haskell-backend repository (to enable cache re-use).
-#
-, src ? null
-}:
+  # Override `src` when this project is imported as a Git submodule:
+  #
+  # > ttuegel.cleanGitSubtree {
+  # >   name = "kore";
+  # >   src = ./parent/repo;
+  # >   subDir = "path/to/submodule";
+  # > };
+  #
+  # Use `cleanGitSubtree` whenever possible to preserve the same source code
+  # layout as the runtimeverification/haskell-backend repository (to enable cache re-use).
+  #
+, src ? null }:
 
 let
   sources = import ./nix/sources.nix;
-  haskell-nix = import sources."haskell.nix" {};
+  haskell-nix = import sources."haskell.nix" { };
   inherit (haskell-nix) pkgs;
   inherit (pkgs) lib;
 
-  ttuegel =
-    let
-      src = builtins.fetchGit {
-        url = "https://github.com/ttuegel/nix-lib";
-        rev = "66bb0ab890ff4d828a2dcfc7d5968465d0c7084f";
-        ref = "main";
-      };
-    in import src { inherit pkgs; };
-in
+  ttuegel = let
+    src = builtins.fetchGit {
+      url = "https://github.com/ttuegel/nix-lib";
+      rev = "66bb0ab890ff4d828a2dcfc7d5968465d0c7084f";
+      ref = "main";
+    };
+  in import src { inherit pkgs; };
 
-let
+in let
   project = pkgs.haskell-nix.stackProject {
     src = ttuegel.cleanSourceWith {
       name = "kore";
       src = ttuegel.orElse src (ttuegel.cleanGitSubtree { src = ./.; });
-      ignore = [
-        "/*"
-        "!/stack.yaml"
-        "!/kore"
-      ];
+      ignore = [ "/*" "!/stack.yaml" "!/kore" ];
     };
     inherit checkMaterialization;
     materialized = ./nix/kore.nix.d;
-    modules = [
-      {
-        # package *
-        enableLibraryProfiling = true;
-        profilingDetail = "none";
-        # package kore
-        packages.kore = {
-          flags = {
-            inherit release threaded;
-          };
-          enableLibraryProfiling = profiling;
-          enableExecutableProfiling = profiling;
-          profilingDetail = "toplevel-functions";
+    modules = [{
+      # package *
+      enableLibraryProfiling = true;
+      profilingDetail = "none";
+      # package kore
+      packages.kore = {
+        flags = { inherit release threaded; };
+        enableLibraryProfiling = profiling;
+        enableExecutableProfiling = profiling;
+        profilingDetail = "toplevel-functions";
 
-          # Add Z3 to PATH for unit tests.
-          components.tests.kore-test.preCheck = ''
-            export PATH="$PATH''${PATH:+:}${lib.getBin pkgs.z3}/bin"
-          '';
-        };
-      }
-    ];
+        # Add Z3 to PATH for unit tests.
+        components.tests.kore-test.preCheck = ''
+          export PATH="$PATH''${PATH:+:}${lib.getBin pkgs.z3}/bin"
+        '';
+      };
+    }];
   };
 
   shell = import ./shell.nix { inherit default checkMaterialization; };
@@ -81,23 +68,19 @@ let
     ${project.stack-nix.passthru.updateMaterialized}
   '';
 
-  default =
-    {
-      inherit pkgs prelude-kore project rematerialize;
-      cache = [
-        project.roots
-        (pkgs.haskell-nix.withInputs shell)
-      ];
+  default = {
+    inherit pkgs prelude-kore project rematerialize;
+    cache = [ project.roots (pkgs.haskell-nix.withInputs shell) ];
 
-      kore = pkgs.symlinkJoin {
-        name = "kore-${version}";
-        paths = pkgs.lib.attrValues project.kore.components.exes;
-      };
-
-      # Change the compiler when updating our own resolver.
-      compiler-nix-name = "ghc8107";
-      ghcVersion = "8.10.7";
-      index-state = "2021-12-03T00:00:00Z";
+    kore = pkgs.symlinkJoin {
+      name = "kore-${version}";
+      paths = pkgs.lib.attrValues project.kore.components.exes;
     };
+
+    # Change the compiler when updating our own resolver.
+    compiler-nix-name = "ghc8107";
+    ghcVersion = "8.10.7";
+    index-state = "2021-12-03T00:00:00Z";
+  };
 
 in default
--- test.nix
+++ test.nix
@@ -1,67 +1,66 @@
 let
   sources = import ./nix/sources.nix;
-  pinned = import sources."nixpkgs" { config = {}; overlays = []; };
-in
+  pinned = import sources."nixpkgs" {
+    config = { };
+    overlays = [ ];
+  };
 
-{ pkgs ? pinned
-, test ? null
-}:
+in { pkgs ? pinned, test ? null }:
 
 let
   inherit (pkgs) stdenv lib;
   inherit (pkgs) bison diffutils jq miller ncurses z3;
 
-  ttuegel =
-    let
-      src = builtins.fetchGit {
-        url = "https://github.com/ttuegel/nix-lib";
-        rev = "66bb0ab890ff4d828a2dcfc7d5968465d0c7084f";
-        ref = "main";
-      };
-    in import src { inherit pkgs; };
+  ttuegel = let
+    src = builtins.fetchGit {
+      url = "https://github.com/ttuegel/nix-lib";
+      rev = "66bb0ab890ff4d828a2dcfc7d5968465d0c7084f";
+      ref = "main";
+    };
+  in import src { inherit pkgs; };
 
-  default = import ./. {};
+  default = import ./. { };
   inherit (default) kore prelude-kore;
 
-  kframework =
-    let
-      tag = lib.fileContents ./deps/k_release;
-      url = "https://github.com/runtimeverification/k/releases/download/${tag}/release.nix";
-      args = import (builtins.fetchurl { inherit url; });
-      src = pkgs.fetchgit args;
-    in import src {};
+  kframework = let
+    tag = lib.fileContents ./deps/k_release;
+    url =
+      "https://github.com/runtimeverification/k/releases/download/${tag}/release.nix";
+    args = import (builtins.fetchurl { inherit url; });
+    src = pkgs.fetchgit args;
+  in import src { };
 
   k = kframework.k.override {
     haskell-backend = kore;
     inherit prelude-kore;
   };
 
-in
-
-stdenv.mkDerivation {
+in stdenv.mkDerivation {
   name = "kore-test";
-  src = ttuegel.cleanGitSubtree { name = "kore"; src = ./.; };
+  src = ttuegel.cleanGitSubtree {
+    name = "kore";
+    src = ./.;
+  };
   preferLocalBuild = true;
   buildInputs = [
-    k kore  # some tests use kore-exec directly, others run through the frontend
-    ncurses  # TODO: .../lib/kframework/setenv: line 31: tput: command not found
-    jq miller # processing test statistics
+    k
+    kore # some tests use kore-exec directly, others run through the frontend
+    ncurses # TODO: .../lib/kframework/setenv: line 31: tput: command not found
+    jq
+    miller # processing test statistics
     z3
   ];
   configurePhase = ''
     export TOP=$(pwd)
   '';
   KORE_EXEC = "${lib.getBin kore}/bin/kore-exec";
-  buildFlags =
-    [
-      "KORE_PARSER=kore-parser"
-      "KORE_EXEC=kore-exec"
-      "KORE_REPL=kore-repl"
-      "--output-sync"
-      "test"
-    ]
-    ++ lib.optional (test != null) "-C ${test}"
-    ;
+  buildFlags = [
+    "KORE_PARSER=kore-parser"
+    "KORE_EXEC=kore-exec"
+    "KORE_REPL=kore-repl"
+    "--output-sync"
+    "test"
+  ] ++ lib.optional (test != null) "-C ${test}";
   enableParallelBuilding = true;
   preBuild = ''
     cd test
