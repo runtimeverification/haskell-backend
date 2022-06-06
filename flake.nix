82ca0feada1a5b12fdf66a9bfc3b6e8238c51662
Sam Balco <goodlyrottenapple@gmail.com>
2022-06-06 12:12:23 +0100

    should work on apple m1

--- flake.nix
+++ flake.nix
@@ -3,76 +3,90 @@
   inputs = {
     haskell-nix.url = "github:input-output-hk/haskell.nix";
     nixpkgs.follows = "haskell-nix/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    z3src = {
+    z3-src = {
       url = "github:Z3Prover/z3/z3-4.8.15";
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, flake-utils, haskell-nix, z3src }:
-    flake-utils.lib.eachSystem [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ] (system:
-      let
-        pkgs = import nixpkgs {
+  outputs = { self, nixpkgs, flake-utils, haskell-nix, z3-src }:
+    let
+      perSystem = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      nixpkgsFor = system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ haskell-nix.overlay ];
+          inherit (haskell-nix) config;
+        };
+      nixpkgsFor' = system:
+        import nixpkgs {
           inherit system;
           inherit (haskell-nix) config;
         };
-        haskell-backend =
-          haskell-nix.legacyPackages.${system}.haskell-nix.stackProject {
-            src = ./.;
-            materialized = ./nix/kore.nix.d;
-            compiler-nix-name = "ghc8107";
+
+      projectOverlay = { shell, pkgs }:
+        pkgs.haskell-nix.stackProject' ({
+          src = ./.;
+          materialized = ./nix/kore.nix.d;
+          compiler-nix-name = "ghc8107";
+          inherit shell;
+        });
+    in {
+      prelude-kore = ./src/main/kore/prelude.kore;
+      project = perSystem (system:
+        let
+          pkgs = nixpkgsFor system;
+          pkgs' = nixpkgsFor' system;
+        in projectOverlay {
+          inherit pkgs;
+
+          shell = {
+            withHoogle = true;
+
+            exactDeps = true;
+
+            # We use the ones from Nixpkgs, since they are cached reliably.
+            # Eventually we will probably want to build these with haskell.nix.
+            nativeBuildInputs = [
+              pkgs'.cabal-install
+              pkgs'.hlint
+              pkgs'.haskellPackages.fourmolu
+              pkgs'.stack
+              pkgs'.nixfmt
+            ];
           };
-        version = haskell-backend.kore.components.exes.kore-exec.version;
-      in {
-        defaultPackage = pkgs.symlinkJoin {
-          name = "kore-${version}";
-          paths = pkgs.lib.attrValues haskell-backend.kore.components.exes;
-        };
-        devShell = haskell-backend.shellFor {
-          buildInputs = with pkgs; [
-            gnumake
-            fd
-            z3
-            # hls-renamed
-            ghcid
-            hlint
-            # fourmolu
-            cabal-install
-            stack
-          ];
-        };
-      }) // {
-        prelude-kore = ./src/main/kore/prelude.kore;
-        overlay = nixpkgs.lib.composeManyExtensions [
-          haskell-nix.overlay
+        });
 
-          (final: prev: {
-            # This overlay adds our project to pkgs
-            k-haskell-backend = prev.haskell-nix.stackProject {
-              src = prev.haskell-nix.haskellLib.cleanGit {
-                name = "k-haskell-backend-src";
-                src = ./.;
-              };
-              compiler-nix-name = "ghc8107";
-            };
+      flake = perSystem (system: self.project.${system}.flake { });
+      packages = perSystem (system: self.flake.${system}.packages);
+
+      apps = perSystem (system: self.flake.${system}.apps);
+      devShell = perSystem (system: self.flake.${system}.devShell);
+
+      overlay = nixpkgs.lib.composeManyExtensions [
+        haskell-nix.overlay
+
+        (final: prev:
+          projectOverlay {
+            pkgs = prev;
+            shell = { };
           })
 
-          (final: prev: {
-            z3 = prev.z3.overrideAttrs (old: {
-              src = z3src;
-              version =
-                # hacky way of keeping the version number of this derivation consistent with
-                # the z3src input.
-                let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-                in builtins.replaceStrings [ "z3-" ] [ "" ]
-                lock.nodes.z3src.original.ref;
-            });
-          })
-        ];
-      };
+        (final: prev: {
+          z3 = prev.z3.overrideAttrs (old: {
+            src = z3-src;
+            version =
+              # hacky way of keeping the version number of this derivation consistent with
+              # the z3-src input.
+              let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+              in builtins.replaceStrings [ "z3-" ] [ "" ]
+              lock.nodes.z3-src.original.ref;
+          });
+        })
+      ];
+    };
 }
