50cd77bd14c17a0784ab7bf1c081f86a8a35b3ea
Sam Balco <goodlyrottenapple@gmail.com>
2022-06-09 17:37:11 +0100

    cleanup

--- flake.nix
+++ flake.nix
@@ -28,63 +28,73 @@
           inherit (haskell-nix) config;
         };
 
-      haskell-src = {pkgs , lts ? null, postPatch ? ""} :
+      haskell-src = { pkgs, postPatch ? "" }:
         pkgs.applyPatches {
           src = ./.;
           postPatch = ''
             substituteInPlace kore/src/Kore/VersionInfo.hs \
               --replace '$(GitRev.gitHash)' '"${self.rev or "dirty"}"'
-
-            ${if lts != null then ''sed -i 's/^resolver:.*$/resolver: ${lts}/g' stack.yaml'' else ""}
             ${postPatch}
           '';
         };
 
-      projectOverlay = { pkgs, src, shell ? { }, compiler-nix-name ? "ghc8107" }:
-        let self =
-        pkgs.haskell-nix.stackProject' ({
-          inherit shell src compiler-nix-name;
-          materialized = ./nix/kore-${compiler-nix-name}.nix.d;
-        }); in
-          self // {
-          rematerialize-kore = pkgs.writeShellScriptBin "rematerialize-kore-nix-${compiler-nix-name}" ''
+      projectOverlay =
+        { pkgs, src, shell ? { }, compiler-nix-name ? "ghc8107" }:
+        let
+          self = pkgs.haskell-nix.stackProject' ({
+            inherit shell src compiler-nix-name;
+            materialized = ./nix + "/kore-${compiler-nix-name}.nix.d";
+          });
+        in self // {
+          rematerialize-kore = pkgs.writeShellScriptBin
+            "rematerialize-kore-nix-${compiler-nix-name}" ''
               #!/bin/sh
               ${self.stack-nix.passthru.generateMaterialized} ./nix/kore-${compiler-nix-name}.nix.d
             '';
         };
 
-      projectForGhc = {ghcVersion, lts ? null}: perSystem (system:
-        let
-          pkgs = nixpkgsFor system;
-          pkgs' = nixpkgsFor' system;
-        in projectOverlay {
-          inherit pkgs;
-          src = haskell-src {inherit lts;  pkgs =pkgs'; postPatch = ''cp stack-nix-ghc9.yaml stack.yaml'';};
-          compiler-nix-name = ghcVersion;
-          shell = {
-            # withHoogle = true;
+      projectForGhc = { ghc, stack-yaml ? null }:
+        perSystem (system:
+          let
+            pkgs = nixpkgsFor system;
+            pkgs' = nixpkgsFor' system;
+          in projectOverlay {
+            inherit pkgs;
+            src = haskell-src {
+              pkgs = pkgs';
+              postPatch = if stack-yaml != null then
+                "cp ${stack-yaml} stack.yaml"
+              else
+                "";
+            };
+            compiler-nix-name = ghc;
+            shell = {
+              # withHoogle = true;
 
-            # exactDeps = true;
+              # exactDeps = true;
 
-            # We use the ones from Nixpkgs, since they are cached reliably.
-            # Eventually we will probably want to build these with haskell.nix.
-            nativeBuildInputs = [
-              pkgs'.cabal-install
-              pkgs'.hlint
-              pkgs'.haskellPackages.fourmolu
-              pkgs'.stack
-              pkgs'.nixfmt
-            ] ++ (if system == "aarch64-darwin" then
-              [ pkgs'.llvm_12 ]
-            else
-              [ ]);
-          };
-        });
+              # We use the ones from Nixpkgs, since they are cached reliably.
+              # Eventually we will probably want to build these with haskell.nix.
+              nativeBuildInputs = [
+                pkgs'.cabal-install
+                pkgs'.hlint
+                pkgs'.haskellPackages.fourmolu
+                pkgs'.stack
+                pkgs'.nixfmt
+              ] ++ (if system == "aarch64-darwin" then
+                [ pkgs'.llvm_12 ]
+              else
+                [ ]);
+            };
+          });
     in {
       prelude-kore = ./src/main/kore/prelude.kore;
 
-      project = projectForGhc {ghcVersion = "ghc8107";};
-      projectGhc9 = projectForGhc {ghcVersion = "ghc902"; lts = "lts-19.9"; };
+      project = projectForGhc { ghc = "ghc8107"; };
+      projectGhc9 = projectForGhc {
+        ghc = "ghc902";
+        stack-yaml = "stack-nix-ghc9.yaml";
+      };
 
       flake = perSystem (system: self.project.${system}.flake { });
       flakeGhc9 = perSystem (system: self.projectGhc9.${system}.flake { });
@@ -93,28 +103,23 @@
         self.flake.${system}.packages // {
           rematerialize = self.project.${system}.rematerialize-kore;
           rematerializeGhc9 = self.projectGhc9.${system}.rematerialize-kore;
-
-          kore-exec-ghc9 = self.flakeGhc9.${system}.packages."kore:exe:kore-exec";
-
-          # rematerializeGhc9 = rematerialize-kore {
-          #   pkgs = nixpkgsFor' system;
-          #   project = self.projectGhc9.${system};
-          #   ghcVersion = "-ghc923";
-          # };
         });
 
       apps = perSystem (system: self.flake.${system}.apps);
+
       devShells = perSystem (system: {
         default = self.flake.${system}.devShell;
         ghc9 = self.flakeGhc9.${system}.devShell;
       });
+      devShell = perSystem (system: self.devShells.${system}.default);
+
       overlay = nixpkgs.lib.composeManyExtensions [
         haskell-nix.overlay
 
         (final: prev: {
           haskell-backend-stackProject = projectOverlay {
             pkgs = prev;
-            src = haskell-src {pkgs = prev;};
+            src = haskell-src { pkgs = prev; };
           };
         })
 
