8df0294c21108aeaad70bee61a203eb01d965be0
Jost Berthold <jost.berthold@gmail.com>
2022-07-04 14:55:40 +1000

    WIP implementing KorePattern -> ParsedPattern

--- kore/src/KoreJson.hs
+++ kore/src/KoreJson.hs
@@ -17,11 +17,11 @@ import GHC.Generics -- FIXME switch to TH-generated Json instances
 import Kore.Attribute.Attributes (ParsedPattern)
 import Kore.Internal.Pattern (Pattern)
 import Kore.Syntax.PatternF (PatternF (..))
-
 -- import Kore.Internal.TermLike.TermLike (TermLikeF(..))
 import Kore.Parser (embedParsedPattern)
 import Kore.Sort qualified as Kore
-import Kore.Syntax.Variable (ElementVariableName (..), SetVariableName (..), SomeVariable (..), SomeVariableName (..), Variable (..), VariableName (..))
+import Kore.Syntax qualified as Kore
+import Kore.Syntax.Variable (ElementVariableName (..), SetVariableName (..), SomeVariableName (..), Variable (..), VariableName (..))
 import Prelude.Kore as Prelude
 
 {- | Json representation of Kore patterns as a Haskell type.
@@ -46,7 +46,7 @@ data KorePattern
       -- | application pattern (symbol, [arg] )
       KJApp
         { name :: Id -- may start by a '\\'
-        , sort :: Sort
+        , sorts :: [Sort]
         , args :: [KorePattern]
         }
     | -- | string literal
@@ -159,7 +159,7 @@ instance FromJSON Sort where
 
 -- TODO could omit the args field if empty (custom instance)
 
--- names and arity of known ML connectives
+-- names of known ML connectives. Arity checked during conversion
 data Connective
     = Top
     | Bottom
@@ -173,16 +173,6 @@ data Connective
 
 -- string-tag encoded in the generated instance. Should they be lower-case?
 
-connArity :: Connective -> Int
-connArity Top = 0
-connArity Bottom = 0
-connArity Not = 1
-connArity And = 2
-connArity Or = 2
-connArity Implies = 2
-connArity Iff = 2
-
---
 data Quant
     = Forall
     | Exists
@@ -223,6 +213,8 @@ decodePattern bs =
 data JsonError
     = -- | Problem reported by json parser
       ParseError String
+    | -- | Wrong arg count for a connective or other construct
+      WrongArgCount String Int
     | -- | Inconsistent data parsed. TODO: refine!
       KoreError String
     | NotImplemented String
@@ -234,31 +226,80 @@ decodeKoreJson = Json.eitherDecode'
 -- see Parser.y
 toParsedPattern :: KorePattern -> Either JsonError ParsedPattern
 toParsedPattern = \case
-    KJEVar n s -> do
-        variableName <- eVarName n
-        variableSort <- mkSort s
-        pure $
-            embedParsedPattern $
-                VariableF $
-                    Const $
-                        Variable{variableName, variableSort}
-    x -> Prelude.Left . NotImplemented $ show x
+    KJEVar n s ->
+      embedVar (SomeVariableNameElement . ElementVariableName) n s
+
+    KJSVar n s ->
+      embedVar (SomeVariableNameSet . SetVariableName) n s
+
+    KJApp n ss as ->
+      fmap (embedParsedPattern . ApplicationF) $
+      Kore.Application <$> toSymbol n ss <*> traverse toParsedPattern as
+
+    KJString t ->
+      embedParsedPattern . StringLiteralF . Const <$> pure (Kore.StringLiteral t)
+
+    KJConnective c s as ->
+      embedParsedPattern <$> mkConnective c s as
+
+    x -> todo $ show x
+
   where
-    mkSort :: Sort -> Either JsonError Kore.Sort
-    mkSort Sort{name, args} =
-        fmap (Kore.SortActualSort . Kore.SortActual (koreId name)) $
-            mapM mkSort args
-    mkSort (SortVariable name) =
-        pure . Kore.SortVariableSort $ Kore.SortVariable (koreId $ Id name)
+    todo = Prelude.Left . NotImplemented
 
-    koreId :: Id -> Kore.Id
-    koreId (Id name) = Kore.Id name Kore.AstLocationNone
+    embedVar :: (VariableName -> SomeVariableName VariableName)
+             -> Id -> Sort -> Either JsonError ParsedPattern
+    embedVar cons n s =
+      fmap (embedParsedPattern . VariableF . Const) $
+      Variable <$> mkVarName cons n <*> mkSort s
 
-    eVarName :: Id -> Either JsonError (SomeVariableName VariableName)
-    eVarName = pure . SomeVariableNameElement . ElementVariableName . flip VariableName Nothing . koreId
+    mkVarName :: (VariableName -> SomeVariableName VariableName)
+            -> Id
+            -> Either JsonError (SomeVariableName VariableName)
+    mkVarName embed = pure . embed . flip VariableName Nothing . koreId
+
+    toSymbol :: Id -> [Sort] -> Either JsonError Kore.SymbolOrAlias
+    toSymbol n sorts = Kore.SymbolOrAlias <$> pure (koreId n) <*> traverse mkSort sorts
 
 -- TODO check well-formed (initial letter, char. set)
 -- FIXME do we need to read a numeric suffix? (-> Parser.y:getVariableName)
+
+koreId :: Id -> Kore.Id
+koreId (Id name) = Kore.Id name Kore.AstLocationNone
+
+mkSort :: Sort -> Either JsonError Kore.Sort
+mkSort Sort{name, args} =
+  fmap (Kore.SortActualSort . Kore.SortActual (koreId name)) $
+  mapM mkSort args
+mkSort (SortVariable name) =
+  pure . Kore.SortVariableSort $ Kore.SortVariable (koreId $ Id name)
+
+mkConnective :: Connective
+             -> Sort
+             -> [KorePattern]
+             -> Either JsonError (PatternF VariableName ParsedPattern)
+mkConnective Top s [] =
+  TopF . Kore.Top <$> (mkSort s)
+mkConnective Bottom s [] =
+  BottomF . Kore.Bottom <$> (mkSort s)
+mkConnective Not s [a] =
+  fmap NotF $
+  Kore.Not <$> mkSort s <*> toParsedPattern a
+mkConnective And s [a,b] =
+  fmap AndF $
+  Kore.And <$> mkSort s <*> toParsedPattern a <*> toParsedPattern b
+mkConnective Or s [a,b] =
+  fmap OrF $
+  Kore.Or <$> mkSort s <*> toParsedPattern a <*> toParsedPattern b
+mkConnective Implies s [a,b] =
+  fmap ImpliesF $
+  Kore.Implies <$> mkSort s <*> toParsedPattern a <*> toParsedPattern b
+mkConnective Iff s [a,b] =
+  fmap IffF $
+  Kore.Iff <$> mkSort s <*> toParsedPattern a <*> toParsedPattern b
+-- fall-through cases because of wrong argument count
+mkConnective conn _ as = Prelude.Left $ WrongArgCount (show conn) (length as)
+
 
 ------------------------------------------------------------
 -- writing
