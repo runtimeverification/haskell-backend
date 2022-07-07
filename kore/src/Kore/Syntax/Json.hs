f8f4d75bacdd74e89bbe84490e257a9e02c7ba9e
Jost Berthold <jost.berthold@gmail.com>
2022-07-07 23:16:13 +1000

    move and split modules to final location, tasty test

---
+++ kore/src/Kore/Syntax/Json.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Kore.Syntax.Json (
+    -- API
+    JsonError (..),
+    encodePattern,
+    encodeKoreJson,
+    decodePattern,
+    decodeKoreJson,
+) where
+
+import Kore.Syntax.Json.Internal
+
+import Data.Aeson as Json
+import Data.Aeson.Encode.Pretty as Json
+import Data.ByteString.Lazy (ByteString)
+import Data.Either.Extra hiding (Left, Right)
+import Kore.Attribute.Attributes (ParsedPattern)
+import Kore.Syntax qualified as Kore
+import Kore.Syntax.Variable (VariableName (..))
+import Prelude.Kore
+
+-- reading
+
+{- | read text into KorePattern, then check for consistency and
+ construct a ParsedPattern
+-}
+decodePattern :: ByteString -> Either JsonError ParsedPattern
+decodePattern bs =
+    mapLeft ParseError (decodeKoreJson bs) >>= toParsedPattern
+
+-- | low-level: read text into KorePattern
+decodeKoreJson :: ByteString -> Either String KorePattern
+decodeKoreJson = Json.eitherDecode'
+
+------------------------------------------------------------
+-- writing
+
+-- | Write a Pattern to a json byte string.
+encodePattern :: Kore.Pattern VariableName ann -> ByteString
+encodePattern = encodeKoreJson . fromPattern
+
+encodeKoreJson :: KorePattern -> ByteString
+encodeKoreJson = Json.encodePretty' prettyJsonOpts
+
+prettyJsonOpts :: Json.Config
+prettyJsonOpts =
+    defConfig
+        { confIndent = Spaces 2
+        , confCompare =
+            keyOrder -- retains the field order in all constructors
+                [ "tag"
+                , "assoc"
+                , "name"
+                , "symbol"
+                , "sort"
+                , "sorts"
+                , "var"
+                , "varSort"
+                , "argSort"
+                , "resultSort"
+                , "arg"
+                , "args"
+                , "source"
+                , "dest"
+                , "value"
+                ]
+        }
--- kore/test/Test/Kore/Syntax/Json.hs
+++ kore/test/Test/Kore/Syntax/Json.hs
@@ -1,5 +1,7 @@
-module Test.KoreJson (
-    module Test.KoreJson,
+module Test.Kore.Syntax.Json (
+    testRoundTrips, -- Tasty wrapper
+    showExamples, -- convenience function for the repl
+    roundTripTests,
 ) where
 
 import Control.Monad (forever)
@@ -11,8 +13,11 @@ import Hedgehog
 import Hedgehog.Gen qualified as Gen
 import Hedgehog.Range qualified as Range
 import Kore.Attribute.Attributes (ParsedPattern)
-import KoreJson
+import Kore.Syntax.Json
+import Kore.Syntax.Json.Internal -- for testing and generating test data
 import Prelude.Kore hiding (Left, Right)
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.Hedgehog
 
 genKorePattern :: Gen KorePattern
 genKorePattern =
@@ -147,6 +152,18 @@ showExamples =
 
 ----------------------------------------
 -- Tests
+
+-- Tasty interface, whole group only
+testRoundTrips :: [TestTree]
+testRoundTrips =
+    [ testGroup
+        "Json -> KorePattern -> ParsedPattern Round trip tests"
+        [ testProperty "KorePattern -> json -> KorePattern" jsonRoundTrip
+        , testProperty "KorePattern (no multi-things) -> ParsedPattern -> KorePattern" parsedRoundTrip
+        , testProperty "ParsedPattern -> KorePattern -> KorePattern" korePatternRoundTrip
+        , testProperty "json (valid, no multi-things) -> ParsedPattern -> json" fullRoundTrip
+        ]
+    ]
 
 roundTripTests :: Group
 roundTripTests = roundTripTestsWith 1000
