d82e72e2a40f88c059c093e8dbdb6e257fab4997
Sam Balco <goodlyrottenapple@gmail.com>
2022-08-17 09:21:29 +0100

    Speed up CI for GHC9 by building non-profile version of kore-exec

--- flake.nix
+++ flake.nix
@@ -177,7 +177,8 @@
         self.flake.${system}.packages // {
           update-cabal = self.project.${system}.rematerialize-kore;
           update-cabal-ghc9 = self.projectGhc9.${system}.rematerialize-kore;
-
+          kore-exec-ghc9 =
+            self.projectGhc9.${system}.hsPkgs.kore.components.exes.kore-exec;
           kore-exec-prof =
             self.projectGhc9ProfilingEventlog.${system}.hsPkgs.kore.components.exes.kore-exec;
           kore-exec-prof-infotable =
