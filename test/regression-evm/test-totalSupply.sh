0c75536f4a9619649ae9ea821a15b1a0e4b8521c
Ana Pantilie <45069775+ana-pantilie@users.noreply.github.com>
2022-09-22 20:44:46 +0300

    Rewrite script which generates regressions tests (#3261)
    * Rewrite script which generates regressions tests
    
    * Clean-up and remove old script
    
    * WIP: new target for running regression tests
    
    * Replace old files with newly generated ones
    
    * WIP: generate bug report tests on old structure
    
    * Generate regressions test files with old structure
    
    * Add generated tests
    
    * Add golden files
    
    * Clean-up
    
    * Switch to linux sed
    
    Co-authored-by: rv-jenkins <admin@runtimeverification.com>

--- test/regression-evm/test-totalSupply.sh
+++ test/regression-evm/test-totalSupply.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
-${KORE_EXEC:?} test-totalSupply-definition.kore --module VERIFICATION --prove test-totalSupply-spec.kore --spec-module TOTALSUPPLY-SPEC "$@"
+exec kore-exec \
+    test-totalSupply-definition.kore \
+    --output test-totalSupply.sh.out \
+    --module VERIFICATION \
+    --strategy all \
+    --max-counterexamples 1 \
+    --smt-timeout 40 \
+    --smt-retry-limit 1 \
+    --smt-reset-interval 100 \
+    --smt z3 \
+    --log-level \
+    warning \
+    --enable-log-timestamps \
+    --prove test-totalSupply-spec.kore \
+    --spec-module TOTALSUPPLY-SPEC \
+    --graph-search breadth-first \
+     \
+     \
+     \
+     \
+    "$@"
