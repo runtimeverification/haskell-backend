62c0def80429a77cf359a60f7ddb355ae9ff0188
Samuel Balco <goodlyrottenapple@gmail.com>
2022-10-05 13:03:13 +0100

    `kore-rpc` now uses aeson-pretty for encoding JSON messages (#3295)
    * kore-rpc now uses aeson-pretty for encoding JSON messages
    
    * Format with fourmolu
    
    * cleanup
    
    * further cleanup
    
    Co-authored-by: github-actions <github-actions@github.com>

--- nix/kore-ghc8107.nix.d/kore.nix
+++ nix/kore-ghc8107.nix.d/kore.nix
@@ -41,6 +41,7 @@
         (hsPkgs."co-log" or (errorHandler.buildDepError "co-log"))
         (hsPkgs."comonad" or (errorHandler.buildDepError "comonad"))
         (hsPkgs."compact" or (errorHandler.buildDepError "compact"))
+        (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
         (hsPkgs."conduit-extra" or (errorHandler.buildDepError "conduit-extra"))
         (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
         (hsPkgs."cryptonite" or (errorHandler.buildDepError "cryptonite"))
@@ -92,6 +93,7 @@
         (hsPkgs."semialign" or (errorHandler.buildDepError "semialign"))
         (hsPkgs."sqlite-simple" or (errorHandler.buildDepError "sqlite-simple"))
         (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+        (hsPkgs."stm-conduit" or (errorHandler.buildDepError "stm-conduit"))
         (hsPkgs."streams" or (errorHandler.buildDepError "streams"))
         (hsPkgs."tar" or (errorHandler.buildDepError "tar"))
         (hsPkgs."template-haskell" or (errorHandler.buildDepError
@@ -101,6 +103,7 @@
         (hsPkgs."these" or (errorHandler.buildDepError "these"))
         (hsPkgs."time" or (errorHandler.buildDepError "time"))
         (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+        (hsPkgs."unliftio" or (errorHandler.buildDepError "unliftio"))
         (hsPkgs."unordered-containers" or (errorHandler.buildDepError
           "unordered-containers"))
         (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
@@ -328,6 +331,7 @@
         "Kore/ModelChecker/Bounded"
         "Kore/ModelChecker/Simplification"
         "Kore/ModelChecker/Step"
+        "Kore/Network/JSONRPC"
         "Kore/Options"
         "Kore/Parser"
         "Kore/Parser/CString"
@@ -695,6 +699,7 @@
           (hsPkgs."co-log" or (errorHandler.buildDepError "co-log"))
           (hsPkgs."comonad" or (errorHandler.buildDepError "comonad"))
           (hsPkgs."compact" or (errorHandler.buildDepError "compact"))
+          (hsPkgs."conduit" or (errorHandler.buildDepError "conduit"))
           (hsPkgs."conduit-extra" or (errorHandler.buildDepError
             "conduit-extra"))
           (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
@@ -749,6 +754,7 @@
           (hsPkgs."sqlite-simple" or (errorHandler.buildDepError
             "sqlite-simple"))
           (hsPkgs."stm" or (errorHandler.buildDepError "stm"))
+          (hsPkgs."stm-conduit" or (errorHandler.buildDepError "stm-conduit"))
           (hsPkgs."streams" or (errorHandler.buildDepError "streams"))
           (hsPkgs."tar" or (errorHandler.buildDepError "tar"))
           (hsPkgs."template-haskell" or (errorHandler.buildDepError
@@ -758,6 +764,7 @@
           (hsPkgs."these" or (errorHandler.buildDepError "these"))
           (hsPkgs."time" or (errorHandler.buildDepError "time"))
           (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."unliftio" or (errorHandler.buildDepError "unliftio"))
           (hsPkgs."unordered-containers" or (errorHandler.buildDepError
             "unordered-containers"))
           (hsPkgs."vector" or (errorHandler.buildDepError "vector"))
