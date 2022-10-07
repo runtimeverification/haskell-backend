d14ba1ae8a67a380d0e92ff3d074269f90b39805
Sam Balco <goodlyrottenapple@gmail.com>
2022-10-07 13:14:55 +0100

    fixed rpc tests and added them to the ci

--- test/rpc-server/runTests.py
+++ test/rpc-server/runTests.py
@@ -146,3 +146,18 @@ for name in os.listdir("./implies"):
       params["consequent"] = consequent
       req = rpc_request_id1("implies", params)
       runTest(implies_def_path, req, resp_golden_path)
+
+
+print("Running simplify tests:")
+
+for name in os.listdir("./simplify"):
+  info(f"- test '{name}'...")
+  simplify_def_path = os.path.join("./simplify", name, "definition.kore")
+  state_json_path = os.path.join("./simplify", name, "state.json")
+  resp_golden_path = os.path.join("./simplify", name, "response.golden")
+  with open(state_json_path, 'r') as state_json:
+      state = json.loads(state_json.read())
+      params = {}
+      params["state"] = state
+      req = rpc_request_id1("simplify", params)
+      runTest(simplify_def_path, req, resp_golden_path)
