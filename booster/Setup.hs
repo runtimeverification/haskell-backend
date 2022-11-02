31132671aaa39f51c88faa2967ea897c14f6d617
Jost Berthold <jost.berthold@gmail.com>
2022-11-02 22:58:26 +1100

    add fourmolu formatting script, format code

--- booster/Setup.hs
+++ booster/Setup.hs
@@ -1,7 +1,7 @@
 -- This script is used to build and install your package. Typically you don't
 -- need to change it. The Cabal documentation has more information about this
 -- file: <https://www.haskell.org/cabal/users-guide/installing-packages.html>.
-import qualified Distribution.Simple
+import Distribution.Simple qualified
 
 main :: IO ()
 main = Distribution.Simple.defaultMain
