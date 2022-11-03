d55acb0fef9c88d3a00ac58542015c82d0377e1c
Jost Berthold <jost.berthold@gmail.com>
2022-11-03 14:20:49 +1100

    build workflow for github (#7)
    The following jobs are set up:
    * `stack test --pedantic`
    * `hpack && cabal v2-test`
    * Style: `scripts/fourmolu.sh` followed by diff check, then `hlint`

--- booster/executable/Main.hs
+++ booster/executable/Main.hs
@@ -1,7 +1,7 @@
 -- It is generally a good idea to keep all your business logic in your library
 -- and only use it in the executable. Doing so allows others to use what you
 -- wrote in their libraries.
-import qualified Example
+import Example qualified
 
 main :: IO ()
 main = Example.main
