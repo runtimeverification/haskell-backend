cc183bc5d7eb3d82b8d512331e397ffd53b84f9a
ana-pantilie <ana.pantilie95@gmail.com>
2022-11-03 13:51:39 +0200

    Change severity of WarnUnsimplifiedPredicate/Condition

--- kore/src/Kore/Log/WarnUnsimplified.hs
+++ kore/src/Kore/Log/WarnUnsimplified.hs
@@ -87,12 +87,12 @@ instance Pretty WarnUnsimplifiedCondition where
             ]
 
 instance Entry WarnUnsimplifiedPredicate where
-    entrySeverity _ = Warning
+    entrySeverity _ = Debug
     oneLineDoc WarnUnsimplifiedPredicate{limit} = Pretty.pretty limit
     helpDoc _ = "warn when a predicate is not simplified"
 
 instance Entry WarnUnsimplifiedCondition where
-    entrySeverity _ = Warning
+    entrySeverity _ = Debug
     oneLineDoc WarnUnsimplifiedCondition{limit} = Pretty.pretty limit
     helpDoc _ = "warn when a condition is not simplified"
 
