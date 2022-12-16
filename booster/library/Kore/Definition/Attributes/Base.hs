74ca10566db61d93bc42b72fd98b4ef1b1d5377a
Ana Pantilie <45069775+ana-pantilie@users.noreply.github.com>
2022-12-16 17:04:27 +0200

    Force evaluation of internalisation (#80)
    * Force evaluation of internalisation
    
    * merge main + fourmolu + hlint
    
    Co-authored-by: Sam Balco <goodlyrottenapple@gmail.com>

--- booster/library/Kore/Definition/Attributes/Base.hs
+++ booster/library/Kore/Definition/Attributes/Base.hs
@@ -29,14 +29,16 @@ data DefinitionAttributes = DefinitionAttributes
     }
     -- none needed
 
-    deriving (Eq, Show)
+    deriving stock (Eq, Show, Generic)
+    deriving anyclass (NFData)
 
 data ModuleAttributes = ModuleAttributes
     {
     }
     -- none needed
 
-    deriving (Eq, Show)
+    deriving stock (Eq, Show, Generic)
+    deriving anyclass (NFData)
 
 {- | Things needed for booster rewrite engine:
   * axiom location (for debug logging and error messages)
@@ -49,12 +51,14 @@ data AxiomAttributes = AxiomAttributes
     , ruleLabel :: Maybe Label
     , simplification :: Bool
     }
-    deriving stock (Eq, Ord, Show)
+    deriving stock (Eq, Ord, Show, Generic)
+    deriving anyclass (NFData)
 
 data ComputedAxiomAttributes = ComputedAxiomAttributes
     { containsAcSymbols, preservesDefinedness :: Bool
     }
-    deriving stock (Eq, Ord, Show)
+    deriving stock (Eq, Ord, Show, Generic)
+    deriving anyclass (NFData)
 
 type Label = Text
 type Priority = Word8
@@ -78,17 +82,20 @@ data SymbolType
     | TotalFunction
     | Constructor
     | SortInjection
-    deriving stock (Eq, Ord, Show)
+    deriving stock (Eq, Ord, Show, Generic)
+    deriving anyclass (NFData)
 
 data SymbolAttributes = SymbolAttributes
     { symbolType :: SymbolType
     , isIdem, isAssoc :: Bool
     }
-    deriving stock (Eq, Ord, Show)
+    deriving stock (Eq, Ord, Show, Generic)
+    deriving anyclass (NFData)
 
 newtype SortAttributes = SortAttributes
     { argCount :: Int
     }
     -- none needed
 
-    deriving stock (Eq, Show)
+    deriving stock (Eq, Show, Generic)
+    deriving anyclass (NFData)
