fb7cd99a3eb2866e407d0254e4d0233c779d2559
Samuel Balco <goodlyrottenapple@gmail.com>
2023-01-12 11:43:29 +0000

    Fix string binary decoding + add binary decoding exe (#90)
    * Fix string binary decoding + add binary decoding testing exe
    
    * from @jberthold fix sort application in llvm marshalling
    
    * fourmolu
    
    * forgot to add the new parsetest-binary
    
    * fourmolu

---
+++ booster/tools/parsetest-binary/ParsetestBinary.hs
@@ -0,0 +1,24 @@
+{- | Stand-alone parser executable for testing and profiling
+
+Copyright   : (c) Runtime Verification, 2022
+License     : BSD-3-Clause
+-}
+module Main (
+    main,
+) where
+
+import System.Environment (getArgs)
+
+import Kore.Pattern.Binary
+
+{- | Tests textual kore parser with given arguments and reports
+   internalisation results.
+
+   * Files given as arguments are parsed and internalised.  When a
+   * directory is given as an argument, it is (recursively) searched
+   * for files named "*.kore", which are parsed and internalised.
+-}
+main :: IO ()
+main = do
+    [binFile] <- getArgs
+    print =<< test Nothing binFile
