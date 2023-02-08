9fadeb6bc54cae07f2972df9fd843ec45d74b0b6
Ana Pantilie <ana.pantilie95@gmail.com>
2023-02-08 20:19:04 +0200

    Fix output of check implication (version 2)

--- kore/src/Kore/Reachability/Claim.hs
+++ kore/src/Kore/Reachability/Claim.hs
@@ -453,8 +453,8 @@ strategyWithMinDepth (MinDepth minDepth) =
         reachabilitySteps
   where
     noCheckReachabilitySteps =
-        reachabilityFirstStepNoCheck :
-        replicate (minDepth - 1) reachabilityNextStepNoCheck
+        reachabilityFirstStepNoCheck
+            : replicate (minDepth - 1) reachabilityNextStepNoCheck
     reachabilitySteps =
         Stream.iterate id reachabilityNextStep
 
@@ -669,10 +669,10 @@ checkImplicationWorker (ClaimPattern.refreshExistentials -> claimPattern) =
         m (CheckImplicationResult ClaimPattern)
     examine AnyUnified{didAnyUnify} stuck
         | didAnyUnify
-          , isBottom condition =
+        , isBottom condition =
             pure $ Implied claimPattern
         | not didAnyUnify
-          , not (isBottom right) =
+        , not (isBottom right) =
             pure $ NotImplied claimPattern
         | otherwise = do
             when (isBottom right) $
@@ -775,9 +775,10 @@ checkSimpleImplication inLeft inRight existentials =
                 SMT.Evaluator.filterMultiOr $srcLoc
                     =<< Pattern.simplify right
 
-        if trivial || rhsBottom
-            then pure (claimToCheck, NotImpliedStuck Nothing)
-            else do
+        case (trivial, rhsBottom) of
+            (True, _) -> pure (claimToCheck, Implied Nothing)
+            (_, False) -> pure (claimToCheck, NotImpliedStuck Nothing)
+            _ -> do
                 -- attempt term unification (to remember the substitution
                 unified <-
                     lift $
@@ -837,22 +838,22 @@ checkSimpleImplication inLeft inRight existentials =
         withContext ("LHS: " <> showPretty leftTerm)
             . withContext ("RHS: " <> showPretty rightTerm)
             . withContext ("existentials: " <> show (map unparse2 existentials))
-            $ koreFailWhen (not $ null nameCollisions) $
-                unwords
-                    ( "Existentials capture free variables of the antecedent:" :
-                      map (show . unparse2) nameCollisions
-                    )
+            $ koreFailWhen (not $ null nameCollisions)
+            $ unwords
+                ( "Existentials capture free variables of the antecedent:"
+                    : map (show . unparse2) nameCollisions
+                )
         -- RHS must not have free variables that aren't free in the LHS
         let rhsFreeElemVars = getFreeElementVariables $ freeVariables right
             offending = rhsFreeElemVars \\ (lhsFreeElemVars <> existentials)
         withContext ("LHS: " <> showPretty leftTerm)
             . withContext ("RHS: " <> showPretty rightTerm)
             . withContext ("existentials: " <> show (map unparse2 existentials))
-            $ koreFailWhen (not $ null offending) $
-                unwords
-                    ( "The RHS must not have free variables not present in the LHS:" :
-                      map (show . unparse2) offending
-                    )
+            $ koreFailWhen (not $ null offending)
+            $ unwords
+                ( "The RHS must not have free variables not present in the LHS:"
+                    : map (show . unparse2) offending
+                )
         -- sorts of LHS and RHS have to agree
         let lSort = termLikeSort leftTerm
             rSort = termLikeSort rightTerm
@@ -902,11 +903,11 @@ checkSimpleImplication inLeft inRight existentials =
                     -- which we achieve by simplifying it
 
                     toRefute <-
-                        liftSimplifier $
-                            Pattern.simplify
+                        liftSimplifier
+                            $ Pattern.simplify
                                 . OrPattern.toPattern sort
                                 . MultiOr.map combineWithAntecedent
-                                $ notRhs
+                            $ notRhs
 
                     liftSimplifier $ SMT.Evaluator.filterMultiOr $srcLoc toRefute
 
