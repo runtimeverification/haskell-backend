729cb532cae307bec6d7e573d20b27bc2a1e40fa
Ana Pantilie <ana.pantilie95@gmail.com>
2023-02-21 13:48:29 +0200

    Change SMT timeout for optimism test

--- test/kevm-optimism-invariant/test-optimism-invariant.sh
+++ test/kevm-optimism-invariant/test-optimism-invariant.sh
@@ -5,7 +5,6 @@ exec kore-exec \
     --module FOUNDRY-MAIN \
     --strategy all \
     --max-counterexamples 1 \
-    --smt-timeout 40 \
     --smt-retry-limit 1 \
     --smt-reset-interval 100 \
     --smt z3 \
