8956a2fb8a4b8f251d808c5ad53fec9df3268e7f
Samuel Balco <goodlyrottenapple@gmail.com>
2023-03-08 00:03:57 +0000

    Add hs-booster-proxy (#135)
    Implements #128 by combining the old backend with the new one into a single executable which pulls in both backends as libraries and keeps the state of both backends internally.

--- booster/library/Server.hs
+++ booster/library/Server.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 {- |
 Copyright   : (c) Runtime Verification, 2022
 License     : BSD-3-Clause
@@ -8,21 +6,14 @@ module Server (main) where
 
 import Control.DeepSeq (force)
 import Control.Exception (evaluate)
-import Control.Monad.Logger (LogLevel (..))
-import Data.List (intercalate, partition)
-import Data.Maybe (fromMaybe)
-import Data.Text (Text, pack)
 import Options.Applicative
 
+import Booster.CLOptions
 import Booster.JsonRpc (runServer)
 import Booster.LLVM.Internal (mkAPI, withDLib)
 import Booster.Syntax.ParsedKore (loadDefinition)
 import Booster.Trace
-import Booster.VersionInfo (VersionInfo (..), versionInfo)
 import Control.Monad (forM_)
-
-import Text.Casing
-import Text.Read
 
 main :: IO ()
 main = do
@@ -52,117 +43,7 @@ main = do
             (clOptionsParser <**> versionInfoParser <**> helper)
             parserInfoModifiers
 
-data CLOptions = CLOptions
-    { definitionFile :: FilePath
-    , mainModuleName :: Text
-    , llvmLibraryFile :: Maybe FilePath
-    , port :: Int
-    , logLevels :: [LogLevel]
-    , eventlogEnabledUserEvents :: [CustomUserEventType]
-    }
-    deriving (Show)
-
 parserInfoModifiers :: InfoMod options
 parserInfoModifiers =
     fullDesc
         <> header "Haskell Backend Booster - a JSON RPC server for quick symbolic execution of Kore definitions"
-
-versionInfoParser :: Parser (a -> a)
-versionInfoParser =
-    infoOption
-        versionInfoStr
-        ( short 'v'
-            <> long "version"
-            <> help "Print version info."
-        )
-
-clOptionsParser :: Parser CLOptions
-clOptionsParser =
-    CLOptions
-        <$> strArgument
-            ( metavar "DEFINITION_FILE"
-                <> help "Kore definition file to verify and use for execution"
-            )
-        <*> strOption
-            ( metavar "MODULE"
-                <> long "module"
-                <> help "The name of the main module in the Kore definition."
-            )
-        <*> optional
-            ( strOption
-                ( metavar "LLVM_BACKEND_LIBRARY"
-                    <> long "llvm-backend-library"
-                    <> help "Path to the .so/.dylib shared LLVM backend library"
-                )
-            )
-        <*> option
-            auto
-            ( metavar "SERVER_PORT"
-                <> long "server-port"
-                <> value 31337
-                <> help "Port for the RPC server to bind to"
-                <> showDefault
-            )
-        <*> many
-            ( option
-                (eitherReader readLogLevel)
-                ( metavar "LEVEL"
-                    <> long "log-level"
-                    <> short 'l'
-                    <> help
-                        ( "Log level: debug, info (default), warn, error, \
-                          \or a custom level from "
-                            <> show allowedLogLevels
-                        )
-                )
-            )
-        <*> many
-            ( option
-                (eitherReader readEventLogTracing)
-                ( metavar "TRACE"
-                    <> long "trace"
-                    <> short 't'
-                    <> help
-                        ( "Eventlog tracing options: "
-                            <> intercalate ", " [toKebab $ fromHumps $ show t | t <- [minBound .. maxBound] :: [CustomUserEventType]]
-                        )
-                )
-            )
-  where
-    readLogLevel :: String -> Either String LogLevel
-    readLogLevel = \case
-        "debug" -> Right LevelDebug
-        "info" -> Right LevelInfo
-        "warn" -> Right LevelWarn
-        "error" -> Right LevelError
-        other
-            | other `elem` allowedLogLevels -> Right (LevelOther $ pack other)
-            | otherwise -> Left $ other <> ": Unsupported log level"
-
-    readEventLogTracing :: String -> Either String CustomUserEventType
-    readEventLogTracing =
-        (\s -> maybe (Left $ s <> " not supported in eventlog tracing") Right $ readMaybe s) . toPascal . fromKebab
-
--- custom log levels that can be selected
-allowedLogLevels :: [String]
-allowedLogLevels = ["Rewrite"]
-
--- Partition provided log levels into standard and custom ones, and
--- select the lowest standard level. Default to 'LevelInfo' if no
--- standard log level was given.
-adjustLogLevels :: [LogLevel] -> (LogLevel, [LogLevel])
-adjustLogLevels ls = (standardLevel, customLevels)
-  where
-    (stds, customLevels) = partition (<= LevelError) ls
-    standardLevel = if null stds then LevelInfo else minimum stds
-
-versionInfoStr :: String
-versionInfoStr =
-    unlines
-        [ "hs-backend-booster version:"
-        , "  revision:\t" <> gitHash <> if gitDirty then " (dirty)" else ""
-        , "  branch:\t" <> fromMaybe "<unknown>" gitBranch
-        , "  last commit:\t" <> gitCommitDate
-        ]
-  where
-    VersionInfo{gitHash, gitDirty, gitBranch, gitCommitDate} = $versionInfo
