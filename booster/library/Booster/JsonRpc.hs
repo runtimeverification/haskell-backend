0891f829daa7086ca7e442a875a812a2c14e0e6c
Samuel Balco <goodlyrottenapple@gmail.com>
2023-03-10 22:36:38 +0000

    Refactor RPC server error types (#139)

--- booster/library/Booster/JsonRpc.hs
+++ booster/library/Booster/JsonRpc.hs
@@ -8,13 +8,9 @@ module Booster.JsonRpc (
     rpcJsonConfig,
 ) where
 
-import Control.Exception (ErrorCall (..), SomeException)
-import Control.Monad.Catch (MonadCatch, MonadMask, handle)
 import Control.Monad.Logger.CallStack (LogLevel (LevelError), MonadLoggerIO)
 import Control.Monad.Logger.CallStack qualified as Log
 import Control.Monad.Trans.Except (runExcept)
-import Data.Aeson (object, toJSON, (.=))
-import Data.Aeson.Types (Value (..))
 import Data.Conduit.Network (serverSettings)
 import Data.Foldable
 import Data.Maybe (fromMaybe, isJust)
@@ -27,24 +23,23 @@ import Booster.Pattern.Base (Pattern)
 import Booster.Pattern.Rewrite (RewriteResult (..), performRewrite)
 import Booster.Syntax.Json (KoreJson (..), addHeader)
 import Booster.Syntax.Json.Externalise (externalisePattern)
-import Booster.Syntax.Json.Internalise (PatternError, internalisePattern)
-import Control.Monad.Logger (logInfoN)
+import Booster.Syntax.Json.Internalise (internalisePattern)
+import Kore.JsonRpc.Error
 import Kore.JsonRpc.Server
 import Kore.JsonRpc.Types
 
 respond ::
     forall m.
-    MonadMask m =>
     MonadLoggerIO m =>
     KoreDefinition ->
     Maybe LLVM.API ->
     Respond (API 'Req) m (API 'Res)
 respond def@KoreDefinition{} mLlvmLibrary =
-    catchingServerErrors . \case
+    \case
         Execute req
-            | isJust req._module -> unsupportedField "module"
-            | isJust req.stepTimeout -> unsupportedField "step-timeout"
-            | isJust req.movingAverageStepTimeout -> unsupportedField "moving-average-step-timeout"
+            | isJust req._module -> pure $ Left $ unsupportedOption ("module" :: String)
+            | isJust req.stepTimeout -> pure $ Left $ unsupportedOption ("step-timeout" :: String)
+            | isJust req.movingAverageStepTimeout -> pure $ Left $ unsupportedOption ("moving-average-step-timeout" :: String)
         Execute req -> do
             -- internalise given constrained term
             let internalised = runExcept $ internalisePattern Nothing def req.state.term
@@ -52,7 +47,7 @@ respond def@KoreDefinition{} mLlvmLibrary =
             case internalised of
                 Left patternError -> do
                     Log.logDebug $ "Error internalising cterm" <> Text.pack (show patternError)
-                    pure $ Left $ reportPatternError patternError
+                    pure $ Left $ backendError CouldNotVerifyPattern patternError
                 Right pat -> do
                     let cutPoints = fromMaybe [] req.cutPointRules
                         terminals = fromMaybe [] req.terminalRules
@@ -60,10 +55,10 @@ respond def@KoreDefinition{} mLlvmLibrary =
                     execResponse <$> performRewrite def mLlvmLibrary mbDepth cutPoints terminals pat
 
         -- this case is only reachable if the cancel appeared as part of a batch request
-        Cancel -> pure $ Left $ ErrorObj "Cancel request unsupported in batch mode" (-32001) Null
+        Cancel -> pure $ Left cancelUnsupportedInBatchMode
         -- using "Method does not exist" error code
 
-        _ -> pure $ Left $ ErrorObj "Not implemented" (-32601) Null
+        _ -> pure $ Left notImplemented
   where
     execResponse :: (Natural, RewriteResult Pattern) -> Either ErrorObj (API 'Res)
     execResponse (_, RewriteSingle{}) =
@@ -122,7 +117,7 @@ respond def@KoreDefinition{} mLlvmLibrary =
         Right $
             Execute
                 ExecuteResult
-                    { reason = Aborted
+                    { reason = Kore.JsonRpc.Types.Aborted
                     , depth = Depth d
                     , state = toExecState p
                     , nextStates = Nothing
@@ -135,27 +130,6 @@ respond def@KoreDefinition{} mLlvmLibrary =
       where
         (t, p) = externalisePattern pat
 
-    reportPatternError :: PatternError -> ErrorObj
-    reportPatternError pErr =
-        ErrorObj "Could not verify KORE pattern" (-32002) $ toJSON pErr
-
-    unsupportedField name = pure $ Left $ ErrorObj ("Unsupported option: " <> name) (-32100) Null
-
-{- | Catches all calls to `error` from the guts of the engine, and
-     returns json with the message and location as context.
--}
-catchingServerErrors ::
-    MonadCatch m =>
-    MonadLoggerIO m =>
-    m (Either ErrorObj res) ->
-    m (Either ErrorObj res)
-catchingServerErrors =
-    let mkError (ErrorCallWithLocation msg loc) =
-            object ["error" .= msg, "context" .= loc]
-     in handle $ \err -> do
-            Log.logError $ "Server error: " <> Text.pack (show err)
-            pure $ Left (ErrorObj "Server error" (-32032) $ mkError err)
-
 runServer :: Int -> KoreDefinition -> Maybe LLVM.API -> (LogLevel, [LogLevel]) -> IO ()
 runServer port internalizedModule mLlvmLibrary (logLevel, customLevels) =
     do
@@ -163,7 +137,7 @@ runServer port internalizedModule mLlvmLibrary (logLevel, customLevels) =
         $ jsonRpcServer
             srvSettings
             (const $ respond internalizedModule mLlvmLibrary)
-            [JsonRpcHandler $ \(err :: SomeException) -> logInfoN (Text.pack $ show err) >> pure (ErrorObj "Server error: crashed" (-32032) $ toJSON $ show err)]
+            [handleErrorCall, handleSomeException]
   where
     levelFilter _source lvl =
         lvl `elem` customLevels || lvl >= logLevel && lvl <= LevelError
