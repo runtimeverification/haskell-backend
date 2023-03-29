8d4c97236bc2557d9bc2302ba305fbcf820a5ac1
Jost Berthold <jost.berthold@runtimeverification.com>
2023-03-29 22:42:13 +1100

    Improve fourmolu script, add hlint script (#147)
    (cherry-picked and squashed from earlier branch)
    
    scripts/fourmolu:
    - check version
    - be more verbose about what is happening
    
    scripts/hlint:
    - factor out script for calling hlint too
    
    Co-authored-by: Everett Hildenbrandt <everett.hildenbrandt@gmail.com>

--- booster/scripts/fourmolu.sh
+++ booster/scripts/fourmolu.sh
@@ -1,1 +1,7 @@
-git ls-files | grep '.*\.hs$' | xargs ${FOURMOLU:-$(which fourmolu)} -o -XImportQualifiedPost -o -XTypeApplications -o -XPatternSynonyms -o -XBangPatterns -o -XOverloadedRecordDot -i
+#!/usr/bin/env bash
+set -euxo pipefail
+expected_fourmolu_version=0.8.2.0
+fourmolu=${FOURMOLU:-$(which fourmolu)} || { echo 'No fourmolu!' ; exit 1 ; }
+fourmolu_version=$(${fourmolu} --version | head -n1 | cut --field=2 --delimiter=' ')
+[[ ${fourmolu_version} == ${expected_fourmolu_version} ]] || { echo "Unexpected fourmolu version, got ${fourmolu_version}, expected ${expected_fourmolu_version}" ; exit 1 ; }
+git ls-files | grep '.*\.hs$' | xargs ${fourmolu} -o -XImportQualifiedPost -o -XTypeApplications -o -XPatternSynonyms -o -XBangPatterns -o -XOverloadedRecordDot -i
