b26dd625a3aa8a3da955c49e5b1bd09cdd38ed16
Sam Balco <goodlyrottenapple@gmail.com>
2023-04-26 16:30:14 +0100

    remove OverloadedRecordDot

--- kore-rpc-types/src/Kore/JsonRpc/Types/Log.hs
+++ kore-rpc-types/src/Kore/JsonRpc/Types/Log.hs
@@ -1,11 +1,11 @@
-{-# LANGUAGE OverloadedRecordDot #-}
 {-# OPTIONS_GHC -Wno-partial-fields #-}
 
 module Kore.JsonRpc.Types.Log (module Kore.JsonRpc.Types.Log) where
-import Kore.Syntax.Json.Types (KoreJson)
+
+import Data.Aeson (FromJSON, ToJSON)
 import Data.Text (Text)
 import GHC.Generics (Generic)
-import Data.Aeson (FromJSON, ToJSON)
+import Kore.Syntax.Json.Types (KoreJson)
 
 import Deriving.Aeson (
     CamelToKebab,
@@ -14,40 +14,41 @@ import Deriving.Aeson (
     OmitNothingFields,
  )
 
-data LogSimplificationResult = Success {
-    rewrittenTerm :: Maybe KoreJson,
-    ruleId :: Text
-  } |
-  Failure {
-    reason :: Text,
-    ruleId :: Text
-  }
-  deriving stock (Generic, Show, Eq)
+data LogSimplificationResult
+    = Success
+        { rewrittenTerm :: Maybe KoreJson
+        , ruleId :: Text
+        }
+    | Failure
+        { reason :: Text
+        , ruleId :: Text
+        }
+    deriving stock (Generic, Show, Eq)
     deriving
         (FromJSON, ToJSON)
         via CustomJSON '[OmitNothingFields, FieldLabelModifier '[CamelToKebab]] LogSimplificationResult
 
-data LogEntry = 
-    RewriteSuccess {
-      rewrittenTerm :: Maybe KoreJson,
-      ruleId :: Text,
-      origin :: Text
-    } |
-    RewriteFailure {
-      reason :: Text,
-      ruleId :: Text,
-      origin :: Text
-    } |
-    LlvmSimplification {
-      originalTerm :: Maybe KoreJson,
-      result :: LogSimplificationResult,
-      origin :: Text
-    } |
-    Simplification {
-      originalTerm :: Maybe KoreJson,
-      result :: LogSimplificationResult,
-      origin :: Text
-    }
+data LogEntry
+    = RewriteSuccess
+        { rewrittenTerm :: Maybe KoreJson
+        , ruleId :: Text
+        , origin :: Text
+        }
+    | RewriteFailure
+        { reason :: Text
+        , ruleId :: Text
+        , origin :: Text
+        }
+    | LlvmSimplification
+        { originalTerm :: Maybe KoreJson
+        , result :: LogSimplificationResult
+        , origin :: Text
+        }
+    | Simplification
+        { originalTerm :: Maybe KoreJson
+        , result :: LogSimplificationResult
+        , origin :: Text
+        }
     deriving stock (Generic, Show, Eq)
     deriving
         (FromJSON, ToJSON)
