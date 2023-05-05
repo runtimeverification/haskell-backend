2aca44599cedbb162b56dfb955bf0831bf15ab72
Jost Berthold <jost.berthold@runtimeverification.com>
2023-05-05 23:03:22 +1000

    simplify conditions in rewrites (#169)
    * first draft code for concrete/symbolic in applying equations
    
    * Code to check concreteness constraints
    
    * fix attribute issue, add a few unit tests
    
    * straighten applyEquation type
    
    * eliminate error call
    
    * 167 assume rules for total functions preserve definedness
    
    * small logging tweaks
    
    * Simplify rule constraints using equations during rewriting
    
    * Check/adjust sort returned from LLVM simplification
    
    * formatting
    
    ---------
    
    Co-authored-by: Ana Pantilie <45069775+ana-pantilie@users.noreply.github.com>

--- booster/library/Booster/LLVM.hs
+++ booster/library/Booster/LLVM.hs
@@ -1,15 +1,18 @@
 module Booster.LLVM (simplifyBool, simplifyTerm) where
 
+import Control.Monad.IO.Class (MonadIO (..))
+import Data.Binary.Get
+import Data.ByteString (fromStrict)
+import Data.Map qualified as Map
+import Data.Set qualified as Set
+import System.IO.Unsafe (unsafePerformIO)
+
 import Booster.Definition.Base
 import Booster.LLVM.Internal qualified as Internal
 import Booster.Pattern.Base
 import Booster.Pattern.Binary
 import Booster.Pattern.Util
 import Booster.Trace qualified as Trace
-import Control.Monad.IO.Class (MonadIO (..))
-import Data.Binary.Get
-import Data.ByteString (fromStrict)
-import System.IO.Unsafe (unsafePerformIO)
 
 simplifyBool :: Internal.API -> Term -> Bool
 simplifyBool api trm = unsafePerformIO $ Internal.runLLVM api $ do
@@ -29,11 +32,22 @@ simplifyTerm api def trm sort = unsafePerformIO $ Internal.runLLVM api $ do
     Trace.traceIO $ Internal.LlvmVar (Internal.somePtr trmPtr) trm
     -- strip away the custom injection added by the LLVM backend
     Trace.timeIO "LLVM.simplifyTerm.decodeTerm" $ case runGet (decodeTerm def) (fromStrict binary) of
-        Injection origSort (SortApp "SortKItem" _) result
-            | origSort == sort ->
+        Injection newSort (SortApp "SortKItem" _) result
+            | newSort == sort ->
                 pure result
+            | Set.member (sortName newSort) subsorts ->
+                pure $ Injection newSort sort result
         someTerm
             | sortOfTerm someTerm == sort ->
                 pure someTerm
-        other ->
-            error $ "Unexpected sort after LLVM simplification: " <> show other
+        other -> do
+            liftIO . putStrLn $
+                "[Error] Unexpected sort after LLVM simplification: "
+                    <> show other
+                    <> " Expected sort "
+                    <> show sort
+            pure trm
+  where
+    sortName (SortApp name _) = name
+    sortName (SortVar name) = name
+    subsorts = maybe Set.empty snd $ Map.lookup (sortName sort) def.sorts
