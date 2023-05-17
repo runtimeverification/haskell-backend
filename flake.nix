2386f8f25f4fae0c4890da7ad52c404b9875a20a
Jost Berthold <jost.berthold@gmail.com>
2023-05-17 10:44:17 +1000

    fix GHC/stack version and cache keys, revert fourmolu to 0.3.0.0

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
 
       compiler-nix-name = "ghc927";
       index-state = "2023-04-24T00:00:00Z";
-      fourmolu-version = "0.12.0.0";
+      fourmolu-version = "0.3.0.0";
 
       haskell-backend-src = pkgs:
         pkgs.applyPatches {
