bde91a5bdca5534ac007d5c81cdf09c73c13f37e
Samuel Balco <goodlyrottenapple@gmail.com>
2023-05-25 03:19:14 +0100

    Remove old scripts (#3592)

--- scripts/format-statistics.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env nix-shell
-#!nix-shell ../test.nix -i bash
-
-mlr --icsv --omd cut -o -f name,allocated_bytes,diff_allocated_bytes,max_live_bytes,diff_max_live_bytes
--- scripts/git-assert-clean.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -exuo pipefail
-
-if [ ! $(git status --porcelain | wc -l) -eq 0 ]; then
-    echo "Found dirty files:"
-    git status --porcelain
-    exit 1
-fi
