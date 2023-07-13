96410d18f030415626fa873147957f24b195a3f6
Jost Berthold <jost.berthold@runtimeverification.com>
2023-07-13 22:30:16 +1000

    Connect auto update to existing update script (#234)
    Related to #233 , extending the update job scope
    
    * Update `cabal.project` and `stack.yaml` with the desired commit hash
    and correct sha (uses existing script)
    * Update `cabal.project.freeze` (in case the updated haskell-backend
    adds/removes library dependencies)
    This uses another existing script, called from the first one (changed).
    * Do this only if the `flake.nix` file has actually changed (to avoid
    running `nix flake update` without need)
    * Combine all changes in a single commit for better overview.
    
    Also contains other small clean-ups in `flake.nix` and
    `cabal.project.freeze`.

--- booster/scripts/freeze-cabal-to-stack-resolver.sh
+++ booster/scripts/freeze-cabal-to-stack-resolver.sh
@@ -2,8 +2,15 @@
 
 set -euxo pipefail
 
+# must remove the prior freeze file to actually update
+rm -f cabal.project.freeze
+
 stack --test ls dependencies \
     | sed -e 's/^\([a-zA-Z0-9.-]*\) \([0-9.]*\)/--constraint="\1 == \2"/' \
     | xargs -x cabal freeze --enable-tests
+
+# ghc-bignum is removed from the freeze file because it is wired into
+# GHC and creates problems when nix builds tools with different GHCs
+sed -i -e '/any\.ghc-bignum/d' -e '/any\.happy/d' -e '/any\.alex/d' cabal.project.freeze
 
 git diff cabal.project.freeze
