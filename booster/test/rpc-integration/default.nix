b31c6f5ae96217426c7a9e238bf262838e4f7cb7
Jost Berthold <jost.berthold@runtimeverification.com>
2023-08-02 10:46:54 +1000

    207 test tarball runner + tweak rpc client logging (#242)
    - [X] Tweaks to `rpc-client`:
      * use `MonadLogger` and a log-level option
    * cancel the request in flight if the user interrupts the program (by
    `^C` or sending `SIGINT`)
    - [x] unpack and use all `rpc_*` directories, with their respective path
    - [x] Add a regression test for running a tarball.
     
    Fixes #207
    
    ---------
    
    Co-authored-by: Sam Balco <goodlyrottenapple@gmail.com>

--- booster/test/rpc-integration/default.nix
+++ booster/test/rpc-integration/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, coreutils, lib, kore-rpc-booster, rpc-client, git, k }:
+{ stdenv, coreutils, lib, kore-rpc-booster, rpc-client, git, k, blockchain-k-plugin, openssl, procps }:
 
 let
   mkIntegrationTest =
@@ -16,7 +16,8 @@ let
           export ${f}
         '') buildFlags}
         export SERVER=${kore-rpc-booster}/bin/kore-rpc-booster
-        export CLIENT=${rpc-client}/bin/rpc-client
+        export CLIENT="${rpc-client}/bin/rpc-client"
+        export PLUGIN_DIR=${blockchain-k-plugin}
 
         patchShebangs runDirectoryTest.sh
         ./runDirectoryTest.sh test-${name} --time ${
@@ -46,4 +47,5 @@ in {
   simplify = mkIntegrationTest { name = "simplify"; };
   get-model = mkIntegrationTest { name = "get-model"; };
   issue212 = mkIntegrationTest { name = "issue212"; };
+  foundry-bug-report.tar.gz = mkIntegrationTest { name = "foundry-bug-report.tar.gz"; nativeBuildInputs = [ k openssl procps ]; };
 }
