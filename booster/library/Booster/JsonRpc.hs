20220d53a08767dd33e12ef3093d0a31e1cb694e
Georgy Lukyanov <georgiylukjanov@gmail.com>
2023-08-25 17:23:48 +0200

    Infere sort in `execStateToKoreJson` insted of always using `SortGeneratedTopCell` (#280)

--- booster/library/Booster/JsonRpc.hs
+++ booster/library/Booster/JsonRpc.hs
@@ -230,9 +230,16 @@ data ServerState = ServerState
 execStateToKoreJson :: RpcTypes.ExecuteState -> KoreJson.KoreJson
 execStateToKoreJson RpcTypes.ExecuteState{term = t, substitution, predicate} =
     let subAndPred = catMaybes [KoreJson.term <$> substitution, KoreJson.term <$> predicate]
+        innerSorts = mapMaybe sortOfJson $ KoreJson.term t : subAndPred
+        topLevelSort = case innerSorts of
+            [] -> KoreJson.SortApp (KoreJson.Id "SortGeneratedTopCell") []
+            xs ->
+                if all (== head xs) (tail xs) -- we know xs is non-empty, hence `head` is safe here
+                    then KoreJson.SortApp (head xs).name []
+                    else KoreJson.SortApp (KoreJson.Id "SortGeneratedTopCell") []
      in t
             { KoreJson.term =
-                foldr (KoreJson.KJAnd $ KoreJson.SortApp (KoreJson.Id "SortGeneratedTopCell") []) t.term subAndPred
+                foldr (KoreJson.KJAnd topLevelSort) t.term subAndPred
             }
 
 execResponse ::
