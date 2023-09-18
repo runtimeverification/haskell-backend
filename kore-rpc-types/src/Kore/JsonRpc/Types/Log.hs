85b5d086840a0f1e89499f086b50d7258598fc8b
Georgy Lukyanov <mail@geo2a.info>
2023-09-18 10:33:19 +0200

    Add `Fallback` to `LogEntry` (#3657)
    This PR modifies the `LogEntry` type in `kore-rpc-types` in two ways:
    * add `origianlTerm` into `Rewrite` log entry constructor (and emit
    Nothing for now in Kore to retain the current behavior. This change is
    not necessary at the moment, but we may need to log these terms in
    future.
    * Add `Fallback` constructor to `LogEntry` to enable `kore-rpc-booster`
    to emit a trace when it falls back from Booster to Kore.

--- kore-rpc-types/src/Kore/JsonRpc/Types/Log.hs
+++ kore-rpc-types/src/Kore/JsonRpc/Types/Log.hs
@@ -3,8 +3,10 @@
 module Kore.JsonRpc.Types.Log (module Kore.JsonRpc.Types.Log) where
 
 import Data.Aeson (FromJSON, ToJSON)
+import Data.List.NonEmpty (NonEmpty)
 import Data.Text (Text)
 import GHC.Generics (Generic)
+import Kore.JsonRpc.Types.Depth (Depth (..))
 import Kore.Syntax.Json.Types (KoreJson)
 
 import Deriving.Aeson (
@@ -16,7 +18,7 @@ import Deriving.Aeson (
     StripPrefix,
  )
 
-data LogOrigin = KoreRpc | Booster | Llvm
+data LogOrigin = KoreRpc | Booster | Llvm | Proxy
     deriving stock (Generic, Show, Eq)
     deriving
         (FromJSON, ToJSON)
@@ -26,13 +28,13 @@ data LogOrigin = KoreRpc | Booster | Llvm
 
 data LogRewriteResult
     = Success
-        { rewrittenTerm :: Maybe KoreJson
-        , substitution :: Maybe KoreJson
-        , ruleId :: Text
+        { rewrittenTerm :: !(Maybe KoreJson)
+        , substitution :: !(Maybe KoreJson)
+        , ruleId :: !Text
         }
     | Failure
-        { reason :: Text
-        , _ruleId :: Maybe Text
+        { reason :: !Text
+        , _ruleId :: !(Maybe Text)
         }
     deriving stock (Generic, Show, Eq)
     deriving
@@ -46,14 +48,31 @@ data LogRewriteResult
 
 data LogEntry
     = Rewrite
-        { result :: LogRewriteResult
-        , origin :: LogOrigin
+        { result :: !(LogRewriteResult)
+        , origin :: !LogOrigin
         }
     | Simplification
-        { originalTerm :: Maybe KoreJson
-        , originalTermIndex :: Maybe [Int]
-        , result :: LogRewriteResult
-        , origin :: LogOrigin
+        { originalTerm :: !(Maybe KoreJson)
+        , originalTermIndex :: !(Maybe [Int])
+        , result :: !LogRewriteResult
+        , origin :: !LogOrigin
+        }
+    | -- | Indicates a fallback of an RPC-server to a more powerful, but slower backup server, i.e. Booster to Kore
+      Fallback
+        { originalTerm :: !(Maybe KoreJson)
+        -- ^ state before fallback
+        , rewrittenTerm :: !(Maybe KoreJson)
+        -- ^ state after fallback
+        , reason :: !Text
+        -- ^ fallback reason
+        , fallbackRuleId :: !Text
+        -- ^ the rule that caused the fallback
+        , recoveryRuleIds :: !(Maybe (NonEmpty Text))
+        -- ^ rules applied during fallback, the first rule is the one that caused the fallback
+        , recoveryDepth :: !Depth
+        -- ^ depth reached in fallback, must be the same as (length ruleIds)
+        , origin :: !LogOrigin
+        -- ^ proxy server the log was emitted from
         }
     deriving stock (Generic, Show, Eq)
     deriving
