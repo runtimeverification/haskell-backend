281d02128f6f4b669743c055a50abc4833b5d798
Sam Balco <goodlyrottenapple@gmail.com>
2023-11-01 15:07:05 +0000

    Catch IOError when a query times out and retry correctly

--- kore/src/Kore/Rewrite/SMT/Evaluator.hs
+++ kore/src/Kore/Rewrite/SMT/Evaluator.hs
@@ -188,7 +188,7 @@ decidePredicate onUnknown sideCondition predicates =
             & runMaybeT
   where
     query :: MaybeT Simplifier Result
-    query = SMT.withSolver . evalTranslator $ do
+    query = onErrorUnknown $ SMT.withSolver . evalTranslator $ do
         tools <- Simplifier.askMetadataTools
         Morph.hoist SMT.liftSMT $ do
             predicates' <-
@@ -197,6 +197,9 @@ decidePredicate onUnknown sideCondition predicates =
                     predicates
             traverse_ SMT.assert predicates'
             SMT.check >>= maybe empty return
+
+    onErrorUnknown action =
+        action `Exception.catch` \(_ :: IOException) -> pure Unknown
 
     retry = retryWithScaledTimeout $ query <* debugRetrySolverQuery predicates
 
