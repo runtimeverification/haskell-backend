1b4402651a049794e176f62ec6500eccb421d9db
Jost Berthold <jost.berthold@runtimeverification.com>
2024-01-09 01:18:51 +1100

    431 handle raw term from llvm part 2 (#439)
    Follow-up to #438 (fixing #431): removes the old handler for the added
    `SortKItem` injection.
    
    Do not merge before
    https://github.com/runtimeverification/llvm-backend/pull/931 has reached
    K master.
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Samuel Balco <goodlyrottenapple@gmail.com>

--- booster/library/Booster/LLVM.hs
+++ booster/library/Booster/LLVM.hs
@@ -1,17 +1,18 @@
+{- |
+Copyright   : (c) Runtime Verification, 2023
+License     : BSD-3-Clause
+-}
 module Booster.LLVM (simplifyBool, simplifyTerm) where
 
 import Control.Monad.IO.Class (MonadIO (..))
 import Data.Binary.Get
 import Data.ByteString (fromStrict)
-import Data.Map qualified as Map
-import Data.Set qualified as Set
 import System.IO.Unsafe (unsafePerformIO)
 
 import Booster.Definition.Base
 import Booster.LLVM.Internal qualified as Internal
 import Booster.Pattern.Base
 import Booster.Pattern.Binary
-import Booster.Pattern.Util
 import Booster.Trace qualified as Trace
 
 simplifyBool :: Internal.API -> Term -> Bool
@@ -31,24 +32,5 @@ simplifyTerm api def trm sort = unsafePerformIO $ Internal.runLLVM api $ do
     binary <- liftIO $ kore.simplify trmPtr sortPtr
     liftIO kore.collect
     Trace.traceIO $ Internal.LlvmVar (Internal.somePtr trmPtr) trm
-    -- strip away the custom injection added by the LLVM backend
-    Trace.timeIO "LLVM.simplifyTerm.decodeTerm" $ case runGet (decodeTerm def) (fromStrict binary) of
-        Injection newSort (SortApp "SortKItem" _) result
-            | newSort == sort ->
-                pure result
-            | Set.member (sortName newSort) subsorts ->
-                pure $ Injection newSort sort result
-        someTerm
-            | sortOfTerm someTerm == sort ->
-                pure someTerm
-        other -> do
-            liftIO . putStrLn $
-                "[Error] Unexpected sort after LLVM simplification: "
-                    <> show other
-                    <> " Expected sort "
-                    <> show sort
-            pure trm
-  where
-    sortName (SortApp name _) = name
-    sortName (SortVar name) = name
-    subsorts = maybe Set.empty snd $ Map.lookup (sortName sort) def.sorts
+    Trace.timeIO "LLVM.simplifyTerm.decodeTerm" $
+        pure (runGet (decodeTerm def) (fromStrict binary))
