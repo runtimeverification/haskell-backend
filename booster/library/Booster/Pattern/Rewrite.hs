e80f8ff7b211f5dc4ade0cdd63d38dcea9fb1397
Georgy Lukyanov <georgiylukjanov@gmail.com>
2024-02-01 11:41:59 +0100

    Remove states from RPC logs, include only rule unique IDs (#484)
    This PR removes the states from Booster's rewrite and equation traces.
    Only rule unique IDs are included in the RPC logs now.
    
    Here's a quick performance comparison on the execute request
    `test-fountry-bug-report/state-007.send` with `log-successful-rewrites`
    and `log-successful-simplifications` set to `true`:
    
    | Git revision | Time | Comment |
    
    |:--------------------------------------|:-----------|----------------------------------------------------|
    | georgy/rpc-logs-only-labels | 3.286555 s | this PR |
    | main (153ea54) | 3.259145294| Georgy's log fix reverted |
    | 7c3696f | 35s | logs with states, `-N` still there |
    
    The performance with unique IDs only seems to be on par with not
    collecting traces at all.
    
    I have also checked that:
    
    - [x] we still emit rewrite logs with states to `stderr` when `-l
    Rewrite/RewriteSuccess` is passed and `log-successful/failed-rewrites:
    false`
    - [x] we still emit simplification logs with states to `stderr` when `-l
    Simplify/SimplifySuccess` is passed and
    `log-successful/failed-simplifications: false `
    
    
    # Summary of changes
    
    The idea is to stop accumulating states in `RewriteTrace`s and
    `EquationTrace`s while still keeping the possibility to emit them in
    `stderr` logs and possibly stream them to the client in future.
    
    ## Refactoring of `EquationTrace`
    
    * the `EquationMetadata` type is introduced to hold an equation's
    location, rule label and unique id information
    * `EquationTrace` is split into two constructors, `EquationApplied` and
    `EquationNotApplied`, where the former can hold the post-term, and the
    latter an `ApplyEquationResult`
    * `EquationTrace` is made parametric in the term type, similar to
    `RewriteTrace`. The old `EquationTrace` now becomes `EquationTrace Term`
    * a function `eraseStates :: EquationTrace Term -> EquationTrace ()` is
    added to remove the terms from a trace, leaving only metadata. This
    function will be called in the rewriting code to erase the states from
    the equation traces.
    
    ## Refactoring of `RewriteTrace`
    
    * The `RewriteSimplified` data constructor is changed to accommodate the
    parametricity of `EquationTrace`. Since `EquationTrace` usually holds a
    `Term`, rather than a `Pattern`, we need the `TracePayload` type family
    to connect the two. The intention is to use `RewriteTrace ()`
    everywhere, and all the parametricity is only needed to make the code
    changes minimal. Once we are happy with the API, we could remove
    parameters from both `RewriteTrace` and `EquationTrace`.
    * As with `EquationTrace`, a we now have the `eraseStates ::
    RewriteTrace Pattern -> RewriteTrace ()` function that drops the patters
    from the traces. It also erases the states in euqational traces stored
    in `RewriteSimplified` constructors.
    
    
    # Future work
    
    The `ApplyEquationResult` holds one success and several error cases: 
    
    ```haskell
    data ApplyEquationResult
        = Success Term
        | FailedMatch MatchFailReason
        | IndeterminateMatch
        | IndeterminateCondition [Predicate]
        | ConditionFalse Predicate
        | EnsuresFalse Predicate
        | RuleNotPreservingDefinedness
        | MatchConstraintViolated Constrained VarName
    ```
    
    I think it would be cleaner to factor out the error cases into a
    separate type:
    
    ```haskell
    data ApplyEquationFailureReason
        = FailedMatch MatchFailReason
        | IndeterminateMatch
        | IndeterminateCondition [Predicate]
        | ConditionFalse Predicate
        | EnsuresFalse Predicate
        | RuleNotPreservingDefinedness
        | MatchConstraintViolated Constrained VarName
    
    data ApplyEquationResult
        = ApplyEquationSuccess Term
        | ApplyEquationFailure ApplyEquationFailureReason
    ```
    
    This would make `EquationTrace` cleaner, as currently it can have
    represent absurd case of `EquationNotApplied ... Success`.
    
    ---------
    
    Co-authored-by: Samuel Balco <goodlyrottenapple@gmail.com>

--- booster/library/Booster/Pattern/Rewrite.hs
+++ booster/library/Booster/Pattern/Rewrite.hs
@@ -45,6 +45,7 @@ import Booster.Pattern.ApplyEquations (
     evaluatePattern,
     simplifyConstraint,
  )
+import Booster.Pattern.ApplyEquations qualified as ApplyEquations
 import Booster.Pattern.Base
 import Booster.Pattern.Bool
 import Booster.Pattern.Index (TermIndex (..), kCellTermIndex)
@@ -474,9 +475,23 @@ data RewriteTrace pat
     | -- | attempted rewrite failed
       RewriteStepFailed (RewriteFailed "Rewrite")
     | -- | Applied simplification to the pattern
-      RewriteSimplified [EquationTrace] (Maybe EquationFailure)
-    deriving stock (Eq, Show)
-    deriving (Functor, Foldable, Traversable)
+      RewriteSimplified [EquationTrace (TracePayload pat)] (Maybe EquationFailure)
+
+type family TracePayload pat where
+    TracePayload Pattern = Term
+    TracePayload () = ()
+
+{- | For the given rewrite trace, construct a new one,
+     removing the heavy-weight information (the states),
+     but keeping the meta-data (rule labels).
+-}
+eraseStates :: RewriteTrace Pattern -> RewriteTrace ()
+eraseStates = \case
+    RewriteSingleStep rule_label mUniqueId _preState _postState -> RewriteSingleStep rule_label mUniqueId () ()
+    RewriteBranchingStep _state branchMetadata -> RewriteBranchingStep () branchMetadata
+    RewriteStepFailed failureInfo -> RewriteStepFailed failureInfo
+    RewriteSimplified equationTraces mbEquationFailure ->
+        RewriteSimplified (map ApplyEquations.eraseStates equationTraces) mbEquationFailure
 
 instance Pretty (RewriteTrace Pattern) where
     pretty = \case
@@ -614,7 +629,7 @@ performRewrite ::
     -- | terminal rule labels
     [Text] ->
     Pattern ->
-    io (Natural, Seq (RewriteTrace Pattern), RewriteResult Pattern)
+    io (Natural, Seq (RewriteTrace ()), RewriteResult Pattern)
 performRewrite doTracing def mLlvmLibrary mSolver mbMaxDepth cutLabels terminalLabels pat = do
     (rr, RewriteStepsState{counter, traces}) <-
         flip runStateT rewriteStart $ doSteps False pat
@@ -633,7 +648,8 @@ performRewrite doTracing def mLlvmLibrary mSolver mbMaxDepth cutLabels terminalL
 
     showCounter = (<> " steps.") . pack . show
 
-    rewriteTrace t = do
+    emitRewriteTrace :: RewriteTrace Pattern -> StateT RewriteStepsState io ()
+    emitRewriteTrace t = do
         let prettyT = pack $ renderDefault $ pretty t
         logRewrite prettyT
         case t of
@@ -642,7 +658,7 @@ performRewrite doTracing def mLlvmLibrary mSolver mbMaxDepth cutLabels terminalL
             _other -> pure ()
         when doTracing $
             modify $
-                \rss@RewriteStepsState{traces} -> rss{traces = traces |> t}
+                \rss@RewriteStepsState{traces} -> rss{traces = traces |> eraseStates t}
     incrementCounter =
         modify $ \rss@RewriteStepsState{counter} -> rss{counter = counter + 1}
 
@@ -658,15 +674,15 @@ performRewrite doTracing def mLlvmLibrary mSolver mbMaxDepth cutLabels terminalL
             logTraces traces
             case res of
                 Right newPattern -> do
-                    rewriteTrace $ RewriteSimplified traces Nothing
+                    emitRewriteTrace $ RewriteSimplified traces Nothing
                     pure $ Just newPattern
                 Left r@(SideConditionFalse _p) -> do
                     logSimplify "A side condition was found to be false, pruning"
-                    rewriteTrace $ RewriteSimplified traces (Just r)
+                    emitRewriteTrace $ RewriteSimplified traces (Just r)
                     pure Nothing
                 Left r@UndefinedTerm{} -> do
                     logSimplify "Term is undefined, pruning"
-                    rewriteTrace $ RewriteSimplified traces (Just r)
+                    emitRewriteTrace $ RewriteSimplified traces (Just r)
                     pure Nothing
                 -- NB any errors here might be caused by simplifying one
                 -- of the constraints, so we cannot use partial results
@@ -682,7 +698,7 @@ performRewrite doTracing def mLlvmLibrary mSolver mbMaxDepth cutLabels terminalL
                             ]
                         )
                     -- could output term before and after at debug or custom log level
-                    rewriteTrace $ RewriteSimplified traces (Just r)
+                    emitRewriteTrace $ RewriteSimplified traces (Just r)
                     pure $ Just p
                 Left r@(EquationLoop (t : ts)) -> do
                     logError "Equation evaluation loop"
@@ -693,11 +709,11 @@ performRewrite doTracing def mLlvmLibrary mSolver mbMaxDepth cutLabels terminalL
                                 <> prettyText l
                                 <> ": \n"
                                 <> Text.unlines (map (prettyText . fst) termDiffs)
-                    rewriteTrace $ RewriteSimplified traces (Just r)
+                    emitRewriteTrace $ RewriteSimplified traces (Just r)
                     pure $ Just p
                 Left other -> do
                     logError . pack $ "Simplification error during rewrite: " <> show other
-                    rewriteTrace $ RewriteSimplified traces (Just other)
+                    emitRewriteTrace $ RewriteSimplified traces (Just other)
                     pure $ Just p
 
     -- Results may change when simplification prunes a false side
@@ -762,12 +778,12 @@ performRewrite doTracing def mLlvmLibrary mSolver mbMaxDepth cutLabels terminalL
                     >>= \case
                         Right (RewriteFinished mlbl uniqueId single, cache) -> do
                             whenJust mlbl $ \lbl ->
-                                rewriteTrace $ RewriteSingleStep lbl uniqueId pat' single
+                                emitRewriteTrace $ RewriteSingleStep lbl uniqueId pat' single
                             updateCache cache
                             incrementCounter
                             doSteps False single
                         Right (terminal@(RewriteTerminal lbl uniqueId single), _cache) -> do
-                            rewriteTrace $ RewriteSingleStep lbl uniqueId pat' single
+                            emitRewriteTrace $ RewriteSingleStep lbl uniqueId pat' single
                             logRewrite $
                                 "Terminal rule after " <> showCounter (counter + 1)
                             incrementCounter
@@ -786,11 +802,11 @@ performRewrite doTracing def mLlvmLibrary mSolver mbMaxDepth cutLabels terminalL
                                 RewriteFinished mlbl uniqueId single -> do
                                     logRewrite "All but one branch pruned, continuing"
                                     whenJust mlbl $ \lbl ->
-                                        rewriteTrace $ RewriteSingleStep lbl uniqueId pat' single
+                                        emitRewriteTrace $ RewriteSingleStep lbl uniqueId pat' single
                                     incrementCounter
                                     doSteps False single
                                 RewriteBranch pat'' branches -> do
-                                    rewriteTrace $ RewriteBranchingStep pat'' $ fmap (\(lbl, uid, _) -> (lbl, uid)) branches
+                                    emitRewriteTrace $ RewriteBranchingStep pat'' $ fmap (\(lbl, uid, _) -> (lbl, uid)) branches
                                     pure simplified
                                 _other -> error "simplifyResult: Unexpected return value"
                         Right (cutPoint@(RewriteCutPoint lbl _ _ _), _) -> do
@@ -807,7 +823,7 @@ performRewrite doTracing def mLlvmLibrary mSolver mbMaxDepth cutLabels terminalL
                         Right (stuck@RewriteStuck{}, cache) -> do
                             logRewrite $ "Stopped after " <> showCounter counter
                             updateCache cache
-                            rewriteTrace $ RewriteStepFailed $ NoApplicableRules pat'
+                            emitRewriteTrace $ RewriteStepFailed $ NoApplicableRules pat'
                             if wasSimplified
                                 then pure stuck
                                 else withSimplified pat' "Retrying with simplified pattern" (doSteps True)
@@ -821,12 +837,12 @@ performRewrite doTracing def mLlvmLibrary mSolver mbMaxDepth cutLabels terminalL
                         -- unsimplified, simplify and retry rewriting once
                         Left failure@RuleApplicationUnclear{}
                             | not wasSimplified -> do
-                                rewriteTrace $ RewriteStepFailed failure
+                                emitRewriteTrace $ RewriteStepFailed failure
                                 -- simplify remainders, substitute and rerun.
                                 -- If failed, do the pattern-wide simplfication and rerun again
                                 withSimplified pat' "Retrying with simplified pattern" (doSteps True)
                         Left failure -> do
-                            rewriteTrace $ RewriteStepFailed failure
+                            emitRewriteTrace $ RewriteStepFailed failure
                             logAborts . renderText $ pretty failure
                             let msg = "Aborted after " <> showCounter counter
                             if wasSimplified
@@ -844,7 +860,7 @@ performRewrite doTracing def mLlvmLibrary mSolver mbMaxDepth cutLabels terminalL
 
 data RewriteStepsState = RewriteStepsState
     { counter :: !Natural
-    , traces :: !(Seq (RewriteTrace Pattern))
+    , traces :: !(Seq (RewriteTrace ()))
     , simplifierCache :: SimplifierCache
     , smtSolver :: Maybe SMT.SMTContext
     }
