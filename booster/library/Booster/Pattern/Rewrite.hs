e85877b08c3585d9c425166421077d125ee20aaf
Georgy Lukyanov <georgiylukjanov@gmail.com>
2024-02-07 13:28:51 +0100

    Rework term indexing and stuck rewrites (#465)
    Closes
    https://github.com/runtimeverification/hs-backend-booster/issues/457
    * do not assign term index `None` to unrecognized terms, use `Anything`
    instead
    * do not abort when there are no rules, get stuck instead
    
    TODO:
    - [x] add unit test that assigns `Anything` to `dotk()` and gets stuck
    - [x] ~Investigate why several KEVM tests fail (see list in the comment
    below)~ -- resolved itself with merging main.
    - [x] run Kontrol performance tests
    - [x] run KEVM performance tests
    
    ---------
    
    Co-authored-by: rv-jenkins <admin@runtimeverification.com>

--- booster/library/Booster/Pattern/Rewrite.hs
+++ booster/library/Booster/Pattern/Rewrite.hs
@@ -124,8 +124,6 @@ rewriteStep cutLabels terminalLabels pat = do
                     then idxRules
                     else Map.unionWith (<>) idxRules anyRules
 
-    when (null rules) $ throw (NoRulesForTerm pat.term)
-
     -- process one priority group at a time (descending priority),
     -- until a result is obtained or the entire rewrite fails.
     processGroups pat rules
@@ -387,9 +385,7 @@ applyRule pat@Pattern{ceilConditions} rule = runRewriteRuleAppT $ do
    information for logging what happened during the rewrite.
 -}
 data RewriteFailed k
-    = -- | No rules have been found
-      NoRulesForTerm Term
-    | -- | All rules have been tried unsuccessfully (rewrite is stuck)
+    = -- | All rules have been tried unsuccessfully (rewrite is stuck)
       NoApplicableRules Pattern
     | -- | It is uncertain whether or not a rule LHS unifies with the term
       RuleApplicationUnclear (RewriteRule k) Term (NonEmpty (Term, Term))
@@ -406,8 +402,6 @@ data RewriteFailed k
     deriving stock (Eq, Show)
 
 instance Pretty (RewriteFailed k) where
-    pretty (NoRulesForTerm term) =
-        "No rules for term " <> pretty term
     pretty (NoApplicableRules pat) =
         "No rules applicable for the pattern " <> pretty pat
     pretty (RuleApplicationUnclear rule term remainder) =
