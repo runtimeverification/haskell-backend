1bc6aeeab2489bd0ee6e24685ab256aa361e5708
Samuel Balco <goodlyrottenapple@gmail.com>
2024-02-23 10:30:50 +0000

    Update nixpkgs+ghc (#515)
    Update nixpkgs to 23.11 and ghc to 9.6.4, inline with
    https://github.com/runtimeverification/haskell-backend/pull/3719
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Jost Berthold <jost.berthold@runtimeverification.com>

--- booster/flake.nix
+++ booster/flake.nix
@@ -2,7 +2,7 @@
   description = "hs-backend-booster";
 
   inputs = {
-    haskell-backend.url = "github:runtimeverification/haskell-backend/ac6908d0ca26f194802c72f66f200e7079139f1b";
+    haskell-backend.url = "github:runtimeverification/haskell-backend/fa0d3b2809154e4c01c0ab62660f757a8fc31dfb";
     stacklock2nix.follows = "haskell-backend/stacklock2nix";
     nixpkgs.follows = "haskell-backend/nixpkgs";
   };
@@ -27,12 +27,13 @@
             makeWrapper ${pkg}/bin/${exe} $out/bin/${exe} --prefix PATH : ${pkgs.z3}/bin
           '';
         };
+      ghcVersion = pkgs: pkgs.haskell.packages.ghc964;
     in {
       overlay = final: prev: {
         booster-backend = final.stacklock2nix {
           stackYaml = ./stack.yaml;
           # This should based on the compiler version from the resolver in stack.yaml.
-          baseHaskellPkgSet = final.haskell.packages.ghc928;
+          baseHaskellPkgSet = ghcVersion final;
           cabal2nixArgsOverrides = args:
             args // {
               # The Haskell package `"graphviz"` depends on the _system_
@@ -43,9 +44,10 @@
             };
           additionalHaskellPkgSetOverrides = hfinal: hprev:
             with final.haskell.lib; {
+              crypton-x509 = dontCheck hprev.crypton-x509;
               decision-diagrams = dontCheck hprev.decision-diagrams;
               fgl = dontCheck hprev.fgl;
-              haskeline = dontCheck hprev.haskeline;
+              graphviz = dontCheck hprev.graphviz;
               smtlib-backends-process = dontCheck hprev.smtlib-backends-process;
               hs-backend-booster = overrideCabal hprev.hs-backend-booster
                 (drv: {
@@ -55,7 +57,6 @@
                     substituteInPlace library/Booster/VersionInfo.hs \
                       --replace '$(GitRev.gitHash)' '"${self.rev or "dirty"}"'
                   '';
-                  buildTarget = "kore-rpc-booster";
                 });
               json-rpc = dontCheck hprev.json-rpc;
               kore = (dontCheck hprev.kore).override {
@@ -63,32 +64,27 @@
                 # and depending on another copy of ghc-compact breaks HLS in the dev shell.
                 ghc-compact = null;
               };
+              lifted-base = dontCheck hprev.lifted-base;
+              prettyprinter = dontCheck hprev.prettyprinter;
               tar = dontCheck hprev.tar;
             };
           # Additional packages that should be available for development.
           additionalDevShellNativeBuildInputs = stacklockHaskellPkgSet:
-            with final; [
-              haskell.packages.ghc928.cabal-install
-              haskellPackages.fourmolu_0_12_0_0
-              (let
-                ghc-lib-parser = haskellPackages.ghc-lib-parser_9_4_5_20230430;
-                ghc-lib-parser-ex =
-                  haskellPackages.ghc-lib-parser-ex_9_4_0_0.override {
-                    inherit ghc-lib-parser;
-                  };
-              in haskellPackages.hlint_3_5.override {
-                inherit ghc-lib-parser ghc-lib-parser-ex;
-              })
-              (haskell-language-server.override {
-                supportedGhcVersions = [ "928" ];
-              })
+            with ghcVersion final; [
+              cabal-install
+              fourmolu
+              hlint
+              # provides haskell-language-server-wrapper
+              final.haskell-language-server
+               # provides the actual haskell-language-server binary
+              haskell-backend.packages.${prev.system}.haskell-language-server
               final.z3
               final.hpack
             ];
           all-cabal-hashes = final.fetchurl {
             url =
-              "https://github.com/commercialhaskell/all-cabal-hashes/archive/779bc22f8c7f8e3566edd5a4922750b73a0e5ed5.tar.gz";
-            sha256 = "sha256-qJ/rrdjCTil5wBlcJQ0w+1NP9F/Cr7X/pAfnnx/ahLc=";
+              "https://github.com/commercialhaskell/all-cabal-hashes/archive/80fe3174b98134e50d4541c9c2a3803601f6fbb7.tar.gz";
+            sha256 = "sha256-b3E6JLu1tBpZnPXBJxNXfjkglY/du8k1l+WTS8Fetr4=";
           };
         };
       };
@@ -101,16 +97,26 @@
             booster-backend.pkgSet.hs-backend-booster;
         in {
           kore-rpc-booster = withZ3 pkgs hs-backend-booster "kore-rpc-booster";
+          kore-rpc-client = withZ3 pkgs hs-backend-booster "kore-rpc-client";
         });
 
       devShells = perSystem (system: {
-        # Separate cabal shell just for CI
+        # Separate fourmolu+hlint and cabal shells just for CI
+        style = with nixpkgsCleanFor system;
+          mkShell {
+            nativeBuildInputs = [
+              (haskell.lib.justStaticExecutables
+                (ghcVersion pkgs).fourmolu)
+              (haskell.lib.justStaticExecutables
+                (ghcVersion pkgs).hlint)
+            ];
+          };
         cabal = let pkgs = nixpkgsFor system;
         in pkgs.booster-backend.pkgSet.shellFor {
           packages = pkgs.booster-backend.localPkgsSelector;
           nativeBuildInputs = [
             pkgs.diffutils
-            pkgs.haskell.packages.ghc928.cabal-install
+            (ghcVersion pkgs).cabal-install
             pkgs.hpack
             pkgs.jq
             pkgs.nix
