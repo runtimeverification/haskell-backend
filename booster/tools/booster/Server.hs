f042236248fb67ed9a933d4543f6d90b1ab34abc
Jost Berthold <jost.berthold@runtimeverification.com>
2024-02-26 21:18:38 +1100

    Hotfix add labelled rule logging for kore (#524)
    The rule logging used for `kore-rpc` in log level `RewriteKore` outputs
    rule locations but not rule labels for successful rewrites. This PR adds
    another log option which outputs each applied rule with its label.
    This is important for rules added via `add-module` requests which don't
    usually have rule locations (but do have labels).
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- booster/tools/booster/Server.hs
+++ booster/tools/booster/Server.hs
@@ -229,7 +229,10 @@ koreExtraLogs =
     Map.map (Set.fromList . mapMaybe (`Map.lookup` Log.textToType Log.registry)) $
         Map.fromList
             [ (LevelOther "SimplifyKore", ["DebugAttemptEquation", "DebugApplyEquation"])
-            , (LevelOther "RewriteKore", ["DebugAttemptedRewriteRules", "DebugAppliedRewriteRules"])
+            ,
+                ( LevelOther "RewriteKore"
+                , ["DebugAttemptedRewriteRules", "DebugAppliedLabeledRewriteRule", "DebugAppliedRewriteRules"]
+                )
             , (LevelOther "SimplifySuccess", ["DebugApplyEquation"])
             , (LevelOther "RewriteSuccess", ["DebugAppliedRewriteRules"])
             ]
