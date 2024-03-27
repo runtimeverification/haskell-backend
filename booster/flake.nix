56cb6369f1bc5ef442404b98e81d3116623fae0a
rv-jenkins <admin@runtimeverification.com>
2024-03-27 01:42:08 -0600

    Update dependency: deps/haskell-backend_release (#562)
    Co-authored-by: devops <devops@runtimeverification.com>

--- booster/flake.nix
+++ booster/flake.nix
@@ -2,7 +2,7 @@
   description = "hs-backend-booster";
 
   inputs = {
-    haskell-backend.url = "github:runtimeverification/haskell-backend/8f97e95b77fe590a9fe5a9b0212f32fcad0be35b";
+    haskell-backend.url = "github:runtimeverification/haskell-backend/e7ba3c7e5626d3b2dd61318e91704fd6bf47228e";
     stacklock2nix.follows = "haskell-backend/stacklock2nix";
     nixpkgs.follows = "haskell-backend/nixpkgs";
   };
