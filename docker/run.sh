87fc3b4f24a43e79f2c8587577215d8f67eba500
Georgy Lukyanov <mail@geo2a.info>
2024-04-16 14:26:56 +0200

    Remove `web/` and `docker/`

--- docker/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Run command in Docker
-# Usage: ./docker/run.sh make test
-docker run --rm --mount src=$(pwd),target=/home/user/kore,type=bind --workdir=/home/user/kore kore "$@"
