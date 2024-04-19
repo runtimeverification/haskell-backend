ce38579d2d9d47354c9f858ddcdb50b228607ff3
Georgy Lukyanov <mail@geo2a.info>
2024-04-19 10:11:19 +0200

    hlint

--- booster/tools/booster/Proxy.hs
+++ booster/tools/booster/Proxy.hs
@@ -103,14 +103,12 @@ respondEither cfg@ProxyConfig{statsVar, boosterState} booster kore req = case re
                             fromMaybe (error $ "Module " <> show m <> " not found") $
                                 Map.lookup m bState.definitions
                     handleExecute logSettings def start execReq
-    Implies impliesReq -> do
-        koreResult <-
-            loggedKore
-                ImpliesM
-                ( Implies
-                    impliesReq
-                )
-        pure koreResult
+    Implies impliesReq ->
+        loggedKore
+            ImpliesM
+            ( Implies
+                impliesReq
+            )
     Simplify simplifyReq ->
         liftIO (getTime Monotonic) >>= handleSimplify simplifyReq . Just
     AddModule _ -> do
