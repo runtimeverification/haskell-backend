ca05e660f32233a8d6e2f94c32e78c4e5d1c8455
Jost Berthold <jost.berthold@gmail.com>
2024-04-24 15:58:07 +1000

    implement more Map hooks and LIST.get

--- booster/library/Booster/Builtin.hs
+++ booster/library/Booster/Builtin.hs
@@ -22,10 +22,15 @@ import Data.ByteString.Char8 (ByteString, pack, unpack)
 import Data.List (findIndex, partition)
 import Data.Map (Map)
 import Data.Map qualified as Map
+import Data.Set qualified as Set
 import Data.Text (Text)
 import Prettyprinter (pretty, vsep)
 import Text.Read (readMaybe)
 
+import Booster.Definition.Attributes.Base (
+    KCollectionSymbolNames (..),
+    KListDefinition (..),
+ )
 import Booster.Pattern.Base
 import Booster.Pattern.Bool
 import Booster.Pattern.Util
@@ -61,6 +66,9 @@ t `shouldHaveSort` s
             [ pretty $ "Argument term has unexpected sort (expected " <> show s <> "):"
             , pretty t
             ]
+
+isConstructorLike_ :: Term -> Bool
+isConstructorLike_ = (.isConstructorLike) . getAttributes
 
 ------------------------------------------------------------
 -- BOOL hooks
@@ -197,11 +205,68 @@ builtinsMAP :: Map ByteString BuiltinFunction
 builtinsMAP =
     Map.mapKeys ("MAP." <>) $
         Map.fromList
-            [ "lookup" ~~> mapLookupHook
+            [ "update" ~~> mapUpdateHook
+            , "remove" ~~> mapRemoveHook
+            , -- removeAll: requires a Set argument
+              "size" ~~> mapSizeHook
+            , "lookup" ~~> mapLookupHook
             , "lookupOrDefault" ~~> mapLookupOrDefaultHook
             , "in_keys" ~~> mapInKeysHook
-            , "update" ~~> mapUpdateHook
+            , -- keys: requires internal Set construction
+              "keys_list" ~~> mapKeysListHook
+            , "values" ~~> mapValuesHook
+            , "inclusion" ~~> mapInclusionHook
             ]
+
+mapUpdateHook :: BuiltinFunction
+mapUpdateHook args
+    | [KMap def pairs mbRest, key, newValue] <- args = do
+        case findIndex ((== key) . fst) pairs of
+            Just idx ->
+                -- key was found (syntactically), update pairs list
+                let newPairs = take idx pairs <> ((key, newValue) : drop (idx + 1) pairs)
+                 in pure $ Just $ KMap def newPairs mbRest
+            Nothing -- key could be in unevaluated or opaque part
+                | Just _ <- mbRest ->
+                    pure Nothing -- have opaque part, no result
+                | any (not . isConstructorLike_ . fst) pairs ->
+                    pure Nothing -- have unevaluated keys, no result
+                | otherwise -> -- key certain to be absent, no rest: add pair
+                    pure $ Just $ KMap def ((key, newValue) : pairs) Nothing
+    | [_other, _, _] <- args =
+        pure Nothing -- not an internalised map, maybe a function call
+    | otherwise =
+        throwE . renderText $ "MAP.update: wrong arity " <> pretty (length args)
+
+mapRemoveHook :: BuiltinFunction
+mapRemoveHook args
+    | [m@(KMap def pairs mbRest), key] <- args = do
+        case findIndex ((== key) . fst) pairs of
+            Just idx ->
+                -- key was found (syntactically), remove it
+                let newPairs = take idx pairs <> drop (idx + 1) pairs
+                 in pure $ Just $ KMap def newPairs mbRest
+            Nothing -- key could be in unevaluated or opaque part
+                | Just _ <- mbRest ->
+                    pure Nothing -- have opaque part, no result
+                | any (not . isConstructorLike_ . fst) pairs ->
+                    pure Nothing -- have unevaluated keys, no result
+                | otherwise -> -- key certain to be absent, no rest: map unchanged
+                    pure $ Just m
+    | [_other, _] <- args =
+        pure Nothing -- not an internalised map, maybe a function call
+    | otherwise =
+        throwE . renderText $ "MAP.remove: wrong arity " <> pretty (length args)
+
+mapSizeHook :: BuiltinFunction
+mapSizeHook args
+    | [KMap _ pairs Nothing] <- args =
+        -- no opaque part, size is association count
+        pure $ Just $ intTerm (fromIntegral $ length pairs)
+    | [_other, _] <- args =
+        pure Nothing -- not an internalised map, maybe a function call
+    | otherwise =
+        throwE . renderText $ "MAP.lookup: wrong arity " <> pretty (length args)
 
 mapLookupHook :: BuiltinFunction
 mapLookupHook args
@@ -210,10 +275,8 @@ mapLookupHook args
         -- regardless of whether the key _could_ still be there.
         pure $ lookup key pairs
     | [_other, _] <- args =
-        -- other `shouldHaveSort` "SortMap"
         pure Nothing -- not an internalised map, maybe a function call
     | otherwise =
-        -- FIXME write a helper function for arity check
         throwE . renderText $ "MAP.lookup: wrong arity " <> pretty (length args)
 
 mapLookupOrDefaultHook :: BuiltinFunction
@@ -226,12 +289,11 @@ mapLookupOrDefaultHook args
             Nothing -- key could be in unevaluated or opaque part
                 | Just _ <- mbRest ->
                     pure Nothing -- have opaque part, no result
-                | any ((\(Term a _) -> not a.isConstructorLike) . fst) pairs ->
+                | any (not . isConstructorLike_ . fst) pairs ->
                     pure Nothing -- have unevaluated keys, no result
                 | otherwise -> -- certain that the key is not in the map
                     pure $ Just defaultValue
     | [_other, _, _] <- args =
-        -- other `shouldHaveSort` "SortMap"
         pure Nothing -- not an internalised map, maybe a function call
     | otherwise =
         throwE . renderText $ "MAP.lookupOrDefault: wrong arity " <> pretty (length args)
@@ -241,7 +303,7 @@ mapInKeysHook args
     | [key, KMap _ pairs mbRest] <- args = do
         -- only consider evaluated keys, return Nothing if any unevaluated ones are present
         let (eval'edKeys, uneval'edKeys) =
-                partition (\(Term a _) -> a.isConstructorLike) (map fst pairs)
+                partition isConstructorLike_ (map fst pairs)
         case (key `elem` eval'edKeys, key `elem` uneval'edKeys) of
             (True, _) ->
                 -- constructor-like (evaluated) key is present
@@ -261,35 +323,105 @@ mapInKeysHook args
     | otherwise =
         throwE . renderText $ "MAP.in_keys: wrong arity " <> pretty (length args)
 
-mapUpdateHook :: BuiltinFunction
-mapUpdateHook args
-    | [KMap def pairs mbRest, key, newValue] <- args = do
-        case findIndex ((== key) . fst) pairs of
-            Just idx ->
-                -- key was found (syntactically), update pairs list
-                let newPairs = take idx pairs <> ((key, newValue) : drop (idx + 1) pairs)
-                 in pure $ Just $ KMap def newPairs mbRest
-            Nothing -- key could be in unevaluated or opaque part
-                | Just _ <- mbRest ->
-                    pure Nothing -- have opaque part, no result
-                | any ((\(Term a _) -> not a.isConstructorLike) . fst) pairs ->
-                    pure Nothing -- have unevaluated keys, no result
-                | otherwise -> -- key certain to be absent, no rest: add pair
-                    pure $ Just $ KMap def ((key, newValue) : pairs) Nothing
-    | [_other, _, _] <- args =
-        -- other `shouldHaveSort` "SortMap"
-        pure Nothing -- not an internalised map, maybe a function call
-    | otherwise =
-        throwE . renderText $ "MAP.update: wrong arity " <> pretty (length args)
+mapKeysListHook :: BuiltinFunction
+mapKeysListHook = \case
+    [KMap _ pairs Nothing] ->
+        -- known keys only, return as list
+        pure $ Just $ KList kItemListDef (map fst pairs) Nothing
+    [KMap _ _ (Just _)] ->
+        -- indeterminate part
+        pure Nothing
+    [_arg] ->
+        -- unevaluated
+        pure Nothing
+    args ->
+        throwE . renderText $ "MAP.keys_list: wrong arity " <> pretty (length args)
 
-------------------------------------------------------------
+mapValuesHook :: BuiltinFunction
+mapValuesHook = \case
+    [KMap _ pairs Nothing] ->
+        -- known values only, return as list
+        pure $ Just $ KList kItemListDef (map snd pairs) Nothing
+    [KMap _ _ (Just _)] ->
+        -- indeterminate part
+        pure Nothing
+    [_arg] ->
+        -- unevaluated
+        pure Nothing
+    args ->
+        throwE . renderText $ "MAP.values: wrong arity " <> pretty (length args)
+
+mapInclusionHook :: BuiltinFunction
+mapInclusionHook = \case
+    [KMap d1 pairs1 mbRest1, KMap d2 pairs2 mbRest2]
+        | d1 /= d2 -> -- different kinds of map
+            pure Nothing
+        | pairs1 == pairs2 -- syntactically identical maps
+        , mbRest1 == mbRest2 ->
+            pure $ Just TrueBool
+    [KMap _ pairs1 Nothing, KMap _ pairs2 mbRest2]
+        -- fully concrete maps
+        | keySet pairs1 `Set.isSubsetOf` keySet pairs2 ->
+            pure $ Just TrueBool
+        | all isConstructorLike_ (keySet pairs1)
+        , all isConstructorLike_ (keySet pairs2)
+        , Nothing <- mbRest2 -> -- fully-known keys, certain to not be subset
+            pure $ Just FalseBool
+        | otherwise ->
+            pure Nothing -- unevaluated keys present, indeterminate
+    [KMap _ _ (Just _), KMap _ _ _] ->
+        pure Nothing -- indeterminate part cannot be checked
+    [_, _] ->
+        pure Nothing
+    args ->
+        throwE . renderText $ "MAP.inclusion: wrong arity " <> pretty (length args)
+  where
+    keySet = Set.fromList . map fst
+
+-----------------------------------------------------------
 -- LIST hooks
+
 builtinsLIST :: Map ByteString BuiltinFunction
 builtinsLIST =
     Map.mapKeys ("LIST." <>) $
         Map.fromList
-            [ "size" ~~> listSizeHook
+            [ "get" ~~> listGetHook
+            , "size" ~~> listSizeHook
             ]
+
+listGetHook :: BuiltinFunction
+listGetHook [KList _ heads mbRest, intArg] =
+    let headLen = length heads
+     in case fromIntegral <$> readIntTerm intArg of
+            Nothing ->
+                intArg `shouldHaveSort` "SortInt" >> pure Nothing
+            Just i
+                | 0 <= i ->
+                    if i < headLen
+                        then pure $ Just $ heads !! i -- positive index in range
+                        else -- headLen <= i
+                        case mbRest of
+                            Nothing ->
+                                -- index too large
+                                pure Nothing -- actually #Bottom
+                            Just _ ->
+                                pure Nothing
+                | otherwise -> -- i < 0, negative index, consider rest
+                    case mbRest of
+                        Nothing
+                            | 0 <= headLen - abs i ->
+                                pure $ Just $ heads !! (headLen - abs i)
+                            | otherwise ->
+                                pure Nothing -- actually #Bottom
+                        Just (_middle, tails)
+                            | 0 <= length tails - abs i ->
+                                pure $ Just $ tails !! (length tails - abs i)
+                            | otherwise ->
+                                pure Nothing -- indeterminate middle
+listGetHook [_other, _] =
+    pure Nothing
+listGetHook args =
+    throwE . renderText $ "LIST.get: wrong arity " <> pretty (length args)
 
 listSizeHook :: BuiltinFunction
 listSizeHook = \case
@@ -301,6 +433,19 @@ listSizeHook = \case
         pure Nothing -- not an internal list, maybe unevaluated function call
     moreArgs ->
         throwE . renderText $ "LIST.size: wrong arity " <> pretty (length moreArgs)
+
+kItemListDef :: KListDefinition
+kItemListDef =
+    KListDefinition
+        { symbolNames =
+            KCollectionSymbolNames
+                { unitSymbolName = "Lbl'Stop'List"
+                , elementSymbolName = "LblListItem"
+                , concatSymbolName = "Lbl'Unds'List'Unds'"
+                }
+        , elementSortName = "SortKItem"
+        , listSortName = "SortList"
+        }
 
 ------------------------------------------------------------
 -- KEQUAL hooks
