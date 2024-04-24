530d57470ae71297434bab076f1f6ae78f32ad6e
Jost Berthold <jost.berthold@gmail.com>
2024-04-24 14:33:25 +1000

    add INT and BOOL hooks, arrange parts as per documentation

--- booster/library/Booster/Builtin.hs
+++ booster/library/Booster/Builtin.hs
@@ -2,10 +2,15 @@
 Copyright   : (c) Runtime Verification, 2023
 License     : BSD-3-Clause
 
-Builtin functions. Only a select few functions are implemented.
+Builtin functions as described in [docs/hooks.md](https://github.com/runtimeverification/haskell-backend/blob/master/docs/hooks.md).
+Only selected functions are implemented.
 
 Built-in functions are looked up by their symbol attribute 'hook' from
 the definition's symbol map.
+
+The built-in function fails outright when its function is called with
+a wrong argument count. When required arguments are unevaluated, the
+hook returns 'Nothing'.
 -}
 module Booster.Builtin (
     hooks,
@@ -13,12 +18,13 @@ module Booster.Builtin (
 
 import Control.Monad
 import Control.Monad.Trans.Except
-import Data.ByteString.Char8 (ByteString, pack)
+import Data.ByteString.Char8 (ByteString, pack, unpack)
 import Data.List (findIndex, partition)
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Text (Text)
 import Prettyprinter (pretty, vsep)
+import Text.Read (readMaybe)
 
 import Booster.Pattern.Base
 import Booster.Pattern.Bool
@@ -32,35 +38,158 @@ type BuiltinFunction = [Term] -> Except Text (Maybe Term)
 hooks :: Map ByteString BuiltinFunction
 hooks =
     Map.unions
-        [ builtinsKEQUAL
+        [ builtinsBOOL
+        , builtinsINT
         , builtinsMAP
         , builtinsLIST
+        , builtinsKEQUAL
         ]
 
 ------------------------------------------------------------
+-- Helpers
+
 (~~>) :: ByteString -> BuiltinFunction -> (ByteString, BuiltinFunction)
 (~~>) = (,)
 
-
-------------------------------------------------------------
--- LIST hooks
-builtinsLIST :: Map ByteString BuiltinFunction
-builtinsLIST =
-    Map.mapKeys ("LIST." <>) $
-        Map.fromList
-            [ "size" ~~> listSizeHook
+-- check for simple (parameter-less) sorts
+shouldHaveSort :: Term -> SortName -> Except Text ()
+t `shouldHaveSort` s
+    | sortOfTerm t == SortApp s [] =
+        pure ()
+    | otherwise =
+        throwE . renderText . vsep $
+            [ pretty $ "Argument term has unexpected sort (expected " <> show s <> "):"
+            , pretty t
             ]
 
-listSizeHook :: BuiltinFunction
-listSizeHook = \case
-    [KList _ heads Nothing] ->
-        pure $ Just $ DomainValue SortInt $ pack (show (length heads))
-    [KList _ _ (Just _)] ->
-        pure Nothing -- tail of list not determined
-    [_other] ->
-        pure Nothing -- not an internal list, maybe unevaluated function call
-    moreArgs ->
-        throwE . renderText $ "LIST.size: wrong arity " <> pretty (length moreArgs)
+------------------------------------------------------------
+-- BOOL hooks
+
+builtinsBOOL :: Map ByteString BuiltinFunction
+builtinsBOOL =
+    Map.mapKeys ("BOOL." <>) $
+        Map.fromList
+            [ "or" ~~> orHook
+            , "and" ~~> andHook
+            , "xor" ~~> boolOperator (/=)
+            , "eq" ~~> boolOperator (==)
+            , "ne" ~~> boolOperator (/=)
+            , "not" ~~> notHook
+            , "implies" ~~> impliesHook
+            ]
+
+-- shortcut evaluations for or and and
+orHook :: BuiltinFunction
+orHook args
+    | length args /= 2 =
+        throwE . renderText $ "BOOL.or: wrong arity " <> pretty (length args)
+    | [TrueBool, _] <- args = pure $ Just TrueBool
+    | [_, TrueBool] <- args = pure $ Just TrueBool
+    | [FalseBool, FalseBool] <- args = pure $ Just FalseBool
+    | otherwise = pure Nothing -- arguments not determined
+
+andHook :: BuiltinFunction
+andHook args
+    | length args /= 2 =
+        throwE . renderText $ "BOOL.and: wrong arity " <> pretty (length args)
+    | [FalseBool, _] <- args = pure $ Just FalseBool
+    | [_, FalseBool] <- args = pure $ Just FalseBool
+    | [TrueBool, TrueBool] <- args = pure $ Just TrueBool
+    | otherwise = pure Nothing -- arguments not determined
+
+notHook :: BuiltinFunction
+notHook [arg]
+    | Just b <- readBoolTerm arg = pure . Just . boolTerm $ not b
+    | otherwise = pure Nothing
+notHook args =
+    throwE . renderText $ "BOOL.not: wrong arity " <> pretty (length args)
+
+impliesHook :: BuiltinFunction
+impliesHook args
+    | length args /= 2 =
+        throwE . renderText $ "BOOL.implies: wrong arity " <> pretty (length args)
+    | [FalseBool, _] <- args = pure $ Just TrueBool
+    | [TrueBool, FalseBool] <- args = pure $ Just FalseBool
+    | [TrueBool, TrueBool] <- args = pure $ Just TrueBool
+    | otherwise = pure Nothing -- arguments not determined
+
+boolOperator :: (Bool -> Bool -> Bool) -> BuiltinFunction
+boolOperator f args
+    | length args /= 2 =
+        throwE . renderText $ "BOOL.<operator>: wrong arity " <> pretty (length args)
+    | [Just arg1, Just arg2] <- map readBoolTerm args =
+        pure . Just . boolTerm $ f arg1 arg2
+    | otherwise = pure Nothing -- arguments not determined
+
+boolTerm :: Bool -> Term
+boolTerm True = TrueBool
+boolTerm False = FalseBool
+
+readBoolTerm :: Term -> Maybe Bool
+readBoolTerm TrueBool = Just True
+readBoolTerm FalseBool = Just False
+readBoolTerm _other = Nothing
+
+------------------------------------------------------------
+-- INT hooks
+
+builtinsINT :: Map ByteString BuiltinFunction
+builtinsINT =
+    Map.mapKeys ("INT." <>) $
+        Map.fromList
+            [ "gt" ~~> compareInt (>)
+            , "ge" ~~> compareInt (>=)
+            , "eq" ~~> compareInt (==)
+            , "le" ~~> compareInt (<=)
+            , "lt" ~~> compareInt (<)
+            , "ne" ~~> compareInt (/=)
+            , -- arithmetic
+              "add" ~~> intOperator (+)
+            , "sub" ~~> intOperator (-)
+            , "mul" ~~> intOperator (*)
+            , "abs" ~~> intModifier abs
+            -- tdiv, tmod (truncating toward zero), ediv, emod (euclidian)
+            -- bitwise operations
+            -- and, or, xor, not, shl, shr
+            -- exponentiation
+            -- pow, powmod, log2 (truncating)
+            ]
+
+compareInt :: (Integer -> Integer -> Bool) -> BuiltinFunction
+compareInt f args
+    | length args /= 2 =
+        throwE . renderText $ "INT.<comparison>: wrong arity " <> pretty (length args)
+    | [arg1, arg2] <- args
+    , Just i1 <- readIntTerm arg1
+    , Just i2 <- readIntTerm arg2 =
+        pure . Just . boolTerm $ f i1 i2
+    | otherwise = pure Nothing
+
+intOperator :: (Integer -> Integer -> Integer) -> BuiltinFunction
+intOperator f args
+    | length args /= 2 =
+        throwE . renderText $ "INT.<operator>: wrong arity " <> pretty (length args)
+    | [arg1, arg2] <- args
+    , Just i1 <- readIntTerm arg1
+    , Just i2 <- readIntTerm arg2 =
+        pure . Just . intTerm $ f i1 i2
+    | otherwise = pure Nothing
+
+intModifier :: (Integer -> Integer) -> BuiltinFunction
+intModifier f args
+    | length args /= 1 =
+        throwE . renderText $ "INT.<operator>: wrong arity " <> pretty (length args)
+    | [arg] <- args
+    , Just i <- readIntTerm arg =
+        pure . Just . intTerm $ f i
+    | otherwise = pure Nothing
+
+intTerm :: Integer -> Term
+intTerm = DomainValue SortInt . pack . show
+
+readIntTerm :: Term -> Maybe Integer
+readIntTerm (DomainValue SortInt val) = readMaybe (unpack val)
+readIntTerm _other = Nothing
 
 ------------------------------------------------------------
 -- MAP hooks
@@ -154,6 +283,26 @@ mapUpdateHook args
         throwE . renderText $ "MAP.update: wrong arity " <> pretty (length args)
 
 ------------------------------------------------------------
+-- LIST hooks
+builtinsLIST :: Map ByteString BuiltinFunction
+builtinsLIST =
+    Map.mapKeys ("LIST." <>) $
+        Map.fromList
+            [ "size" ~~> listSizeHook
+            ]
+
+listSizeHook :: BuiltinFunction
+listSizeHook = \case
+    [KList _ heads Nothing] ->
+        pure $ Just $ DomainValue SortInt $ pack (show (length heads))
+    [KList _ _ (Just _)] ->
+        pure Nothing -- tail of list not determined
+    [_other] ->
+        pure Nothing -- not an internal list, maybe unevaluated function call
+    moreArgs ->
+        throwE . renderText $ "LIST.size: wrong arity " <> pretty (length moreArgs)
+
+------------------------------------------------------------
 -- KEQUAL hooks
 builtinsKEQUAL :: Map ByteString BuiltinFunction
 builtinsKEQUAL =
@@ -226,14 +375,3 @@ evalEqualsK l r =
     if l == r
         then pure TrueBool
         else fail "cannot evaluate" -- i.e., result is Nothing
-
--- check for simple (parameter-less) sorts
-shouldHaveSort :: Term -> SortName -> Except Text ()
-t `shouldHaveSort` s
-    | sortOfTerm t == SortApp s [] =
-        pure ()
-    | otherwise =
-        throwE . renderText . vsep $
-            [ pretty $ "Argument term has unexpected sort (expected " <> show s <> "):"
-            , pretty t
-            ]
