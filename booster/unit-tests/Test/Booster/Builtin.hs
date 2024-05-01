2b601e5d5984e065000f245f64bcd2d06cc0356a
Jost Berthold <jost.berthold@gmail.com>
2024-05-01 17:25:27 +1000

    unit tests for LIST.get

--- booster/unit-tests/Test/Booster/Builtin.hs
+++ booster/unit-tests/Test/Booster/Builtin.hs
@@ -9,6 +9,8 @@ module Test.Booster.Builtin (
 ) where
 
 import Control.Monad.Trans.Except
+import Data.Bifunctor (second)
+import Data.ByteString.Char8 qualified as BS
 import Data.Either (isLeft)
 import Data.Map qualified as Map
 import Data.Maybe (fromMaybe)
@@ -33,6 +35,7 @@ test_builtins :: [TestTree]
 test_builtins =
     [ testIntHooks
     , testListSizeHooks
+    , testListGetHooks
     ]
 
 testIntHooks :: TestTree
@@ -80,7 +83,7 @@ testListSizeHooks :: TestTree
 testListSizeHooks =
     testGroup
         "LIST.size hooks"
-        [ testProperty "LIST.size on concrete lists works" . property $ do
+        [ testProperty "LIST.size on concrete lists" . property $ do
             l <- forAll $ Gen.int (Range.constant 0 12)
             let aList =
                     KList Fixture.testKListDef (replicate l [trm| \dv{SomeSort{}}("thing")|]) Nothing
@@ -100,4 +103,106 @@ testListSizeHooks =
             let assertException = assertBool "Unexpected success" . isLeft . runExcept
             assertException $ hook []
             assertException $ hook $ replicate 2 [trm| X:SortList{} |]
+            assertException $ hook $ replicate 3 [trm| X:SortList{} |]
         ]
+
+testListGetHooks :: TestTree
+testListGetHooks =
+    testGroup
+        "LIST.get hooks"
+        [ testCase "LIST.get arity is checked" $ do
+            hook <- maybe (error "List.get hook not found") pure $ Map.lookup "LIST.get" Builtin.hooks
+            let assertException = assertBool "Unexpected success" . isLeft . runExcept
+            assertException $ hook []
+            assertException $ hook $ [[trm| X:SortList{} |]]
+            assertException $ hook $ replicate 3 [trm| X:SortList{} |]
+        , testProperty "LIST.get with empty lists has no result" . property $ do
+            hook <- maybe failure pure $ Map.lookup "LIST.get" Builtin.hooks
+            i <- forAll $ Gen.int (Range.constant (-42) 42)
+            let iTerm = Builtin.intTerm $ fromIntegral i
+            result <- evalEither $ runExcept $ hook [aList [] Nothing, iTerm]
+            Nothing === result
+        , -- positive index
+          testProperty "LIST.get with idx >= 0 on concrete lists" . property $ do
+            hook <- maybe failure pure $ Map.lookup "LIST.get" Builtin.hooks
+            l <- forAll $ Gen.int (Range.constant 0 42)
+            i <- forAll $ Gen.int (Range.constant 0 l)
+            let iTerm = Builtin.intTerm $ fromIntegral i
+            result <- evalEither $ runExcept $ hook [aList [0 .. l] Nothing, iTerm]
+            Just (mkDV i) === result
+        , testProperty "LIST.get with idx >= 0 on list with symbolic tail" . property $ do
+            hook <- maybe failure pure $ Map.lookup "LIST.get" Builtin.hooks
+            l <- forAll $ Gen.int (Range.constant 0 42)
+            i <- forAll $ Gen.int (Range.constant 0 l)
+            let iTerm = Builtin.intTerm $ fromIntegral i
+            result <-
+                evalEither . runExcept $
+                    hook [aList [0 .. l] (Just ([trm|X:SortList|], [])), iTerm]
+            Just (mkDV i) === result
+        , testProperty "List.get with idx >= 0 where concrete head too short" . property $ do
+            hook <- maybe failure pure $ Map.lookup "LIST.get" Builtin.hooks
+            l <- forAll $ Gen.int (Range.constant 0 42)
+            delta <- forAll $ Gen.int (Range.constant 1 42)
+            let iTerm = Builtin.intTerm $ fromIntegral (l + delta)
+            result <-
+                evalEither . runExcept $
+                    hook [aList [0 .. l] Nothing, iTerm]
+            result2 <-
+                evalEither . runExcept $
+                    hook [aList [0 .. l] (Just ([trm|X:SortList|], [])), iTerm]
+            Nothing === result
+            Nothing === result2
+        , testProperty "LIST.get with idx >= 0 on list with symbolic head" . property $ do
+            hook <- maybe failure pure $ Map.lookup "LIST.get" Builtin.hooks
+            l <- forAll $ Gen.int (Range.constant 0 42)
+            i <- forAll $ Gen.int (Range.constant 0 l)
+            let symList = aList [] $ Just ([trm| X:SortList|], [0 .. l])
+                iTerm = Builtin.intTerm $ fromIntegral i
+            result <- evalEither $ runExcept $ hook [symList, iTerm]
+            Nothing === result
+        , -- negative indexes
+          testProperty "LIST.get with idx < 0 on concrete lists" . property $ do
+            hook <- maybe failure pure $ Map.lookup "LIST.get" Builtin.hooks
+            l <- forAll $ Gen.int (Range.constant 0 42)
+            i <- forAll $ Gen.int (Range.constant 1 $ l + 1)
+            let iTerm = Builtin.intTerm $ fromIntegral $ negate i
+            result <- evalEither $ runExcept $ hook [aList [0 .. l] Nothing, iTerm]
+            Just (mkDV (l + 1 - i)) === result
+        , testProperty "LIST.get with idx < 0 on list with symbolic tail" . property $ do
+            hook <- maybe failure pure $ Map.lookup "LIST.get" Builtin.hooks
+            l <- forAll $ Gen.int (Range.constant 0 42)
+            i <- forAll $ Gen.int (Range.constant 1 $ l + 1)
+            let iTerm = Builtin.intTerm $ fromIntegral $ negate i
+            result <-
+                evalEither . runExcept $
+                    hook [aList [0 .. l] (Just ([trm|X:SortList|], [])), iTerm]
+            Nothing === result
+        , testProperty "List.get with idx < 0 where concrete tail too short" . property $ do
+            hook <- maybe failure pure $ Map.lookup "LIST.get" Builtin.hooks
+            l <- forAll $ Gen.int (Range.constant 0 42)
+            delta <- forAll $ Gen.int (Range.constant 1 42)
+            let iTerm = Builtin.intTerm $ fromIntegral $ negate (l + 1 + delta)
+            result <-
+                evalEither . runExcept $
+                    hook [aList [] (Just ([trm|X:SortList|], [0 .. l])), iTerm]
+            result2 <-
+                evalEither . runExcept $
+                    hook [aList [0 .. l] (Just ([trm|X:SortList|], [0 .. l])), iTerm]
+            Nothing === result
+            Nothing === result2
+        , testProperty "LIST.get on list with symbolic head, concrete tail" . property $ do
+            hook <- maybe failure pure $ Map.lookup "LIST.get" Builtin.hooks
+            l <- forAll $ Gen.int (Range.constant 0 42)
+            i <- forAll $ Gen.int (Range.constant 1 $ l + 1)
+            let iTerm = Builtin.intTerm $ fromIntegral $ negate i
+            result <-
+                evalEither . runExcept $
+                    hook [aList [] $ Just ([trm| X:SortList|], [0 .. l]), iTerm]
+            Just (mkDV (l + 1 - i)) === result
+        ]
+  where
+    aList :: [Int] -> Maybe (Term, [Int]) -> Term
+    aList heads mbTail =
+        KList Fixture.testKListDef (map mkDV heads) (fmap (second $ map mkDV) mbTail)
+    -- FIXME strictly-speaking, we would need injections to KItem here
+    mkDV = dv someSort . BS.pack . show
