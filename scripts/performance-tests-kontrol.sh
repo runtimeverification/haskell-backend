4a7b239aff848fac7c0c753c787947a87ceeef97
Georgy Lukyanov <mail@geo2a.info>
2024-05-17 18:05:02 +0200

    Juggle quotes

--- scripts/performance-tests-kontrol.sh
+++ scripts/performance-tests-kontrol.sh
@@ -136,16 +136,17 @@ if [[ $FRESH_TEMPD -eq 0 ]]; then
 fi
 
 # set test arguments and select which tests to run
-TEST_ARGS='--foundry-root $FOUNDRY_DIR --maxfail=0 --numprocesses=$PYTEST_PARALLEL -vv $BUG_REPORT -k "not (test_kontrol_cse or test_foundry_minimize_proof or test_kontrol_end_to_end)"'
+QUOTE='"'
+TEST_ARGS="--foundry-root $FOUNDRY_DIR --maxfail=0 --numprocesses=$PYTEST_PARALLEL -vv $BUG_REPORT -k 'not (test_kontrol_cse or test_foundry_minimize_proof or test_kontrol_end_to_end)'"
 
-feature_shell "make test-integration TEST_ARGS=\"$TEST_ARGS\" | tee $SCRIPT_DIR/logs/kontrol-$KONTROL_VERSION-$FEATURE_BRANCH_NAME.log"
+feature_shell "make test-integration TEST_ARGS=$QUOTE$TEST_ARGS$QUOTE | tee $SCRIPT_DIR/logs/kontrol-$KONTROL_VERSION-$FEATURE_BRANCH_NAME.log"
 killall kore-rpc-booster || echo "no zombie processes found"
 
 if [ -z "$BUG_REPORT" ]; then
 if [ ! -e "$SCRIPT_DIR/logs/kontrol-$KONTROL_VERSION-master-$MASTER_COMMIT_SHORT.log" ]; then
   # remove proofs so that they are not reused by the master shell call
   rm -rf $FOUNDRY_DIR/out/proofs
-  master_shell "make test-integration TEST_ARGS=\"$TEST_ARGS\" | tee $SCRIPT_DIR/logs/kontrol-$KONTROL_VERSION-master-$MASTER_COMMIT_SHORT.log"
+  master_shell "make test-integration TEST_ARGS=$QUOTE$TEST_ARGS$QUOTE | tee $SCRIPT_DIR/logs/kontrol-$KONTROL_VERSION-master-$MASTER_COMMIT_SHORT.log"
   killall kore-rpc-booster || echo "no zombie processes found"
 fi
 
