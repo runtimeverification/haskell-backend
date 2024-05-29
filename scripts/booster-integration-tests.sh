701f978f5b4f72415a42befced63601896966e8e
Jost Berthold <jost.berthold@gmail.com>
2024-05-29 10:38:22 +1000

    persist the unexpected kore-rpc-dev (and kore-rpc-booster) result

--- scripts/booster-integration-tests.sh
+++ scripts/booster-integration-tests.sh
@@ -45,14 +45,14 @@ for dir in $(ls -d test-*); do
             SERVER=$KORE_RPC_BOOSTER ./runDirectoryTest.sh test-$name $@
             SERVER=$BOOSTER_DEV SERVER_OPTS="--no-smt" ./runDirectoryTest.sh test-$name $@
             ;;
-        "questionmark-vars")
+        "questionmark-vars" | "simplify-smt")
             SERVER=$BOOSTER_DEV ./runDirectoryTest.sh test-$name $@
             SERVER=$KORE_RPC_DEV ./runDirectoryTest.sh test-$name $@
             ;;
         "compute-ceil" | "no-evaluator")
             SERVER=$BOOSTER_DEV ./runDirectoryTest.sh test-$name $@
             ;;
-        "simplify" | "simplify-smt")
+        "simplify")
             SERVER=$BOOSTER_DEV ./runDirectoryTest.sh test-$name $@
             ;;
         "log-simplify-json")
