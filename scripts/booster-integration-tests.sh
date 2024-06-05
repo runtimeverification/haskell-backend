e5735cd271101725652e86ec4ad55ba8c868e6cf
Sam Balco <goodlyrottenapple@gmail.com>
2024-06-05 09:21:31 +0200

    only run the new integration test in booster dev, to avoid timeout due to slow internalisation of the definition in kore

--- scripts/booster-integration-tests.sh
+++ scripts/booster-integration-tests.sh
@@ -37,7 +37,7 @@ for dir in $(ls -d test-*); do
             SERVER=$KORE_RPC_DEV ./runDirectoryTest.sh test-$name $@
             SERVER=$KORE_RPC_BOOSTER ./runDirectoryTest.sh test-$name $@
             ;;
-        "get-model" | "get-model-subsorts" | "collectiontest")
+        "get-model" | "collectiontest")
             SERVER=$BOOSTER_DEV ./runDirectoryTest.sh test-$name $@
             SERVER=$KORE_RPC_BOOSTER ./runDirectoryTest.sh test-$name $@
             ;;
@@ -49,10 +49,7 @@ for dir in $(ls -d test-*); do
             SERVER=$BOOSTER_DEV ./runDirectoryTest.sh test-$name $@
             SERVER=$KORE_RPC_DEV ./runDirectoryTest.sh test-$name $@
             ;;
-        "compute-ceil" | "no-evaluator" | "non-linear-int-requires")
-            SERVER=$BOOSTER_DEV ./runDirectoryTest.sh test-$name $@
-            ;;
-        "simplify")
+        "compute-ceil" | "no-evaluator" | "non-linear-int-requires" | "get-model-subsorts" | "simplify")
             SERVER=$BOOSTER_DEV ./runDirectoryTest.sh test-$name $@
             ;;
         "log-simplify-json")
