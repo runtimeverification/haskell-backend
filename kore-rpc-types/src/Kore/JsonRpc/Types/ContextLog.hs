31e682bb541d4b07025b3c35b0b20a6a0d23a903
Jost Berthold <jost.berthold@gmail.com>
2024-06-18 15:25:37 +1000

    adjust context type, reduce logline type just for testing

--- kore-rpc-types/src/Kore/JsonRpc/Types/ContextLog.hs
+++ kore-rpc-types/src/Kore/JsonRpc/Types/ContextLog.hs
@@ -8,52 +8,68 @@ module Kore.JsonRpc.Types.ContextLog (
     module Kore.JsonRpc.Types.ContextLog,
 ) where
 
-import Control.Applicative ((<|>))
-import Data.Aeson.Types (FromJSON (..), ToJSON (..), (.:))
+import Data.Aeson.Types (FromJSON (..), ToJSON (..))
 import Data.Aeson.Types qualified as JSON
-import Data.Aeson.KeyMap qualified as JSON
+import Data.Data
 import Data.List (stripPrefix)
+import Data.Map (Map)
+import Data.Map qualified as Map
 import Data.Maybe (fromMaybe)
 import Data.Text (Text, pack, unpack)
-import Data.Time
+import Data.Text qualified as Text
+import Data.Tuple (swap)
 import Deriving.Aeson
 import Numeric
 
-import Kore.Syntax.Json.Types (KoreJson, KORE)
-
--- | result type
-data ContextLog
-    = CtxLog
-      { context :: [CLContext]
-      , entries :: [ContextLog]
-      , timestamp :: Maybe TimeOfDay
-      , duration :: Maybe DiffTime
-      }
-    | CLEntry
-      { message :: Maybe Text
-      , term :: Maybe KoreJson
-      }
-    deriving stock (Generic, Show, Eq)
-    deriving
-        (FromJSON, ToJSON)
-        via CustomJSON '[OmitNothingFields] ContextLog
-
 data CLContext
-    = CtxBooster
+    = CLNullary SimpleContext
+    | CLWithId IdContext
+    deriving stock (Generic, Eq)
+
+instance Show CLContext where
+    show (CLNullary c) = fromMaybe (error $ show c) $ Map.lookup c simpleShow
+    show (CLWithId withId) = show withId
+
+instance ToJSON CLContext where
+    toJSON (CLNullary c) = JSON.String $ maybe (error $ show c) pack $ Map.lookup c simpleShow
+    toJSON (CLWithId withId) = JSON.genericToJSON options withId
+
+instance FromJSON CLContext where
+    parseJSON = \case
+        JSON.String simple
+            | Just c <- Map.lookup (unpack simple) simpleRead ->
+                pure $ CLNullary c
+        obj@JSON.Object{} ->
+            CLWithId <$> JSON.genericParseJSON options obj
+        other ->
+            JSON.typeMismatch "Object or string" other
+
+options :: JSON.Options
+options =
+    JSON.defaultOptions
+    { JSON.sumEncoding = JSON.ObjectWithSingleField
+    , JSON.constructorTagModifier = toJsonName
+    , JSON.allNullaryToStringTag = True
+    }
+
+toJsonName :: String -> String
+toJsonName name = JSON.camelTo2 '-' $ fromMaybe name $ stripPrefix "Ctx" name
+
+----------------------------------------
+data SimpleContext
+    = -- component
+      CtxBooster
     | CtxKore
-    -- "modes"
+    | CtxProxy
+    -- method
     | CtxExecute
     | CtxSimplify
     | CtxImplies
     | CtxGetModel
+    -- mode/phase
     | CtxMatch
-    -- entities with hex identifier
-    | CtxRewrite UniqueId
-    | CtxSimplification UniqueId
-    | CtxFunction UniqueId
-    | CtxTerm UniqueId
-    -- entities with name
-    | CtxHook Text
+    | CtxDefinedness
+    | CtxConstraint
     -- results
     | CtxFailure
     | CtxAbort
@@ -65,65 +81,78 @@ data CLContext
     | CtxError
     | CtxWarn
     | CtxInfo
-    -- free-form
-    | Ctx Text
-    deriving stock (Generic, Show, Eq)
+    deriving stock (Generic, Data, Enum, Show, Eq, Ord)
 
-instance ToJSON CLContext where
-    -- nullary constructors encoded as simple strings
+-- translation table for nullary constructors
+translateSimple :: [(SimpleContext, String)]
+translateSimple =
+    [ (con, toJsonName $ show con)
+    | con <- [CtxBooster .. CtxInfo]
+    ]
+
+simpleShow :: Map SimpleContext String
+simpleShow = Map.fromList translateSimple
+
+simpleRead :: Map String SimpleContext
+simpleRead = Map.fromList $ map swap translateSimple
+
+----------------------------------------
+data IdContext
+    = -- entities with hex identifier
+      CtxRewrite UniqueId
+    | CtxSimplification UniqueId
+    | CtxFunction UniqueId
+    | CtxTerm UniqueId
+    -- entities with name
+    | CtxHook Text
+    deriving stock (Generic, Eq)
+
+-- To/FromJSON by way of Generic
+
+instance Show IdContext where
+    show (CtxRewrite uid) = "rewrite " <> show uid
+    show (CtxSimplification uid) = "simplification " <> show uid
+    show (CtxFunction uid) = "function " <> show uid
+    show (CtxTerm uid) = "term " <> show uid
+    show (CtxHook name) = "simplification " <> unpack name
+
+----------------------------------------
+data UniqueId
+    = Hex7 Integer -- short hashes (7 char)
+    | Hex64 Integer -- long hashes (64 char)
+    | UNKNOWN
+    deriving stock (Generic, Eq, Ord)
+
+instance Show UniqueId where
+    show (Hex7 i) = showHex i ""
+    show (Hex64 i) = showHex i "" -- or show shortened version?
+    show UNKNOWN = "UNKNOWN"
+
+instance FromJSON UniqueId where
+    parseJSON = JSON.withText "Hexadecimal Hash" parseHex
+      where
+        parseHex :: Text -> JSON.Parser UniqueId
+        parseHex "UNKNOWN" = pure UNKNOWN
+        parseHex hex =
+            case readHex $ unpack hex of
+                [(h, "")]
+                    | Text.length hex < 8 -> pure $ Hex7 h
+                    | Text.length hex <= 64 -> pure $ Hex64 h
+                _otherwise -> JSON.parseFail $ "Bad hash value: " <> show hex
+
+instance ToJSON UniqueId where
     toJSON = \case
-        CtxBooster -> JSON.String $ camelToKebab "Booster"
-        CtxKore -> JSON.String $ camelToKebab "Kore"
-        CtxExecute -> JSON.String $ camelToKebab "Execute"
-        CtxSimplify -> JSON.String $ camelToKebab "Simplify"
-        CtxImplies -> JSON.String $ camelToKebab "Implies"
-        CtxGetModel -> JSON.String $ camelToKebab "GetModel"
-        CtxMatch -> JSON.String $ camelToKebab "Match"
-        CtxFailure -> JSON.String $ camelToKebab "Failure"
-        CtxAbort -> JSON.String $ camelToKebab "Abort"
-        CtxSuccess -> JSON.String $ camelToKebab "Success"
-        CtxKoreTerm -> JSON.String $ camelToKebab "KoreTerm"
-        CtxDetail -> JSON.String $ camelToKebab "Detail"
-        CtxError -> JSON.String $ camelToKebab "Error"
-        CtxWarn -> JSON.String $ camelToKebab "Warn"
-        CtxInfo -> JSON.String $ camelToKebab "Info"
-        Ctx txt-> JSON.String txt
-        -- entities with hex identifier
-        other -> JSON.genericToJSON options other
+        Hex7 x ->
+            JSON.String . pad0 7 . pack $ showHex x ""
+        Hex64 x ->
+            JSON.String . pad0 64 . pack $ showHex x ""
+        UNKNOWN ->
+            JSON.String "UNKNOWN"
+      where
+          pad0 l = Text.takeEnd l . (Text.replicate l "0" <>)
 
-instance FromJSON CLContext where
-    parseJSON = \case
-        JSON.String "booster" -> pure CtxBooster
-        JSON.String "kore" -> pure CtxKore
-        JSON.String "execute" -> pure CtxExecute
-        JSON.String "simplify" -> pure CtxSimplify
-        JSON.String "implies" -> pure CtxImplies
-        JSON.String "get-model" -> pure CtxGetModel
-        JSON.String "match" -> pure CtxMatch
-        JSON.String "failure" -> pure CtxFailure
-        JSON.String "abort" -> pure CtxAbort
-        JSON.String "success" -> pure CtxSuccess
-        JSON.String "kore-term" -> pure CtxKoreTerm
-        JSON.String "detail" -> pure CtxDetail
-        JSON.String "error" -> pure CtxError
-        JSON.String "warn" -> pure CtxWarn
-        JSON.String "info" -> pure CtxInfo
-        JSON.String other -> pure $ Ctx other
 
-        obj -> JSON.genericParseJSON options obj
-
-camelToKebab :: String -> Text
-camelToKebab = pack . JSON.camelTo2 '-'
-
-options :: JSON.Options
-options =
-    JSON.defaultOptions
-    { JSON.sumEncoding = JSON.ObjectWithSingleField
-    , JSON.constructorTagModifier = \name ->
-            JSON.camelTo2 '-' $ fromMaybe name $ stripPrefix "Ctx" name
-    , JSON.allNullaryToStringTag = True
-    }
-
+----------------------------------------
 data LogLine
     = LogLine
       { context :: [CLContext]
@@ -136,54 +165,21 @@ data LogLine
 
 data CLMessage
     = CLText Text  -- generic log message
-    | CLArray [Text] -- array of partial symbol names (ceil analysis)
-    | CLTerm KoreJson -- kore-term
-    | CLId JSON.Key UniqueId -- simplification/rewrite/match success
-    deriving stock (Generic, Show, Eq)
+    | CLValue JSON.Value -- other stuff
+    deriving stock (Generic, Eq)
+
+instance Show CLMessage where
+    show (CLText t) = unpack t
+    show (CLValue value) = show value
 
 -- a message is a term if it is an object with format: KORE
 instance FromJSON CLMessage where
-    parseJSON obj@(JSON.Object o) =
-        parseTerm <|> parseId
-      where
-        parseTerm = do
-            _ :: KORE <- o .: "format" -- must be KORE
-            CLTerm <$> parseJSON obj
-        parseId = case JSON.toList o of
-            [(aKey, uniqueId)] -> CLId aKey <$> parseJSON uniqueId
-            _other -> JSON.typeMismatch "Singleton object" obj
-    parseJSON (JSON.String msg) =
-        pure $ CLText msg
-    parseJSON arr@JSON.Array{} =
-        CLArray <$> parseJSON arr
+    parseJSON (JSON.String msg) = pure $ CLText msg
+    parseJSON obj@JSON.Object{} = pure $ CLValue obj
+    parseJSON arr@JSON.Array{} = pure $ CLValue arr
     parseJSON other =
         JSON.typeMismatch "Object, array, or string" other
 
 instance ToJSON CLMessage where
     toJSON (CLText text) = toJSON text
-    toJSON (CLArray msgs) = toJSON msgs
-    toJSON (CLTerm term) = toJSON term
-    toJSON (CLId key uniqueId) = JSON.Object $ JSON.singleton key (toJSON uniqueId)
-
-data UniqueId
-    = Hex Integer
-    | UNKNOWN
-    deriving stock (Generic, Eq, Ord)
-
-instance Show UniqueId where
-    show (Hex i) = showHex i ""
-    show UNKNOWN = "UNKNOWN"
-
-instance FromJSON UniqueId where
-    parseJSON = JSON.withText "Hexadecimal Hash" parseHex
-      where
-        parseHex :: Text -> JSON.Parser UniqueId
-        parseHex "UNKNOWN" = pure UNKNOWN
-        parseHex hex =
-            case readHex $ unpack hex of
-                [(h, "")] -> pure  $ Hex h
-                _otherwise -> JSON.parseFail $ "Bad hash value: " <> show hex
-
-instance ToJSON UniqueId where
-    toJSON (Hex x) = JSON.String . pack $ showHex x ""
-    toJSON UNKNOWN = JSON.String "UNKNOWN"
+    toJSON (CLValue value) = value
