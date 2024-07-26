334891503647bc8285efbd40bdb2d93e40c731f3
Sam Balco <goodlyrottenapple@gmail.com>
2024-07-26 10:22:10 +0100

    Add pyk implication tests to the rpc integration test suite

--- scripts/booster-integration-tests.sh
+++ scripts/booster-integration-tests.sh
@@ -37,7 +37,7 @@ for dir in $(ls -d test-*); do
             SERVER=$KORE_RPC_DEV ./runDirectoryTest.sh test-$name $@
             SERVER=$KORE_RPC_BOOSTER ./runDirectoryTest.sh test-$name $@
             ;;
-        "get-model" | "collectiontest")
+        "get-model" | "collectiontest" | "implies" | "implies2")
             SERVER=$BOOSTER_DEV ./runDirectoryTest.sh test-$name $@
             SERVER=$KORE_RPC_BOOSTER ./runDirectoryTest.sh test-$name $@
             ;;
