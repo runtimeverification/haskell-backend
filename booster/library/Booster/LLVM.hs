3ed541bb10bf0353717dbdc530a34df923cb6f0f
Jost Berthold <jost.berthold@gmail.com>
2024-08-07 22:29:51 +1000

    Only send unevaluated functions to LLVM (#4018)
    This change steps away from maximising the terms we send to the LLVM
    backend for evaluation, and only sends terms with an unevaluated
    function call at the top. It addresses a particular constellation that
    has been observed in downstream semantics/proofs:
    * a configuration containing large data structures in cells that remain
    unchanged
    * a long-running sequence of rewrites that keep inserting concrete
    function applications into cells required for the next rewrite step,
    * and therefore require frequent configuration simplifications
    
    Each time the configuration is simplified, the large unchanged data
    structure is sent to the LLVM backend, which incurs considerable
    overhead.
    
    On the flip side, we will send each function that needs to be evaluated
    individually. However, rewrites or equations will usually not create
    more than a handful of new unevaluated function calls at one time, and
    we will still send _nested_ unevaluated expressions together.
    
    Also includes a logging call to measure the duration of LLVM
    interactions (including argument/result conversion), and necessary code
    restructuring.
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- booster/library/Booster/LLVM.hs
+++ booster/library/Booster/LLVM.hs
@@ -6,27 +6,36 @@ module Booster.LLVM (
 ) where
 
 import Control.Monad.IO.Class (MonadIO (..))
+import Data.Aeson
 import Data.Binary.Get
 import Data.ByteString (fromStrict)
+import Data.ByteString.Char8 qualified as BS
 import Data.Map qualified as Map
 import Data.Set qualified as Set
+import Data.Text qualified as T
 
 import Booster.Definition.Base
 import Booster.LLVM.Internal qualified as Internal
+import Booster.Log
 import Booster.Pattern.Base
 import Booster.Pattern.Binary
 import Booster.Pattern.Util
-import Data.ByteString.Char8 qualified as BS
+import Booster.Util (secWithUnit, timed)
 
-simplifyBool :: MonadIO io => Internal.API -> Term -> io (Either Internal.LlvmError Bool)
-simplifyBool api trm = liftIO $ Internal.runLLVM api $ do
+simplifyBool :: LoggerMIO io => Internal.API -> Term -> io (Either Internal.LlvmError Bool)
+simplifyBool api trm = ioWithTiming $ Internal.runLLVM api $ do
     kore <- Internal.ask
     trmPtr <- Internal.marshallTerm trm
     liftIO $ kore.simplifyBool trmPtr
 
 simplifyTerm ::
-    MonadIO io => Internal.API -> KoreDefinition -> Term -> Sort -> io (Either Internal.LlvmError Term)
-simplifyTerm api def trm sort = liftIO $ Internal.runLLVM api $ do
+    LoggerMIO io =>
+    Internal.API ->
+    KoreDefinition ->
+    Term ->
+    Sort ->
+    io (Either Internal.LlvmError Term)
+simplifyTerm api def trm sort = ioWithTiming $ Internal.runLLVM api $ do
     kore <- Internal.ask
     trmPtr <- Internal.marshallTerm trm
     sortPtr <- Internal.marshallSort sort
@@ -56,3 +65,11 @@ simplifyTerm api def trm sort = liftIO $ Internal.runLLVM api $ do
     sortName (SortApp name _) = name
     sortName (SortVar name) = name
     subsorts = maybe Set.empty snd $ Map.lookup (sortName sort) def.sorts
+
+ioWithTiming :: LoggerMIO io => IO a -> io a
+ioWithTiming action = do
+    (result, time) <- liftIO $ timed action
+    withContext CtxTiming . logMessage $
+        WithJsonMessage (object ["time" .= time]) $
+            "Performed LLVM call in " <> T.pack (secWithUnit time)
+    pure result
