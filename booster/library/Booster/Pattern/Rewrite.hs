ec272fb521951570bfedbc896043fe3bbb758cce
Georgy Lukyanov <mail@geo2a.info>
2024-10-07 13:12:28 +0200

    Attach rules to their application result outside applyRule

--- booster/library/Booster/Pattern/Rewrite.hs
+++ booster/library/Booster/Pattern/Rewrite.hs
@@ -177,7 +177,14 @@ rewriteStep cutLabels terminalLabels pat = do
         -- try all rules of the priority group. This will immediately
         -- fail the rewrite if anything is uncertain (unification,
         -- definedness, rule conditions)
-        results <- filter (/= NotApplied) <$> mapM (applyRule pattr) rules
+        results <-
+            filter (/= NotApplied)
+                <$> forM
+                    rules
+                    ( \rule -> do
+                        result <- applyRule pattr rule
+                        pure (fmap (rule,) result)
+                    )
 
         -- simplify and filter out bottom states
 
@@ -289,7 +296,7 @@ applyRule ::
     LoggerMIO io =>
     Pattern ->
     RewriteRule "Rewrite" ->
-    RewriteT io (RewriteRuleAppResult (RewriteRule "Rewrite", Pattern))
+    RewriteT io (RewriteRuleAppResult Pattern)
 applyRule pat@Pattern{ceilConditions} rule =
     withRuleContext rule $
         runRewriteRuleAppT $
@@ -397,7 +404,7 @@ applyRule pat@Pattern{ceilConditions} rule =
                                 ceilConditions
                     withContext CtxSuccess $
                         withPatternContext rewritten $
-                            return (rule, rewritten)
+                            return rewritten
   where
     filterOutKnownConstraints :: Set.Set Predicate -> [Predicate] -> RewriteT io [Predicate]
     filterOutKnownConstraints priorKnowledge constraitns = do
