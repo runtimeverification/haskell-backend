f286d6dafbef72cbe88f5c0800706284867d4201
Jost Berthold <jost.berthold@gmail.com>
2025-02-24 18:21:26 +1100

    do not test bytebuild (dependency issue)

--- flake.nix
+++ flake.nix
@@ -46,6 +46,7 @@
             };
           additionalHaskellPkgSetOverrides = hfinal: hprev:
             with final.haskell.lib; {
+              bytebuild = dontCheck hprev.bytebuild;
               crypton-x509 = dontCheck hprev.crypton-x509;
               decision-diagrams = dontCheck hprev.decision-diagrams;
               fgl = dontCheck hprev.fgl;
