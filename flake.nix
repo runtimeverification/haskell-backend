f64cbdeb5f13f521b5d82a3410110b52331b90a7
Julian Kuners <julian.kuners@gmail.com>
2025-03-12 11:11:08 +0100

    WIP fix broken haskell dependencies without some-cabal-hashes

--- flake.nix
+++ flake.nix
@@ -11,11 +11,16 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  # see https://github.com/lf-/nix-lib/blob/main/examples/some-cabal-hashes/flake.nix
   outputs = { self, rv-utils, nixpkgs, z3, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
     let
-      ghcVer = "ghc965";
-      makeHaskellOverlay = overlay: final: prev: {
+      allCabalHashesOverlay = final: prev: {
+        all-cabal-hashes = final.fetchurl {
+          url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/ce857734d7d4c0fad3f6dda3a4db052836ed4619.tar.gz";
+          sha256 = "sha256-Q7Zg32v5ubjVJMQXGiyyMmeFg08jTzVRKC18laiHCPE=";
+        };
+      };
+      makeOverlayForHaskell = overlay: final: prev: {
         haskell = prev.haskell // {
           packages = prev.haskell.packages // {
             ${ghcVer} = prev.haskell.packages."${ghcVer}".override (oldArgs: {
@@ -26,81 +31,96 @@
           };
         };
       };
-
-      out = system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            config.allowBroken = true; # for json-rpc
-            overlays = [ self.overlays.haskell-versions self.overlays.default ];
-          };
-        in
-        {
-          packages = rec {
-            default = hs-backend-booster;
-            kore-rpc-types = pkgs.haskell.packages.${ghcVer}.kore-rpc-types;
-            kore = pkgs.haskell.packages.${ghcVer}.kore;
-            hs-backend-booster = pkgs.haskell.packages.${ghcVer}.hs-backend-booster;
-            hs-backend-booster-dev-tools = pkgs.haskell.packages.${ghcVer}.hs-backend-booster-dev-tools;
-          };
-
-          devShells.default =
-            let haskellPackages = pkgs.haskell.packages.${ghcVer};
-            in
-            haskellPackages.shellFor {
-              packages = p: [ self.packages.${system}.sample ];
-              withHoogle = false;
-              buildInputs = with haskellPackages; [
-                cabal-install
-              ];
-            };
+      haskellBackendOverlay = makeOverlayForHaskell (final: prev: hfinal: hprev: {
+        kore-rpc-types = hfinal.callCabal2nix "kore-rpc-types" ./kore-rpc-types { };
+        kore = hfinal.callCabal2nix "kore" ./kore { };
+        hs-backend-booster = hfinal.callCabal2nix "hs-backend-booster" ./booster { };
+        hs-backend-booster-dev-tools = hfinal.callCabal2nix "hs-backend-booster-dev-tools" ./dev-tools { };
+      });
+      haskellBackendVersionInfoOverlay = makeOverlayForHaskell (final: prev: hfinal: hprev:
+      let
+        hlib = final.haskell.lib;
+      in {
+        kore = (hlib.overrideCabal hprev.kore (drv: {
+          doCheck = false;
+          postPatch = ''
+            ${drv.postPatch or ""}
+            substituteInPlace src/Kore/VersionInfo.hs \
+              --replace '$(GitRev.gitHash)' '"${self.rev or "dirty"}"'
+          '';
+          # TODO: this was part of the previous flake, but can most probably be removed
+          # postInstall = ''
+          #   ${drv.postInstall or ""}
+          # '';
+        })).override {
+          # bit pathological, but ghc-compact is already included with the ghc compiler
+          # and depending on another copy of ghc-compact breaks HLS in the dev shell.
+          ghc-compact = null;
         };
-    in
-    flake-utils.lib.eachDefaultSystem out // {
-      overlays = {
-        haskell-versions = makeHaskellOverlay (final: prev:
-          import ./nix/some-cabal-hashes.nix {
-            self = final;
-
-            overrides =
-              let
-                hprev = prev.haskell.packages.${ghcVer};
-                hfinal = final.haskell.packages.${ghcVer};
-              in {
-                # FIXME these two overrides cause an error in some-cabal-hashes.nix
-                # tasty-test-reporter =
-                #     final.fetchFromGitHub {
-                #       owner = "goodlyrottenapple";
-                #       repo = "tasty-test-reporter";
-                #       rev = "b704130545aa3925a8487bd3e92f1dd5ce0512e2";
-                #       sha256 = "sha256-9CN59Im0BC3vSVhL85v5eXPYYoPbV3NAuv893tXpr/U=";
-                #     };
-                # profiteur =
-                #     final.fetchFromGitHub {
-                #       owner = "goodlyrottenapple";
-                #       repo = "profiteur";
-                #       rev = "7b30bbe6b2a6b72a5b4896f3a0eed5587a5bf465";
-                #       sha256 = "sha256-9CN59Im0BC3vSVhL85v5eXPYYoPbV3NAuv893tXpr/U=";
-                #     };
-
-                # FIXME a jailbreak does not work either (Problem is the ansi-terminal version)
-                # hfinal.tasty-test-reporter = hfinal.doJailbreak hprev.tasty-test-reporter;
-
-                # FIXME likewise, these two `dontCheck` (one inlined) are seemingly ignored.
-                decision-diagrams = prev.haskell.lib.overrideCabal hprev.decision-diagrams
-                    (drv: { doCheck = false; }); # build runs failing tests
-                json-rpc = prev.haskell.lib.dontCheck hprev.json-rpc; # 1.0.4 marked as broken!
-              };
-          });
-        default = makeHaskellOverlay
-          (final: prev: hfinal: hprev:
-            let hlib = prev.haskell.lib; in
-            {
-              kore-rpc-types               = hfinal.callCabal2nix "kore-rpc-types" ./kore-rpc-types { };
-              kore                         = hfinal.callCabal2nix "kore" ./kore { };
-              hs-backend-booster           = hfinal.callCabal2nix "hs-backend-booster" ./booster { };
-              hs-backend-booster-dev-tools = hfinal.callCabal2nix "hs-backend-booster-dev-tools" ./dev-tools { };
-            });
+        hs-backend-booster = hlib.overrideCabal hprev.hs-backend-booster (drv: {
+          doCheck = false;
+          postPatch = ''
+            ${drv.postPatch or ""}
+            substituteInPlace library/Booster/VersionInfo.hs \
+              --replace '$(GitRev.gitHash)' '"${self.rev or "dirty"}"'
+          '';
+        });
+      });
+      haskellDependenciesOverlay = makeOverlayForHaskell (final: prev: hfinal: hprev:
+      let
+        hlib = final.haskell.lib;
+      in {
+        json-rpc = hlib.markUnbroken hprev.json-rpc;
+        smtlib-backends-process = hlib.markUnbroken (hlib.dontCheck hprev.smtlib-backends-process);
+        decision-diagrams = hlib.markUnbroken (hlib.dontCheck hprev.decision-diagrams);
+      });
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          allCabalHashesOverlay
+          haskellBackendOverlay
+          haskellBackendVersionInfoOverlay
+          haskellDependenciesOverlay
+        ];
       };
-    };
+      pkgsClean = import nixpkgs {
+        inherit system;
+      };
+      # This should based on the compiler version from the resolver in stack.yaml.
+      ghcVer = "ghc965";
+    in {
+      packages = rec {
+        default = hs-backend-booster;
+        kore-rpc-types = pkgs.haskell.packages.${ghcVer}.kore-rpc-types;
+        kore = pkgs.haskell.packages.${ghcVer}.kore;
+        hs-backend-booster = pkgs.haskell.packages.${ghcVer}.hs-backend-booster;
+        hs-backend-booster-dev-tools = pkgs.haskell.packages.${ghcVer}.hs-backend-booster-dev-tools;
+      };
+
+      # TODO: replicate style devshell
+      # TODO: replicate cabal devshell
+      # TODO: replicate default devshell
+      # note: consider using haskellPackages.shellFor
+      devShells = {
+        # Separate fourmolu and cabal shells just for CI
+        style = pkgsClean.mkShell {
+          name = "haskell style check shell";
+          packages = [
+
+          ];
+        };
+        cabal = pkgs.mkShell {
+          name = "haskell cabal shell";
+          packages = [
+
+          ];
+        };
+        default = pkgs.mkShell {
+          name = "haskell default shell";
+          packages = [
+
+          ];
+        };
+      };
+    });
 }
